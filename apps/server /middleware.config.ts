2220c8229eaa062099753a901fcd1055ccb309b5
lukasborawski <lukas.borawski@gmail.com>
2023-06-22 10:32:30 +0200

    chore: initial setup

---
+++ apps/server /middleware.config.ts
@@ -0,0 +1,10 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+const config = {
+  integrations: {
+    /* VSF integration config */
+  },
+};
+
+export default config;
---
+++ apps/web/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Home Page</div>
+</template>
+
+<script setup lang="ts">
+defineOptions({
+  name: 'PageIndex',
+  inheritAttrs: false,
+});
+definePageMeta({
+  title: 'Home Page',
+});
+</script>
