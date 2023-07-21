b70f5eeeb5b2f6208f7c62eba850b87396ef4e08
daaf <44862757+dawid-ziobro@users.noreply.github.com>
2023-07-21 10:24:35 +0200

    feat: add search page (#42)

--- apps/web/pages/search.vue
+++ apps/web/pages/search.vue
@@ -1,3 +1,26 @@
 <template>
-  <h1>Search page</h1>
+  <NuxtLayout name="default">
+    <CategoryPageContent
+      v-if="productsCatalog"
+      :title="$t('resultsFor', { phrase: query.search })"
+      :total-products="productsCatalog.pagination.totalResults"
+      :products="productsCatalog.products"
+    >
+      <template #sidebar>
+        <CategorySorting />
+        <CategoryFilters :facets="productsCatalog.facets" />
+      </template>
+    </CategoryPageContent>
+  </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: false,
+});
+
+const { query } = useRoute();
+const { fetchProducts, data: productsCatalog } = useProducts();
+
+await fetchProducts();
+</script>
