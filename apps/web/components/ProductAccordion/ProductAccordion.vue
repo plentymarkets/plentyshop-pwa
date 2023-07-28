e350764c4e522fc991966530e3cd90b2b4d7b42f
rbedeoan-plenty <raul.bedeoan@plentymarkets.com>
2023-07-28 11:54:43 +0300

    product Accordion

--- apps/web/components/ProductAccordion/ProductAccordion.vue
+++ apps/web/components/ProductAccordion/ProductAccordion.vue
@@ -9,7 +9,7 @@
           {{ $t('productDetails') }}
         </h2>
       </template>
-      <p>{{ product.description }}</p>
+      <p>{{ productGetters.getDescription(product) }}</p>
     </UiAccordionItem>
     <UiDivider class="my-4" />
     <UiAccordionItem
@@ -27,13 +27,13 @@
 
 <script lang="ts" setup>
 import type { ProductAccordionPropsType } from '~/components/ProductAccordion/types';
-
+import { productGetters } from '../../../../../plentymarkets-sdk/packages/sdk/src/index';
 const props = defineProps<ProductAccordionPropsType>();
 
 const { product } = toRefs(props);
 
-const { data: productReviews, fetchProductReviews } = useProductReviews(product.value.slug);
-fetchProductReviews(product.value.slug);
+const { data: productReviews, fetchProductReviews } = useProductReviews(productGetters.getSlug(product.value));
+fetchProductReviews(productGetters.getSlug(product.value));
 
 const productDetailsOpen = ref(true);
 </script>
--- apps/web/components/ProductAccordion/types.ts
+++ apps/web/components/ProductAccordion/types.ts
@@ -1,5 +1,4 @@
-import type { SfProduct } from '@vue-storefront/unified-data-model';
-
+import type { ProductItemDocumentData } from '../../../../../plentymarkets-sdk/packages/api-client';
 export type ProductAccordionPropsType = {
-  product: SfProduct;
+  product: ProductItemDocumentData;
 };
