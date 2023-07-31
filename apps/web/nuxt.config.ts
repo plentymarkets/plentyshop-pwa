3f701d63a9f2c5e695e2565af3a3ef67db31a4bf
daaf <44862757+dawid-ziobro@users.noreply.github.com>
2023-07-31 14:34:06 +0200

    perf: add images caching (#83)

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -74,6 +74,11 @@ export default defineNuxtConfig({
       routes: ['/'],
     },
   },
+  routeRules: {
+    '/_ipx/**': { headers: { 'cache-control': `public, max-age=31536000, immutable` } },
+    '/icons/**': { headers: { 'cache-control': `public, max-age=31536000, immutable` } },
+    '/favicon.ico': { headers: { 'cache-control': `public, max-age=31536000, immutable` } },
+  },
   pwa: {
     registerType: 'autoUpdate',
     workbox: {
