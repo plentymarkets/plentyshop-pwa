4201d080c6d6f67b67f67f5ffa8ec7d5b64ea25e
abocsan-plenty <129151096+abocsan-plenty@users.noreply.github.com>
2023-08-02 16:10:04 +0300

    fix: lint fix

--- apps/web/composables/useProductAttribute/useProductAttribute.ts
+++ apps/web/composables/useProductAttribute/useProductAttribute.ts
@@ -1,39 +1,17 @@
 import { ref } from 'vue';
-import type { ProductItemDocumentData } from '@plentymarkets/plentymarkets-sdk/packages/api-client/src';
-import { SfAttribute } from '@vue-storefront/unified-data-model';
 
 /**
  * composable for getting product attributes data
- * @param {SfProduct} product Product object
- * @param {TAttribute} attributesNames get specific product attributes
  */
 export function useProductAttribute<TAttribute extends string>(
-  product: ProductItemDocumentData,
-  attributesNames: TAttribute[] = [],
 ) {
-  // const attributes = groupBy(
-  //   uniqBy(
-  //     (product?.variants || []).flatMap((variant) => variant?.attributes),
-  //     'value',
-  //   ),
-  //   'name',
-  // );
-  const attributes = [];
-
-  const mapAttribute = (attributes: SfAttribute[] = []) => {
-    const defaults = attributesNames.map((attribute) => ({ name: attribute, value: null }));
-
-    return Object.fromEntries([...defaults, ...attributes].map(({ name, value }) => [name, value]));
-  };
-
-  // const selectedAttrs = ref(mapAttribute(product.attributes));
   const selectedAttrs = ref([]);
 
   return {
     // getAttributeList: (attributeName: TAttribute) => attributes[attributeName] || [],
-    getAttributeList: (attributeName: TAttribute) => [],
+    getAttributeList: () => [],
     // getAttribute: (attributeName: TAttribute) => selectedAttrs.value[attributeName],
-    getAttribute: (attributeName: TAttribute) => [],
+    getAttribute: () => [],
     setAttribute: (attributeName: TAttribute, attributeValue: string) => {
       selectedAttrs.value = {
         ...selectedAttrs.value,
