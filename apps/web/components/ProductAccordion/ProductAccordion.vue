1fd7ec9bc0b2269cdbfcbae02cd46038360d74ae
Fabian Gerke <124085586+FabianGerke@users.noreply.github.com>
2023-08-03 15:48:56 +0200

    feat: add technical data and change long description to html

--- apps/web/components/ProductAccordion/ProductAccordion.vue
+++ apps/web/components/ProductAccordion/ProductAccordion.vue
@@ -1,6 +1,7 @@
 <template>
   <div data-testid="product-accordion">
     <UiAccordionItem
+      v-if="productGetters.getDescription(product)?.length"
       summary-class="md:rounded-md w-full hover:bg-neutral-100 py-2 pl-4 pr-3 flex justify-between items-center"
       v-model="productDetailsOpen"
     >
@@ -9,7 +10,19 @@
           {{ $t('productDetails') }}
         </h2>
       </template>
-      <p>{{ productGetters.getDescription(product) }}</p>
+      <div v-html="productGetters.getDescription(product)"></div>
+    </UiAccordionItem>
+    <UiAccordionItem
+      v-if="productGetters.getTechnicalData(product)?.length"
+      summary-class="md:rounded-md w-full hover:bg-neutral-100 py-2 pl-4 pr-3 flex justify-between items-center"
+      v-model="technicalDataOpen"
+    >
+      <template #summary>
+        <h2 class="font-bold font-headings text-lg leading-6 md:text-2xl">
+          {{ $t('technicalData') }}
+        </h2>
+      </template>
+      <div v-html="productGetters.getTechnicalData(product)"></div>
     </UiAccordionItem>
     <UiDivider class="my-4" />
     <UiAccordionItem
@@ -46,4 +59,5 @@ const { data: productReviews, fetchProductReviews } = useProductReviews(
 fetchProductReviews(product.value.variation.id, product.value.item.id);
 
 const productDetailsOpen = ref(true);
+const technicalDataOpen = ref(false);
 </script>
