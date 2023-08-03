90cffa5262082fcb2bcb2d000918074bed49335f
Jakub Freisler <jakub@frsource.org>
2023-08-03 12:08:59 +0100

    feat: add auth layout [SV-101] (#93)

---
+++ apps/web/layouts/auth.vue
@@ -0,0 +1,14 @@
+<template>
+  <UiNavbarTop />
+  <main class="mx-auto px-4 pt-4 pb-20 md:max-w-[630px] md:px-0 md:mt-4 md:mb-8">
+    <h1 class="font-bold mb-10 typography-headline-3 md:typography-headline-2">{{ heading }}</h1>
+    <slot />
+  </main>
+  <UiFooter />
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  heading: string;
+}>();
+</script>
