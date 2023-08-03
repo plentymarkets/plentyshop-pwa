6d58707a1e1b50b30b2db140be2b34d3ba96e75c
abocsan-plenty <129151096+abocsan-plenty@users.noreply.github.com>
2023-08-03 18:22:28 +0300

    fix: lint fixes

--- apps/web/components/CategoryFilters/Filter.vue
+++ apps/web/components/CategoryFilters/Filter.vue
@@ -8,43 +8,43 @@
     </template>
     <!-- TODO: implement filters -->
 
-<!--    <ul v-if="facetGetters.getType(facet) === 'size'" class="flex flex-wrap gap-4 px-1.5">-->
-<!--      <li v-for="{ value } in facetGetters.getFilters(facet)" :key="value">-->
-<!--        <SfChip size="sm" :input-props="{ value: value }" v-model="selectedProxy">-->
-<!--          <span class="leading-4">{{ value }}</span>-->
-<!--        </SfChip>-->
-<!--      </li>-->
-<!--    </ul>-->
+    <!--    <ul v-if="facetGetters.getType(facet) === 'size'" class="flex flex-wrap gap-4 px-1.5">-->
+    <!--      <li v-for="{ value } in facetGetters.getFilters(facet)" :key="value">-->
+    <!--        <SfChip size="sm" :input-props="{ value: value }" v-model="selectedProxy">-->
+    <!--          <span class="leading-4">{{ value }}</span>-->
+    <!--        </SfChip>-->
+    <!--      </li>-->
+    <!--    </ul>-->
 
-<!--    <template v-if="facetGetters.getType(facet) === 'color'">-->
-<!--      <SfListItem-->
-<!--        v-for="{ value, label, productCount } in facetGetters.getFilters(facet)"-->
-<!--        :key="value"-->
-<!--        tag="label"-->
-<!--        size="sm"-->
-<!--        :class="['px-1.5 bg-transparent hover:bg-transparent', { 'font-medium': isItemActive(value) }]"-->
-<!--        :selected="isItemActive(value)"-->
-<!--      >-->
-<!--        <template #prefix>-->
-<!--          <input v-model="selectedProxy" :value="value" class="appearance-none peer" type="checkbox" />-->
-<!--          <span-->
-<!--            class="inline-flex items-center justify-center p-1 transition duration-300 rounded-full cursor-pointer -->
-<!--            ring-1 ring-neutral-200 ring-inset outline-offset-2 outline-secondary-600 peer-checked:ring-2 -->
-<!--            peer-checked:ring-primary-700 peer-hover:bg-primary-100 -->
-<!--            peer-[&:not(:checked):hover]:ring-primary-200 peer-active:bg-primary-200 peer-active:ring-primary-300 -->
-<!--            peer-disabled:cursor-not-allowed peer-disabled:bg-disabled-100 peer-disabled:opacity-50 -->
-<!--            peer-disabled:ring-1 peer-disabled:ring-disabled-200 peer-disabled:hover:ring-disabled-200 -->
-<!--            peer-checked:hover:ring-primary-700 peer-checked:active:ring-primary-700 peer-focus-visible:outline"-->
-<!--          >-->
-<!--            <SfThumbnail size="sm" :style="{ backgroundColor: value }" />-->
-<!--          </span>-->
-<!--        </template>-->
-<!--        <p>-->
-<!--          <span class="mr-2 typography-text-sm">{{ label }}</span>-->
-<!--          <SfCounter size="sm">{{ productCount }}</SfCounter>-->
-<!--        </p>-->
-<!--      </SfListItem>-->
-<!--    </template>-->
+    <!--    <template v-if="facetGetters.getType(facet) === 'color'">-->
+    <!--      <SfListItem-->
+    <!--        v-for="{ value, label, productCount } in facetGetters.getFilters(facet)"-->
+    <!--        :key="value"-->
+    <!--        tag="label"-->
+    <!--        size="sm"-->
+    <!--        :class="['px-1.5 bg-transparent hover:bg-transparent', { 'font-medium': isItemActive(value) }]"-->
+    <!--        :selected="isItemActive(value)"-->
+    <!--      >-->
+    <!--        <template #prefix>-->
+    <!--          <input v-model="selectedProxy" :value="value" class="appearance-none peer" type="checkbox" />-->
+    <!--          <span-->
+    <!--            class="inline-flex items-center justify-center p-1 transition duration-300 rounded-full cursor-pointer -->
+    <!--            ring-1 ring-neutral-200 ring-inset outline-offset-2 outline-secondary-600 peer-checked:ring-2 -->
+    <!--            peer-checked:ring-primary-700 peer-hover:bg-primary-100 -->
+    <!--            peer-[&:not(:checked):hover]:ring-primary-200 peer-active:bg-primary-200 peer-active:ring-primary-300 -->
+    <!--            peer-disabled:cursor-not-allowed peer-disabled:bg-disabled-100 peer-disabled:opacity-50 -->
+    <!--            peer-disabled:ring-1 peer-disabled:ring-disabled-200 peer-disabled:hover:ring-disabled-200 -->
+    <!--            peer-checked:hover:ring-primary-700 peer-checked:active:ring-primary-700 peer-focus-visible:outline"-->
+    <!--          >-->
+    <!--            <SfThumbnail size="sm" :style="{ backgroundColor: value }" />-->
+    <!--          </span>-->
+    <!--        </template>-->
+    <!--        <p>-->
+    <!--          <span class="mr-2 typography-text-sm">{{ label }}</span>-->
+    <!--          <SfCounter size="sm">{{ productCount }}</SfCounter>-->
+    <!--        </p>-->
+    <!--      </SfListItem>-->
+    <!--    </template>-->
   </SfAccordionItem>
 </template>
 
@@ -56,7 +56,7 @@ import type { FilterProps } from '~/components/CategoryFilters/types';
 defineProps<FilterProps>();
 // const emit = defineEmits<FilterEmits>();
 
-// const open = ref(true);
+const open = ref(true);
 //
 // const selectedProxy = computed({
 //   get: () => props.selected,
