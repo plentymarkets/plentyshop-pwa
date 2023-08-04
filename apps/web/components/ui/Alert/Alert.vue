8d6f9c6ec0ac7774e2e00ea5eb9316c00114bb01
Jakub Freisler <jakub@frsource.org>
2023-08-04 10:54:53 +0100

    feat: login page [SV-101] (#95)
    clean up Alert component a bit and add "neutral" variant
    add FormPasswordInput
    fix form labels styling issue

--- apps/web/components/ui/Alert/Alert.vue
+++ apps/web/components/ui/Alert/Alert.vue
@@ -4,28 +4,31 @@
   </div>
 </template>
 
+<script lang="ts">
+const sizeClasses = {
+  sm: 'text-xs p-1 gap-1',
+  base: 'text-sm p-1.5 gap-1.5',
+};
+</script>
+
 <script setup lang="ts">
 import type { AlertProps } from '~/components/ui/Alert/types';
 
-const { strong, variant, size } = withDefaults(defineProps<AlertProps>(), {
+const props = withDefaults(defineProps<AlertProps>(), {
   size: 'base',
   strong: false,
   variant: 'primary',
 });
 
-const classes = computed(() => ({
-  'text-sm p-1.5 gap-1.5': size === 'base',
-  'text-xs p-1 gap-1': size === 'sm',
-  'text-white font-medium rounded-none': strong,
-  'rounded-md font-normal': !strong,
-  'text-primary-800': variant === 'primary',
-  'bg-primary-600': variant === 'primary' && strong,
-  'bg-primary-100': variant === 'primary' && !strong,
-  'text-secondary-800': variant === 'secondary',
-  'bg-secondary-800': variant === 'secondary' && strong,
-  'bg-secondary-100': variant === 'secondary' && !strong,
-  'text-negative-800': variant === 'negative',
-  'bg-negative-600': variant === 'secondary' && strong,
-  'bg-negative-100': variant === 'secondary' && !strong,
-}));
+const classes = computed(() => [
+  sizeClasses[props.size],
+  props.strong ? 'text-white font-medium rounded-none' : 'rounded-md font-normal',
+  {
+    [`text-primary-800 ${props.strong ? 'bg-primary-600' : 'bg-primary-100'}`]: props.variant === 'primary',
+    [`text-secondary-800 ${props.strong ? 'bg-secondary-800' : 'bg-secondary-100'}`]: props.variant === 'secondary',
+    [`text-negative-800 ${props.strong ? 'bg-negative-600' : 'bg-negative-100'}`]: props.variant === 'negative',
+    [`text-neutral-900 border border-neutral-200 ${props.strong ? 'bg-neutral-600' : 'bg-neutral-100'}`]:
+      props.variant === 'neutral',
+  },
+]);
 </script>
---
+++ apps/web/pages/login.vue
@@ -0,0 +1,61 @@
+<template>
+  <NuxtLayout name="auth" :heading="$t('auth.login.heading')">
+    <form @submit.prevent="login" class="border-neutral-200 md:border flex flex-col gap-4 md:p-6 rounded-md">
+      <label>
+        <UiFormLabel>{{ $t('form.emailLabel') }}</UiFormLabel>
+        <SfInput name="email" type="email" autocomplete="email" v-model="email" required />
+      </label>
+
+      <label>
+        <UiFormLabel>{{ $t('form.passwordLabel') }}</UiFormLabel>
+        <UiFormPasswordInput name="password" autocomplete="current-password" v-model="password" required />
+      </label>
+
+      <label class="mt-2 flex items-center gap-2">
+        <SfCheckbox name="rememberMe" v-model="rememberMe" />
+        {{ $t('auth.login.rememberMeLabel') }}
+      </label>
+
+      <SfButton type="submit" class="mt-2" :disabled="isLoading">
+        <SfLoaderCircular v-if="isLoading" class="flex justify-center items-center" size="base" />
+        <span v-else>
+          {{ $t('auth.login.submitLabel') }}
+        </span>
+      </SfButton>
+      <SfButton :tag="NuxtLink" to="/reset-password" variant="tertiary">
+        {{ $t('auth.login.forgotPasswordLabel') }}
+      </SfButton>
+    </form>
+
+    <UiAlert class="mt-6 w-full p-4 md:p-6 !justify-start typography-text-base" variant="neutral">
+      <i18n-t tag="span" keypath="auth.login.createAccountBanner">
+        <SfLink :tag="NuxtLink" to="/signup" variant="primary">{{ $t('auth.login.createAccountLinkLabel') }}</SfLink>
+      </i18n-t>
+    </UiAlert>
+  </NuxtLayout>
+</template>
+
+<script lang="ts" setup>
+import { SfButton, SfLink, SfCheckbox, SfInput, SfLoaderCircular } from '@storefront-ui/vue';
+
+definePageMeta({
+  layout: false,
+});
+
+const NuxtLink = resolveComponent('NuxtLink');
+
+const router = useRouter();
+
+const email = ref('');
+const password = ref('');
+const rememberMe = ref<boolean>();
+const isLoading = ref<boolean>();
+
+const login = () => {
+  isLoading.value = true;
+  // mimics waiting an async api call
+  setTimeout(() => {
+    void router.push('/').then(() => (isLoading.value = false));
+  }, 4000);
+};
+</script>
