e89be730b768afa5d9a7d3156f5c3319848e667e
Justyna <46591755+justyna-13@users.noreply.github.com>
2023-08-07 13:34:13 +0200

    feat: get customer address (#104)
    * feat: get customer address
    
    * fix: useCustomerAddress name
    
    * fix: cr fixes
    
    * fix: cr fixes

---
+++ apps/web/composables/useCustomerAddress/useCustomerAddress.ts
@@ -0,0 +1,50 @@
+import { toRefs } from '@vueuse/shared';
+import type {
+  UseCustomerAddressReturn,
+  UseCustomerAddressState,
+  FetchCustomerAddress,
+} from '~/composables/useCustomerAddress/types';
+
+/**
+ * @description Composable managing address data
+ * @returns {@link UseCustomerAddressReturn}
+ * @example
+ * const { data, loading, fetchCustomerAddress } = useCustomerAddress();
+ */
+export const useCustomerAddress: UseCustomerAddressReturn = () => {
+  const state = useState<UseCustomerAddressState>(`useCustomerAddress`, () => ({
+    data: null,
+    loading: false,
+  }));
+
+  /** Function for fetching address data
+   * @example
+   * fetchCustomerAddress();
+   */
+  const fetchCustomerAddress: FetchCustomerAddress = async () => {
+    state.value.loading = true;
+    const { data, error } = await useAsyncData(() =>
+      Promise.resolve({
+        firstName: 'Hieronim',
+        lastName: 'Anonim',
+        address1: 'Oak Drive',
+        address2: '3633',
+        city: 'Colonie',
+        country: 'US',
+        phoneNumber: '+1 321 765 0987',
+        postalCode: '12205',
+        state: 'NY',
+        titleCode: '',
+      }),
+    );
+    useHandleError(error.value);
+    state.value.data = data.value;
+    state.value.loading = false;
+    return data;
+  };
+
+  return {
+    fetchCustomerAddress,
+    ...toRefs(state.value),
+  };
+};
