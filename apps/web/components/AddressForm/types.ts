ed6ddb656b00720f60806f663369762a906baee6
Fabian Gerke <124085586+FabianGerke@users.noreply.github.com>
2023-08-16 11:05:51 +0200

    fix: update tests

--- apps/web/components/AddressForm/types.ts
+++ apps/web/components/AddressForm/types.ts
@@ -1,4 +1,9 @@
-import type { Address, ActiveShippingCountry, AddressType } from '@plentymarkets/plentymarkets-sdk/packages/api-client/src';
+import type {
+  Address,
+  ActiveShippingCountry,
+  AddressType,
+} from '@plentymarkets/plentymarkets-sdk/packages/api-client/src';
+
 export type AddressFormProps = {
   type: AddressType;
   savedAddress?: Address;
