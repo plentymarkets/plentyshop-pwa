c270e45658eb599a5068a3905ebaa3bab3b5c5bf
rbedeoan-plenty <raul.bedeoan@plentymarkets.com>
2023-08-17 13:15:09 +0300

    fix: layout

--- apps/web/components/ProductProperties/ProductProperties.vue
+++ apps/web/components/ProductProperties/ProductProperties.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="px-4" data-testid="product-properties">
-    <SfSelect @update:modelValue="changeVariationId" v-model="selectedVariation" size="sm" placeholder="-- Select --">
-      <option v-for="{ value, label } in options" :key="value" :value="value">
+  <div v-if="attributeGroups && attributeGroups.length" class="px-4" data-testid="product-properties">
+    <SfSelect class="mb-2" v-for="(attributeGroup, index) in attributeGroups" :key="index"
+      @update:modelValue="changeVariationId" v-model="selectedVariation" size="sm" placeholder="-- Select --">
+      <option v-for="{ value, label } in attributeGroup.map(x => ({ value: x.variationId.toString(), label: x.value }))" :key="value"
+        :value="value">
         {{ label }}
       </option>
     </SfSelect>
@@ -9,15 +11,31 @@
 </template>
 
 <script setup lang="ts">
-import Vue from 'vue';
 import { productGetters } from '@plentymarkets/plentymarkets-sdk/packages/sdk/src';
 import { SfSelect } from '@storefront-ui/vue';
 import { ProductPropertiesProps } from '~/components/ProductProperties/types';
-
+import { RouteRecordName, RouteParamValueRaw } from 'vue-router';
 const props = defineProps<ProductPropertiesProps>();
 const product = props.product;
-// this should be refactored..if you have multiple variation types
-const options = computed(() => productGetters.getAttributes([product], ['color', 'size'])[0][0].map(x => ({ value: x.variationId, label: x.value })));
+
+interface ProductAttributeInterface {
+  type: string;
+  id: string;
+  value: string;
+  label: string;
+  variationId: number;
+};
+
+
+const attributeGroups = computed((): ProductAttributeInterface[][] => {
+  const groupsForProducts = productGetters.getAttributes([product], ['color', 'size']) as ProductAttributeInterface[][][];
+  if (groupsForProducts) {
+    return groupsForProducts[0]
+  } else {
+    return []
+  }
+}
+);
 
 const router = useRouter();
 const route = useRoute();
@@ -27,8 +45,8 @@ const selectedVariation = ref(productId);
 
 const changeVariationId = (newId: String) => {
   const delimiter = '-' as string;
-  const newLink = slug.substring(0, slug.lastIndexOf(delimiter)) + delimiter + `${newId}` as String;
-  router.push({ name: route.name, params: { slug: newLink } });
+  const newLink = slug.substring(0, slug.lastIndexOf(delimiter)) + delimiter + `${newId}` as RouteParamValueRaw | (string | number)[];
+  router.push({ name: route.name as RouteRecordName | undefined, params: { slug: newLink } });
 };
 
 </script>
