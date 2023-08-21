521b6d02f6046dfe7d03e012f9df556c334da7f8
rbedeoan-plenty <raul.bedeoan@plentymarkets.com>
2023-08-21 16:14:42 +0300

    fix: paddings

--- apps/web/components/Cookiebar/Cookiebar.vue
+++ apps/web/components/Cookiebar/Cookiebar.vue
@@ -2,16 +2,16 @@
   <client-only>
     <div
       v-if="!hideBanner"
-      class="fixed font-sf-xl z-50 w-full xl:w-3/5 xl:right-2 bottom-0 xl:bottom-2 shadow-2xl p-10 bg-white"
+      class="fixed font-sf-xl z-50 w-full xl:w-3/5 xl:right-2 bottom-0 xl:bottom-2 shadow-2xl p-2 bg-white"
     >
       <div v-if="!furtherSettingsOn">
         <!-- cookie info -->
         <div
-          class="text-left md:text-center ml-sf-xs md:ml-0 non-italic font-sf-semibold pt-sf-xs text-sf-xl text-sf-c-primary"
+          class="text-left font-medium md:text-center ml-sf-xs md:ml-0 non-italic font-sf-semibold pt-sf-xs text-sf-xl text-sf-c-primary"
         >
           {{ cookieGroups?.barTitle }}
         </div>
-        <div class="non-italic font-sf-normal text-sf-xs md:text-sf-sm leading-relaxed p-2 pb-5">
+        <div class="non-italic font-sf-normal text-sf-xs md:text-sf-xs leading-relaxed p-2 pb-5">
           {{ cookieGroups.barDescription }}
 
           <SfLink href="/PrivacyPolicy" class="sf-button--text">
@@ -23,7 +23,7 @@
           v-if="cookieJson"
           class="flex flex-col md:flex-row flex-wrap md:flex-nowrap ml-sf-xs justify-between items-center"
         >
-          <div v-for="(cookieGroup, index) in cookieJson" :key="index" class="mr-sf-sm flex items-center">
+          <div v-for="(cookieGroup, index) in cookieJson" :key="index" class="mr-sf-sm mb-5 flex items-center">
             <SfCheckbox
               :id="cookieGroup.name"
               v-model="cookieGroup.accepted"
@@ -40,9 +40,9 @@
           </div>
         </div>
       </div>
-      <div v-else class="overflow-y-auto h-80 px-2 pb-2">
-        <div v-for="(cookieGroup, groupIndex) in cookieJson" :key="groupIndex" class="mb-2 p-2">
-          <div>
+      <div v-else class="overflow-y-auto h-80 pb-2">
+        <div v-for="(cookieGroup, groupIndex) in cookieJson" :key="groupIndex" class="mb-2 bg-gray-100">
+          <div class="px-2">
             <SfCheckbox
               :id="cookieGroup.name"
               v-model="cookieGroup.accepted"
@@ -59,23 +59,24 @@
               {{ cookieGroup.description }}
             </div>
             <div v-if="cookieGroup.showMore ?? false">
-              <div v-for="(cookie, cookieIndex) in cookieGroup.cookies" :key="cookieIndex" class="mb-5">
-                <div class="flex w-full items-center">
+              <div v-for="(cookie, cookieIndex) in cookieGroup.cookies" :key="cookieIndex" class="mb-2">
+                <div class="flex w-full items-center bg-white mb-2 pa-2">
                   <SfCheckbox
+                    class="ml-1"
                     :id="cookie.name"
                     v-model="cookie.accepted"
                     :disabled="groupIndex === defaultCheckboxIndex"
                     @update:model-value="updateParentCheckbox(cookieGroup)"
                   />
                   <label
-                    class="ml-3 text-base text-gray-900 cursor-pointer font-body peer-disabled:text-disabled-900"
+                    class="ml-3 mt-2 mb-2 text-base text-gray-900 cursor-pointer font-body peer-disabled:text-disabled-900"
                     :for="cookieGroup.name"
                   >
                     {{ cookie.name }}
                   </label>
                 </div>
                 <div v-for="propKey in Object.keys(cookie)" :key="propKey">
-                  <div v-if="propKey !== 'name' && propKey !== 'accepted'" class="flex w-full mb-2 p-2 bg-white">
+                  <div v-if="propKey !== 'name' && propKey !== 'accepted'" class="flex w-full mb-1 p-2 bg-white">
                     <div class="w-1/4">
                       {{ propKey }}
                     </div>
@@ -87,7 +88,6 @@
                         </SfLink>
                       </template>
                       <template v-else>
-                        <!-- TODO -->
                         {{ cookie.name }}
                       </template>
                     </div>
@@ -96,36 +96,37 @@
               </div>
             </div>
           </div>
-          <div>
-            <SfButton
+          <div class="ml-2">
+            <SfLink
               v-if="!cookieGroup.showMore ?? false"
-              type="button"
               size="sm"
-              variant="secondary"
               @click="cookieGroup.showMore = true"
+              class="sf-button--text"
             >
-              {{ $t('CookieBar.More information') }}
-            </SfButton>
-            <SfButton v-else type="button" size="sm" @click="cookieGroup.showMore = false">
+              <div class="text-black-200">
+                {{ $t('CookieBar.More information') }}
+              </div>
+            </SfLink>
+            <SfLink v-else type="button" size="sm" @click="cookieGroup.showMore = false" class="sf-button--text text-gray-100">
               {{ $t('CookieBar.Show less') }}
-            </SfButton>
+            </SfLink>
           </div>
         </div>
       </div>
       <!-- further settings / back button -->
       <div class="my-2 text-center">
-        <SfButton v-if="!furtherSettingsOn" type="button" variant="secondary" @click="furtherSettingsOn = true">
+        <SfLink v-if="!furtherSettingsOn" @click="furtherSettingsOn = true" class="sf-button--text">
           {{ $t('CookieBar.Further Settings') }}
-        </SfButton>
-        <SfButton v-else type="button" variant="secondary" @click="furtherSettingsOn = false">
+        </SfLink>
+        <SfLink v-else @click="furtherSettingsOn = false" class="sf-button--text">
           {{ $t('CookieBar.Back') }}
-        </SfButton>
+        </SfLink>
       </div>
       <!-- action buttons -->
       <div class="w-full flex flex-col xl:flex-row mt-5 gap-2">
         <div class="flex-1 mb-2">
           <SfButton
-            class="w-full"
+            class="w-full text-red-600"
             :aria-disabled="false"
             type="button"
             aria-label="button"
