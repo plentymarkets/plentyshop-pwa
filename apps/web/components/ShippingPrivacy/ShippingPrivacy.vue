9d983cb984e235166a4f29858d4420a6fa92f468
abocsan-plenty <129151096+abocsan-plenty@users.noreply.github.com>
2023-08-23 12:36:55 +0300

    fix: shipping provider name fix

--- apps/web/components/ShippingPrivacy/ShippingPrivacy.vue
+++ apps/web/components/ShippingPrivacy/ShippingPrivacy.vue
@@ -27,7 +27,7 @@ const { shippingPrivacyAgreement, setShippingPrivacyAgreement } = useAdditionalI
 const { selectedMethod } = useCartShippingMethods();
 
 const parcelServiceInformation = computed(() => {
-  selectedMethod.value ? shippingProviderGetters.getShippingMethodName(selectedMethod.value) : '';
+  return shippingProviderGetters.getShippingMethodName(selectedMethod.value);
 });
 
 const changeHint = (value: boolean) => {
