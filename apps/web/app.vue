f845132887e957a6651de23d62ce32c70fe9f2bf
abocsan-plenty <129151096+abocsan-plenty@users.noreply.github.com>
2023-08-24 18:59:49 +0300

    fix: double request

--- apps/web/app.vue
+++ apps/web/app.vue
@@ -9,8 +9,12 @@
 <script setup lang="ts">
 const { getCategoryTree } = useCategoryTree();
 const { setInitialData } = useInitialSetup();
+const route = useRoute();
+
+if (route.layoutName !== 'checkout') {
+  setInitialData();
+}
 
 getCategoryTree();
-setInitialData();
 usePageTitle();
 </script>
