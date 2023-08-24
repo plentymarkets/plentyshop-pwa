bb1968d066890f6f6f5145bf8c439f676b999eb2
Maximilian Röll <30629157+maxiroellplenty@users.noreply.github.com>
2023-08-24 14:57:57 +0200

    fix: linting

--- apps/web/composables/useInitalSetup/useInitialSetup.ts
+++ apps/web/composables/useInitalSetup/useInitialSetup.ts
@@ -2,6 +2,21 @@ import { Cart, SessionResult } from '@plentymarkets/plentymarkets-sdk/packages/a
 import { useSdk } from '~/sdk';
 import { SetInitialData, UseInitialSetupReturn } from './types';
 
+/** Function for getting current customer/cart data from session
+ * @example
+ * setInitialData();
+ */
+const setInitialData: SetInitialData = async () => {
+  const { data, error } = await useAsyncData(() => useSdk().plentysystems.getSession());
+  useHandleError(error.value);
+
+  const { setUser } = useCustomer();
+  setUser(data.value?.data as SessionResult);
+
+  const { setCart } = useCart();
+  setCart(data.value?.data.basket as Cart);
+};
+
 /**
  * @description Composable to get initial customer and cart data
  * @returns {@link UseInitialSetupReturn}
@@ -10,22 +25,6 @@ import { SetInitialData, UseInitialSetupReturn } from './types';
  */
 
 export const useInitialSetup: UseInitialSetupReturn = () => {
-
-  /** Function for getting current customer/cart data from session
-   * @example
-   * setInitialData();
-   */
-  const setInitialData: SetInitialData = async () => {
-    const { data, error } = await useAsyncData(() => useSdk().plentysystems.getSession());
-    useHandleError(error.value);
-
-    const { setUser } = useCustomer();
-    setUser(data.value?.data as SessionResult);
-
-    const { setCart } = useCart();
-    setCart(data.value?.data.basket as Cart);
-  };
-
   return {
     setInitialData,
   };
