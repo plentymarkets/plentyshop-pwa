ffea86fc661867fbaf5e907148e99ff5bae786df
Maximilian Röll <30629157+maxiroellplenty@users.noreply.github.com>
2023-08-28 09:40:29 +0200

    fix: lint and cors import

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,26 +1,22 @@
 import { createServer } from '@vue-storefront/middleware';
+import consola from 'consola';
+import cors from 'cors';
 import config from '../middleware.config';
-const cors = require("cors");
 
 (async () => {
   const app = await createServer({ integrations: config.integrations });
   const host = process.argv[2] ?? '0.0.0.0';
   const port = Number(process.argv[3]) || 8181;
-  const CORS_MIDDLEWARE_NAME = "corsMiddleware";
+  const CORS_MIDDLEWARE_NAME = 'corsMiddleware';
 
-  const corsMiddleware = app._router.stack.find(
-    (middleware: any) => middleware.name === CORS_MIDDLEWARE_NAME
-  );
+  const corsMiddleware = app._router.stack.find((middleware: any) => middleware.name === CORS_MIDDLEWARE_NAME);
 
   corsMiddleware.handle = cors({
-    origin: [
-      "http://localhost:3000",
-      ...(process.env.MIDDLEWARE_ALLOWED_ORIGINS?.split(",") ?? []),
-    ],
-    credentials: true
+    origin: ['http://localhost:3000', ...(process.env.MIDDLEWARE_ALLOWED_ORIGINS?.split(',') ?? [])],
+    credentials: true,
   });
 
   app.listen(port, host, () => {
-    console.log(`Middleware started: ${host}:${port}`);
+    consola.log(`Middleware started: ${host}:${port}`);
   });
 })();
