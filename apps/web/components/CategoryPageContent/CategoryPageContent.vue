40cb5056459d4ed83f2f33e49ad3ed348864afbc
rbedeoan-plenty <raul.bedeoan@plentymarkets.com>
2023-08-28 16:31:02 +0300

    fix: actual price in category page

--- apps/web/components/CategoryPageContent/CategoryPageContent.vue
+++ apps/web/components/CategoryPageContent/CategoryPageContent.vue
@@ -20,38 +20,25 @@
               {{ $t('listSettings') }}
             </SfButton>
           </div>
-          <section
-            v-if="products"
+          <section v-if="products"
             class="grid grid-cols-1 2xs:grid-cols-2 gap-4 md:gap-6 md:grid-cols-2 lg:grid-cols-3 3xl:grid-cols-4 mb-10 md:mb-5"
-            data-testid="category-grid"
-          >
+            data-testid="category-grid">
             <NuxtLazyHydrate when-visible v-for="(product, index) in products" :key="productGetters.getId(product)">
-              <UiProductCard
-                :product="product"
-                :name="productGetters.getName(product) ?? ''"
+              <UiProductCard :product="product" :name="productGetters.getName(product) ?? ''"
                 :rating-count="productGetters.getTotalReviews({} as ReviewAverage)"
-                :rating="productGetters.getAverageRating({} as ReviewAverage)"
-                :price="productGetters.getPrice(product)?.special ?? productGetters.getPrice(product)?.regular ?? 0"
-                :image-url="productGetters.getCoverImage(product)"
-                :image-alt="productGetters.getName(product) ?? ''"
-                :slug="productGetters.getSlug(product) + `-${productGetters.getId(product)}`"
-                :priority="index === 0"
+                :rating="productGetters.getAverageRating({} as ReviewAverage)" 
+                :price="actualPrice(product)"
+                :image-url="productGetters.getCoverImage(product)" :image-alt="productGetters.getName(product) ?? ''"
+                :slug="productGetters.getSlug(product) + `-${productGetters.getId(product)}`" :priority="index === 0"
                 :base-price="productGetters.getDefaultBasePrice(product)"
-                :unit-content="productGetters.getUnitContent(product)"
-                :unit-name="productGetters.getUnitName(product)"
-                :show-base-price="productGetters.showPricePerUnit(product)"
-              />
+                :unit-content="productGetters.getUnitContent(product)" :unit-name="productGetters.getUnitName(product)"
+                :show-base-price="productGetters.showPricePerUnit(product)" />
             </NuxtLazyHydrate>
           </section>
           <LazyCategoryEmptyState v-else />
           <NuxtLazyHydrate when-visible>
-            <UiPagination
-              v-if="totalProducts > 0"
-              :current-page="getFacetsFromURL().page ?? 1"
-              :total-items="totalProducts"
-              :page-size="itemsPerPage"
-              :max-visible-pages="maxVisiblePages"
-            />
+            <UiPagination v-if="totalProducts > 0" :current-page="getFacetsFromURL().page ?? 1"
+              :total-items="totalProducts" :page-size="itemsPerPage" :max-visible-pages="maxVisiblePages" />
           </NuxtLazyHydrate>
         </div>
       </div>
@@ -60,6 +47,7 @@
 </template>
 
 <script setup lang="ts">
+import { Product } from '@plentymarkets/plentymarkets-sdk/packages/api-client/src';
 import { ReviewAverage } from '@plentymarkets/plentymarkets-sdk/packages/api-client/server';
 import { productGetters } from '@plentymarkets/plentymarkets-sdk/packages/sdk/src';
 import { SfButton, SfIconTune, useDisclosure } from '@storefront-ui/vue';
@@ -86,4 +74,12 @@ watch(isTabletScreen, (value) => {
     close();
   }
 });
+
+const actualPrice = (product: Product): number => {
+  const price = productGetters.getPrice(product);
+  if (price && (price.special || price.regular)) {
+    return price.special ?? price.regular ?? 0;
+  }
+  return 0;
+};
 </script>
