beab1bf83bbb2ddb0e865d2730bbaf074a34b57b
rbedeoan-plenty <raul.bedeoan@plentymarkets.com>
2023-08-28 15:46:03 +0300

    fix: separate comp simple and readable

---
+++ apps/web/components/Price/Price.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="text-sm">
+    <div class="my-1">
+      <span class="mr-2 text-secondary-700 font-bold font-headings text-2xl" data-testid="price">
+        {{ $n(actualPrice, 'currency') }}
+      </span>
+      <span v-if="price.special" class="text-base font-normal text-neutral-500 line-through">
+        {{ $n(price.regular, 'currency') }}
+      </span>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { PriceProps } from '~/components/Price/types';
+
+const props = defineProps<PriceProps>();
+const { price } = props;
+
+const actualPrice = computed(() => {
+  if (price && (price.special || price.regular)) {
+    return price.special ? price.special : price.regular;
+  }
+  return 0;
+});
+</script>
---
+++ apps/web/components/Price/types.ts
@@ -0,0 +1,5 @@
+import { AgnosticPrice } from '@plentymarkets/plentymarkets-sdk/packages/sdk/src/getters/agnostic.types';
+
+export type PriceProps = {
+  price: AgnosticPrice;
+};
