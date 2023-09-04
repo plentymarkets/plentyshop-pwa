7761e18edb809c759ee06211a0f24e8ee1940f3c
Maximilian Röll <30629157+maxiroellplenty@users.noreply.github.com>
2023-09-04 16:10:50 +0200

    fix: linting

--- apps/web/components/ui/Notifications/Notifications.vue
+++ apps/web/components/ui/Notifications/Notifications.vue
@@ -1,11 +1,7 @@
 <template>
   <div data-testid="notifications" class="z-50 absolute w-50 right-2 top-2 max-w-[600px]">
-    <div v-for="notification of notifications" class="my-2">
-      <Component
-        :key="notification.id"
-        :notification="notification"
-        :is="getNotificationComponentByType(notification.type)"
-      ></Component>
+    <div v-for="notification of notifications" class="my-2" :key="notification.id">
+      <Component :notification="notification" :is="getNotificationComponentByType(notification.type)"></Component>
     </div>
   </div>
 </template>
