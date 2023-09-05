cb2739bf4eba56c4b4abb5a0e3de8f12132ac0b9
Maximilian Röll <30629157+maxiroellplenty@users.noreply.github.com>
2023-09-05 16:10:11 +0200

    fix: useAsyncData caching the restcall for products

--- apps/web/composables/useProduct/useProduct.ts
+++ apps/web/composables/useProduct/useProduct.ts
@@ -23,7 +23,7 @@ export const useProduct: UseProductReturn = (slug) => {
    */
   const fetchProduct: FetchProduct = async (slug) => {
     state.value.loading = true;
-    const { data, error } = await useAsyncData(() => useSdk().plentysystems.getProduct({ id: slug }));
+    const { data, error } = await useAsyncData(slug + new Date().getTime(), () => useSdk().plentysystems.getProduct({ id: slug }));
     useHandleError(error.value);
 
     state.value.data = data.value?.data ?? state.value.data;
