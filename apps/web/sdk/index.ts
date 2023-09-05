a04a958d25dd0275b1df34f4787320f12350a678
Maximilian Röll <30629157+maxiroellplenty@users.noreply.github.com>
2023-09-05 13:02:17 +0200

    feat: use process env to set endpoint url

--- apps/web/sdk/index.ts
+++ apps/web/sdk/index.ts
@@ -5,7 +5,7 @@ import { createSharedComposable } from '@vueuse/core';
 export const useSdk = createSharedComposable(() => {
   const sdkConfig = {
     plentysystems: buildModule<PlentysystemsModuleType>(plentysystemsModule, {
-      apiUrl: 'https://fcsgmt0e4z97.c14-01.plentymarkets.com/plentysystems',
+      apiUrl: process.env.API_ENDPOINT ? `${process.env.API_ENDPOINT}/plentysystems` : 'http://localhost:8181/plentysystems',
     }),
   };
 
