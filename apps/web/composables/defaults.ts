65a0f6291a2100e4be0bf203216cc8be332ab783
Fabian Gerke <124085586+FabianGerke@users.noreply.github.com>
2023-09-06 16:49:42 +0200

    fix: add DEFAULT value for orders per page

--- apps/web/composables/defaults.ts
+++ apps/web/composables/defaults.ts
@@ -1,9 +1,11 @@
 const DEFAULT_ITEMS_PER_PAGE = 50;
+const DEFAULT_ORDERS_PER_PAGE = 5;
 const DEFAULT_PAGE = 1;
 const PER_PAGE_STEPS: number[] = [10, 20, 50, 100];
 
 export const defaults = {
   DEFAULT_ITEMS_PER_PAGE,
+  DEFAULT_ORDERS_PER_PAGE,
   DEFAULT_PAGE,
   PER_PAGE_STEPS,
 };
