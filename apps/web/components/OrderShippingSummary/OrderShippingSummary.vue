8861c2e26fa85fdb24e30b845be6b65011a848c0
Fabian Gerke <124085586+FabianGerke@users.noreply.github.com>
2023-09-13 12:11:55 +0200

    feat: adding item list to the ThankYou page / components

---
+++ apps/web/components/OrderShippingSummary/OrderShippingSummary.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="font-bold text-primary-700 font-headings md:text-lg mb-3">
+    {{ $t('account.ordersAndReturns.shippingSummary.heading') }}
+  </div>
+  <h2 class="font-medium text-base">{{ $t('account.ordersAndReturns.shippingSummary.shipTo') }}</h2>
+  <OrderAddressData v-if="shippingAddress" :address="shippingAddress" />
+
+  <h2 class="font-medium text-base mt-4">
+    {{ $t('account.ordersAndReturns.shippingSummary.shippingMethod') }}
+  </h2>
+  <p>{{ orderGetters.getShippingProvider(order) }}</p>
+</template>
+
+<script setup lang="ts">
+import { orderGetters } from '@plentymarkets/shop-sdk';
+import { OrderShippingSummaryPropsType } from './types';
+
+const props = defineProps<OrderShippingSummaryPropsType>();
+
+const shippingAddress = orderGetters.getShippingAddress(props.order);
+</script>
---
+++ apps/web/components/OrderTotals/OrderTotals.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="grid grid-cols-2 mb-2">
+    <p class="font-medium text-base">{{ $t('orderConfirmation.subTotal') }}:</p>
+    <p class="text-right">{{ $n(orderGetters.getSubTotal(order.totals), 'currency') }}</p>
+  </div>
+  <div class="grid grid-cols-2 mb-2">
+    <p class="font-medium text-base">{{ $t('orderConfirmation.shipping') }}:</p>
+    <p class="text-right">{{ $n(orderGetters.getShippingCost(order) ?? 0, 'currency') }}</p>
+  </div>
+  <div class="grid grid-cols-2 mb-2">
+    <p class="font-medium text-base">
+      {{ $t('orderConfirmation.vat') }} ({{ orderGetters.getVatRate(order.totals) }}%):
+    </p>
+    <p class="text-right">{{ $n(orderGetters.getVatAmount(order.totals), 'currency') }}</p>
+  </div>
+  <UiDivider class="mb-2" />
+  <div class="grid grid-cols-2">
+    <p class="font-medium text-base">{{ $t('orderConfirmation.total') }}:</p>
+    <p class="text-right">{{ $n(orderGetters.getTotal(order.totals), 'currency') }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { orderGetters } from '@plentymarkets/shop-sdk';
+import { OrderTotalsPropsType } from './types';
+
+defineProps<OrderTotalsPropsType>();
+</script>
