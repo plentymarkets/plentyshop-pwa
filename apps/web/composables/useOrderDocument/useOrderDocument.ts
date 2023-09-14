163acf1a2ddd5d6f44a179559955e85a2565ee41
abocsan-plenty <129151096+abocsan-plenty@users.noreply.github.com>
2023-09-14 17:49:37 +0300

    feat: order documents cleanup

--- apps/web/composables/useOrderDocument/useOrderDocument.ts
+++ apps/web/composables/useOrderDocument/useOrderDocument.ts
@@ -5,10 +5,10 @@ import type { UseOrderDocumentState, UseOrderDocumentMethodsReturn } from './typ
 import { DownloadFile, GetDocument } from './types';
 
 /**
- * @description Composable for getting the legal information.
+ * @description Composable for getting the order document.
  * @returns {@link UseOrderDocumentMethodsReturn}
  * @example
- * const { data, loading } = UseOrderDocument();
+ * const { data, loading, getDocument, downloadFile } = UseOrderDocument();
  */
 export const useOrderDocument: UseOrderDocumentMethodsReturn = () => {
   const state = useState<UseOrderDocumentState>('useOrderDocument', () => ({
@@ -16,6 +16,12 @@ export const useOrderDocument: UseOrderDocumentMethodsReturn = () => {
     loading: false,
   }));
 
+  /**
+   * @description Function for getting the order document array buffer.
+   *
+   * @param document
+   * @param accessKey
+   */
   const getDocument: GetDocument = async (document: OrderDocument, accessKey: string) => {
     state.value.loading = true;
     try {
@@ -30,11 +36,20 @@ export const useOrderDocument: UseOrderDocumentMethodsReturn = () => {
     }
   };
 
+  /**
+   * @description Function for downloading an array buffer file.
+   *
+   * @param bufferArray
+   * @param name
+   * @param type
+   *
+   * @example downloadFile(bufferArray, 'test.pdf', 'application/pdf');
+   */
   const downloadFile: DownloadFile = (bufferArray: number[], name: string, type: string) => {
-    const base64PDF = window.btoa(String.fromCharCode.apply(null, bufferArray));
+    const base64 = window.btoa(String.fromCharCode.apply(null, bufferArray));
 
     const link = document.createElement('a');
-    link.href = `data:${type};base64,${base64PDF}`;
+    link.href = `data:${type};base64,${base64}`;
     link.download = name;
     link.click();
 
