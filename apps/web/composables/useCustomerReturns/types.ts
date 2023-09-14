14809f90d2ceb34efe483f43bf4482c0107c68d3
Fabian Gerke <124085586+FabianGerke@users.noreply.github.com>
2023-09-14 18:41:13 +0200

    fix: add pagination

--- apps/web/composables/useCustomerReturns/types.ts
+++ apps/web/composables/useCustomerReturns/types.ts
@@ -1,12 +1,12 @@
 import type { Ref } from 'vue';
-import { Order, PaginatedResult } from '@plentymarkets/shop-api';
+import {Order, PaginatedResult, UseUserOrderSearchParams} from '@plentymarkets/shop-api';
 
 export interface UseCustomerReturnsState {
   data: PaginatedResult<Order>;
   loading: boolean;
 }
 
-export type FetchCustomerReturns = () => Promise<PaginatedResult<Order>>;
+export type FetchCustomerReturns = (params: UseUserOrderSearchParams) => Promise<PaginatedResult<Order>>;
 
 export interface UseCustomerReturns {
   data: Readonly<Ref<UseCustomerReturnsState['data']>>;
--- apps/web/composables/useCustomerReturns/useCustomerReturns.ts
+++ apps/web/composables/useCustomerReturns/useCustomerReturns.ts
@@ -23,13 +23,10 @@ export const useCustomerReturns: UseCustomerReturnsReturn = () => {
    * @example
    * fetchCustomerReturns();
    */
-  const fetchCustomerReturns: FetchCustomerReturns = async () => {
+  const fetchCustomerReturns: FetchCustomerReturns = async (params) => {
     state.value.loading = true;
-    const { data, error } = await useAsyncData(() =>
-      useSdk().plentysystems.getReturns({
-        page: 1,
-        items: 5,
-      }),
+    const { data, error } = await useAsyncData((params.page ?? 1).toString(), () =>
+      useSdk().plentysystems.getReturns(params),
     );
     useHandleError(error.value);
     state.value.data = data.value?.data || state.value.data;
