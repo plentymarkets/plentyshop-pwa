e8536ade7777481ff458fae6f8ec5f6930485346
doproiescu-plenty <126688988+doproiescu-plenty@users.noreply.github.com>
2023-09-15 10:50:20 +0300

    Update AttributeSelect.vue

--- apps/web/components/AttributeSelect/AttributeSelect.vue
+++ apps/web/components/AttributeSelect/AttributeSelect.vue
@@ -1,74 +1,133 @@
 <template>
-  <div v-if="attributeGroups && attributeGroups.length > 0" class="px-4" data-testid="product-properties">
-    <SfSelect
-      v-for="(attributeGroup, index) in attributeGroups"
-      :key="index"
-      class="mb-2"
-      @update:model-value="changeVariationId"
-      v-model="selectedVariation"
-      size="sm"
-      placeholder="Please select"
+  <pre></pre>
+  <SfSelect
+    v-for="(attribute, index) in transformedProduct.attributes"
+    :key="index"
+    class="mb-2"
+    @update:model-value="changeVariationId($event, index)"
+    v-model="selectedVariations[index]"
+    size="sm"
+    placeholder="Select"
+  >
+    <option
+      v-for="value in attribute.values"
+      :key="value.attributeValueId"
+      :value="value.attributeValueId"
+      :disabled="value.disabled"
     >
-      <option v-for="{ value, label } in mapValueAndLabel(attributeGroup)" :key="value" :value="value">
-        {{ label }}
-      </option>
-    </SfSelect>
-  </div>
+      {{ value.name }}
+    </option>
+  </SfSelect>
+  <button @click="resetAttributes">RESET</button>
 </template>
 
 <script setup lang="ts">
-import { RouteRecordName, RouteParamValueRaw } from 'vue-router';
-import { productGetters } from '@plentymarkets/shop-sdk';
-import { ProductAttributeValue } from '@plentymarkets/shop-sdk/lib/getters/agnostic.types';
 import { SfSelect } from '@storefront-ui/vue';
 import { AttributeSelectProps } from '~/components/AttributeSelect/types';
 
 const props = defineProps<AttributeSelectProps>();
 const product = props.product;
 
-const attributeGroups = computed((): ProductAttributeValue[][] => {
-  const groupsForProducts = productGetters.getAttributes([product]) as ProductAttributeValue[][][];
-  return groupsForProducts[0] ?? [];
-});
+const transformProductData = (product: any) => {
+  const { attributes, variations } = product.variationAttributeMap;
 
-const mapValueAndLabel = (attributeGroup: ProductAttributeValue[]) => {
-  return attributeGroup.map((x) => ({ value: x.variationId.toString(), label: x.value }));
+  const transformedAttributes = attributes.map(({ attributeId, name, values }) => ({
+    attributeId,
+    name,
+    values: values.map(({ attributeValueId, name }) => ({
+      attributeValueId,
+      name,
+      disabled: false, // Assuming all values are initially available
+    })),
+  }));
+
+  const transformedCombinations = variations.map(({ variationId, isSalable, attributes }) => ({
+    variationId,
+    isSalable,
+    attributeCombination: attributes,
+  }));
+
+  return {
+    attributes: transformedAttributes,
+    combinations: transformedCombinations,
+  };
 };
 
-const router = useRouter();
-const route = useRoute();
-const slug = route.params.slug as string;
-const productId = slug.split('-').pop() ?? '0';
-const selectedVariation = ref(productId);
+const transformedProduct = computed(() => transformProductData(product));
 
-const preselectVariation = () => {
-  if (product.value && product.value.variations) {
-    const defaultVariation = product.value.variations.find((variation) => variation.isDefault);
-    if (defaultVariation) {
-      selectedVariation.value = defaultVariation.id;
-    } else if (product.value.variations.length > 0) {
-      selectedVariation.value = product.value.variations[0].id;
+let selectedVariations = ref(Array.from({ length: transformedProduct.value.attributes.length }).fill(null));
+
+const changeVariationId = (selectedValue, index) => {
+  selectedVariations.value[index] = Number.parseInt(selectedValue);
+
+  if (selectedVariations.value.every((value) => value !== null)) {
+    // Check if all attributes are selected
+    for (const combination of transformedProduct.value.combinations) {
+      const match = combination.attributeCombination.every(
+        (attribute, index_) =>
+          selectedVariations.value[index_] === null || attribute.attributeValueId === selectedVariations.value[index_],
+      );
+
+      if (match && combination.isSalable) {
+        console.log(combination.variationId);
+        // If you want to obtain the variationId, you can use combination.variationId here.
+        // For instance, you can emit this value to the parent or store it in a ref variable.
+        break;
+      }
+    }
+  }
+
+  updateAvailableOptions();
+};
+
+const updateAvailableOptions = () => {
+  for (const attribute of transformedProduct.value.attributes) {
+    for (const value of attribute.values) {
+      value.disabled = !isOptionAvailable(attribute.attributeId, value.attributeValueId, selectedVariations.value);
     }
   }
 };
 
-watch(
-  product,
-  () => {
-    if (product.value) {
-      preselectVariation();
+const isOptionAvailable = (attributeId, valueId, currentSelections) => {
+  // Iterate through each product combination
+  for (const combination of transformedProduct.value.combinations) {
+    if (!combination.isSalable) {
+      continue; // Skip if this combination is not salable
     }
-  },
-  { immediate: true },
-);
 
-preselectVariation();
+    const attributeInCombination = combination.attributeCombination.find(
+      (attribute) => attribute.attributeId === attributeId,
+    );
 
-const changeVariationId = (updatedId: String) => {
-  const delimiter = '-';
-  const link = (slug.slice(0, Math.max(0, slug.lastIndexOf(delimiter))) + delimiter + `${updatedId}`) as
-    | RouteParamValueRaw
-    | (string | number)[];
-  router.push({ name: route.name as RouteRecordName | undefined, params: { slug: link } });
+    // If the combination doesn't have the attribute or the value, skip
+    if (!attributeInCombination || attributeInCombination.attributeValueId !== valueId) {
+      continue;
+    }
+
+    const isValidCombination = currentSelections.every((selectedValue, index) => {
+      // If the selection is null, then it is considered valid since it hasn't been chosen yet
+      if (selectedValue === null) return true;
+
+      // If the current attribute value is part of the combination, it is valid
+      const attributeInCurrentCombination = combination.attributeCombination[index];
+      return attributeInCurrentCombination.attributeValueId === selectedValue;
+    });
+
+    if (isValidCombination) {
+      return true; // Found a valid combination with the attribute value
+    }
+  }
+
+  return false; // No valid combinations found for this attribute value
 };
+
+const resetAttributes = () => {
+  selectedVariations.value = Array.from({ length: transformedProduct.value.attributes.length }).fill(null);
+  updateAvailableOptions(); // Update available options after resetting
+};
+
+// Initialize available options
+watchEffect(() => {
+  updateAvailableOptions();
+});
 </script>
