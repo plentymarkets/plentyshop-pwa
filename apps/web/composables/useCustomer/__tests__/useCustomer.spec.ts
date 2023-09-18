7aa84b704bcd8631768c94f523b8932656d531dd
Justyna <46591755+justyna-13@users.noreply.github.com>
2023-09-18 14:06:58 +0200

    chore: unit tests workflow [SV-214] (#141)
    Co-authored-by: Szymon Dziewoński <szymon.dziewonski@gmail.com>

--- apps/web/composables/useCustomer/__tests__/useCustomer.spec.ts
+++ apps/web/composables/useCustomer/__tests__/useCustomer.spec.ts
@@ -1,5 +1,19 @@
 import { useCustomer } from '~/composables/useCustomer/useCustomer';
 
+vi.mock('~/sdk', () => ({
+  useSdk: () => ({
+    commerce: {
+      getCustomer: vi.fn(() => ({
+        id: 'SfId',
+        email: 'hieronim.anonim@gmail.com',
+        firstName: 'hieronim',
+        lastName: 'anonim'
+      })),
+    },
+  }),
+}));
+
+
 describe('useCustomer', () => {
   it('should return account data', async () => {
     const { fetchCustomer, data } = useCustomer();
