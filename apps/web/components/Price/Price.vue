24cdedcf3f866a9741ddb58c485da5dd4aaf6ecb
doproiescu-plenty <126688988+doproiescu-plenty@users.noreply.github.com>
2023-09-19 12:22:18 +0300

    fix: include net prices in price component

--- apps/web/components/Price/Price.vue
+++ apps/web/components/Price/Price.vue
@@ -3,6 +3,7 @@
     <div class="my-1">
       <span class="mr-2 text-secondary-700 font-bold font-headings text-2xl" data-testid="price">
         {{ $n(mainPrice, 'currency') }}
+        <span v-if="showNetPrices">{{ $t('asterisk') }} </span>
       </span>
       <span v-if="oldPrice && oldPrice !== mainPrice" class="text-base font-normal text-neutral-500 line-through">
         {{ $n(oldPrice, 'currency') }}
@@ -16,6 +17,9 @@ import { PriceProps } from '~/components/Price/types';
 
 const props = defineProps<PriceProps>();
 const { price } = props;
+
+const runtimeConfig = useRuntimeConfig();
+const showNetPrices = runtimeConfig.public.showNetPrices;
 
 const mainPrice = computed<number>(() => {
   if (!price) return 0;
