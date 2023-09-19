a40b8904229993f8e15b75719001b0ec37ff93a5
Fabian Gerke <124085586+FabianGerke@users.noreply.github.com>
2023-09-19 11:04:15 +0200

    fix: add clearCartItems

--- apps/web/components/PayPal/PayPalExpressButton.vue
+++ apps/web/components/PayPal/PayPalExpressButton.vue
@@ -17,7 +17,7 @@ const currency = vsfCurrency?.length > 0 ? vsfCurrency : fallbackCurrency;
 const paypalUuid = uuid();
 const { loadScript, createTransaction, approveOrder, executeOrder } = usePayPal();
 const { createOrder } = useMakeOrder();
-const { data: cart, getCart } = useCart();
+const { data: cart, clearCartItems } = useCart();
 const router = useRouter();
 const emits = defineEmits(['on-click']);
 
@@ -55,7 +55,7 @@ const onApprove = async (data: OnApproveData) => {
     paypalMerchantId: paypalGetters.getMerchantId() ?? '',
   });
 
-  await getCart();
+  clearCartItems();
 
   if (order?.order?.id) {
     router.push('/thank-you/?orderId=' + order.order.id + '&accessKey=' + order.order.accessKey);
