15d2be723acba22fcde3970a5940249feb43898d
ksted <kevin.stederoth@plentymarkets.com>
2023-09-26 07:55:54 +0200

    fix: consistent env var names

--- apps/server/middleware.config.ts
+++ apps/server/middleware.config.ts
@@ -7,7 +7,7 @@ const config = {
       location: '@plentymarkets/shop-api/server',
       configuration: {
         api: {
-          url: process.env.API_ENDPOINT ? `${process.env.API_ENDPOINT}` : 'https://mevofvd5omld.c01-14.plentymarkets.com'
+          url: process.env.API_URL ? `${process.env.API_URL}` : 'https://mevofvd5omld.c01-14.plentymarkets.com'
         }
       },
     }
