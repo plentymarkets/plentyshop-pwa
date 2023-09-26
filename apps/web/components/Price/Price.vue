e9d1e43b4cf538fead8a6ce338e7a28834c3ee21
rbedeoan-plenty <raul.bedeoan@plentymarkets.com>
2023-09-26 15:00:23 +0300

    fix: linting

--- apps/web/components/Price/Price.vue
+++ apps/web/components/Price/Price.vue
@@ -2,7 +2,7 @@
   <div class="text-sm">
     <div class="my-1">
       <span class="mr-2 text-secondary-700 font-bold font-headings text-2xl" data-testid="price">
-        {{ $n(mainPrice, 'currency') }}
+        {{ $n(price, 'currency') }}
         <span v-if="showNetPrices">{{ $t('asterisk') }} </span>
       </span>
       <span v-if="oldPrice && oldPrice !== price" class="text-base font-normal text-neutral-500 line-through">
@@ -16,18 +16,8 @@
 import { PriceProps } from '~/components/Price/types';
 
 const props = defineProps<PriceProps>();
-const { price } = props;
+const { price, oldPrice } = props;
 
 const runtimeConfig = useRuntimeConfig();
 const showNetPrices = runtimeConfig.public.showNetPrices;
-
-const mainPrice = computed(() => {
-  if (!price) return 0;
-
-  if (price.special) return price.special;
-  if (price.regular) return price.regular;
-
-  return 0;
-});
-const oldPrice = computed<number>(() => price.regular ?? 0);
 </script>
