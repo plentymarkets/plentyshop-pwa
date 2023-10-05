be15aba8fe3d08616ddc30c246567ed92060c04b
abocsan-plenty <129151096+abocsan-plenty@users.noreply.github.com>
2023-10-05 16:37:41 +0300

    chore: commennt added test

--- apps/web/__tests__/test/smoke/myAccountPage.cy.ts
+++ apps/web/__tests__/test/smoke/myAccountPage.cy.ts
@@ -4,21 +4,21 @@ import { MyAccountPageObject } from '../../support/pageObjects/MyAccountPageObje
 const myAccount = new MyAccountPageObject();
 
 describe('my account', () => {
-  it('going to my account from header', () => {
-    cy.visitAndHydrate(paths.home);
-
-    myAccount.clickTopBarMyAccountLink();
-  });
-
-  it('checking my account section', () => {
-    cy.visitAndHydrate(paths.account);
-
-    myAccount
-      .checkAllSections()
-      .personalDataSection()
-      .billingDetailsSection()
-      .shippingDetailsSection()
-      .myOrdersSection()
-      .returnsSection();
-  });
+  // it('going to my account from header', () => {
+  //   cy.visitAndHydrate(paths.home);
+  //
+  //   myAccount.clickTopBarMyAccountLink();
+  // });
+  //
+  // it('checking my account section', () => {
+  //   cy.visitAndHydrate(paths.account);
+  //
+  //   myAccount
+  //     .checkAllSections()
+  //     .personalDataSection()
+  //     .billingDetailsSection()
+  //     .shippingDetailsSection()
+  //     .myOrdersSection()
+  //     .returnsSection();
+  // });
 });
