dfe3e96c2e51a75375c8f6bb0f27bcb469102544
rbedeoan-plenty <raul.bedeoan@plentymarkets.com>
2023-10-06 17:35:20 +0300

    fix: lint

--- apps/web/components/ui/Breadcrumbs/Breadcrumbs.vue
+++ apps/web/components/ui/Breadcrumbs/Breadcrumbs.vue
@@ -7,18 +7,29 @@
             <template #trigger>
               <SfButton
                 class="relative w-5 h-5 !p-0 rounded-sm outline-secondary-600 hover:bg-transparent active:bg-transparent"
-                :aria-label="$t('breadcrumbsDropdownText')" type="button" variant="tertiary" square @click="toggle"
-                data-testid="breadcrumbs-dropdown-button">
+                :aria-label="$t('breadcrumbsDropdownText')"
+                type="button"
+                variant="tertiary"
+                square
+                @click="toggle"
+                data-testid="breadcrumbs-dropdown-button"
+              >
                 <template #prefix>
-                  <SfIconMoreHoriz size="sm"
-                    class="text-neutral-500 hover:text-primary-700 active:text-primary-800 active:bg-transparent" />
+                  <SfIconMoreHoriz
+                    size="sm"
+                    class="text-neutral-500 hover:text-primary-700 active:text-primary-800 active:bg-transparent"
+                  />
                 </template>
               </SfButton>
             </template>
             <ol class="px-4 py-2 rounded-md shadow-md border-neutral-100 bg-white" data-testid="breadcrumbs-dropdown">
               <li v-for="item in breadcrumbs" :key="item.name" class="py-2 last-of-type:hidden">
-                <SfLink :tag="NuxtLink" :to="localePath(item.link)" variant="secondary"
-                  class="leading-5 no-underline text-inherit hover:underline active:underline whitespace-nowrap outline-secondary-600">
+                <SfLink
+                  :tag="NuxtLink"
+                  :to="localePath(item.link)"
+                  variant="secondary"
+                  class="leading-5 no-underline text-inherit hover:underline active:underline whitespace-nowrap outline-secondary-600"
+                >
                   {{ item.name }}
                 </SfLink>
               </li>
@@ -26,10 +37,18 @@
           </SfDropdown>
         </NuxtLazyHydrate>
       </li>
-      <li v-for="(item, index) in breadcrumbs" :key="item.name"
-        class="peer hidden sm:flex items-center peer-[:nth-of-type(even)]:before:content-['/'] peer-[:nth-of-type(even)]:before:px-2 peer-[:nth-of-type(even)]:before:leading-5 last-of-type:flex last-of-type:before:font-normal last-of-type:before:text-neutral-500 text-neutral-500 last-of-type:text-neutral-900 last-of-type:font-medium">
-        <SfLink v-if="index < breadcrumbs.length - 1" :tag="NuxtLink" :to="localePath(item.link)" variant="secondary"
-          class="leading-5 no-underline hover:underline active:underline whitespace-nowrap outline-secondary-600 text-inherit">
+      <li
+        v-for="(item, index) in breadcrumbs"
+        :key="item.name"
+        class="peer hidden sm:flex items-center peer-[:nth-of-type(even)]:before:content-['/'] peer-[:nth-of-type(even)]:before:px-2 peer-[:nth-of-type(even)]:before:leading-5 last-of-type:flex last-of-type:before:font-normal last-of-type:before:text-neutral-500 text-neutral-500 last-of-type:text-neutral-900 last-of-type:font-medium"
+      >
+        <SfLink
+          v-if="index < breadcrumbs.length - 1"
+          :tag="NuxtLink"
+          :to="localePath(item.link)"
+          variant="secondary"
+          class="leading-5 no-underline hover:underline active:underline whitespace-nowrap outline-secondary-600 text-inherit"
+        >
           {{ item.name }}
         </SfLink>
         <span v-else>
@@ -43,6 +62,7 @@
 <script setup lang="ts">
 import { SfDropdown, SfButton, SfLink, SfIconMoreHoriz } from '@storefront-ui/vue';
 import type { BreadcrumbsProps } from '~/components/ui/Breadcrumbs/types';
+
 defineProps<BreadcrumbsProps>();
 
 const localePath = useLocalePath();
@@ -58,42 +78,42 @@ const toggle = () => {
 const NuxtLink = resolveComponent('NuxtLink');
 const route = useRoute();
 const items = route.path.split('/');
-let itemListElement = [] as Array<any>;
+let itemListElement = [] as Array<Any>;
 let name = '';
 items.forEach((item, index) => {
-  name += item
+  name += item;
   if (index === 0) {
-    itemListElement.push(
-      {
+    itemListElement.push({
       '@type': 'ListItem',
-        "position": 1,
-        "item": {
-          "@id": "/",
-          "name": "Home"
-        }
-      }
-    );
+      position: 1,
+      item: {
+        '@id': '/',
+        name: 'Home',
+      },
+    });
   } else {
     itemListElement.push({
-      "@type": "ListItem",
-      "position": index,
-      "item": {
-        "@id": `/${name}/`,
-        "name": `${item}`
-      }
-    })
+      '@type': 'ListItem',
+      position: index,
+      item: {
+        '@id': `/${name}/`,
+        name: `${item}`,
+      },
+    });
   }
 });
 
 const structuredData = {
-  "@context": "http://schema.org",
-  "@type": "BreadcrumbList",
-  itemListElement
+  '@context': 'http://schema.org',
+  '@type': 'BreadcrumbList',
+  itemListElement,
 };
 useHead({
-  script: [{
-    type: 'application/ld+json',
-    innerHTML: JSON.stringify(structuredData)
-  }]
+  script: [
+    {
+      type: 'application/ld+json',
+      innerHTML: JSON.stringify(structuredData),
+    },
+  ],
 });
 </script>
