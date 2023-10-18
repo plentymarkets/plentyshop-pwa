9f4d25654209fb0408bb50e1259efc081c235534
abocsan-plenty <129151096+abocsan-plenty@users.noreply.github.com>
2023-10-18 10:43:14 +0300

    chore: code cleanup

--- apps/web/__tests__/test/unit/i18n.spec.ts
+++ apps/web/__tests__/test/unit/i18n.spec.ts
@@ -46,13 +46,9 @@ function mergeDeep(...objects: any) {
             const pVal = prev[key];
             const oVal = obj[key];
 
-            if (Array.isArray(pVal) && Array.isArray(oVal)) {
-                prev[key] = pVal.concat(...oVal);
-            }
-            else if (isObject(pVal) && isObject(oVal)) {
+            if (isObject(pVal) && isObject(oVal)) {
                 prev[key] = mergeDeep(pVal, oVal);
-            }
-            else {
+            } else {
                 prev[key] = oVal;
             }
         });
