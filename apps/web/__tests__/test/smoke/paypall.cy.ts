b30c4ca4a9c132243a70fef4e9f4810b82829019
rbedeoan-plenty <raul.bedeoan@plentymarkets.com>
2023-10-23 11:04:44 +0300

    fix: testing

---
+++ apps/web/__tests__/test/smoke/paypall.cy.ts
@@ -0,0 +1,29 @@
+import { CartPageObject } from '../../support/pageObjects/CartPageObject';
+import { CheckoutPageObject } from '../../support/pageObjects/CheckoutPageObject';
+import { HomePageObject } from '../../support/pageObjects/HomePageObject';
+import { ProductListPageObject } from '../../support/pageObjects/ProductListPageObject';
+import { paths } from '../../../utils/paths';
+
+const checkout = new CheckoutPageObject();
+const cart = new CartPageObject();
+const homePage = new HomePageObject();
+const productListPage = new ProductListPageObject();
+
+describe('Smoke: Cart Page', () => {
+  it('[smoke] Check if status on order gets updated', () => {
+    cy.visitAndHydrate(paths.home);
+
+    homePage.goToCategory();
+    productListPage.addToCart()
+
+    cart.openCart();
+    checkout
+        .goToCheckout()
+        .fillContactInformationForm()
+        .addBillingAddress()
+        .fillBillingAddressForm()
+        .acceptTerms()
+        .placeOrderButton()
+        .displaySuccessPage();
+  });
+});
