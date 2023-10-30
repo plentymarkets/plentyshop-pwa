c69b805e754078fa2d3278475f606384311f9588
Fabian Gerke <124085586+FabianGerke@users.noreply.github.com>
2023-10-30 18:13:56 +0100

    fix: refactor

--- apps/web/components/GraduatedPriceList/GraduatedPriceList.vue
+++ apps/web/components/GraduatedPriceList/GraduatedPriceList.vue
@@ -4,7 +4,7 @@
   </div>
   <table class="w-full text-left typography-text-sm mb-2" v-if="graduatedList.length > 0">
     <thead class="border-b-2 border-neutral-200">
-      <tr class="w-full">
+      <tr>
         <th class="lg:py-4 py-2 lg:pr-4 pr-2 font-medium">{{ $t('graduatedPrices.price') }}</th>
         <th class="lg:p-4 p-2 font-medium lg:whitespace-nowrap">{{ $t('graduatedPrices.quantity') }}</th>
         <th class="lg:p-4 p-2 font-medium">{{ $t('graduatedPrices.discount') }}</th>
