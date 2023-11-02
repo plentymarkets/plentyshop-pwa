e83056cb9de1b50b591490eb3269884113494411
Fabian Gerke <124085586+FabianGerke@users.noreply.github.com>
2023-11-02 17:43:11 +0100

    fix: throw error

--- apps/web/__tests__/support/failOnError.ts
+++ apps/web/__tests__/support/failOnError.ts
@@ -1,8 +1,5 @@
 Cypress.on("window:before:load", win => {
   cy.stub(win.console, "error").callsFake(msg => {
-    // log out to the terminal
-    // cy.now("task", "error", msg)
-    // log to Command Log and fail the test
     throw new Error(msg)
   })
 });
