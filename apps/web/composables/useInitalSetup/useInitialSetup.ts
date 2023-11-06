28591eac6cd317dc6c7312f5768bcde43c8153c9
Fabian Gerke <124085586+FabianGerke@users.noreply.github.com>
2023-11-06 11:57:16 +0100

    fix: remove import

--- apps/web/composables/useInitalSetup/useInitialSetup.ts
+++ apps/web/composables/useInitalSetup/useInitialSetup.ts
@@ -1,7 +1,6 @@
 import { Cart, SessionResult } from '@plentymarkets/shop-api';
 import { useSdk } from '~/sdk';
 import { SetInitialData, UseInitialSetupReturn, UseInitialSetupState } from './types';
-import { toRefs } from '@vueuse/shared/index';
 
 /** Function for getting current customer/cart data from session
  * @return SetInitialData
