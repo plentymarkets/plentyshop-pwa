7c22e223f7796a91b985289d3425d57de9ef7920
Fabian Gerke <124085586+FabianGerke@users.noreply.github.com>
2023-11-07 10:44:49 +0100

    fix: update e2e tests

--- apps/web/__tests__/test/smoke/homePage.cy.ts
+++ apps/web/__tests__/test/smoke/homePage.cy.ts
@@ -2,6 +2,9 @@ import { HomePageObject } from '../../support/pageObjects/HomePageObject';
 import { paths } from '../../../utils/paths';
 
 const homePage = new HomePageObject();
+const cookieName = 'vsf-locale';
+const locale_de = 'de';
+const locale_en = 'en';
 
 beforeEach(() => {
   cy.visitAndHydrate(paths.home);
@@ -11,5 +14,19 @@ describe('Smoke: Homepage', () => {
 
   it('[smoke] Check if Category button exists ', () => {
     homePage.checkHeaderCategory();
+  });
+
+  it('[smoke] Check if DE on SSR works ', () => {
+    cy.setCookie(cookieName, locale_de);
+    cy.setCookie('i18n_redirected', locale_de);
+    cy.visitAndHydrate(paths.home);
+    homePage.checkLanguage(locale_de);
+  });
+
+  it('[smoke] Check if EN on SSR works ', () => {
+    cy.setCookie(cookieName, locale_en);
+    cy.setCookie('i18n_redirected', locale_en);
+    cy.visitAndHydrate(paths.home);
+    homePage.checkLanguage(locale_en);
   });
 });
