954ed678ef95f60ee3fe3e507e91f56f996c2bc0
Kevin Stederoth <43753494+ksted@users.noreply.github.com>
2023-11-07 17:28:53 +0100

    feat: composables docs (#211)
    * feat: add vitepress
    
    * feat: add exports for composable types
    
    * docs: update homepage
    
    * fix: lint
    
    ---------
    
    Co-authored-by: Maximilian Röll <30629157+maxiroellplenty@users.noreply.github.com>

---
+++ apps/web/docs/.vitepress/config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vitepress'
+import typedocSidebar from '../composables/typedoc-sidebar.json';
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "plentyShop PWA Docs",
+  description: "Documentation of plentyShop PWA",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Composables', link: '/composables/' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Composables',
+        items: typedocSidebar,
+      },
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ]
+  }
+})
