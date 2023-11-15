6f115f8e8f6ad64892782272e9f4644a7534409d
Bedeoan Raul <raul.bedeoan@plentymarkets.com>
2023-11-15 22:07:11 +0200

    fix: refactor

--- apps/web/components/LanguageSelector/LanguageSelector.vue
+++ apps/web/components/LanguageSelector/LanguageSelector.vue
@@ -1,16 +1,5 @@
 <template>
-  <div class="w-full bg-white flex items-center py-10 justify-center flex-col shadow-xl md:hidden">
-    <SfButton
-      v-for="locale in localeCodes"
-      :key="locale"
-      :variant="locale === currentLocale ? 'primary' : 'tertiary'"
-      square
-      :aria-label="$t('lang.' + locale)"
-      @click="switchLocale(locale)"
-      >{{ $t('lang.' + locale) }}</SfButton
-    >
-  </div>
-  <div class="hidden md:flex w-full bg-white items-center py-10 justify-center absolute z-[99999] top-[10%] shadow-xl">
+  <div :class="isDesktop ? 'desktopFlagLayout' : 'mobileFlagLayout'">
     <SfButton
       v-for="locale in localeCodes"
       :key="locale"
@@ -19,17 +8,32 @@
       class="ml-3"
       :aria-label="$t('lang.' + locale)"
       @click="switchLocale(locale)"
-      >{{ $t('lang.' + locale) }}</SfButton
+    >
+      <div :class="isDesktop ? 'desktopCountryFlags' : 'mobileCountryFlags'" v-html="flagList[locale]"></div>
+      {{ $t('lang.' + locale) }}</SfButton
     >
   </div>
 </template>
 <script setup lang="ts">
 import { SfButton } from '@storefront-ui/vue';
-
+import { flagImports } from './flags';
+const { isDesktop } = useBreakpoints();
 const { setLocaleCookie, localeCodes, locale: currentLocale } = useI18n();
 const route = useRoute();
 const switchLocalePath = useSwitchLocalePath();
 const { toggle } = useLanguageSelect();
+
+const flagList: { [key: string]: string } = {};
+
+const dynamicFlagImport = async () => {
+  for (const local of localeCodes.value) {
+    const localLang = flagImports[local];
+    if (localLang) {
+      flagList[local] = localLang;
+    }
+  }
+};
+dynamicFlagImport();
 
 const switchLocale = (language: string) => {
   setLocaleCookie(language);
@@ -37,3 +41,18 @@ const switchLocale = (language: string) => {
   toggle();
 };
 </script>
+
+<style>
+.desktopCountryFlags {
+  width: 2vw;
+}
+.mobileCountryFlags {
+  width: 10vw;
+}
+.desktopFlagLayout {
+  @apply hidden md:flex w-full bg-white items-center py-10 justify-center absolute z-[99999] top-[10%] shadow-xl;
+}
+.mobileFlagLayout {
+  @apply w-full bg-white flex items-center py-10 justify-center flex-col shadow-xl md:hidden;
+}
+</style>
---
+++ apps/web/components/LanguageSelector/flags.ts
@@ -0,0 +1,10 @@
+// import USFlag from 'country-flag-icons/string/3x2/US';
+// import DEFlag from 'country-flag-icons/string/3x2/DE';
+
+export interface FlagImports {
+  [key: string]: string;
+}
+export const flagImports: FlagImports = {
+  en: '' as string,
+  de: '' as string,
+};
