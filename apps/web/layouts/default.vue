4fd6e7b4d511a09baf7615bafa27359b4f8003b0
cristian sandru <cristian.sandru@plentysystems.com>
2023-11-21 08:17:43 +0200

    fix: different pagination desktop mobile

--- apps/web/layouts/default.vue
+++ apps/web/layouts/default.vue
@@ -1,128 +1,131 @@
 <template>
   <div>
     <MegaMenu :categories="categoryTree">
-    <NuxtLazyHydrate when-visible>
-      <UiSearch class="hidden md:block flex-1" />
-    </NuxtLazyHydrate>
-    <nav class="hidden ml-4 md:flex md:flex-row md:flex-nowrap">
       <NuxtLazyHydrate when-visible>
+        <UiSearch class="hidden md:block flex-1" />
+      </NuxtLazyHydrate>
+      <nav class="hidden ml-4 md:flex md:flex-row md:flex-nowrap">
+        <NuxtLazyHydrate when-visible>
+          <SfButton
+            class="group relative text-white hover:text-white active:text-white hover:bg-primary-800 active:bg-primary-900 mr-1 -ml-0.5 rounded-md cursor-pointer"
+            :aria-label="$t('languageSelector')"
+            variant="tertiary"
+            square
+            data-testid="open-languageselect-button"
+            @click="toggleLanguageSelect"
+          >
+            <template #prefix>
+              <SfIconLanguage class="relative" />
+            </template>
+          </SfButton>
+          <SfButton
+            class="group relative text-white hover:text-white active:text-white hover:bg-primary-800 active:bg-primary-900 mr-1 -ml-0.5 rounded-md"
+            :tag="NuxtLink"
+            :to="localePath(paths.cart)"
+            :aria-label="$t('numberInCart', cartItemsCount)"
+            variant="tertiary"
+            square
+          >
+            <template #prefix>
+              <SfIconShoppingCart />
+              <SfBadge
+                :content="cartItemsCount"
+                class="outline outline-primary-700 bg-white !text-neutral-900 group-hover:outline-primary-800 group-active:outline-primary-900 flex justify-center"
+                data-testid="cart-badge"
+              />
+            </template>
+          </SfButton>
+        </NuxtLazyHydrate>
+        <NuxtLazyHydrate when-visible>
+          <SfDropdown v-if="isAuthorized" v-model="isAccountDropdownOpen" placement="bottom-end">
+            <template #trigger>
+              <SfButton
+                variant="tertiary"
+                class="relative text-white hover:text-white active:text-white hover:bg-primary-800 active:bg-primary-900 rounded-md"
+                :class="{ 'bg-primary-900': isAccountDropdownOpen }"
+                @click="accountDropdownToggle()"
+                data-testid="account-dropdown-button"
+              >
+                <template #prefix>
+                  <SfIconPerson />
+                </template>
+                {{ user.user?.firstName }}
+              </SfButton>
+            </template>
+            <ul class="rounded bg-white shadow-md border border-neutral-100 text-neutral-900 min-w-[152px] py-2">
+              <li v-for="{ label, link } in accountDropdown" :key="label">
+                <template v-if="label === 'account.logout'">
+                  <UiDivider class="my-2" />
+                  <SfListItem
+                    tag="button"
+                    class="text-left"
+                    data-testid="account-dropdown-list-item"
+                    @click="logOut()"
+                    >{{ $t(label) }}</SfListItem
+                  >
+                </template>
+                <SfListItem
+                  v-else
+                  :tag="NuxtLink"
+                  :to="localePath(link)"
+                  :class="{ 'bg-neutral-200': $route.path === link }"
+                  data-testid="account-dropdown-list-item"
+                >
+                  {{ $t(label) }}
+                </SfListItem>
+              </li>
+            </ul>
+          </SfDropdown>
+          <SfButton
+            v-else
+            @click="openAuthentication"
+            class="group relative text-white hover:text-white active:text-white hover:bg-primary-800 active:bg-primary-900 mr-1 -ml-0.5 rounded-md"
+            variant="tertiary"
+            square
+          >
+            <SfIconPerson />
+          </SfButton>
+        </NuxtLazyHydrate>
+      </nav>
+      <div>
         <SfButton
-          class="group relative text-white hover:text-white active:text-white hover:bg-primary-800 active:bg-primary-900 mr-1 -ml-0.5 rounded-md cursor-pointer"
-          :aria-label="$t('languageSelector')"
           variant="tertiary"
+          class="relative text-white hover:text-white active:text-white hover:bg-primary-800 active:bg-primary-900 rounded-md md:hidden"
           square
           data-testid="open-languageselect-button"
           @click="toggleLanguageSelect"
+          :aria-label="$t('languageSelector')"
         >
-          <template #prefix>
-            <SfIconLanguage class="relative" />
-          </template>
+          <SfIconLanguage />
         </SfButton>
         <SfButton
-          class="group relative text-white hover:text-white active:text-white hover:bg-primary-800 active:bg-primary-900 mr-1 -ml-0.5 rounded-md"
-          :tag="NuxtLink"
-          :to="localePath(paths.cart)"
-          :aria-label="$t('numberInCart', cartItemsCount)"
           variant="tertiary"
+          class="relative text-white hover:text-white active:text-white hover:bg-primary-800 active:bg-primary-900 rounded-md md:hidden"
           square
+          @click="searchModalOpen"
+          :aria-label="$t('openSearchModalButtonLabel')"
         >
-          <template #prefix>
-            <SfIconShoppingCart />
-            <SfBadge
-              :content="cartItemsCount"
-              class="outline outline-primary-700 bg-white !text-neutral-900 group-hover:outline-primary-800 group-active:outline-primary-900 flex justify-center"
-              data-testid="cart-badge"
-            />
-          </template>
+          <SfIconSearch />
         </SfButton>
-      </NuxtLazyHydrate>
-      <NuxtLazyHydrate when-visible>
-        <SfDropdown v-if="isAuthorized" v-model="isAccountDropdownOpen" placement="bottom-end">
-          <template #trigger>
-            <SfButton
-              variant="tertiary"
-              class="relative text-white hover:text-white active:text-white hover:bg-primary-800 active:bg-primary-900 rounded-md"
-              :class="{ 'bg-primary-900': isAccountDropdownOpen }"
-              @click="accountDropdownToggle()"
-              data-testid="account-dropdown-button"
-            >
-              <template #prefix>
-                <SfIconPerson />
-              </template>
-              {{ user.user?.firstName }}
-            </SfButton>
-          </template>
-          <ul class="rounded bg-white shadow-md border border-neutral-100 text-neutral-900 min-w-[152px] py-2">
-            <li v-for="{ label, link } in accountDropdown" :key="label">
-              <template v-if="label === 'account.logout'">
-                <UiDivider class="my-2" />
-                <SfListItem tag="button" class="text-left" data-testid="account-dropdown-list-item" @click="logOut()">{{
-                  $t(label)
-                }}</SfListItem>
-              </template>
-              <SfListItem
-                v-else
-                :tag="NuxtLink"
-                :to="localePath(link)"
-                :class="{ 'bg-neutral-200': $route.path === link }"
-                data-testid="account-dropdown-list-item"
-              >
-                {{ $t(label) }}
-              </SfListItem>
-            </li>
-          </ul>
-        </SfDropdown>
-        <SfButton
-          v-else
-          @click="openAuthentication"
-          class="group relative text-white hover:text-white active:text-white hover:bg-primary-800 active:bg-primary-900 mr-1 -ml-0.5 rounded-md"
-          variant="tertiary"
-          square
-        >
-          <SfIconPerson />
-        </SfButton>
-      </NuxtLazyHydrate>
-    </nav>
-    <div>
-      <SfButton
-        variant="tertiary"
-        class="relative text-white hover:text-white active:text-white hover:bg-primary-800 active:bg-primary-900 rounded-md md:hidden"
-        square
-        data-testid="open-languageselect-button"
-        @click="toggleLanguageSelect"
-        :aria-label="$t('languageSelector')"
-      >
-        <SfIconLanguage />
-      </SfButton>
-      <SfButton
-        variant="tertiary"
-        class="relative text-white hover:text-white active:text-white hover:bg-primary-800 active:bg-primary-900 rounded-md md:hidden"
-        square
-        @click="searchModalOpen"
-        :aria-label="$t('openSearchModalButtonLabel')"
-      >
-        <SfIconSearch />
-      </SfButton>
-    </div>
-  </MegaMenu>
-  </div>
-  <LanguageSelector v-if="isLanguageSelectOpen" />
-  <UiNotifications />
-  <UiModal
-    v-model="isAuthenticationOpen"
-    tag="section"
-    class="h-full md:w-[500px] md:h-fit m-0 p-0"
-    aria-labelledby="login-modal"
-  >
-    <header>
-      <div class="text-lg font-medium ml-8">
-        <span v-if="isLogin">{{ $t('auth.login.heading') }}</span>
-        <span v-else>{{ $t('auth.signup.heading') }}</span>
       </div>
-      <SfButton square variant="tertiary" class="absolute right-2 top-2" @click="closeAuthentication">
-        <SfIconClose />
-      </SfButton>
-    </header>
+    </MegaMenu>
+    <LanguageSelector v-if="isLanguageSelectOpen" />
+    <UiNotifications />
+    <UiModal
+      v-model="isAuthenticationOpen"
+      tag="section"
+      class="h-full md:w-[500px] md:h-fit m-0 p-0"
+      aria-labelledby="login-modal"
+    >
+      <header>
+        <div class="text-lg font-medium ml-8">
+          <span v-if="isLogin">{{ $t('auth.login.heading') }}</span>
+          <span v-else>{{ $t('auth.signup.heading') }}</span>
+        </div>
+        <SfButton square variant="tertiary" class="absolute right-2 top-2" @click="closeAuthentication">
+          <SfIconClose />
+        </SfButton>
+      </header>
 
       <login v-if="isLogin" @change-view="isLogin = false" @logged-in="closeAuthentication" />
 
