55b34e36c415dadad566f7454561ac6ab7a89e7f
Maximilian Röll <30629157+maxiroellplenty@users.noreply.github.com>
2023-11-30 13:49:33 +0100

    chore: add preview mode component back

--- apps/web/layouts/default.vue
+++ apps/web/layouts/default.vue
@@ -146,6 +146,9 @@
     <NuxtLazyHydrate when-idle>
       <Cookiebar />
     </NuxtLazyHydrate>
+    <NuxtLazyHydrate when-idle>
+      <PreviewMode />
+    </NuxtLazyHydrate>
     <NuxtLazyHydrate when-visible>
       <UiFooter />
     </NuxtLazyHydrate>
