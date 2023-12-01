189aa8d976fd494d6dff7fcf8f1a8636ec8ae061
Fabian Gerke <124085586+FabianGerke@users.noreply.github.com>
2023-12-01 12:40:36 +0100

    feat: implement first &- lastname

--- apps/web/components/NewsletterSubscribe/NewsletterSubscribe.vue
+++ apps/web/components/NewsletterSubscribe/NewsletterSubscribe.vue
@@ -1,20 +1,34 @@
 <template>
   <div class="relative">
     <div class="bg-neutral-100 p-4 sm:p-10 text-center">
-      <p class="typography-headline-4 sm:typography-headline-3 font-bold">
+      <p class="typography-headline-4 sm:typography-headline-3 font-bold mb-2">
         {{ $t('newsletter.heading') }}
       </p>
-      <form class="mb-4 flex flex-col gap-4 max-w-[550px] mx-auto items-center" @submit.prevent="subscribeNewsletter()">
-        <div class="w-full flex">
+      <form class="mb-4 flex flex-col gap-2 max-w-[550px] mx-auto items-center" @submit.prevent="subscribeNewsletter()">
+        <!-- <div class="w-full flex flex-col sm:flex-row gap-2">
+          <SfInput
+            v-model="firstNameValue"
+            type="email"
+            wrapper-class="grow"
+            :placeholder="$t('form.firstNameLabel')"
+          />
+          <SfInput v-model="lastNameValue" type="email" wrapper-class="grow" :placeholder="$t('form.lastNameLabel')" />
+        </div> -->
+        <div class="w-full flex flex-col sm:flex-row">
           <div class="w-full">
             <SfInput
-              v-model="inputValue"
+              v-model="emailValue"
               type="email"
               wrapper-class="grow"
               :placeholder="$t('auth.resetPassword.email')"
             />
           </div>
-          <SfButton class="ml-2 h-[40px] flex-shrink-0" type="submit" size="lg" :disabled="loading || turnstile === ''">
+          <SfButton
+            class="ml-0 h-[40px] flex-shrink-0 mt-2 sm:mt-0 sm:ml-2"
+            type="submit"
+            size="lg"
+            :disabled="loading || turnstile === ''"
+          >
             <SfLoaderCircular v-if="loading" />
             <span v-else>{{ $t('newsletter.subscribe') }}</span>
           </SfButton>
@@ -46,19 +60,26 @@ import { SfButton, SfInput, SfLink, SfLoaderCircular } from '@storefront-ui/vue'
 
 const { subscribe, loading } = useNewsletter();
 const localePath = useLocalePath();
-const inputValue = ref('');
+const emailValue = ref('');
+// const firstNameValue = ref('');
+// const lastNameValue = ref('');
+
 const turnstile = ref('');
 const turnstileElement = ref();
 
 const subscribeNewsletter = async () => {
   if (
     await subscribe({
-      email: inputValue.value,
+      email: emailValue.value,
       'cf-turnstile-response': turnstile.value,
       emailFolder: 1,
+      // firstName: firstNameValue.value,
+      // lastName: lastNameValue.value,
     })
   ) {
-    inputValue.value = '';
+    emailValue.value = '';
+    // firstNameValue.value = '';
+    // lastNameValue.value = '';
   }
   turnstile.value = '';
   turnstileElement.value?.reset();
