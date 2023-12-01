599818d615b3c6a8160bdcb3dc052ad2e6a794c0
Fabian Gerke <124085586+FabianGerke@users.noreply.github.com>
2023-12-01 12:17:01 +0100

    fix: changes newsletter

--- apps/web/components/NewsletterSubscribe/NewsletterSubscribe.vue
+++ apps/web/components/NewsletterSubscribe/NewsletterSubscribe.vue
@@ -2,28 +2,39 @@
   <div class="relative">
     <div class="bg-neutral-100 p-4 sm:p-10 text-center">
       <p class="typography-headline-4 sm:typography-headline-3 font-bold">
-        Subscribe and get discount on your first purchase!
+        {{ $t('newsletter.heading') }}
       </p>
-      <p class="typography-text-sm sm:typography-text-base my-2 mb-4">
-        Be aware of upcoming sales and events. Receive gifts and special offers!
-      </p>
-      <form class="mb-4 flex flex-col gap-4 max-w-[450px] mx-auto items-center" @submit.prevent="subscribeNewsletter()">
-        <div class="w-full">
-          <SfInput v-model="inputValue" type="email" wrapper-class="grow" placeholder="Type your email" />
+      <form class="mb-4 flex flex-col gap-4 max-w-[550px] mx-auto items-center" @submit.prevent="subscribeNewsletter()">
+        <div class="w-full flex">
+          <div class="w-full">
+            <SfInput
+              v-model="inputValue"
+              type="email"
+              wrapper-class="grow"
+              :placeholder="$t('auth.resetPassword.email')"
+            />
+          </div>
+          <SfButton class="ml-2 h-[40px] flex-shrink-0" type="submit" size="lg" :disabled="loading || turnstile === ''">
+            <SfLoaderCircular v-if="loading" />
+            <span v-else>{{ $t('newsletter.subscribe') }}</span>
+          </SfButton>
         </div>
-        <NuxtTurnstile
-          v-model="turnstile"
-          ref="turnstileElement"
-          :options="{ theme: 'light' }"
-        />
-        <SfButton type="submit" size="lg" :disabled="loading || turnstile === ''">
-          <SfLoaderCircular v-if="loading" />
-          <span v-else>Subscribe to Newsletter</span>
-        </SfButton>
+        <NuxtTurnstile v-model="turnstile" ref="turnstileElement" :options="{ theme: 'light' }" />
       </form>
       <div class="typography-text-xs text-neutral-600">
-        To learn how we process your data, visit our <SfLink href="#" class="!text-neutral-600">Privacy Notice</SfLink>.
-        You can <SfLink href="#" class="!text-neutral-600">unsubscribe</SfLink> at any time without costs.
+        <label class="ml-3 text-base text-neutral-900 peer-disabled:text-disabled-900" for="privacyPolicy">
+          <i18n-t keypath="newsletter.policy">
+            <template #privacyPolicy>
+              <SfLink
+                :href="localePath(paths.privacyPolicy)"
+                target="_blank"
+                class="focus:outline focus:outline-offset-2 focus:outline-2 outline-secondary-600 rounded"
+              >
+                {{ $t('privacyPolicy') }}
+              </SfLink>
+            </template>
+          </i18n-t>
+        </label>
       </div>
     </div>
   </div>
@@ -34,6 +45,7 @@ import { ref } from 'vue';
 import { SfButton, SfInput, SfLink, SfLoaderCircular } from '@storefront-ui/vue';
 
 const { subscribe, loading } = useNewsletter();
+const localePath = useLocalePath();
 const inputValue = ref('');
 const turnstile = ref('');
 const turnstileElement = ref();
