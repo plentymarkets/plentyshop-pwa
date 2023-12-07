9d34bcc4ac0b413f304ef511857ebe9714584a12
abocsan-plenty <129151096+abocsan-plenty@users.noreply.github.com>
2023-12-07 16:11:32 +0200

    chore: fix tests

--- apps/web/__tests__/test/smoke/cartPage.cy.ts
+++ apps/web/__tests__/test/smoke/cartPage.cy.ts
@@ -10,13 +10,12 @@ describe('Smoke: Cart Page', () => {
     cy.setCookie('consent-cookie', '{"Essentials":{"Session":true,"Consent":true,"Session2":true},"External Media":{"Session":false,"Consent":false,"Session2":false},"Functional":{"Session":false,"Consent":false,"Session2":false},"Marketing":{"Session":false,"Consent":false,"Session2":false}}')
   });
 
-
   it('[smoke] Add coupon to cart, check the totals, then remove coupon and check totals again', () => {
     cy.visitAndHydrate('/study-room-office/office-chair/design-chair-brookhaven-leather-black_105_1003');
     cy.intercept('/plentysystems/doAddCartItem').as('doAddCartItem');
     cy.wait(1000);
     cy.getByTestId('add-to-cart').click();
-    cy.wait(5000);
+    cy.wait('@doAddCartItem');
     cy.visitAndHydrate(paths.cart);
     cart.openCouponAccordion();
     cart.addCoupon('KB82AZ');
