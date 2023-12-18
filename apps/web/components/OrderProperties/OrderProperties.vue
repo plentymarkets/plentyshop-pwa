bf2afc4b0aa649d413434bd3a2ccc0a50beb6201
Bedeoan Raul <raul.bedeoan@plentymarkets.com>
2023-12-18 22:23:47 +0200

    fix: refactor label

--- apps/web/components/OrderProperties/OrderProperties.vue
+++ apps/web/components/OrderProperties/OrderProperties.vue
@@ -1,26 +1,43 @@
 <template>
-  <div v-for="(group, groupIndex) in productOrderPropertyGroups" :key="`group-${groupIndex}`">
-    <div class="font-semibold">
-      {{ group.name }}
-    </div>
+  <div>
+    <div v-for="(group, groupIndex) in productOrderPropertyGroups" :key="`group-${groupIndex}`">
+      <div class="font-semibold">
+        {{ productPropertyGetters.getGroupName(group) }}
+      </div>
 
-    <div v-if="group.description" class="font-normal typography-text-sm">
-      {{ group.description }}
-    </div>
+      <div class="font-normal typography-text-sm">
+        {{ productPropertyGetters.getGroupDescription(group) }}
+      </div>
 
-    <div v-for="(productProperty, propIndex) in group.orderProperties" :key="`group-prop-${propIndex}`">
-      <div v-if="!productPropertyGetters.isHidden(productProperty.property)" class="mt-4 flex items-center">
-        <SfCheckbox
-          v-if="productPropertyGetters.isCheckBox(productProperty.property)"
-          :value="productPropertyGetters.getOrderPropertyId(productProperty.property)"
-          :id="`prop-${productPropertyGetters.getOrderPropertyId(productProperty.property)}`"
-        />
-        <label
-          class="ml-2 cursor-pointer peer-disabled:text-disabled-900"
-          :for="`prop-${productPropertyGetters.getOrderPropertyId(productProperty.property)}`"
-        >
-          {{ productPropertyGetters.getOrderPropertyName(productProperty.property) }}
-        </label>
+      <div v-for="(productProperty, propIndex) in group.orderProperties" :key="`group-prop-${propIndex}`">
+        <div v-if="!productPropertyGetters.isHidden(productProperty.property)" class="mt-4 flex items-center">
+          <SfCheckbox
+            v-if="productPropertyGetters.isCheckBox(productProperty.property)"
+            v-model="checkedOrderProperties"
+            :value="productPropertyGetters.getOrderPropertyId(productProperty.property)"
+            :id="`prop-${productPropertyGetters.getOrderPropertyId(productProperty.property)}`"
+          />
+          <label
+            class="ml-2 cursor-pointer peer-disabled:text-disabled-900"
+            :for="`prop-${productPropertyGetters.getOrderPropertyId(productProperty.property)}`"
+          >
+            {{ productPropertyGetters.getOrderPropertyName(productProperty.property) }}
+            <span v-if="productPropertyGetters.getOrderPropertyLabel(productProperty.property).surchargeType">
+              ({{ productPropertyGetters.getOrderPropertyLabel(productProperty.property).surchargeType }}
+              {{ $n(productProperty.property.surcharge, 'currency') }})
+            </span>
+            {{ productPropertyGetters.getOrderPropertyLabel(productProperty.property).surchargeIndicator }}
+            <span
+              v-if="
+                productPropertyGetters.getOrderPropertyLabel(productProperty.property).surchargeIndicator &&
+                productPropertyGetters.getOrderPropertyLabel(productProperty.property).requiredIndicator
+              "
+            >
+              ,
+            </span>
+            {{ productPropertyGetters.getOrderPropertyLabel(productProperty.property).requiredIndicator }}
+          </label>
+        </div>
       </div>
     </div>
   </div>
@@ -30,21 +47,21 @@
 import { productPropertyGetters } from '@plentymarkets/shop-sdk';
 import { OrderPropertiesProps } from '~/components/OrderProperties/types';
 import { SfCheckbox } from '@storefront-ui/vue';
-// import { ref, Ref } from 'vue';
+import { ref, Ref } from 'vue';
 
 const props = defineProps<OrderPropertiesProps>();
 const product = props.product;
 const productOrderPropertyGroups = productPropertyGetters.getOrderPropertiesGroups(product);
 
-// const checkedOrderProperties: Ref<number[]> = ref([]);
+const checkedOrderProperties: Ref<number[]> = ref([]);
 
-// const preCheckProperties = () => {
-//   if (Object.values(productOrderPropertyGroups)[0]) {
-//     checkedOrderProperties.value = Object.values(productOrderPropertyGroups)[0]
-//       .filter((property) => productPropertyGetters.isChecked(property.property))
-//       .map((property) => property.property.id);
-//   }
-// };
+const preCheckProperties = () => {
+  if (Object.values(productOrderPropertyGroups)[0]) {
+    checkedOrderProperties.value = Object.values(productOrderPropertyGroups)[0]
+      .orderProperties.filter((property) => productPropertyGetters.isChecked(property.property))
+      .map((property) => property.property.id);
+  }
+};
 
-// preCheckProperties();
+preCheckProperties();
 </script>
