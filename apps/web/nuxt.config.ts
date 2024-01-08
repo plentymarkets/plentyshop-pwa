530ba5a060d0b0c4274355c1056ab6dc7ab3e232
cristian sandru <cristian.sandru@plentysystems.com>
2024-01-08 11:08:29 +0200

    fix: ignore upstream changes

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -89,7 +89,6 @@ export default defineNuxtConfig({
     '@vite-pwa/nuxt',
     'nuxt-vitest',
     'nuxt-lazy-hydrate',
-    '@vue-storefront/nuxt',
   ],
   // eslint-disable-next-line unicorn/expiring-todo-comments
   // TODO: build is consistently failing because of this. check whether we need pre-render check.
@@ -184,10 +183,5 @@ export default defineNuxtConfig({
       ],
     },
     registerWebManifestInRouteRules: true,
-  },
-  vsf: {
-    middleware: {
-      apiUrl: 'http://localhost:4000',
-    },
   },
 });
--- apps/web/sdk.config.ts
+++
@@ -1,5 +0,0 @@
-import { SdkModule, sdkModule } from '@vue-storefront/storefront-boilerplate-sdk';
-
-export default defineSdkConfig(({ buildModule }) => ({
-  commerce: buildModule<SdkModule>(sdkModule),
-}));
