07f5415f57e3d6d4efc8ada96b0d84242a5a8519
Fabian Gerke <124085586+FabianGerke@users.noreply.github.com>
2024-01-18 18:41:32 +0100

    feat: file upload order property (#303)
    * version 1.0
    
    * feat: upload file
    
    * fix: lint
    
    * fix: update sdk
    
    * fix: styling & uploading
    
    * fix: remove unused stuff
    
    * Update apps/web/composables/useProductOrderProperties/useProductOrderProperties.ts
    
    ---------
    
    Co-authored-by: Maximilian Röll <30629157+maxiroellplenty@users.noreply.github.com>

--- apps/web/composables/useProductOrderProperties/types.ts
+++ apps/web/composables/useProductOrderProperties/types.ts
@@ -1,8 +1,14 @@
 import type { Ref } from 'vue';
-import type { BasketItemOrderParamsProperty, ProductProperty, Product } from '@plentymarkets/shop-api';
+import type {
+  UploadFileForOrderPropertyResponse,
+  BasketItemOrderParamsProperty,
+  ProductProperty,
+  Product,
+} from '@plentymarkets/shop-api';
 
 export interface UseProductOrderPropertiesState {
   data: BasketItemOrderParamsProperty[];
+  loading: boolean;
 }
 
 export type SetProperties = (productProperties: ProductProperty[]) => void;
@@ -10,10 +16,12 @@ export type GetPropertiesPrice = (product: Product) => number;
 
 export interface UseProductOrderProperties {
   data: Ref<UseProductOrderPropertiesState['data']>;
+  loading: Ref<UseProductOrderPropertiesState['loading']>;
   setProperties: SetProperties;
   getPropertyById: (id: number) => BasketItemOrderParamsProperty | undefined;
   getPropertiesForCart: () => BasketItemOrderParamsProperty[];
   getPropertiesPrice: GetPropertiesPrice;
+  uploadFile: (file: File) => Promise<UploadFileForOrderPropertyResponse | null>;
 }
 
 export type UseProductOrderPropertiesReturn = () => UseProductOrderProperties;
