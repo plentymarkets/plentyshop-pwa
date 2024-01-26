f3d7e1006293524f471ceb9afc0751d2b223208c
abocsan-plenty <129151096+abocsan-plenty@users.noreply.github.com>
2024-01-26 12:43:07 +0200

    feat: wishlist e2e (#313)
    * feat: wishlist e2e
    
    * feat: wishlist e2e
    
    * feat: wishlist e2e
    
    * feat: wishlist e2e
    
    * Update apps/web/lang/de.json
    
    Co-authored-by: Maximilian Röll <30629157+maxiroellplenty@users.noreply.github.com>
    
    * chore: folder structure
    
    ---------
    
    Co-authored-by: Maximilian Röll <30629157+maxiroellplenty@users.noreply.github.com>

--- apps/web/components/WishlistPageContent/WishlistPageContent.vue
+++ apps/web/components/WishlistPageContent/WishlistPageContent.vue
@@ -39,8 +39,16 @@
         </div>
       </div>
       <div v-else class="flex items-center justify-center flex-col pt-24 pb-32" data-testid="wishlist-page-content">
-        <NuxtImg src="/images/empty-cart.svg" :alt="$t('emptyCartImgAlt')" width="192" height="192" />
-        <h2 class="mt-8 typography-headline-3 font-bold">{{ $t('emptyWishlist') }}</h2>
+        <NuxtImg
+          data-testid="empty-wishlist-image"
+          src="/images/empty-cart.svg"
+          :alt="$t('emptyCartImgAlt')"
+          width="192"
+          height="192"
+        />
+        <h2 data-testid="empty-wishlist-text" class="mt-8 typography-headline-3 font-bold">
+          {{ $t('emptyWishlist') }}
+        </h2>
       </div>
     </div>
   </NarrowContainer>
