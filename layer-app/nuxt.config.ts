95eeda6658d8a584a5e037fa0775b0d1d0ebdf07
aoltea-plenty <alexandru.oltean@plentymarkets.com>
2024-01-29 12:22:08 +0200

    Testing Layers(2)

--- layer-app/nuxt.config.ts
+++ layer-app/nuxt.config.ts
@@ -1,14 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import path from 'path';
 
 export default defineNuxtConfig({
   devtools: { enabled: true },
   extends: [
       "@storefront-ui/vue",
+      "vue-tsc",
       "../apps/web",
 
   ],
-
-  alias: {
-      '~': "../apps/web",
-  }
+    alias: {
+        "~": path.resolve(__dirname, '../apps/web'),
+        "@": path.resolve(__dirname, '../apps/web'),
+    },
 })
