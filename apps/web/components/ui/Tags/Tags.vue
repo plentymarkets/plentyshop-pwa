9534d45ad327bc458f5d71c6e794f1146c59e045
Noah Feist <noah.feist@plentysystems.com>
2024-02-15 09:33:04 +0100

    Change: from productGetters to tagsGetters

--- apps/web/components/ui/Tags/Tags.vue
+++ apps/web/components/ui/Tags/Tags.vue
@@ -1,18 +1,23 @@
 <template>
-    <div>
-        <ul v-if="tags && tags.length > 0" class="flex flex-row">
-          <li v-for="(tag, index) in tags" :key="index">
-            <p class="border rounded-xl inline-block text-center px-1 text-xs shadow-md" :style="{ backgroundColor: tag.color } as any">{{ tag.names.name }}</p>
-          </li>
-        </ul>
-      </div>
+  <div>
+    <ul v-if="tags && tags.length > 0" class="flex flex-row flex-wrap">
+      <li v-for="(tag, index) in tags" :key="index">
+        <p class="border rounded-xl inline-block text-center px-1 text-xs shadow-md" 
+        :class="isDarkText[index] ? 'text-white' : 'text-dark'" 
+        :style="{ backgroundColor: tag.color || undefined}">
+          {{ tag.names.name }}
+        </p>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { productGetters } from '@plentymarkets/shop-sdk';
+import { productTagsGetters } from '@plentymarkets/shop-sdk';
 import type { TagsProps } from '~/components/ui/Tags/types';
 
 const { product } = withDefaults(defineProps<TagsProps>(), {});
 
-const tags = productGetters.getTags(product);
+const tags = productTagsGetters.getTags(product);
+const isDarkText = productTagsGetters.getTagsTextColor(product);
 </script>
