f4b4a23e22f1ee7641c439d064ec19a788c3745f
cristian sandru <cristian.sandru@plentysystems.com>
2024-02-20 12:05:12 +0200

    chore: remove unused footer urls

--- apps/web/mocks/footerData.ts
+++ apps/web/mocks/footerData.ts
@@ -1,6 +1,3 @@
-import { SfIconFacebook, SfIconInstagram, SfIconTwitter, SfIconYoutube } from '@storefront-ui/vue';
-import SfLinkedin from '~/components/ui/Icons/IconLinkedin.vue';
-
 export const categories = [
   {
     key: 'howToBuy',
@@ -63,41 +60,4 @@ export const categories = [
   },
 ];
 
-export const socialMedia = [
-  {
-    label: 'Facebook',
-    link: 'https://www.facebook.com/plentymarkets',
-    icon: SfIconFacebook,
-  },
-  {
-    label: 'Twitter',
-    link: 'https://twitter.com/plentymarkets',
-    icon: SfIconTwitter,
-  },
-  {
-    label: 'Linkedin',
-    link: 'https://www.linkedin.com/company/plentysystems',
-    icon: SfLinkedin,
-  },
-  {
-    label: 'Instagram',
-    link: 'https://www.instagram.com/plentysystems',
-    icon: SfIconInstagram,
-  },
-  {
-    label: 'Youtube',
-    link: 'https://www.youtube.com/channel/UCauJsvmhbPNp6ii7tCGwxMg',
-    icon: SfIconYoutube,
-  },
-];
-export const bottomLinks = [
-  {
-    key: 'terms',
-    link: paths.home,
-  },
-  {
-    key: 'privacyPolicy',
-    link: paths.home,
-  },
-];
 export const companyName = `© PLENTYSYSTEMS AG ${new Date().getFullYear()}`;
