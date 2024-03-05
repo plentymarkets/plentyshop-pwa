120b89e21834bd5b8caa8e4be60dc0b4bf77c285
Fabian Gerke <124085586+FabianGerke@users.noreply.github.com>
2024-03-05 12:06:10 +0100

    Update ProductAttributes.vue

--- apps/web/components/ProductAttributes/ProductAttributes.vue
+++ apps/web/components/ProductAttributes/ProductAttributes.vue
@@ -16,7 +16,7 @@
 import { ProductAttributesProps, ComponentsMapper } from './types';
 import AttributeDropdown from './AttributeDropdown/AttributeDropdown.vue';
 import AttributeBox from './AttributeBox/AttributeBox.vue';
-import AttributeImage from './AttributeImage/AttributeImage.vue';
+// import AttributeImage from './AttributeImage/AttributeImage.vue';
 import { productAttributeGetters } from '@plentymarkets/shop-sdk';
 
 const componentsMapper: ComponentsMapper = {
