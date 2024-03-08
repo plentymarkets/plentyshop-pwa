9e09230534c117ee8056c1fcc3f95041f425f7ee
Fabian Gerke <124085586+FabianGerke@users.noreply.github.com>
2024-03-08 11:08:18 +0100

    fix: test

--- apps/web/components/ProductAttributes/ProductAttributes.vue
+++ apps/web/components/ProductAttributes/ProductAttributes.vue
@@ -16,13 +16,13 @@
 import { ProductAttributesProps, ComponentsMapper } from './types';
 import AttributeDropdown from './AttributeDropdown/AttributeDropdown.vue';
 import AttributeBox from './AttributeBox/AttributeBox.vue';
-import AttributeImage from './AttributeImage/AttributeImage.vue';
+// import AttributeImage from './AttributeImage/AttributeImage.vue';
 import { productAttributeGetters } from '@plentymarkets/shop-sdk';
 
 const componentsMapper: ComponentsMapper = {
   dropdown: AttributeDropdown,
   box: AttributeBox,
-  image: AttributeImage,
+  image: AttributeBox,
 };
 
 const { attributes, setAttribute } = useProductAttributes();
