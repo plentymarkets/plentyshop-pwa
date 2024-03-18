816e5de9821cb11899cb17694df343a2b43686d1
cristian sandru <cristian.sandru@plentysystems.com>
2024-03-18 10:05:01 +0200

    fix: styling issues

--- apps/web/components/Coupon/Coupon.vue
+++ apps/web/components/Coupon/Coupon.vue
@@ -2,7 +2,7 @@
   <div class="border-b border-neutral-200">
     <SfAccordionItem v-if="!cartGetters.getCouponDiscount(cart)" data-testid="couponZone" v-model="openedCoupon">
       <template #summary>
-        <div :class="['flex justify-between font-medium p-3', { 'mb-3 bg-gray-100 mt-2': openedCoupon }]">
+        <div :class="['flex justify-between font-medium p-3', { 'my-4 bg-gray-100 rounded-md': openedCoupon }]">
           <p class="pl-3">{{ $t('coupon.title') }}</p>
           <SfIconChevronLeft
             :class="['text-neutral-500', { 'rotate-90': openedCoupon, '-rotate-90': !openedCoupon }]"
