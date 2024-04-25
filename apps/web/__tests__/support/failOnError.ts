23c09f20010d5daa0756b348668dc57803d991f6
Fabian Gerke <124085586+FabianGerke@users.noreply.github.com>
2024-04-25 11:55:29 +0200

    fix: rm logs

--- apps/web/__tests__/support/failOnError.ts
+++ apps/web/__tests__/support/failOnError.ts
@@ -5,12 +5,10 @@ Cypress.on("window:before:load", win => {
 });
 
 Cypress.on('uncaught:exception', (err, runnable) => {
-  // return false to prevent the error from failing this test
   if (err.message.includes('zoid')) {
     return false;
   }
-  // If it's not a zoid error, you might still want to see it in the console
+
   console.error('Uncaught exception', err);
-  // Throw error further if it's not related to zoid
   throw err;
 });
