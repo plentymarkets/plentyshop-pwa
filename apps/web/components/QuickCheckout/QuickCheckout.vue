639ee940c61796f44d784fe674a7a723a33dab2a
Maximilian Röll <30629157+maxiroellplenty@users.noreply.github.com>
2024-05-14 11:35:06 +0200

    fix: sdk import

--- apps/web/components/QuickCheckout/QuickCheckout.vue
+++ apps/web/components/QuickCheckout/QuickCheckout.vue
@@ -90,7 +90,7 @@
 <script setup lang="ts">
 import { SfButton, SfIconClose } from '@storefront-ui/vue';
 import type { QuickCheckoutProps } from './types';
-import { cartGetters, productGetters } from '@plentymarkets/shop-sdk';
+import { cartGetters, productGetters } from '@plentymarkets/shop-api';
 import ProductPrice from '~/components/ProductPrice/ProductPrice.vue';
 
 defineProps<QuickCheckoutProps>();
