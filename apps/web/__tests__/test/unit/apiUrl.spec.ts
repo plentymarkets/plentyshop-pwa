b65ab73b8a13864ede8d15d916ba98f9662af2ca
Noah Feist <noah.feist@plentysystems.com>
2024-05-15 14:12:07 +0200

    forgot the renaming of the helper function in unit tests

--- apps/web/__tests__/test/unit/apiUrl.spec.ts
+++ apps/web/__tests__/test/unit/apiUrl.spec.ts
@@ -1,21 +1,21 @@
 import { describe, expect, it } from 'vitest'
-import { removeTrailingSlash } from '../../../utils/pathHelper';
+import { validateApiUrl } from '../../../utils/pathHelper';
 
 describe('removeTrailingSlash function', () => {
     const expectedApiUrl = 'http://localhost:8181';
 
     it('should remove one trailing slash', () => {
         const apiUrl = `${expectedApiUrl}/`;
-        expect(removeTrailingSlash(apiUrl)).toEqual(expectedApiUrl);
+        expect(validateApiUrl(apiUrl)).toEqual(expectedApiUrl);
     });
 
     it('should remove multiple trailing slashes', () => {
         const apiUrl = `${expectedApiUrl}/\//`;
-        expect(removeTrailingSlash(apiUrl)).toEqual(expectedApiUrl);
+        expect(validateApiUrl(apiUrl)).toEqual(expectedApiUrl);
     });
 
     it('should return the same URL if there is no trailing slash', () => {
         const apiUrl = expectedApiUrl;
-        expect(removeTrailingSlash(apiUrl)).toEqual(apiUrl);
+        expect(validateApiUrl(apiUrl)).toEqual(apiUrl);
     });
 });
