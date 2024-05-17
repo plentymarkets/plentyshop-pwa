f6dcd6619641e2e03de0cb5ab200862b81ae7968
Noah Feist <noah.feist@plentysystems.com>
2024-05-17 10:00:44 +0200

    still error on fetchProduct()

--- apps/web/composables/useProduct/__tests__/useProduct.spec.ts
+++ apps/web/composables/useProduct/__tests__/useProduct.spec.ts
@@ -1,16 +1,15 @@
 import type { Product, ProductParams } from '@plentymarkets/shop-api';
 import { useProduct } from "../useProduct";
 import { ProductMock } from "../../../__tests__/__mocks__/product.mock";
-import { fetch } from '@nuxt/test-utils';
 
-let expected_product = ProductMock;
-let productId = ProductMock.item.id;
+let expected_product : Product = ProductMock;
+let productId = ProductMock.item.id.toString();
 let productParams: ProductParams = {id: productId};
 
 
 describe('useProduct works properly', () => {
     it('should return Product', async () => {
-        const { data: product, fetchProduct } = useProduct(productId.toString());
+        const { data: product, fetchProduct } = useProduct(productId);
 
         await fetchProduct(productParams);
 
