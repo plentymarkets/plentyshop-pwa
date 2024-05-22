c55acb71881e186f37839dbe5881bb2a77803213
Paul Cuta <paul.cuta@plentysystems.com>
2024-05-22 13:56:52 +0300

    test: enable debug logs

--- apps/server/middleware.config.ts
+++ apps/server/middleware.config.ts
@@ -13,6 +13,7 @@ const config = {
         api: {
           url: process.env.API_ENDPOINT,
           securityToken: process.env.API_SECURITY_TOKEN ?? '',
+          debug: true,
         },
       },
     },
