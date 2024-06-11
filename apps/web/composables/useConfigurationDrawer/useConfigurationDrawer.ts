b66cb6dbd24971bb836d2591afd040960a95b69b
ksted <kevin.stederoth@plentymarkets.com>
2024-06-11 14:42:11 +0200

    feat: generate tailwind color palette

--- apps/web/composables/useConfigurationDrawer/useConfigurationDrawer.ts
+++ apps/web/composables/useConfigurationDrawer/useConfigurationDrawer.ts
@@ -1,22 +1,85 @@
-const open = ref(false);
+import { buildSpectrum, spectrumToList, type SpectrumList } from '@effective/color/dist/engine';
+import { converter, formatRgb } from 'culori/fn';
 
-const hexToRgb = (hex: string) => {
-  const result = /^#?([\da-f]{2})([\da-f]{2})([\da-f]{2})$/i.exec(hex);
-  if (!result) return '';
-  const r = Number.parseInt(result[1], 16);
-  const g = Number.parseInt(result[2], 16);
-  const b = Number.parseInt(result[3], 16);
-  return `${r} ${g} ${b}`;
+const open = ref(false);
+const rgb = converter('rgb');
+
+const spectrumToTailwindMapper = [
+  { id: '-5', weight: '50' },
+  { id: '-4', weight: '100' },
+  { id: '-3', weight: '200' },
+  { id: '-2', weight: '300' },
+  { id: '-1', weight: '400' },
+  { id: '0', weight: '500' },
+  { id: '+1', weight: '600' },
+  { id: '+2', weight: '700' },
+  { id: '+3', weight: '800' },
+  { id: '+4', weight: '900' },
+  { id: '+5', weight: '950' },
+];
+
+const spectrumToTailwind = (spectrumList: SpectrumList) => {
+  const tailwindColors = [
+    { weight: '50', value: '' },
+    { weight: '100', value: '' },
+    { weight: '200', value: '' },
+    { weight: '300', value: '' },
+    { weight: '400', value: '' },
+    { weight: '500', value: '' },
+    { weight: '600', value: '' },
+    { weight: '700', value: '' },
+    { weight: '800', value: '' },
+    { weight: '900', value: '' },
+    { weight: '950', value: '' },
+  ];
+
+  spectrumList.forEach((spectrumListEntry) => {
+    const spectrumId = spectrumListEntry.id;
+    const tailwindWeight = spectrumToTailwindMapper.find((entry) => entry.id === spectrumId);
+    const tailwindColorsEntry = tailwindColors.find((entry) => entry.weight === tailwindWeight?.weight);
+
+    const colorValue = spectrumListEntry.value;
+    if (tailwindColorsEntry && colorValue && typeof colorValue === 'string') {
+      tailwindColorsEntry.value = colorValue;
+    }
+  });
+
+  return tailwindColors;
+};
+
+const oklchToRgb = (oklch: string) => {
+  const rgbColor = rgb(oklch) ?? { mode: 'rgb', r: 0, g: 0, b: 0 };
+  const rgbColorFormatted = formatRgb(rgbColor);
+  const rgbColorString = rgbColorFormatted.slice(rgbColorFormatted.indexOf('(') + 1, rgbColorFormatted.indexOf(')'));
+
+  return rgbColorString.replaceAll(',', '');
+};
+
+const getTailwindColorsOklch = (hexColor: string) => {
+  const spectrum = buildSpectrum(hexColor);
+  const spectrumList = spectrumToList(spectrum);
+  return spectrumToTailwind(spectrumList);
+};
+
+const setColorProperties = (type: string, tailwindColors: any) => {
+  tailwindColors.forEach((color: {weight: string, value: string}) => {
+    if (color.value) {
+      const rgb = oklchToRgb(color.value);
+      document.documentElement.style.setProperty(`--colors-${type}-${color.weight}`, rgb);
+    }
+  });
 };
 
 const updatePrimaryColor = (hexColor: string) => {
-  const rgb = hexToRgb(hexColor);
-  document.documentElement.style.setProperty('--colors-primary-700', rgb);
+  const tailwindColors = getTailwindColorsOklch(hexColor);
+
+  setColorProperties('primary', tailwindColors);
 };
 
 const updateSecondaryColor = (hexColor: string) => {
-  const rgb = hexToRgb(hexColor);
-  document.documentElement.style.setProperty('--colors-secondary-700', rgb);
+  const tailwindColors = getTailwindColorsOklch(hexColor);
+
+  setColorProperties('secondary', tailwindColors);
 };
 
 export const useConfigurationDrawer = () => {
