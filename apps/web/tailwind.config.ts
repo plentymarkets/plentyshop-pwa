3960400386c82f43caeab89ca664d57ed392f663
ksted <kevin.stederoth@plentymarkets.com>
2024-06-12 09:57:37 +0200

    fix: storefront styles
    - re-add preset
    - change css variable names

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,8 +1,10 @@
 import sfTypography from '@storefront-ui/typography';
+import { tailwindConfig } from '@storefront-ui/vue/tailwind-config';
 import type { Config } from 'tailwindcss';
 import defaultTheme from 'tailwindcss/defaultTheme';
 
 export default {
+  presets: [tailwindConfig],
   content: ['./**/*.vue', '../../node_modules/@storefront-ui/vue/**/*.{js,mjs}'],
   theme: {
     extend: {
@@ -12,30 +14,30 @@ export default {
       },
       colors: {
         primary: {
-          '50': 'rgb(var(--colors-primary-50) / <alpha-value>)',
-          '100': 'rgb(var(--colors-primary-100) / <alpha-value>)',
-          '200': 'rgb(var(--colors-primary-200) / <alpha-value>)',
-          '300': 'rgb(var(--colors-primary-300) / <alpha-value>)',
-          '400': 'rgb(var(--colors-primary-400) / <alpha-value>)',
-          '500': 'rgb(var(--colors-primary-500) / <alpha-value>)',
-          '600': 'rgb(var(--colors-primary-600) / <alpha-value>)',
-          '700': 'rgb(var(--colors-primary-700) / <alpha-value>)',
-          '800': 'rgb(var(--colors-primary-800) / <alpha-value>)',
-          '900': 'rgb(var(--colors-primary-900) / <alpha-value>)',
-          '950': 'rgb(var(--colors-primary-950) / <alpha-value>)',
+          '50': 'rgb(var(--colors-2-primary-50) / <alpha-value>)',
+          '100': 'rgb(var(--colors-2-primary-100) / <alpha-value>)',
+          '200': 'rgb(var(--colors-2-primary-200) / <alpha-value>)',
+          '300': 'rgb(var(--colors-2-primary-300) / <alpha-value>)',
+          '400': 'rgb(var(--colors-2-primary-400) / <alpha-value>)',
+          '500': 'rgb(var(--colors-2-primary-500) / <alpha-value>)',
+          '600': 'rgb(var(--colors-2-primary-600) / <alpha-value>)',
+          '700': 'rgb(var(--colors-2-primary-700) / <alpha-value>)',
+          '800': 'rgb(var(--colors-2-primary-800) / <alpha-value>)',
+          '900': 'rgb(var(--colors-2-primary-900) / <alpha-value>)',
+          '950': 'rgb(var(--colors-2-primary-950) / <alpha-value>)',
         },
         secondary: {
-          '50': 'rgb(var(--colors-secondary-50) / <alpha-value>)',
-          '100': 'rgb(var(--colors-secondary-100) / <alpha-value>)',
-          '200': 'rgb(var(--colors-secondary-200) / <alpha-value>)',
-          '300': 'rgb(var(--colors-secondary-300) / <alpha-value>)',
-          '400': 'rgb(var(--colors-secondary-400) / <alpha-value>)',
-          '500': 'rgb(var(--colors-secondary-500) / <alpha-value>)',
-          '600': 'rgb(var(--colors-secondary-600) / <alpha-value>)',
-          '700': 'rgb(var(--colors-secondary-700) / <alpha-value>)',
-          '800': 'rgb(var(--colors-secondary-800) / <alpha-value>)',
-          '900': 'rgb(var(--colors-secondary-900) / <alpha-value>)',
-          '950': 'rgb(var(--colors-secondary-950) / <alpha-value>)',
+          '50': 'rgb(var(--colors-2-secondary-50) / <alpha-value>)',
+          '100': 'rgb(var(--colors-2-secondary-100) / <alpha-value>)',
+          '200': 'rgb(var(--colors-2-secondary-200) / <alpha-value>)',
+          '300': 'rgb(var(--colors-2-secondary-300) / <alpha-value>)',
+          '400': 'rgb(var(--colors-2-secondary-400) / <alpha-value>)',
+          '500': 'rgb(var(--colors-2-secondary-500) / <alpha-value>)',
+          '600': 'rgb(var(--colors-2-secondary-600) / <alpha-value>)',
+          '700': 'rgb(var(--colors-2-secondary-700) / <alpha-value>)',
+          '800': 'rgb(var(--colors-2-secondary-800) / <alpha-value>)',
+          '900': 'rgb(var(--colors-2-secondary-900) / <alpha-value>)',
+          '950': 'rgb(var(--colors-2-secondary-950) / <alpha-value>)',
         },
       },
       gridTemplateAreas: {
