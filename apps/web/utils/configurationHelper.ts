2982d99a0f9d82694d7a5c73b34b35259b807d7c
aoltean-plenty <alexandru.oltean@plentysystems.com>
2024-06-19 04:26:11 -0700

    Send to another colleague

--- apps/web/utils/configurationHelper.ts
+++ apps/web/utils/configurationHelper.ts
@@ -12,12 +12,7 @@ const getConfiguration = () => {
     .get(`/storefront/settings/${process.env.API_CONFIG_ID}`)
     // eslint-disable-next-line promise/always-return
     .then((res) => {
-      process.env.ID = res.data.userId;
-      process.env.TITLE = res.data.title;
-      process.env.COMPLETED = res.data.completed;
-      console.log(process.env.ID);
-      console.log(process.env.TITLE);
-      console.log(process.env.COMPLETED);
+     console.log(res.data);
     })
     // eslint-disable-next-line unicorn/prefer-top-level-await
     .catch((error: unknown) => console.error(error));
