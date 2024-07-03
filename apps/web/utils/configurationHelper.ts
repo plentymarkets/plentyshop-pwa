21a1f00755c6b55fe31ef12343fe9496956681c4
clincu-plenty <cristian.lincu@plentysystems.com>
2024-07-03 12:21:19 +0300

    Applying code review changes.

--- apps/web/utils/configurationHelper.ts
+++ apps/web/utils/configurationHelper.ts
@@ -1,109 +1,95 @@
 import axios from 'axios';
-import https from 'https';
-import fs from 'fs';
-import os from 'os';
-import path from 'path';
+import https from 'node:https';
+import fs from 'node:fs';
+import os from 'node:os';
+import path from 'node:path';
 
-const envFilePath = path.resolve(__dirname, "../.env");
-const envTmpFilePath = path.resolve(__dirname, '../.env.tmp');
+const environmentFilePath = path.resolve(__dirname, '../.env');
+const environmentTemporaryFilePath = path.resolve(__dirname, '../.env.tmp');
 
-// read .env file & convert to array
-const readEnvVars = (path: string) => fs.readFileSync(path, "utf-8").split(os.EOL);
+const readEnvironmentVariables = (path: string) => fs.readFileSync(path, 'utf8').split(os.EOL);
 
-const getEnvValue = (path: string, key: string) => {
-  // find the line that contains the key (exact match)
-  const matchedLine = readEnvVars(path).find((line) => line.split("=")[0] === key);
-  // split the line (delimiter is '=') and return the item at index 2
-  return matchedLine !== undefined ? matchedLine.split("=")[1] : null;
+const getEnvironmentValue = (path: string, key: string) => {
+  const matchedLine = readEnvironmentVariables(path).find((line) => line.split('=')[0] === key);
+  return matchedLine === undefined ? null : matchedLine.split('=')[1];
 };
 
-const setEnvValue = (path: string, key: string, value: string) => {
-  const envVars = readEnvVars(path);
-  const targetLine = envVars.find((line) => line.split("=")[0] === key);
-  if (targetLine !== undefined) {
-    // update existing line
-    const targetLineIndex = envVars.indexOf(targetLine);
-    // replace the key/value with the new value
-    envVars.splice(targetLineIndex, 1, `${key.toUpperCase()}="${value}"`);
+const setEnvironmentValue = (path: string, key: string, value: string) => {
+  const environmentVariables = readEnvironmentVariables(path);
+  const targetLine = environmentVariables.find((line) => line.split('=')[0] === key);
+  if (targetLine === undefined) {
+    environmentVariables.push(`${key.toUpperCase()}="${value}"`);
   } else {
-    // create new key value
-    envVars.push(`${key.toUpperCase()}="${value}"`);
+    const targetLineIndex = environmentVariables.indexOf(targetLine);
+    environmentVariables.splice(targetLineIndex, 1, `${key.toUpperCase()}="${value}"`);
   }
-  // write everything back to the file system
-  fs.writeFileSync(path, envVars.join(os.EOL));
+  fs.writeFileSync(path, environmentVariables.join(os.EOL));
 };
 
 const getConfiguration = async () => {
-  const requiredEnvVars = ['API_ENDPOINT', 'API_SECURITY_TOKEN', 'CONFIG_ID'];
-  let requiredEnvData: string = '';
+  const fetchingStatus = getEnvironmentValue(environmentFilePath, 'DISABLE_SETTINGS_FETCH');
+  if (fetchingStatus === '1') {
+    console.warn(`Fetching PWA settings is disabled! Check DISABLE_SETTINGS_FETCH in .env file.`);
+    return;
+  }
 
-  console.log('Verifying required env variables...');
+  const requiredEnvironmentVariables = ['API_ENDPOINT', 'API_SECURITY_TOKEN', 'CONFIG_ID'];
+  let requiredEnvironmentData = '';
 
-  for (let i = 0; i < requiredEnvVars.length; i++) {
-    const envVar = requiredEnvVars[i];
-    if (!getEnvValue(envFilePath, envVar)) {
-      console.log(`Missing or invalid required environment variable: ${envVar}`);
+  for (let index = 0; index < requiredEnvironmentVariables.length; index++) {
+    const environmentVariable = requiredEnvironmentVariables[index];
+    if (!getEnvironmentValue(environmentFilePath, environmentVariable)) {
+      console.error(`Missing or invalid required environment variable: ${environmentVariable}`);
       return;
     }
 
-    const envValue = process.env[envVar];
-    requiredEnvData += `${envVar}=${envValue}`;
+    const environmentValue = process.env[environmentVariable];
+    requiredEnvironmentData += `${environmentVariable}=${environmentValue}`;
 
-    // For last iteration just skip adding
-    if (i !== requiredEnvVars.length - 1) {
-      requiredEnvData += '\n';
+    if (index !== requiredEnvironmentVariables.length - 1) {
+      requiredEnvironmentData += '\n';
     }
   }
 
-  console.log('Required env variables found ✅');
+  fs.writeFile(environmentTemporaryFilePath, requiredEnvironmentData, () => {});
 
-  // Create .env.tmp file
-  console.log('Creating .env.tmp file...');
-  fs.writeFile(envTmpFilePath, requiredEnvData, () => {});
-
-  // Create axios instance
   const instance = axios.create({
     withCredentials: true,
     baseURL: process.env.API_ENDPOINT,
     headers: {
-      'X-Security-Token': process.env.API_SECURITY_TOKEN // Testing this what happen when bad token
+      'X-Security-Token': process.env.API_SECURITY_TOKEN,
     },
     httpsAgent: new https.Agent({
-      rejectUnauthorized: false
-    })
+      rejectUnauthorized: false,
+    }),
   });
 
   let response;
 
   try {
-    console.log('Fetching PWA settings...');
     const { data } = await instance.get(`/storefront/settings/${process.env.CONFIG_ID}`);
     response = data;
 
-    // console.log('Response', response, data);
     for (const category in response) {
       if (Array.isArray(data[category])) {
-        response[category].forEach((item: any) => {
-          setEnvValue(envTmpFilePath, item.key, item.value);
-        })
+        response[category].forEach((item: { [key: string]: string }) => {
+          setEnvironmentValue(environmentTemporaryFilePath, item.key, item.value);
+        });
       }
     }
   } catch (error: any) {
     if (axios.isAxiosError(error)) {
-      console.error('PWA settings error:', error.response!.data.error);
+      console.error('PWA settings error:', error.response?.data.error);
       return;
     }
   }
 
-  // Copy the contents of .env.tmp to .env
-  fs.copyFile(envTmpFilePath, envFilePath, () => {});
-  console.log('.env.tmp data has been copied to .env');
+  fs.copyFile(environmentTemporaryFilePath, environmentFilePath, () => {});
+  fs.unlink(environmentTemporaryFilePath, () => {});
 
-  // Remove the .env.tmp file
-  fs.unlink(envTmpFilePath, () => {});
-  console.log('.env.tmp file has been removed');
+  console.log('New .env file generated!');
 
-  return Promise.resolve(response);
+  return response;
 };
 
-export default getConfiguration
+export default getConfiguration;
