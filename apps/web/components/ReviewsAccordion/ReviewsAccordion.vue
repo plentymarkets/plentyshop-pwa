f92cb1af224e4dcfefe5f9514c76a4c97eac084b
abocsan-plenty <129151096+abocsan-plenty@users.noreply.github.com>
2024-07-04 12:05:46 +0300

    fix: review hydration error

--- apps/web/components/ReviewsAccordion/ReviewsAccordion.vue
+++ apps/web/components/ReviewsAccordion/ReviewsAccordion.vue
@@ -4,85 +4,83 @@
     class="relative col-span-5 md:sticky md:top-10 h-fit"
     :class="{ 'pointer-events-none opacity-50': loading }"
   >
-    <ClientOnly>
-      <SfLoaderCircular v-if="loading" class="absolute top-[130px] right-0 left-0 m-auto z-[999]" size="2xl" />
+    <SfLoaderCircular v-if="loading" class="absolute top-[130px] right-0 left-0 m-auto z-[999]" size="2xl" />
 
-      <div data-testid="reviews-accordion" id="customerReviewsAccordion">
-        <UiAccordionItem
-          v-model="reviewsOpen"
-          summary-class="md:rounded-md w-full hover:bg-neutral-100 py-2 pl-4 pr-3 flex justify-between items-center select-none"
-        >
-          <template #summary>
-            <h2 class="font-bold font-headings text-lg leading-6 md:text-2xl" id="customerReviewsClick">
-              {{ t('customerReviews') }}
-            </h2>
-          </template>
+    <div data-testid="reviews-accordion" id="customerReviewsAccordion">
+      <UiAccordionItem
+        v-model="reviewsOpen"
+        summary-class="md:rounded-md w-full hover:bg-neutral-100 py-2 pl-4 pr-3 flex justify-between items-center select-none"
+      >
+        <template #summary>
+          <h2 class="font-bold font-headings text-lg leading-6 md:text-2xl" id="customerReviewsClick">
+            {{ t('customerReviews') }}
+          </h2>
+        </template>
 
-          <div class="flex justify-start mb-4 lg:mb-0">
-            <div class="lg:flex my-2">
-              <div class="lg:w-1/2 flex flex-col lg:mr-8">
-                <p class="text-center text-sm">{{ t('averageRating') }}</p>
-                <div class="flex justify-center">
-                  <SfRating
-                    class="pb-2"
-                    size="lg"
-                    :max="5"
-                    :value="reviewAverageStars || reviewAverageText"
-                    :half-increment="true"
-                  />
-                  <h3 class="font-bold text-xl ml-2">
-                    {{ reviewAverageText }}
-                  </h3>
-                </div>
-                <p class="text-xs text-center text-">{{ t('basedOnratings', { count: totalReviews }) }}</p>
-                <SfButton
-                  @click="isAuthorized ? openReviewModal() : openAuthentication()"
-                  data-testid="create-review"
-                  class="mt-2 mb-4 mx-auto"
-                  size="base"
-                >
-                  {{ t('createCustomerReview') }}
-                </SfButton>
+        <div class="flex justify-start mb-4 lg:mb-0">
+          <div class="lg:flex my-2">
+            <div class="lg:w-1/2 flex flex-col lg:mr-8">
+              <p class="text-center text-sm">{{ t('averageRating') }}</p>
+              <div class="flex justify-center">
+                <SfRating
+                  class="pb-2"
+                  size="lg"
+                  :max="5"
+                  :value="reviewAverageStars || reviewAverageText"
+                  :half-increment="true"
+                />
+                <h3 class="font-bold text-xl ml-2">
+                  {{ reviewAverageText }}
+                </h3>
               </div>
+              <p class="text-xs text-center text-">{{ t('basedOnratings', { count: totalReviews }) }}</p>
+              <SfButton
+                @click="isAuthorized ? openReviewModal() : openAuthentication()"
+                data-testid="create-review"
+                class="mt-2 mb-4 mx-auto"
+                size="base"
+              >
+                {{ t('createCustomerReview') }}
+              </SfButton>
+            </div>
 
-              <div class="flex flex-col">
-                <div v-for="(proportionalRating, key) in ratingPercentages" :key="key" class="flex items-center">
-                  <p class="w-4 text-center">{{ 5 - key }}</p>
-                  <SfIconStarFilled class="lg:mx-2 pb-1 text-warning-500" size="base" />
-                  <SfProgressLinear
-                    class="self-center"
-                    size="sm"
-                    :value="proportionalRating"
-                    aria-label="proportional-rating-in-percent"
-                  />
-                  <p class="w-20 ml-2">( {{ splitRatings[key] }} )</p>
-                </div>
+            <div class="flex flex-col">
+              <div v-for="(proportionalRating, key) in ratingPercentages" :key="key" class="flex items-center">
+                <p class="w-4 text-center">{{ 5 - key }}</p>
+                <SfIconStarFilled class="lg:mx-2 pb-1 text-warning-500" size="base" />
+                <SfProgressLinear
+                  class="self-center"
+                  size="sm"
+                  :value="proportionalRating"
+                  aria-label="proportional-rating-in-percent"
+                />
+                <p class="w-20 ml-2">( {{ splitRatings[key] }} )</p>
               </div>
             </div>
           </div>
+        </div>
 
-          <UiReview
-            v-for="(reviewItem, key) in paginatedProductReviews"
-            :key="key"
-            :review-item="reviewItem"
-            @on-submit="saveReview"
-            @review-updated="fetchReviews"
-            @review-deleted="deleteReview"
-          />
-          <p v-if="!totalReviews && paginatedProductReviews.length === 0" class="font-bold leading-6 w-full py-2">
-            {{ t('customerReviewsNone') }}
-          </p>
-          <UiPagination
-            v-if="paginatedProductReviews.length > 0"
-            :current-page="getFacetsFromURL().feedbackPage ?? 1"
-            :total-items="totalReviews"
-            :page-size="getFacetsFromURL().feedbacksPerPage ?? 1"
-            :max-visible-pages="maxVisiblePages"
-            current-page-name="feedbackPage"
-          />
-        </UiAccordionItem>
-      </div>
-    </ClientOnly>
+        <UiReview
+          v-for="(reviewItem, key) in paginatedProductReviews"
+          :key="key"
+          :review-item="reviewItem"
+          @on-submit="saveReview"
+          @review-updated="fetchReviews"
+          @review-deleted="deleteReview"
+        />
+        <p v-if="!totalReviews && paginatedProductReviews.length === 0" class="font-bold leading-6 w-full py-2">
+          {{ t('customerReviewsNone') }}
+        </p>
+        <UiPagination
+          v-if="paginatedProductReviews.length > 0"
+          :current-page="getFacetsFromURL().feedbackPage ?? 1"
+          :total-items="totalReviews"
+          :page-size="getFacetsFromURL().feedbacksPerPage ?? 1"
+          :max-visible-pages="maxVisiblePages"
+          current-page-name="feedbackPage"
+        />
+      </UiAccordionItem>
+    </div>
   </div>
 
   <UiModal
@@ -193,8 +191,6 @@ const deleteReview = () => {
 };
 const maxVisiblePages = computed(() => (viewport.isGreaterOrEquals('lg') ? 10 : 1));
 
-onMounted(() => fetchReviews());
-
 watch(
   () => reviewsOpen.value,
   (value) => {
@@ -213,9 +209,8 @@ watch(
 
 watch(
   () => route.query,
-  async () => {
+  () => {
     fetchReviews();
   },
-  { immediate: true },
 );
 </script>
