be61087a8a5aab49f2f94de740385aa29c7c94d3
ksted <kevin.stederoth@plentymarkets.com>
2024-07-04 14:42:31 +0200

    refactor: fetch script

--- apps/web/build/fetchConfiguration.ts
+++ apps/web/build/fetchConfiguration.ts
@@ -7,57 +7,48 @@ import path from 'node:path';
 const environmentFilePath = path.resolve(__dirname, '../.env');
 const environmentTemporaryFilePath = path.resolve(__dirname, '../.env.tmp');
 
-const readEnvironmentVariables = (path: string) => fs.readFileSync(path, 'utf8').split(os.EOL);
-
-const getEnvironmentValue = (path: string, key: string) => {
-  const matchedLine = readEnvironmentVariables(path).find((line) => line.split('=')[0] === key);
-  return matchedLine === undefined ? null : matchedLine.split('=')[1];
+const environmentMap = {
+  DISABLE_SETTINGS_FETCH: process.env.DISABLE_SETTINGS_FETCH,
+  API_ENDPOINT: process.env.API_ENDPOINT,
+  API_SECURITY_TOKEN: process.env.API_SECURITY_TOKEN,
+  CONFIG_ID: process.env.CONFIG_ID,
 };
 
-const setEnvironmentValue = (path: string, key: string, value: string) => {
-  const environmentVariables = readEnvironmentVariables(path);
-  const targetLine = environmentVariables.find((line) => line.split('=')[0] === key);
-  if (targetLine === undefined) {
-    environmentVariables.push(`${key.toUpperCase()}="${value}"`);
-  } else {
-    const targetLineIndex = environmentVariables.indexOf(targetLine);
-    environmentVariables.splice(targetLineIndex, 1, `${key.toUpperCase()}="${value}"`);
-  }
-  fs.writeFileSync(path, environmentVariables.join(os.EOL));
-};
-
-const fetchConfiguration = async () => {
-  const fetchingStatus = getEnvironmentValue(environmentFilePath, 'DISABLE_SETTINGS_FETCH');
-  if (fetchingStatus === '1') {
-    console.warn(`Fetching PWA settings is disabled! Check DISABLE_SETTINGS_FETCH in .env file.`);
-    return;
-  }
-
-  const requiredEnvironmentVariables = ['API_ENDPOINT', 'API_SECURITY_TOKEN', 'CONFIG_ID'];
+const setupTemporaryEnvironment = () => {
   let requiredEnvironmentData = '';
 
-  for (let index = 0; index < requiredEnvironmentVariables.length; index++) {
-    const environmentVariable = requiredEnvironmentVariables[index];
-    if (!getEnvironmentValue(environmentFilePath, environmentVariable)) {
-      console.error(`Missing or invalid required environment variable: ${environmentVariable}`);
+  for (const [key, value] of Object.entries(environmentMap)) {
+    if (key !== 'DISABLE_SETTINGS_FETCH' && !value) {
+      console.error(`Missing or invalid required environment variable: ${value}`);
       return;
     }
 
-    const environmentValue = process.env[environmentVariable];
-    requiredEnvironmentData += `${environmentVariable}=${environmentValue}`;
-
-    if (index !== requiredEnvironmentVariables.length - 1) {
-      requiredEnvironmentData += '\n';
-    }
+    requiredEnvironmentData += `${key}=${value}\n`;
   }
 
   fs.writeFile(environmentTemporaryFilePath, requiredEnvironmentData, () => {});
+};
 
+const writeConfigurationToTemporaryEnvironment = (data: Array<Array<{ [key: string]: string }>>) => {
+  const environmentVariables = fs.readFileSync(environmentTemporaryFilePath, 'utf8').split(os.EOL);
+
+  for (const category in data) {
+    if (Array.isArray(data[category])) {
+      data[category].forEach((item: { [key: string]: string }) => {
+        environmentVariables.push(`${item.key.toUpperCase()}="${item.value}"`);
+      });
+    }
+  }
+
+  fs.writeFileSync(environmentTemporaryFilePath, environmentVariables.join(os.EOL));
+};
+
+const fetchAndWriteRemoteConfiguration = async () => {
   const instance = axios.create({
     withCredentials: true,
-    baseURL: process.env.API_ENDPOINT,
+    baseURL: environmentMap.API_ENDPOINT,
     headers: {
-      'X-Security-Token': process.env.API_SECURITY_TOKEN,
+      'X-Security-Token': environmentMap.API_SECURITY_TOKEN,
     },
     httpsAgent: new https.Agent({
       rejectUnauthorized: false,
@@ -65,24 +56,30 @@ const fetchConfiguration = async () => {
   });
 
   try {
-    const { data } = await instance.get(`/storefront/settings/${process.env.CONFIG_ID}`);
-
-    for (const category in data) {
-      if (Array.isArray(data[category])) {
-        data[category].forEach((item: { [key: string]: string }) => {
-          setEnvironmentValue(environmentTemporaryFilePath, item.key, item.value);
-        });
-      }
-    }
+    const { data } = await instance.get(`/storefront/settings/${environmentMap.CONFIG_ID}`);
+    writeConfigurationToTemporaryEnvironment(data);
   } catch (error: any) {
     if (axios.isAxiosError(error)) {
       console.error('PWA settings error:', error.response?.data.error);
       return;
     }
   }
+};
 
+const convertTemporaryToPermanentEnvironment = () => {
   fs.copyFile(environmentTemporaryFilePath, environmentFilePath, () => {});
   fs.unlink(environmentTemporaryFilePath, () => {});
+};
+
+const fetchConfiguration = async () => {
+  if (environmentMap.DISABLE_SETTINGS_FETCH === 'undefined' || environmentMap.DISABLE_SETTINGS_FETCH === '0') {
+    console.warn(`Fetching PWA settings is disabled! Check DISABLE_SETTINGS_FETCH in .env file.`);
+    return;
+  }
+
+  setupTemporaryEnvironment();
+  fetchAndWriteRemoteConfiguration();
+  convertTemporaryToPermanentEnvironment();
 };
 
 export default fetchConfiguration;
