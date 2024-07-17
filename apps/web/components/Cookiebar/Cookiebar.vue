2721272c1215da650d335f37ac369d1f2dabcb43
cristian sandru <cristian.sandru@plentysystems.com>
2024-07-17 16:44:51 +0300

    chore: add tooltip

--- apps/web/components/Cookiebar/Cookiebar.vue
+++ apps/web/components/Cookiebar/Cookiebar.vue
@@ -149,6 +149,7 @@
         </div>
       </div>
     </div>
+    <!-- button to open cookie tab -->
     <div v-else class="z-10 h-auto w-12 fixed bottom-[4.3rem] sm:bottom-2 left-2 xl:left-auto xl:right-2">
       <SfTooltip :label="$t('CookieBar.Cookie Settings')" placement="left">
         <SfButton
