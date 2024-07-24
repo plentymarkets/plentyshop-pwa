e1a875761eac17d8c05fdbc86e5f397d14e105cb
aoltean-plenty <alexandru.oltean@plentysystems.com>
2024-07-24 06:23:19 -0700

    Addapt script to no existing hex

--- apps/web/build/generate-scss-variables.ts
+++ apps/web/build/generate-scss-variables.ts
@@ -2,7 +2,7 @@ import fs from 'node:fs';
 import path from 'node:path';
 import { parse } from 'culori';
 
-const hex2rgb = (hex:string) => {
+const hex2rgb = (hex: string) => {
   const r = Number.parseInt(hex.slice(1, 3), 16);
   const g = Number.parseInt(hex.slice(3, 5), 16);
   const b = Number.parseInt(hex.slice(5, 7), 16);
@@ -10,32 +10,39 @@ const hex2rgb = (hex:string) => {
 };
 
 const generateScssVariables = () => {
-  const primary700Hex = process.env.PRIMARY700 || '';
-  const secondary700Hex = process.env.SECONDARY700 || '';
+  const primary700Hex = process.env.PRIMARY700 || '12 121 146';
+  const secondary700Hex = process.env.SECONDARY700 || '165 105 3';
 
-  if (!primary700Hex) {
-    throw new Error('PRIMARY700 environment variable is not set.');
+  let scssContent = '';
+
+  const isRgb = (value : string)  => /^\d{1,3}\s\d{1,3}\s\d{1,3}$/.test(value);
+
+  if (isRgb(primary700Hex)) {
+    scssContent += `$primary700: ${primary700Hex};\n`;
+  } else {
+    const primary700 = parse(primary700Hex);
+
+    if (!primary700) {
+      throw new Error('Failed to parse PRIMARY700 hex value.');
+    }
+
+    const parsedMain = hex2rgb(primary700Hex);
+    scssContent += `$primary700: ${parsedMain.r} ${parsedMain.g} ${parsedMain.b};\n`;
   }
 
-  if (!secondary700Hex) {
-    throw new Error('SECONDARY700 environment variable is not set.');
+  if (isRgb(secondary700Hex)) {
+    scssContent += `$secondary700: ${secondary700Hex};`;
+  } else {
+    const secondary700 = parse(secondary700Hex);
+
+    if (!secondary700) {
+      throw new Error('Failed to parse SECONDARY700 hex value.');
+    }
+
+    const parsedSecond = hex2rgb(secondary700Hex);
+    scssContent += `$secondary700: ${parsedSecond.r} ${parsedSecond.g} ${parsedSecond.b};`;
   }
 
-  const primary700 = parse(primary700Hex);
-  const secondary700 = parse(secondary700Hex);
-
-  if (!primary700) {
-    throw new Error('Failed to parse PRIMARY700 hex value.');
-  }
-
-  if (!secondary700) {
-    throw new Error('Failed to parse SECONDARY700 hex value.');
-  }
-
-  const parsedMain = hex2rgb(primary700Hex);
-  const parsedSecond = hex2rgb(secondary700Hex);
-  
-  const scssContent = `$primary700: ${parsedMain.r} ${parsedMain.g} ${parsedMain.b};\n$secondary700: ${parsedSecond.r} ${parsedSecond.g} ${parsedSecond.b};`;
   const scssVariablesDir = path.resolve(__dirname, '../assets');
   const scssVariablesFilePath = path.resolve(scssVariablesDir, '_variables.scss');
 
