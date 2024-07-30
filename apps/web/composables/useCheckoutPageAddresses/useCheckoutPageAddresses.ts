e42024188cd00a7e8339a527c03b66354337efcc
Maximilian Röll <30629157+maxiroellplenty@users.noreply.github.com>
2024-07-30 17:12:01 +0200

    feat: handle initial checkout address logic in composable

--- apps/web/composables/useCheckoutPageAddresses/useCheckoutPageAddresses.ts
+++ apps/web/composables/useCheckoutPageAddresses/useCheckoutPageAddresses.ts
@@ -1,10 +1,11 @@
-import { AddressType } from "@plentymarkets/shop-api";
+import { AddressType, cartGetters, userAddressGetters } from "@plentymarkets/shop-api";
 
 export const useCheckoutPageAddresses = () => {
-    const { onCreate: onShippingCreate, onSet: onSetShipping, unsubscribeAll } = useAddressStore(AddressType.Shipping);
-    const { onCreate: onBillingCreate } = useAddressStore(AddressType.Billing);
-    
-    onNuxtReady( async () => {
+    const { onCreate: onShippingCreate, onSet: onSetShipping, unsubscribeAll, get: getShipping } = useAddressStore(AddressType.Shipping);
+    const { onCreate: onBillingCreate, onSet: onSetBilling, get: getBilling } = useAddressStore(AddressType.Billing);
+    const { data: cart } = useCart();
+
+    onNuxtReady(async () => {
         const { fetchServer: fetchShipping } = useFetchAdddress(AddressType.Shipping);
         fetchShipping();
         const { fetchServer: fetchBilling } = useFetchAdddress(AddressType.Billing);
@@ -18,9 +19,41 @@ export const useCheckoutPageAddresses = () => {
 
     onSetShipping((event) => {
         const { set: setCheckoutAddress } = useCheckoutAddress(AddressType.Shipping);
-        setCheckoutAddress(event.payload[0], true);
+        const cartShippingAddressId = cartGetters.getCustomerShippingAddressId(cart.value);
+        const primaryAddress = userAddressGetters.getDefault(event.payload);
+
+        if (event.payload[0]) {
+            const address = ref(event.payload[0]);
+            const cartAddress = ref();
+
+            if (cartShippingAddressId) {
+                cartAddress.value = getShipping(cartShippingAddressId);
+            }
+
+            address.value = cartAddress.value ?? primaryAddress ?? event.payload[0];
+            setCheckoutAddress(address.value, true);
+        }
     });
-    
+
+    onSetBilling((event) => {
+        const { set: setCheckoutAddress } = useCheckoutAddress(AddressType.Billing);
+        const cartBillingAddressId = cartGetters.getCustomerInvoiceAddressId(cart.value);
+        const primaryAddress = userAddressGetters.getDefault(event.payload);
+
+        if (event.payload[0]) {
+            const address = ref(event.payload[0]);
+            const cartAddress = ref();
+
+            if (cartBillingAddressId) {
+                cartAddress.value = getBilling(cartBillingAddressId);
+            }
+
+            address.value = cartAddress.value ?? primaryAddress ?? event.payload[0];
+            setCheckoutAddress(address.value, true);
+        }
+
+    });
+
     onShippingCreate((event) => {
         const { set: setCheckoutAddress } = useCheckoutAddress(AddressType.Shipping);
         setCheckoutAddress(event.payload[0]);
