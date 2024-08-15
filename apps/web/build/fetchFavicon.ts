4c0082b425993bb020fdddcd2adab28df887fb1f
ksted <kevin.stederoth@plentymarkets.com>
2024-08-15 13:07:33 +0200

    refactor: create fetch file helper

--- apps/web/build/fetchFavicon.ts
+++ apps/web/build/fetchFavicon.ts
@@ -1,10 +1,9 @@
-import axios from 'axios';
-import fs from 'node:fs';
 import path from 'node:path';
+import fetchFile from './helpers/fetchFileHelper';
 import { ConfigItem } from './types/ConfigItem';
 
 const fetchFavicon = async (data: { [key: string]: Array<ConfigItem> }) => {
-  const faviconUrl = data['store'].find((setting: ConfigItem) => setting.key === 'favicon');
+  const faviconUrl = data['store'].find((setting: ConfigItem) => setting.key === 'favIcon');
 
   if (!faviconUrl?.value) {
     console.error('FavIcon URL not found.');
@@ -16,23 +15,12 @@ const fetchFavicon = async (data: { [key: string]: Array<ConfigItem> }) => {
     return;
   }
 
-  try {
-    // eslint-disable-next-line no-console
-    console.log('Fetching favicon from:', faviconUrl.value);
+  // eslint-disable-next-line no-console
+  console.log('Fetching favicon from:', faviconUrl.value);
 
-    const response = await axios({
-      url: faviconUrl.value,
-      method: 'GET',
-      responseType: 'arraybuffer',
-    });
+  const icoPath = path.resolve(__dirname, `../public/favicon.ico`);
 
-    const imageBuffer = response.data;
-    const icoPath = path.resolve(__dirname, `../public/favicon.ico`);
-
-    fs.writeFileSync(icoPath, imageBuffer);
-  } catch (error) {
-    console.error('Error during the request:', error);
-  }
+  fetchFile(faviconUrl.value, icoPath);
 };
 
 export default fetchFavicon;
--- apps/web/build/fetchLogo.ts
+++ apps/web/build/fetchLogo.ts
@@ -1,6 +1,5 @@
-import axios from 'axios';
-import fs from 'node:fs';
 import path from 'node:path';
+import fetchFile from './helpers/fetchFileHelper';
 import { ConfigItem } from './types/ConfigItem';
 
 const fetchLogo = async (data: { [key: string]: Array<ConfigItem> }) => {
@@ -11,24 +10,13 @@ const fetchLogo = async (data: { [key: string]: Array<ConfigItem> }) => {
     return;
   }
 
-  try {
-    // eslint-disable-next-line no-console
-    console.log('Fetching logo from:', logoUrl.value);
+  // eslint-disable-next-line no-console
+  console.log('Fetching logo from:', logoUrl.value);
 
-    const response = await axios({
-      url: logoUrl.value,
-      method: 'GET',
-      responseType: 'arraybuffer',
-    });
+  const fileType = logoUrl.value.split('.').pop();
+  const logoPath = path.resolve(__dirname, `../public/logo.${fileType}`);
 
-    const imageBuffer = response.data;
-    const fileType = logoUrl.value.split('.').pop();
-    const logoPath = path.resolve(__dirname, `../public/logo.${fileType}`);
-
-    fs.writeFileSync(logoPath, imageBuffer);
-  } catch (error) {
-    console.error('Error during the request:', error);
-  }
+  fetchFile(logoUrl.value, logoPath);
 };
 
 export default fetchLogo;
