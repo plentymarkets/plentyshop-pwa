4364ed471763b624ea0f722fee9da03a120ba18a
rbedeoan-plenty <raul.bedeoan@plentymarkets.com>
2024-08-26 13:43:44 +0300

    fix: display file property

--- apps/web/components/VariationPropertyFile/VariationPropertyFile.vue
+++ apps/web/components/VariationPropertyFile/VariationPropertyFile.vue
@@ -3,11 +3,9 @@
     <div class="font-semibold">
       {{ productPropertyGetters.getPropertyName(variationProperty) }}
     </div>
-    <div>
-      <button @click="downloadPropertyFile">
-        {{ productPropertyGetters.getPropertyValue(variationProperty) }}
-      </button>
-    </div>
+    <a target="_blank" :href="productPropertyGetters.getPropertyDownloadLink(variationProperty)">
+      {{ productPropertyGetters.getPropertyValue(variationProperty) }}
+    </a>
   </div>
 </template>
 
@@ -15,16 +13,4 @@
 import { productPropertyGetters } from '@plentymarkets/shop-api';
 import type { VariationPropertyFileProps } from './types';
 const props = defineProps<VariationPropertyFileProps>();
-const variationProperty = props.variationProperty;
-// const downloadLink = `https://s3-eu-central-1.amazonaws.com/plentymarkets-public-92/mevofvd5omld/propertyItems/${productPropertyGetters.getPropertyValue(productProperty)}`
-const { downloadFile } = useProductOrderProperties();
-const loading = ref(false);
-const downloadPropertyFile = async () => {
-  if (loading.value) {
-    return;
-  }
-  loading.value = true;
-  await downloadFile(productPropertyGetters.getPropertyValue(variationProperty));
-  loading.value = false;
-};
 </script>
