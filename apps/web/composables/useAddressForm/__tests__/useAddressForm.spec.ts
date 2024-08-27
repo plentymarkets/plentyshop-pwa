5aff4dae63ccbda169af72b70be49d6619a7c7ce
Maximilian Röll <30629157+maxiroellplenty@users.noreply.github.com>
2024-08-27 14:55:50 +0200

    fix: useAddressForm spec

--- apps/web/composables/useAddressForm/__tests__/useAddressForm.spec.ts
+++ apps/web/composables/useAddressForm/__tests__/useAddressForm.spec.ts
@@ -1,4 +1,59 @@
+import { mockNuxtImport } from "@nuxt/test-utils/runtime";
 import { AddressType } from "@plentymarkets/shop-api";
 
+const addressFixture = {
+    id: 1, 
+    name1: 'Test',
+    firstName: 'Max',
+    lastName: 'Power',
+    streetName: 'Test street',
+    apartment: '1',
+    city: 'Kassel',
+    country: '1',
+    zipCode: '12345',
+    phoneNumber: '1231244534654',
+    primary: 0
+};
+
+const { useCreateAddress } = vi.hoisted(() => {
+    return {
+        useCreateAddress: vi.fn().mockReturnValue({
+            create: vi.fn()
+        })
+    }
+});
+
+
+mockNuxtImport('useCreateAddress', () => {
+    return useCreateAddress;
+});
+
 describe('useAddressForm', () => {
+
+    afterAll(() => {
+        clearNuxtState();
+    });
+
+    it('should create address', () => {
+        const createSpy = vi.fn();
+        useCreateAddress.mockImplementation(() => {
+            return {
+                create: createSpy
+            }
+    
+        });
+
+        const { save, addressToSave } = useAddressForm(AddressType.Billing);
+        addressToSave.value = addressFixture;
+        
+        save();
+        expect(createSpy).toHaveBeenCalledWith(addressFixture);
+
+    });
+    
+    it('should return if the address to save is empty', () => {
+        const { save } = useAddressForm(AddressType.Billing);
+        const result = save();
+        expect(result).toBeTruthy();
+    })
 });
