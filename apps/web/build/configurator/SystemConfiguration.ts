ae5bcf0130537abf8e8ecbfc040d0a5df479748c
Kevin Stederoth <43753494+ksted@users.noreply.github.com>
2024-09-03 13:42:01 +0200

    fix: add color fallback values

--- apps/web/build/configurator/SystemConfiguration.ts
+++ apps/web/build/configurator/SystemConfiguration.ts
@@ -34,8 +34,8 @@ export class SystemConfiguration {
 
   getBaseColors(): BaseColors {
     return {
-      primary: this.findValueInResponseByKey('styling', 'primary'),
-      secondary: this.findValueInResponseByKey('styling', 'secondary'),
+      primary: this.findValueInResponseByKey('styling', 'primary') ?? '#0c7992',
+      secondary: this.findValueInResponseByKey('styling', 'secondary') ?? '#008ebd',
     };
   }
 
