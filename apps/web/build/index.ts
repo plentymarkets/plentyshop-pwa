366a9f16ffa6475a2f59d32a6c8a1f307d9545b2
Kevin Stederoth <43753494+ksted@users.noreply.github.com>
2024-09-03 08:28:00 +0200

    refactor: encapsulate index script in main function

--- apps/web/build/index.ts
+++ apps/web/build/index.ts
@@ -12,18 +12,24 @@ dotenv.config({
   path: path.resolve(__dirname, '../.env'),
 });
 
-if (process.env.FETCH_REMOTE_CONFIG === '1') {
-  console.log('Fetching remote configuration...');
-  const systemConfiguration = new SystemConfiguration();
-  await systemConfiguration.fetch();
+const main = async () => {
+  if (process.env.FETCH_REMOTE_CONFIG === '1') {
+    console.log('Fetching remote configuration...');
+    const systemConfiguration = new SystemConfiguration();
+    await systemConfiguration.fetch();
 
-  const appConfigurator = new AppConfigurator();
-  appConfigurator.generateEnvironment(systemConfiguration.getResponse());
-  appConfigurator.generateScssVariables(systemConfiguration.getBaseColors());
+    const appConfigurator = new AppConfigurator();
+    appConfigurator.generateEnvironment(systemConfiguration.getResponse());
+    appConfigurator.generateScssVariables(systemConfiguration.getBaseColors());
 
-  const assetDownloader = new AssetDownloader();
-  await assetDownloader.downloadFavicon(systemConfiguration.getFaviconUrl());
-  await assetDownloader.downloadLogo(systemConfiguration.getLogoUrl());
-} else {
-  console.warn(`Fetching PWA settings is disabled! Set FETCH_REMOTE_CONFIG in .env file.`);
-}
+    const assetDownloader = new AssetDownloader();
+    await assetDownloader.downloadFavicon(systemConfiguration.getFaviconUrl());
+    await assetDownloader.downloadLogo(systemConfiguration.getLogoUrl());
+  } else {
+    console.warn(`Fetching PWA settings is disabled! Set FETCH_REMOTE_CONFIG in .env file.`);
+  }
+};
+
+await main().catch((error: unknown) => {
+  console.error('An error occurred:', error);
+});
