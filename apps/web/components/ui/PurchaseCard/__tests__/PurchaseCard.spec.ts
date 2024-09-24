9ecd45ccf4f892e3089e83a70b5f00f0dc053330
plentydev <iulian.vladu@plentymarkets.com>
2024-09-24 15:15:03 +0300

    remove unused average vars

--- apps/web/components/ui/PurchaseCard/__tests__/PurchaseCard.spec.ts
+++ apps/web/components/ui/PurchaseCard/__tests__/PurchaseCard.spec.ts
@@ -1,6 +1,6 @@
 import { mount } from '@vue/test-utils';
 import { UiPurchaseCard }from '#components';
-import type {ReviewAverage, ReviewCounts} from '@plentymarkets/shop-api';
+import type { ReviewCounts } from '@plentymarkets/shop-api';
 import { ProductMock } from '../../../../__tests__/__mocks__/product.mock';
 
 describe('<PurchaseCard />', () => {
