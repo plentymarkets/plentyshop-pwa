78810104a721176b22d6fcfcc5927a1e7cb5bd75
Fabian Gerke <124085586+FabianGerke@users.noreply.github.com>
2024-10-08 13:53:06 +0200

    chore: update paypal color to gold

--- apps/web/components/PayPal/PayPalExpressButton.vue
+++ apps/web/components/PayPal/PayPalExpressButton.vue
@@ -93,7 +93,7 @@ const renderButton = (fundingSource: FUNDING_SOURCE) => {
       style: {
         layout: 'vertical',
         label: props.type === TypeCartPreview || props.type === TypeSingleItem ? 'checkout' : 'buynow',
-        color: 'blue',
+        color: 'gold',
       },
       fundingSource: fundingSource,
       async onClick(data, actions) {
