a8c3292158d2a60f7a9712f68ac73377e4579cb3
aoltean-plenty <alexandru.oltean@plentysystems.com>
2024-10-17 03:25:23 -0700

    Remove research

--- apps/web/components/SwiperControls/SwiperControls.vue
+++
@@ -1,8 +0,0 @@
-<!-- <template>
-  <button @click="swiper.slideNext()">Next</button>
-  <button @click="swiper.slidePrev()">Previous</button>
-</template>
-
-<script setup lang="ts">
-const swiper = useSwiper();
-</script> -->
