bcf212f3add35b4bb1675379816b0bac94a5b922
aoltean-plenty <alexandru.oltean@plentysystems.com>
2024-10-18 02:43:54 -0700

    Mobile

--- apps/web/components/ui/HeroContent/HeroContent.vue
+++ apps/web/components/ui/HeroContent/HeroContent.vue
@@ -9,16 +9,17 @@
         class="h-full object-cover object-left md:h-full md:object-contain"
       />
     </div>
+
     <div class="p-4 md:p-10 md:max-w-[768px] md:flex md:flex-col md:justify-center md:items-start md:basis-2/4">
       <p class="typography-text-xs md:typography-text-sm font-bold tracking-widest text-neutral-500 uppercase">
         {{ heroItemProps.tagline }}
       </p>
       <h1
-        class="typography-display-2 md:typography-display-1 md:leading-[67.5px] font-bold text-center md:text-left mt-2 mb-4"
+        class="typography-display-3 md:typography-display-1 md:leading-[67.5px] font-bold text-xs text-center md:text-left mt-2 mb-4"
       >
         {{ heroItemProps.heading }}
       </h1>
-      <p class="typography-text-base md:typography-text-lg text-center md:text-left font-normal">
+      <p class="typography-text-sm md:typography-text-lg text-center md:text-left font-normal">
         {{ heroItemProps.description }}
       </p>
       <div class="flex flex-col md:flex-row gap-4 mt-6">
