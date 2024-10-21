4d91ec610b959dc64672152c55f29cfdae6e40ab
aoltean-plenty <alexandru.oltean@plentysystems.com>
2024-10-21 04:31:03 -0700

    Draft #1

--- apps/web/components/ui/VsfLogo.vue
+++ apps/web/components/ui/VsfLogo.vue
@@ -1,10 +1,21 @@
 <template>
   <picture>
-    <NuxtImg :src="imagePath" alt="plentyShop PWA Logo" class="w-100 h-10 py-2" width="150" height="40" preload />
+    <NuxtImg ref="logo" :src="imagePath" alt="plentyShop PWA Logo" class="py-2 w-100 h-auto logoImg img-fluid" />
   </picture>
 </template>
 
 <script setup lang="ts">
 const imageExtension = useRuntimeConfig().public.headerLogo.split('.').pop();
 const imagePath = '/images/logo.' + imageExtension;
+
+if (process.client) {
+  const img = new Image();
+  img.src = imagePath;
+
+  img.onload = function () {
+    const width = img.width;
+    const height = img.height;
+    console.log(`Width: ${width}, Height: ${height}`);
+  };
+}
 </script>
