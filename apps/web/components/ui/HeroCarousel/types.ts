9902e02da598ac07d5ecdd0feb864c8311b59b20
oivan-plenty <oana.ivan@plentysystems.com>
2024-10-23 15:40:55 +0300

    wip

--- apps/web/components/ui/HeroCarousel/types.ts
+++ apps/web/components/ui/HeroCarousel/types.ts
@@ -3,11 +3,9 @@ export type Size = {
   height: string;
 };
 
-export type Sizes = {
-  lg: Size;
-  md: Size;
-  sm: Size;
-};
+export type SizeKey = 'lg' | 'md' | 'sm' | 'xs';
+
+export type Sizes = Record<SizeKey, { width: string; height: string }>;
 
 export type HeroItem = {
   image: string;
