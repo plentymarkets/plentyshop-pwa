0610f9dd84d397aed434852f9e094348efe01cef
aoltean-plenty <alexandru.oltean@plentysystems.com>
2024-10-23 05:42:49 -0700

    test

--- apps/web/components/ui/VsfLogo.vue
+++ apps/web/components/ui/VsfLogo.vue
@@ -1,51 +1,25 @@
 <template>
   <picture>
-    <img id="logo" :src="imagePath" alt="Header Logo" style="width: auto; height: auto; object-fit: contain" />
+    <img id="logo" :src="imagePath" alt="Header Logo" style="object-fit: contain" />
   </picture>
 </template>
 
-<!--<script setup lang="ts">-->
-<!--const imageExtension = useRuntimeConfig().public.headerLogo.split('.').pop();-->
-<!--const imagePath = '/images/logo.' + imageExtension;-->
+<script setup lang="ts">
+import { onMounted } from 'vue';
 
-<!--if (process.client) {-->
-<!--  const img = new Image();-->
-<!--  img.src = imagePath;-->
-
-<!--  img.onload = function () {-->
-<!--    const width = img.width;-->
-<!--    const height = img.height;-->
-<!--    console.log(`Width: ${width}, Height: ${height}`);-->
-<!--  };-->
-<!--}-->
-<!--</script>-->
-
- <script setup lang="ts">
 const imageExtension = useRuntimeConfig().public.headerLogo.split('.').pop();
 const imagePath = '/images/logo.' + imageExtension;
 
 if (process.client) {
-  const img = new Image();
-  img.src = imagePath;
-
-  img.onload = function () {
+  const setLogoDimensions = (img: HTMLImageElement) => {
     const intrinsicWidth = img.width;
     const intrinsicHeight = img.height;
     const aspectRatio = intrinsicWidth / intrinsicHeight;
 
-    // Adjust width based on aspect ratio
-    let desiredWidth;
-    if (aspectRatio > 1.5) {
-      // Wider logos get a smaller width
-      desiredWidth = 200;
-    } else if (aspectRatio < 1) {
-      // Taller logos can take a larger width
-      desiredWidth = 150;
-    } else {
-      // For square-like logos, use the default width
-      desiredWidth = 150;
-    }
+    console.log(`Aspect Ratio: ${aspectRatio}`);
 
+    // Desired width logic
+    const desiredWidth = aspectRatio > 1.5 ? 250 : 150;
     const newHeight = desiredWidth / aspectRatio;
 
     console.log(`Original Width: ${intrinsicWidth}, Original Height: ${intrinsicHeight}`);
@@ -58,5 +32,12 @@ if (process.client) {
       logoElement.style.height = `${newHeight}px`;
     }
   };
+
+  onMounted(() => {
+    const img = new Image();
+    img.src = imagePath;
+
+    img.onload = () => setLogoDimensions(img);
+  });
 }
 </script>
