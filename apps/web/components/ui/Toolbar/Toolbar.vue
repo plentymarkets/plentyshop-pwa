5ec6739d15ddf662b6605099e2a1e95a111102d9
aoltean-plenty <alexandru.oltean@plentysystems.com>
2024-10-31 04:36:29 -0700

    Add Translations

--- apps/web/components/ui/Toolbar/Toolbar.vue
+++ apps/web/components/ui/Toolbar/Toolbar.vue
@@ -2,11 +2,13 @@
   <div class="sticky top-0 bg-[#FDD835] py-2 z-50">
     <div class="flex justify-center md:justify-end pr-5 space-x-2">
       <UiButton variant="secondary" :size="buttonSize" class="self-start" @click="toggleEdit">
-        {{ isEditing ? 'Stop Editing' : 'Edit' }}
+        {{ isEditing ? $t('editMode.stopEdit') : $t('editMode.edit') }}
       </UiButton>
-      <UiButton variant="secondary" :size="buttonSize" class="self-start" @click="$emit('on-click')"> Save </UiButton>
       <UiButton variant="secondary" :size="buttonSize" class="self-start" @click="$emit('on-click')">
-        Publish
+        {{ $t('editMode.save') }}
+      </UiButton>
+      <UiButton variant="secondary" :size="buttonSize" class="self-start" @click="$emit('on-click')">
+        {{ $t('editMode.publish') }}
       </UiButton>
     </div>
   </div>
