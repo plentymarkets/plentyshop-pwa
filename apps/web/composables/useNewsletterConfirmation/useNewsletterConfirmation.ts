f6551718617f70caa4f6bb0e8ce57411aa648cb7
Petrisor Frincu <petrisor.frincu@plentysystems.com>
2024-11-06 13:12:21 +0200

    Update accordingly to PR

--- apps/web/composables/useNewsletterConfirmation/useNewsletterConfirmation.ts
+++ apps/web/composables/useNewsletterConfirmation/useNewsletterConfirmation.ts
@@ -1,6 +1,7 @@
 import { EmailConfirmationOptin, UseNewsletterConfirmationReturn } from '~/composables/useNewsletterConfirmation/types';
 
 export const useNewsletterConfirmation: UseNewsletterConfirmationReturn = () => {
+  // eslint-disable-next-line unicorn/consistent-function-scoping
   const confirmEmail: EmailConfirmationOptin = async (newsletterEmailId, authString) => {
     return await useSdk().plentysystems.doEmailConfirmation({
       newsletterEmailId,
