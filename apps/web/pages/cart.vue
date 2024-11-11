7cfb7fbee1e52b3371c666888d722d8bd6c835d7
Maximilian Röll <30629157+maxiroellplenty@users.noreply.github.com>
2024-11-11 15:38:45 +0100

    fix: empty cart message when user navigates to cart page

--- apps/web/pages/cart.vue
+++ apps/web/pages/cart.vue
@@ -53,7 +53,4 @@ const localePath = useLocalePath();
 const { isAuthorized } = useCustomer();
 const { data: cart, cartIsEmpty, loading } = useCart();
 const goToCheckout = () => (isAuthorized.value ? localePath(paths.checkout) : localePath(paths.guestLogin));
-onNuxtReady(() => {
-  if (cartIsEmpty.value) send({ type: 'neutral', message: t('emptyCartNotification') });
-});
 </script>
