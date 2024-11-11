ae8dcf2e74885b1b6cd42c4f5b2aa980302d1266
abocsan-plenty <129151096+abocsan-plenty@users.noreply.github.com>
2024-11-11 16:24:11 +0200

    feat: zoom in image design changes

--- apps/web/components/Drift/Drift.vue
+++ apps/web/components/Drift/Drift.vue
@@ -6,6 +6,7 @@
 
 <script setup lang="ts">
 import Drift from 'drift-zoom';
+import './Drift.scss';
 import type { DriftProps } from '~/components/Drift/types';
 
 const { index } = defineProps<DriftProps>();
@@ -22,58 +23,8 @@ onMounted(() => {
     inlinePane: 375,
     handleTouch: true,
     touchDelay: 200,
+    inlineOffsetY: -85,
+    injectBaseStyles: true, // Ensure base styles are injected
   });
 });
 </script>
-
-<style scoped>
-.detail {
-  position: relative;
-  width: 65%;
-  margin-left: 5%;
-  float: left;
-}
-
-@media (max-width: 610px) {
-  .detail,
-  .demo-trigger {
-    float: none;
-  }
-
-  .demo-trigger {
-    display: block;
-    width: 50%;
-    max-width: 200px;
-    margin: 0 auto;
-  }
-
-  .detail {
-    margin: 0;
-    width: auto;
-  }
-
-  p {
-    margin: 0 auto 1em;
-  }
-
-  .responsive-hint {
-    display: none;
-  }
-  h3 {
-    margin-top: 20px;
-  }
-}
-</style>
-
-<style>
-.drift-zoom-pane {
-  background: white;
-}
-
-.drift-bounding-box {
-  border: none !important;
-  background: rgba(0, 0, 0, 0.5) !important;
-  width: 160px !important;
-  height: 160px !important;
-}
-</style>
