2f24da9b6822ede743718696ac143b40f4fe5b05
Maximilian Röll <30629157+maxiroellplenty@users.noreply.github.com>
2024-11-19 16:20:36 +0100

    docs: useCookieConsent

--- apps/web/composables/useCookieConsent/useCookieConsent.ts
+++ apps/web/composables/useCookieConsent/useCookieConsent.ts
@@ -1,8 +1,16 @@
+/**
+ * @description Helper function to read and react to a specific cookie registered in the cookiebar
+ * @param name name of the cookie registered in the cookie bar apps/web/configuration/cookie.config.ts
+ * @example const { consent } = useCookieConsent('CookieBar.functional.cookies.payPal.name');
+ */
 export const useCookieConsent = (name: string) => {
   const state = useState('useCookieConsent_' + name, () => ({
     consent: false,
   }));
 
+  /**
+   * @description Function to accept the specific cookie outside of the cookiebar
+   */
   const accept = () => {
     const { data, setConsent } = useReadCookieBar();
 
