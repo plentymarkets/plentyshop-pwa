0f5398c981873ade6cb64e2c586e94f01998e9ce
cristian sandru <cristian.sandru@plentysystems.com>
2024-11-19 16:04:40 +0200

    code refactor

--- apps/web/composables/useCartTotalChange/useCartTotalChange.ts
+++ apps/web/composables/useCartTotalChange/useCartTotalChange.ts
@@ -29,7 +29,7 @@ export const useCartTotalChange = () => {
     if (restrictedAddresses.value) {
       state.value.changedTotal =
         cartGetters.getTotals(customerData.value.basket).total.toString() !== state.value.initialTotal;
-      if (state.value.changedTotal) await getCart();
+      await getCart();
     }
   };
 
