828568fa002dbfe83dd492d6f291abce235245cf
Cristi Sandru <149154151+csandru-plenty@users.noreply.github.com>
2024-11-20 16:31:08 +0200

    feat: handle blocked payment method (#809)
    * feat: handle blocked payment method
    
    * feat: handle blocked payment method
    
    * feat: handle blocked payment method
    
    * feat: handle blocked payment method
    
    * feat: handle blocked payment method
    
    * sdk bump
    
    * fix: what if the first array entry payment method is blocked
    
    * chore: linting
    
    ---------
    
    Co-authored-by: Maximilian Röll <30629157+maxiroellplenty@users.noreply.github.com>

--- apps/web/composables/useCheckoutPagePaymentAndShipping/useCheckoutPagePaymentAndShipping.ts
+++ apps/web/composables/useCheckoutPagePaymentAndShipping/useCheckoutPagePaymentAndShipping.ts
@@ -1,30 +1,65 @@
-import { shippingProviderGetters } from '@plentymarkets/shop-api';
+import {
+  PaymentMethod,
+  paymentProviderGetters,
+  ShippingMethod,
+  shippingProviderGetters,
+} from '@plentymarkets/shop-api';
 import { scrollToHTMLObject } from '~/utils/scollHelper';
 
 const ID_SHIPPING_CHECKBOX = '#shipping-agreement-checkbox';
 
+const getFallbackPaymentMethod = (
+  paymentMethods: PaymentMethod[],
+  selectedShippingMethod: ShippingMethod | undefined,
+) => {
+  return paymentMethods.find((method) => {
+    return (
+      !paymentProviderGetters.isPaymentMethodExcluded(selectedShippingMethod, method.id) &&
+      !paymentProviderGetters.isPaymentMethodUnavailable(paymentMethods, method.id)
+    );
+  });
+};
+
 export const useCheckoutPagePaymentAndShipping = () => {
+  const { $i18n } = useNuxtApp();
+  const { send } = useNotification();
+  const { getCart, data: cart } = useCart();
   const { loading: loadPayment, data: paymentMethodData, fetchPaymentMethods, savePaymentMethod } = usePaymentMethods();
   const { shippingPrivacyAgreement, setShippingPrivacyAgreement, setShippingPrivacyAgreementErrors } =
     useAdditionalInformation();
-  const { selectedMethod } = useCartShippingMethods();
-  const { getCart, data: cart } = useCart();
 
   const {
     loading: loadShipping,
     data: shippingMethodData,
+    selectedMethod: selectedShippingMethod,
     getShippingMethods,
     saveShippingMethod,
   } = useCartShippingMethods();
 
   const paymentMethods = computed(() => paymentMethodData.value);
   const shippingMethods = computed(() => shippingProviderGetters.getShippingProviders(shippingMethodData.value));
-  const selectedPaymentId = computed(() => cart.value.methodOfPaymentId);
+  const selectedPaymentId = computed(() => cart?.value?.methodOfPaymentId || 0);
 
   const handleShippingMethodUpdate = async (shippingMethodId: string) => {
     await saveShippingMethod(Number(shippingMethodId));
     await fetchPaymentMethods();
     await getCart();
+
+    const isPaymentMethodExcluded = paymentProviderGetters.isPaymentMethodExcluded(
+      selectedShippingMethod.value,
+      selectedPaymentId.value,
+    );
+    const isPaymentMethodUnavailable = paymentProviderGetters.isPaymentMethodUnavailable(
+      paymentMethods.value.list,
+      selectedPaymentId.value,
+    );
+
+    if (isPaymentMethodExcluded || isPaymentMethodUnavailable) {
+      send({ message: $i18n.t('billing.methodChanged'), type: 'warning' });
+
+      const fallbackMethod = getFallbackPaymentMethod(paymentMethods.value.list, selectedShippingMethod.value);
+      if (fallbackMethod) await savePaymentMethod(fallbackMethod.id);
+    }
 
     setShippingPrivacyAgreement(false);
   };
@@ -35,8 +70,8 @@ export const useCheckoutPagePaymentAndShipping = () => {
   };
 
   const validateShippingTerms = () => {
-    const shouldAcceptAgreement = selectedMethod.value
-      ? shippingProviderGetters.getDataPrivacyAgreementHint(selectedMethod.value)
+    const shouldAcceptAgreement = selectedShippingMethod.value
+      ? shippingProviderGetters.getDataPrivacyAgreementHint(selectedShippingMethod.value)
       : false;
 
     if (shouldAcceptAgreement) {
