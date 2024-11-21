aafe6467de376cc16f07f5f0422bdc561453b9b5
aoltean-plenty <alexandru.oltean@plentysystems.com>
2024-11-21 03:46:55 -0800

    Put lazy hydrate back

--- apps/web/pages/index.vue
+++ apps/web/pages/index.vue
@@ -25,18 +25,16 @@
     </NuxtLazyHydrate>
 
     <div v-if="loadComponents" class="max-w-screen-3xl mx-auto md:px-6 lg:px-10 mb-10">
-      <template v-for="(item, index) in recommendedProductsCategories" :key="index">
-        <section class="mb-10 overflow-hidden">
-          <p data-testid="recommended-products" class="mb-4 typography-text-lg text-center md:text-left">
-            {{ item.headline }}
-          </p>
-          <ProductRecommendedProducts
-            cache-key="homepage"
-            :category-id="item.categoryId"
-            @data-fetched="onDataFetched"
-          />
-        </section>
-      </template>
+      <NuxtLazyHydrate when-visible>
+        <template v-for="(item, index) in recommendedProductsCategories" :key="index">
+          <section class="mb-10 overflow-hidden">
+            <p data-testid="recommended-products" class="mb-4 typography-text-lg text-center md:text-left">
+              {{ item.headline }}
+            </p>
+            <ProductRecommendedProducts cache-key="homepage" :category-id="item.categoryId" />
+          </section>
+        </template>
+      </NuxtLazyHydrate>
       <NuxtLazyHydrate when-visible>
         <NewsletterSubscribe v-if="showNewsletter" />
       </NuxtLazyHydrate>
@@ -70,7 +68,7 @@ onMounted(async () => {
 });
 
 function onDataFetched() {
-  console.log('Data fetched from ProductRecommendedProducts FROM debbug branch #1');
+  console.log('Data fetched from ProductRecommendedProducts FROM debbug branch #2');
   loadComponents.value = true;
 }
 </script>
