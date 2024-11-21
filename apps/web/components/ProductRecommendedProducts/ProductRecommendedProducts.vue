117e90defc30ad1b1f9622dd3eaf714c585f267d
clincu-plenty <cristian.lincu@plentysystems.com>
2024-11-21 16:09:34 +0200

    debug recommended products

--- apps/web/components/ProductRecommendedProducts/ProductRecommendedProducts.vue
+++ apps/web/components/ProductRecommendedProducts/ProductRecommendedProducts.vue
@@ -8,9 +8,10 @@ import type { ProductRecommendedProductsProps } from './types';
 const props = defineProps<ProductRecommendedProductsProps>();
 const { data: recommendedProducts, fetchProductRecommended } = useProductRecommended(props.categoryId + props.cacheKey);
 const loadComponent = ref(false);
-console.log('my cristian test', props, recommendedProducts, loadComponent.value);
-if (props.categoryId) {
+console.log('my cristian test 1', props, recommendedProducts, loadComponent.value);
+onMounted(async () => {
   await fetchProductRecommended(props.categoryId);
   loadComponent.value = true;
-}
+  console.log('my cristian test 2', props, recommendedProducts, loadComponent.value);
+});
 </script>
