e495faacf5832f17f8f5e51050ceabf3d25751f8
aoltean-plenty <alexandru.oltean@plentysystems.com>
2024-11-21 06:17:57 -0800

    debugg stuff

--- apps/web/pages/index.vue
+++ apps/web/pages/index.vue
@@ -25,16 +25,14 @@
     </NuxtLazyHydrate>
 
     <div v-if="loadComponents" class="max-w-screen-3xl mx-auto md:px-6 lg:px-10 mb-10">
-      <NuxtLazyHydrate when-visible>
-        <template v-for="(item, index) in recommendedProductsCategories" :key="index">
-          <section class="mb-10 overflow-hidden">
-            <p data-testid="recommended-products" class="mb-4 typography-text-lg text-center md:text-left">
-              {{ item.headline }}
-            </p>
-            <ProductRecommendedProducts cache-key="homepage" :category-id="item.categoryId" />
-          </section>
-        </template>
-      </NuxtLazyHydrate>
+      <template v-for="(item, index) in recommendedProductsCategories" :key="index">
+        <section class="mb-10 overflow-hidden">
+          <p data-testid="recommended-products" class="mb-4 typography-text-lg text-center md:text-left">
+            {{ item.headline }}
+          </p>
+          <ProductRecommendedProducts cache-key="homepage" :category-id="item.categoryId" />
+        </section>
+      </template>
       <NuxtLazyHydrate when-visible>
         <NewsletterSubscribe v-if="showNewsletter" />
       </NuxtLazyHydrate>
@@ -52,19 +50,14 @@ definePageMeta({ pageType: 'static', middleware: ['newsletter-confirmation'] });
 const { showNewsletter } = useNewsletter();
 
 const loadComponents = ref(false);
-const triggerReRender = ref(false);
 
 onMounted(async () => {
   await fetchPageTemplate();
   loadComponents.value = true;
   console.log('Test from branch');
+});
 
-  // Hacky
-  setTimeout(() => {
-    triggerReRender.value = true;
-    recommendedProductsCategories.value.forEach((category) => {
-      useProductRecommended(category.categoryId);
-    });
-  }, 1000);
+watchEffect(() => {
+  console.log('loadComponents:', loadComponents.value);
 });
 </script>
