d97e0edf062ad9e5ddb8d08be50a179604428061
abocsan-plenty <129151096+abocsan-plenty@users.noreply.github.com>
2024-11-21 18:17:55 +0200

    fix: recommended products display

--- apps/web/pages/index.vue
+++ apps/web/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <Editor v-if="isEditing" />
   <div v-else class="content">
-    <div v-if="loadComponents" class="max-w-screen-3xl mx-auto md:px-6 lg:px-10 mb-10">
+    <div v-if="!loading" class="max-w-screen-3xl mx-auto md:px-6 lg:px-10 mb-10">
       <UiHeroCarousel :hero-item-props="hero" />
     </div>
     <div v-else class="max-w-screen-3xl mx-auto md:px-6 lg:px-10 mb-10">
@@ -9,7 +9,7 @@
     </div>
 
     <NuxtLazyHydrate when-visible>
-      <div v-if="loadComponents" class="max-w-screen-3xl mx-auto md:px-6 lg:px-10 mb-10">
+      <div v-if="!loading" class="max-w-screen-3xl mx-auto md:px-6 lg:px-10 mb-10">
         <UiMediaCard
           v-for="(item, index) in mediaCard"
           :key="index"
@@ -24,23 +24,31 @@
       </div>
     </NuxtLazyHydrate>
 
-    <ProductRecommendedProducts cache-key="homepage" :category-id="'49'" />
+    <div v-if="!loading" class="max-w-screen-3xl mx-auto md:px-6 lg:px-10 mb-10">
+      <template v-for="(item, index) in recommendedProductsCategories" :key="index">
+        <section class="mb-10 overflow-hidden">
+          <p data-testid="recommended-products" class="mb-4 typography-text-lg text-center md:text-left">
+            {{ item.headline }}
+          </p>
+          <ProductRecommendedProducts cache-key="homepage" :category-id="item.categoryId" />
+        </section>
+      </template>
 
-    <NuxtLazyHydrate when-visible>
-      <NewsletterSubscribe v-if="showNewsletter" />
-    </NuxtLazyHydrate>
+      <NuxtLazyHydrate when-visible>
+        <NewsletterSubscribe v-if="showNewsletter" />
+      </NuxtLazyHydrate>
+    </div>
+    <div v-else class="max-w-screen-3xl mx-auto md:px-6 lg:px-10 mb-10">
+      <UiSkeletonLoader />
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup async>
 const { isEditing } = useEditor();
-const { hero, mediaCard, fetchPageTemplate } = useHomepage();
+const { hero, mediaCard, fetchPageTemplate, recommendedProductsCategories, loading } = useHomepage();
 definePageMeta({ pageType: 'static', middleware: ['newsletter-confirmation'] });
 const { showNewsletter } = useNewsletter();
 
-const loadComponents = ref(false);
-onMounted(async () => {
-  await fetchPageTemplate();
-  loadComponents.value = true;
-});
+await fetchPageTemplate();
 </script>
