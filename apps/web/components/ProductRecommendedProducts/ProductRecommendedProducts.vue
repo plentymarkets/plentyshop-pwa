01c126708b57d7b3efb273ee3b60d72abd3528b5
aoltean-plenty <alexandru.oltean@plentysystems.com>
2024-11-21 07:12:08 -0800

    Wait for products

--- apps/web/components/ProductRecommendedProducts/ProductRecommendedProducts.vue
+++ apps/web/components/ProductRecommendedProducts/ProductRecommendedProducts.vue
@@ -1,14 +1,24 @@
 <template>
-  <RecommendedProducts :products="recommendedProducts"></RecommendedProducts>
-  ?
+  <div v-if="recommendedProducts.length > 0">
+    <RecommendedProducts :products="recommendedProducts" />
+  </div>
+  <UiSkeletonLoader v-else />
 </template>
 
 <script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import { useProductRecommended } from '~/composables/useProductRecommended';
 import type { ProductRecommendedProductsProps } from './types';
 
 const props = defineProps<ProductRecommendedProductsProps>();
 const { data: recommendedProducts, fetchProductRecommended } = useProductRecommended(props.categoryId + props.cacheKey);
-if (props.categoryId) {
-  fetchProductRecommended(props.categoryId);
-}
+
+const loadProducts = ref(false);
+
+onMounted(async () => {
+  if (props.categoryId) {
+    await fetchProductRecommended(props.categoryId);
+    loadProducts.value = true;
+  }
+});
 </script>
