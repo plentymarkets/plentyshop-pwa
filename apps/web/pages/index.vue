d1c289f0640ab8d8d31e6692a844e5d71f5d83dc
abocsan-plenty <129151096+abocsan-plenty@users.noreply.github.com>
2024-11-21 17:49:45 +0200

    fix: recommended products display

--- apps/web/pages/index.vue
+++ apps/web/pages/index.vue
@@ -24,25 +24,13 @@
       </div>
     </NuxtLazyHydrate>
 
-    <div v-if="loadComponents" class="max-w-screen-3xl mx-auto md:px-6 lg:px-10 mb-10">
-      <template v-for="(item, index) in recommendedProductsCategories" :key="index">
-        <section class="mb-10 overflow-hidden">
-          <p data-testid="recommended-products" class="mb-4 typography-text-lg text-center md:text-left">
-            {{ item.headline }}
-          </p>
-          <ProductRecommendedProducts cache-key="homepage" :category-id="item.categoryId" />
-        </section>
-      </template>
-    </div>
-    <div v-else class="max-w-screen-3xl mx-auto md:px-6 lg:px-10 mb-10">
-      <UiSkeletonLoader />
-    </div>
+    <ProductRecommendedProducts cache-key="homepage" :category-id="'49'" />
   </div>
 </template>
 
 <script lang="ts" setup async>
 const { isEditing } = useEditor();
-const { hero, mediaCard, fetchPageTemplate, recommendedProductsCategories } = useHomepage();
+const { hero, mediaCard, fetchPageTemplate } = useHomepage();
 definePageMeta({ pageType: 'static', middleware: ['newsletter-confirmation'] });
 
 const loadComponents = ref(false);
