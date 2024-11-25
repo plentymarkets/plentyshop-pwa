6a1b83f3cd41adaf526587a3096633f8d30a5dec
cristian sandru <cristian.sandru@plentysystems.com>
2024-11-25 10:13:31 +0200

    fix: cookie anchor without an href

--- apps/web/components/PayPal/PayPalCookieDisabledBanner.vue
+++ apps/web/components/PayPal/PayPalCookieDisabledBanner.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="bg-disabled-100 p-5 rounded-md border-dashed border-2 border-disabled-300 text-center">
     <span class="text-neutral-500">{{ $t('paypal.cookieNotAccepted') }}</span>
-    <SfLink href="#" variant="secondary" class="cursor-pointer" @click="visible = true">open cookie settings</SfLink>
+    <SfLink href="#" variant="secondary" class="cursor-pointer" @click="visible = true">
+      {{ $t('paypal.openSettings') }}
+    </SfLink>
   </div>
 </template>
 
