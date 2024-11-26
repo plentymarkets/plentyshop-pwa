395bf92119b2e3177cd162f005a23feeda779be3
aoltean-plenty <alexandru.oltean@plentysystems.com>
2024-11-25 23:13:47 -0800

    Hero carousel not working

--- apps/web/components/ui/HeroCarousel/HeroCarousel.vue
+++ apps/web/components/ui/HeroCarousel/HeroCarousel.vue
@@ -8,7 +8,7 @@
       pagination
       @slide-change="onSlideChange"
     >
-      <SwiperSlide v-for="(heroItem, index) in heroItemProps" :key="index" class="md:px-7 lg:px-15">
+      <SwiperSlide v-for="(heroItem, index) in hero" :key="index" class="md:px-7 lg:px-15">
         <UiHeroContent :hero-item-props="heroItem" />
       </SwiperSlide>
     </Swiper>
@@ -16,17 +16,11 @@
 </template>
 
 <script setup lang="ts">
-import { HeroContentProps } from './types';
 import { Swiper, SwiperSlide } from 'swiper/vue';
 import { Navigation, Pagination } from 'swiper/modules';
+
+const { hero } = useHomepage();
 const { handleArrows, onSlideChange } = useCarousel();
-import '@/assets/libraries/swiper/swiper.min.css';
-import '@/assets/libraries/swiper/navigation.min.css';
-import '@/assets/libraries/swiper/pagination.min.css';
 
-const { heroItemProps } = defineProps<{
-  heroItemProps: HeroContentProps[];
-}>();
-
-const enableModules = computed(() => heroItemProps.length > 1);
+const enableModules = computed(() => hero.value.length > 1);
 </script>
---
+++ apps/web/pages/test.vue
@@ -0,0 +1,42 @@
+<template>
+  <template v-for="(block, index) in testEn.blocks" :key="index">
+    <component :is="getComponent(block.name)" v-bind="block.options" />
+  </template>
+</template>
+
+<script lang="ts" setup>
+import testEn from './testEn.json';
+
+const getComponent = (name: string) => {
+    console.log(`Resolving component: ${name}`);
+
+  if (name === 'UiSkeletonLoader') {
+    return resolveComponent('UiSkeletonLoader');
+  }
+
+  if (name === 'UiHeroCarousel') {
+    return resolveComponent('UiHeroCarousel');
+  }
+
+  if (name === 'UiMediaCard') {
+    return resolveComponent('UiMediaCard');
+  }
+
+  if (name === 'ProductRecommendedProducts') {
+    return resolveComponent('ProductRecommendedProducts');
+  }
+};
+
+// const template = [
+//   {
+//     name: 'UiSkeletonLoader',
+//     props: {},
+//   },
+//   {
+//     name: 'UiMediaCard',
+//     options: {
+//       text: 'Hello World',
+//     },
+//   },
+// ];
+</script>
