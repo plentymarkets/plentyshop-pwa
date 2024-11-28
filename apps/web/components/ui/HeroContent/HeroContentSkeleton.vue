86570e21b0333e25454f887acc8334e3ab1e5fa3
Kevin Stederoth <43753494+ksted@users.noreply.github.com>
2024-11-28 08:13:09 +0100

    fix: set separate hero skeleton heights for mobile and desktop

--- apps/web/components/ui/HeroContent/HeroContentSkeleton.vue
+++ apps/web/components/ui/HeroContent/HeroContentSkeleton.vue
@@ -3,7 +3,7 @@
     class="flex flex-col md:flex-row-reverse md:justify-center mx-auto mb-10 text-center animate-pulse p-4 border-neutral-200 border-b"
   >
     <div class="flex flex-col md:basis-2/4 md:items-stretch md:overflow-hidden">
-      <div class="h-[567px] bg-gray-300 rounded-sm"></div>
+      <div class="h-[376px] md:h-[500px] bg-gray-300 rounded-sm"></div>
     </div>
     <div class="p-4 md:p-10 md:max-w-[768px] md:flex md:flex-col md:justify-center md:items-start md:basis-2/4">
       <div class="h-6 sm:h-8 bg-gray-300 rounded-sm w-full mb-2"></div>
