95e9d2f31427f9efa8e1c984546e209ee97914dd
abocsan-plenty <129151096+abocsan-plenty@users.noreply.github.com>
2024-12-02 13:20:45 +0200

    fix: destroy zoom on unmounted

--- apps/web/components/Drift/Drift.vue
+++ apps/web/components/Drift/Drift.vue
@@ -9,11 +9,13 @@ import type { DriftProps } from '~/components/Drift/types';
 
 const { index } = defineProps<DriftProps>();
 
+let drift: Drift | null = null;
+
 onMounted(() => {
   let demoTrigger = document.querySelector(`.demo-trigger-${index}`) as HTMLElement;
   let paneContainer = document.querySelector('.drift-zoom-image') as HTMLElement;
 
-  new Drift(demoTrigger, {
+  drift = new Drift(demoTrigger, {
     paneContainer: paneContainer,
     containInline: true,
     zoomFactor: 2,
@@ -22,5 +24,12 @@ onMounted(() => {
     touchDelay: 300,
     injectBaseStyles: true,
   });
+});
+
+onUnmounted(() => {
+  if (drift) {
+    drift.destroy();
+    drift = null;
+  }
 });
 </script>
