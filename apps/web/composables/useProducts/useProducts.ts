930dafaaf2a794c0b43dd43e085cd3c29f019dd1
doproiescu-plenty <126688988+doproiescu-plenty@users.noreply.github.com>
2024-12-04 18:13:18 +0200

    feat: add login redirect on category rights

--- apps/web/composables/useProducts/useProducts.ts
+++ apps/web/composables/useProducts/useProducts.ts
@@ -1,7 +1,8 @@
-import { FacetSearchCriteria, Product } from '@plentymarkets/shop-api';
+import { categoryGetters, FacetSearchCriteria, Product } from '@plentymarkets/shop-api';
 import { Facet } from '@plentymarkets/shop-api';
 import { defaults, type SetCurrentProduct } from '~/composables';
 import { type FetchProducts, type UseProductsReturn, UseProductsState } from '~/composables/useProducts/types';
+import { paths } from '../../utils/paths';
 
 /**
  * @description Composable for managing products.
@@ -36,6 +37,7 @@ export const useProducts: UseProductsReturn = (category = '') => {
    */
   const fetchProducts: FetchProducts = async (params: FacetSearchCriteria) => {
     state.value.loading = true;
+    const localePath = useLocalePath();
 
     if (params.categoryUrlPath?.endsWith('.js')) return state.value.data;
 
@@ -44,6 +46,11 @@ export const useProducts: UseProductsReturn = (category = '') => {
     state.value.productsPerPage = params.itemsPerPage || defaults.DEFAULT_ITEMS_PER_PAGE;
 
     if (data.value?.data) {
+      if(categoryGetters.hasCustomerRight(data.value?.data.category)) {
+        await navigateTo(localePath(paths.authLogin));
+        return state.value.data;
+      }
+
       data.value.data.pagination.perPageOptions = defaults.PER_PAGE_STEPS;
       state.value.data = data.value.data;
     }
--- docs/changelog/changelog_en.md
+++ docs/changelog/changelog_en.md
@@ -14,6 +14,7 @@
 - Added cookie consent management helper functions read more at https://pwa-docs.plentymarkets.com/guide/how-to/cookie#read-and-react-to-a-registered-cookie.
 - Added dynamic structured data from the SEO config.
 - Added more manufacturer information to legal details drawer.
+- Add login redirect on category "after login" visibility.
 
 ### 👷 Changed
 
