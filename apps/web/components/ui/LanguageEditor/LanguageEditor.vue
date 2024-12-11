db8e88818288c9310f51c197fa7bb744ecd3d1fb
aoltean-plenty <alexandru.oltean@plentysystems.com>
2024-12-11 05:46:54 -0800

    Language Component

---
+++ apps/web/components/ui/LanguageEditor/LanguageEditor.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex items-center space-x-2">
+    <SfIconLanguage />
+    <select class="form-select focus:outline-none focus:ring-0 focus:border-transparent">
+      <option class="font-medium text-base">English</option>
+      <option class="font-medium text-base">German</option>
+    </select>
+  </div>
+</template>
+<script setup="ts">
+import { SfIconLanguage } from '@storefront-ui/vue';
+</script>
--- apps/web/components/ui/Toolbar/Toolbar.vue
+++ apps/web/components/ui/Toolbar/Toolbar.vue
@@ -1,30 +1,35 @@
 <template>
   <div class="sticky top-0 bg-white py-2 z-50">
-    <div class="flex justify-center md:justify-end pr-5 space-x-2">
-      <UiButton
-        variant="secondary"
-        :size="buttonSize"
-        class="self-start"
-        @click="toggleEdit"
-        :disabled="!isEditingEnabled"
-        data-testid="edit-preview-button"
-      >
-        {{ disableActions ? 'Preview' : 'Edit' }}
-      </UiButton>
-      <UiButton
-        variant="primary"
-        :size="buttonSize"
-        class="self-start"
-        :disabled="!isEditingEnabled || !isLocalTemplate()"
-        data-testid="edit-save-button"
-        @click="updatePageTemplate"
-      >
-        <template v-if="loading">
-          <SfLoaderCircular class="animate-spin w-4 h-4" />
-        </template>
+    <div class="relative flex items-center pr-5">
+      <div class="absolute left-1/2 transform -translate-x-1/2">
+        <UiLanguageEditor />
+      </div>
+      <div class="ml-auto flex space-x-2">
+        <UiButton
+          variant="secondary"
+          :size="buttonSize"
+          class="self-start"
+          @click="toggleEdit"
+          :disabled="!isEditingEnabled"
+          data-testid="edit-preview-button"
+        >
+          {{ disableActions ? 'Preview' : 'Edit' }}
+        </UiButton>
+        <UiButton
+          variant="primary"
+          :size="buttonSize"
+          class="self-start"
+          :disabled="!isEditingEnabled || !isLocalTemplate()"
+          data-testid="edit-save-button"
+          @click="updatePageTemplate"
+        >
+          <template v-if="loading">
+            <SfLoaderCircular class="animate-spin w-4 h-4" />
+          </template>
 
-        <template v-else> Save </template>
-      </UiButton>
+          <template v-else> Save </template>
+        </UiButton>
+      </div>
     </div>
   </div>
 </template>
