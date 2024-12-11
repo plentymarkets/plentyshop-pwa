666551893474afd0b30889a666920f1b6e5e4133
Cristi Sandru <149154151+csandru-plenty@users.noreply.github.com>
2024-12-11 11:01:18 +0200

    fix: cookiebar routing history (#884)

--- apps/web/components/PayPal/PayPalCookieDisabledBanner.vue
+++ apps/web/components/PayPal/PayPalCookieDisabledBanner.vue
@@ -2,8 +2,8 @@
   <div class="bg-disabled-100 p-5 rounded-md border-dashed border-2 border-disabled-300 text-center">
     <span class="text-neutral-500">
       <i18n-t keypath="paypal.cookieNotAccepted" scope="global">
-        <template #phrase
-          ><SfLink href="#" variant="secondary" class="cursor-pointer" @click="visible = true">
+        <template #phrase>
+          <SfLink @click.prevent="visible = true" tag="button" variant="secondary">
             {{ $t('paypal.openSettings') }}
           </SfLink>
         </template>
