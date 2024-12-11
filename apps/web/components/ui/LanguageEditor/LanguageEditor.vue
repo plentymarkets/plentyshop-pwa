925df665fd3bcb8b87e50512574bc7e328e5c38f
aoltean-plenty <alexandru.oltean@plentysystems.com>
2024-12-11 05:54:05 -0800

    Language logic

--- apps/web/components/ui/LanguageEditor/LanguageEditor.vue
+++ apps/web/components/ui/LanguageEditor/LanguageEditor.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="flex items-center space-x-2">
     <SfIconLanguage />
-    <select class="form-select focus:outline-none focus:ring-0 focus:border-transparent">
-      <option class="font-medium text-base">English</option>
-      <option class="font-medium text-base">German</option>
+    <select class="form-select focus:outline-none focus:ring-0 focus:border-transparent" v-model="currentLocale">
+      <option v-for="locale in localeCodes" :key="locale" :value="locale" class="font-medium text-base">
+        {{ $t(`lang.${locale}`) }}
+      </option>
     </select>
   </div>
 </template>
-<script setup="ts">
+
+<script setup lang="ts">
 import { SfIconLanguage } from '@storefront-ui/vue';
+import { useI18n } from 'vue-i18n';
+
+const { localeCodes, locale: currentLocale } = useI18n();
 </script>
