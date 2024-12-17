ca63c407238127289ca8f7ef08283eb33da18d98
aoltean-plenty <alexandru.oltean@plentysystems.com>
2024-12-16 23:54:55 -0800

    Minimum mobile styling

--- apps/web/components/ui/LanguageEditor/LanguageEditor.vue
+++ apps/web/components/ui/LanguageEditor/LanguageEditor.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="flex items-center space-x-2">
-    <SfIconLanguage />
+  <div class="flex items-center space-x-1 md:space-x-2">
+    <SfIconLanguage class="w-4 h-4 md:w-6 md:h-6" />
     <select
       @change="switchLanguage(currentLocale)"
-      class="form-select focus:outline-none focus:ring-0 focus:border-transparent"
+      class="form-select focus:outline-none focus:ring-0 focus:border-transparent text-sm md:text-base"
       v-model="currentLocale"
     >
-      <option v-for="locale in localeCodes" :key="locale" :value="locale" class="font-medium text-base">
+      <option v-for="locale in localeCodes" :key="locale" :value="locale" class="font-medium text-sm md:text-base">
         {{ $t(`lang.${locale}`) }}
       </option>
     </select>
--- apps/web/components/ui/Toolbar/Toolbar.vue
+++ apps/web/components/ui/Toolbar/Toolbar.vue
@@ -1,22 +1,26 @@
 <template>
-  <div class="sticky top-0 bg-white py-2 z-50">
+  <div class="sticky top-0 bg-white z-50">
     <div class="relative flex items-center pr-5">
+      <div class="">
+        <img :src="imagePath" alt="Brand Logo" ref="logo" preload class="h-[54px] w-[54px]" />
+      </div>
+
       <div class="absolute left-1/2 transform -translate-x-1/2">
         <UiLanguageEditor v-if="experimentalAddBlock" />
       </div>
       <div class="ml-auto flex space-x-2">
         <button
-          class="self-start text-[#062633] px-4 py-2 rounded-md font-inter font-medium text-base leading-6 flex items-center"
+          class="self-start text-[#062633] px-2 py-1 rounded-md font-inter font-medium text-sm leading-5 flex items-center md:px-4 md:py-2 md:text-base md:leading-6"
           @click="toggleEdit"
           :disabled="!isEditingEnabled"
           data-testid="edit-preview-button"
         >
           <template v-if="disableActions">
-            <SfIconVisibility class="mr-[10px]" />
+            <SfIconVisibility class="mr-[5px] md:mr-[10px]" />
             Preview
           </template>
           <template v-else>
-            <SfIconBase size="xs" viewBox="0 0 18 18" class="mr-[10px] fill-primary-900 cursor-pointer">
+            <SfIconBase size="xs" viewBox="0 0 18 18" class="mr-[5px] md:mr-[10px] fill-primary-900 cursor-pointer">
               <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path :d="editPath" fill="black" />
               </svg>
@@ -25,13 +29,13 @@
           </template>
         </button>
         <button
-          class="self-start bg-[#062633] text-white px-4 py-2 rounded-md font-inter font-medium text-base leading-6 flex items-center"
+          class="self-start bg-[#062633] text-white px-2 py-1 rounded-md font-inter font-medium text-sm leading-5 flex items-center md:px-4 md:py-2 md:text-base md:leading-6"
           :disabled="!isEditingEnabled || !isLocalTemplate()"
           data-testid="edit-save-button"
           @click="updatePageTemplate"
         >
           <template v-if="!loading">
-            <SfIconBase size="xs" class="mr-[10px]">
+            <SfIconBase size="xs" class="mr-[5px] md:mr-[10px]">
               <svg width="16" height="16" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path
                   d="M14 0H2C0.89 0 0 0.9 0 2V16C0 17.1 0.89 18 2 18H16C17.1 18 18 17.1 18 16V4L14 0ZM9 16C7.34 16 6 14.66 6 13C6 11.34 7.34 10 9 10C10.66 10 12 11.34 12 13C12 14.66 10.66 16 9 16ZM12 6H2V2H12V6Z"
@@ -53,12 +57,14 @@ import { SfLoaderCircular } from '@storefront-ui/vue';
 import { editPath } from 'assets/icons/paths/edit';
 import { SfIconBase } from '@storefront-ui/vue';
 import { SfIconVisibility } from '@storefront-ui/vue';
+const runtimeConfig = useRuntimeConfig();
 const { isEditingEnabled, disableActions } = useEditor();
+
+const imagePath = '/images/brand-logo.png';
 
 const { loading } = useHomepage();
 const { updatePageTemplate } = useUpdatePageTemplate();
 
-const runtimeConfig = useRuntimeConfig();
 const homepageCategoryId = runtimeConfig.public.homepageCategoryId;
 const experimentalAddBlock = runtimeConfig.public.experimentalAddBlock;
 
