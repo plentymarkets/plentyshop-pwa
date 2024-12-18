5b18fb0319c34dc31e61bd4b6137635aef38b15a
aoltean-plenty <alexandru.oltean@plentysystems.com>
2024-12-18 01:00:11 -0800

    More styling

--- apps/web/components/ui/BlockActions/BlockActions.vue
+++ apps/web/components/ui/BlockActions/BlockActions.vue
@@ -3,18 +3,6 @@
     class="absolute z-50 right-0 top-0 flex items-center space-x-4 border border-[#538AEA] bg-white p-2 shadow-md"
     data-testid="edit-block-actions"
   >
-    <button @click="triggerDelete" class="text-black hover:bg-gray-100 p-1 rounded" data-testid="delete-block-button">
-      <SfIconDelete />
-    </button>
-
-    <button class="text-black hover:bg-gray-100 p-1 rounded" data-testid="move-down-button">
-      <SfIconArrowUpward />
-    </button>
-
-    <button class="text-black hover:bg-gray-100 p-1 rounded" data-testid="move-up-button">
-      <SfIconArrowDownward />
-    </button>
-
     <button @click="triggerEdit" class="text-black hover:bg-gray-100 p-1 rounded" data-testid="open-editor-button">
       <SfIconBase size="xs" viewBox="0 0 18 18" class="fill-primary-900 cursor-pointer">
         <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -22,12 +10,27 @@
         </svg>
       </SfIconBase>
     </button>
+
+    <!-- Re add in when we reach the sorting PBI's Re. Add imports as well-->
+
+    <!-- <div class="w-px h-4 bg-gray-300"></div>
+    <button class="text-black hover:bg-gray-100 p-1 rounded" data-testid="move-down-button">
+      <SfIconArrowUpward />
+    </button>
+
+    <button class="text-black hover:bg-gray-100 p-1 rounded" data-testid="move-up-button">
+      <SfIconArrowDownward />
+    </button> -->
+
+    <div class="w-px h-4 bg-gray-300"></div>
+
+    <button @click="triggerDelete" class="text-black hover:bg-gray-100 p-1 rounded" data-testid="delete-block-button">
+      <SfIconDelete />
+    </button>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { SfIconArrowDownward } from '@storefront-ui/vue';
-import { SfIconArrowUpward } from '@storefront-ui/vue';
 import { SfIconDelete } from '@storefront-ui/vue';
 import { editPath } from 'assets/icons/paths/edit';
 
