1d6597cb9a9f698bbf3dc79e5b837b345cd34398
Miron Pingel <miron@web-wikinger.de>
2024-12-18 15:06:07 +0100

    change default language in config to german

--- apps/web/configuration/app.config.ts
+++ apps/web/configuration/app.config.ts
@@ -2,7 +2,7 @@ export const appConfiguration = {
   head: {
     viewport: 'minimum-scale=1, initial-scale=1, width=device-width',
     htmlAttrs: {
-      lang: process.env.DEFAULTLANGUAGE ?? 'en',
+      lang: process.env.DEFAULTLANGUAGE ?? 'de',
     },
     meta: [
       { name: 'shop-name', content: process.env.STORENAME || 'PLENTYSYSTEMS AG' },
