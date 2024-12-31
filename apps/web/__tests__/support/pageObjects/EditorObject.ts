e2f6274ed12e6e4fd3998e477dbc542f1dd5954e
aoltean <78078265+aoltean-plenty@users.noreply.github.com>
2024-12-31 02:50:24 -0800

    feat: half blocks automation (#903)
    Co-authored-by: aoltean-plenty <alexandru.oltean@plentysystems.com>

--- apps/web/__tests__/support/pageObjects/EditorObject.ts
+++ apps/web/__tests__/support/pageObjects/EditorObject.ts
@@ -174,6 +174,26 @@ export class EditorObject extends PageObject {
     this.languageSwitcher.select('de');
     cy.wait(['@getCart', '@getCategoryTree', '@getFacet']);
     this.headline.first().should('have.text', 'Sound auf höchstem Niveau');
-   }
+  }
+
+  addBlockTop() {
+    this.blockWrapper.then(initialBlocks => {
+      const initialLength = initialBlocks.length;
+      this.topBlockButton.invoke('removeClass', 'opacity-0');
+      this.topBlockButton.first().should('exist').click();
+      cy.wait(1000);
+      this.blockWrapper.should('have.length', initialLength + 1);
+    });
+  }
+
+  addBlockBottom() {
+    this.blockWrapper.then(initialBlocks => {
+      const initialLength = initialBlocks.length;
+      this.bottomBlockButton.invoke('removeClass', 'opacity-0');
+      this.bottomBlockButton.first().should('exist').click();
+      cy.wait(1000);
+      this.blockWrapper.should('have.length', initialLength + 1);
+    });
+  }
 }
 
--- apps/web/__tests__/test/editor/blocks.cy.ts
+++ apps/web/__tests__/test/editor/blocks.cy.ts
@@ -3,18 +3,19 @@ import { EditorObject } from '../../support/pageObjects/EditorObject';
 
 describe('Blocks', () => {
     const editor = new EditorObject();
-  
+
     beforeEach(() => {
       cy.visitAndHydrate(paths.home);
     });
-  
+
     it('should ensure correct block logic and template', () => {
         editor.buttonsExistWithGroupClasses();
         editor.deleteBlock();
+        editor.addBlockTop();
+        editor.addBlockBottom();
     });
 
     it('should check for recommended products', () => {
       editor.recommendedProductsExist();
     });
   });
-  
