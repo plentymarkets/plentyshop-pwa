6490a55f0143133c8ae4711c78428d735dc25a34
Kevin Stederoth <43753494+ksted@users.noreply.github.com>
2025-01-03 14:07:11 +0100

    create new hero props type

--- apps/web/components/ui/HeroCarousel/types.ts
+++ apps/web/components/ui/HeroCarousel/types.ts
@@ -19,3 +19,31 @@ export type HeroContentProps = {
   callToAction?: string;
   link?: string;
 };
+
+export type HeroContentProps2 = {
+  image: {
+    lg: string;
+    md: string;
+    sm: string;
+    xs: string;
+    alt: string;
+    brightness: number;
+  };
+  text: {
+    color: string;
+    bgcolor: string;
+    pretitle: string;
+    title: string;
+    subtitle: string;
+    description: string;
+    descriptionIsHtml: boolean;
+    textAlignment: 'left' | 'center' | 'right';
+    justify: 'start' | 'center' | 'end';
+    align: 'start' | 'center' | 'end';
+  };
+  button: {
+    label: string;
+    link: string;
+    variant: 'primary' | 'secondary';
+  };
+};
