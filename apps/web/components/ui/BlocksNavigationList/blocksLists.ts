84e86ab8d5b342ad26a595a0970d750c91736eee
aoltean-plenty <alexandru.oltean@plentysystems.com>
2025-01-22 03:31:12 -0800

    re-order

--- apps/web/components/ui/BlocksNavigationList/blocksLists.ts
+++ apps/web/components/ui/BlocksNavigationList/blocksLists.ts
@@ -3,39 +3,38 @@
 // We could also send configuration here --- that will be the JSON
 
 export const blocksLists = [
-    // FORMS
-    {
-      id: 1,
-      title: 'Forms Preview',
-      category: 'forms',
-      categoryTitle: 'Forms',
-      imagePath: '/images/blocks/forms_preview.png',
-    },
-
   // IMAGE BANNER
   {
-    id: 2,
-    title: 'Image Banner Center',
-    category: 'image-banner',
-    categoryTitle: 'Image Banner',
-    imagePath: '/images/blocks/image_banner_center.png',
-  },
-  {
-    id: 3,
+    id: 1,
     title: 'Image Banner Left',
     category: 'image-banner',
     categoryTitle: 'Image Banner',
     imagePath: '/images/blocks/image_banner_left.png',
   },
   {
-    id: 4,
+    id: 2,
     title: 'Image Banner Right',
     category: 'image-banner',
     categoryTitle: 'Image Banner',
     imagePath: '/images/blocks/image_banner_right.png',
   },
+  {
+    id: 3,
+    title: 'Image Banner Center',
+    category: 'image-banner',
+    categoryTitle: 'Image Banner',
+    imagePath: '/images/blocks/image_banner_center.png',
+  },
 
   // IMAGE WITH TEXT
+  {
+    id: 4,
+    title: 'Image Right Text',
+    category: 'image-with-text',
+    categoryTitle: 'Image with Text',
+    imagePath: '/images/blocks/image_right_text.png',
+  },
+
   {
     id: 5,
     title: 'Image Left Text',
@@ -43,17 +42,10 @@ export const blocksLists = [
     categoryTitle: 'Image with Text',
     imagePath: '/images/blocks/image_left_text.png',
   },
-  {
-    id: 6,
-    title: 'Image Right Text',
-    category: 'image-with-text',
-    categoryTitle: 'Image with Text',
-    imagePath: '/images/blocks/image_right_text.png',
-  },
 
   // RICH TEXT
   {
-    id: 8,
+    id: 6,
     title: 'Rich Text',
     category: 'rich-text',
     categoryTitle: 'Rich text',
@@ -69,4 +61,12 @@ export const blocksLists = [
     imagePath: '/images/blocks/product_galleries.png',
   },
 
+  // FORMS
+  {
+    id: 8,
+    title: 'Forms Preview',
+    category: 'forms',
+    categoryTitle: 'Forms',
+    imagePath: '/images/blocks/forms_preview.png',
+  },
 ];
