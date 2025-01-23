945cd2dd83dfe397351a09d6a5c8390e12b5d633
aoltean-plenty <alexandru.oltean@plentysystems.com>
2025-01-23 06:26:09 -0800

    Remove code

--- apps/web/composables/useEditor/useEditor.ts
+++ apps/web/composables/useEditor/useEditor.ts
@@ -2,6 +2,5 @@ export const useEditor = () => {
   const isEditing = useState('isEditing', () => false);
   const disableActions = useState('disableActions', () => true);
   const isEditingEnabled = useState('isEditingEnabled', () => true);
-  const displayBlockList = useState('displayBlockList', () => false);
-  return { isEditing, isEditingEnabled, disableActions, displayBlockList };
+  return { isEditing, isEditingEnabled, disableActions };
 };
