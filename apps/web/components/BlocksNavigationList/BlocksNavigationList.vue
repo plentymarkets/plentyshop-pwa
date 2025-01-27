2d6b79dc6e7557916c02f7470b46472520e0c494
aoltean-plenty <alexandru.oltean@plentysystems.com>
2025-01-27 01:54:09 -0800

    Final css changes

--- apps/web/components/BlocksNavigationList/BlocksNavigationList.vue
+++ apps/web/components/BlocksNavigationList/BlocksNavigationList.vue
@@ -1,14 +1,17 @@
 <template>
-  <div class="bg-[#F1F3F5] overflow-y-auto max-h-[100vh] pt-5 sticky top-0">
+  <div class="bg-[#F1F3F5] overflow-y-auto max-h-[100vh] pt-5 sticky top-0 px-2">
     <div class="overflow-y-auto">
       <div v-for="(category, categoryIndex) in blocksLists" :key="categoryIndex">
-        <div class="mb-3 relative left-[15%]">
+        <div :class="['mb-3 relative', isTablet ? 'text-center' : 'left-[15%]']">
           {{ category.title }}
         </div>
         <div v-for="(variation, variationIndex) in category.variations" :key="variationIndex" class="relative mb-10">
           <NuxtImg :src="variation.image" class="block mx-auto" :alt="variation.title" width="253" height="120" />
           <button
-            class="right-[15%] absolute transform -translate-y-1/2 bg-[#72CBEE] text-[#062633] w-10 h-10 rounded-full flex items-center justify-center shadow-md"
+            :class="[
+              'transform -translate-y-1/2 bg-[#72CBEE] text-[#062633] w-10 h-10 rounded-full shadow-md cursor-pointer',
+              isTablet ? 'block mx-auto' : 'right-[15%] absolute',
+            ]"
           >
             <SfIconAdd class="cursor-pointer" />
           </button>
@@ -21,4 +24,6 @@
 <script setup lang="ts">
 import { blocksLists } from './blocksLists';
 import { SfIconAdd } from '@storefront-ui/vue';
+const viewport = useViewport();
+const isTablet = computed(() => viewport.isLessThan('lg'));
 </script>
