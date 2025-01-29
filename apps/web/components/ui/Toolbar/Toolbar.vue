2d8959c517384868e6408121793995c2288d765b
Kevin Stederoth <43753494+ksted@users.noreply.github.com>
2025-01-29 12:20:43 +0100

    fix: save button condition for template edits

--- apps/web/components/ui/Toolbar/Toolbar.vue
+++ apps/web/components/ui/Toolbar/Toolbar.vue
@@ -73,7 +73,7 @@ const { updatePageTemplate } = useUpdatePageTemplate();
 
 const homepageCategoryId = runtimeConfig.public.homepageCategoryId;
 
-const isLocalTemplate = computed(() => typeof homepageCategoryId === 'number');
+const isLocalTemplate = computed(() => typeof homepageCategoryId !== 'number');
 
 const isTouched = computed(() => settingsIsDirty.value || (!isLocalTemplate.value && isEditingEnabled.value));
 
