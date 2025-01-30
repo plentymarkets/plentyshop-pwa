01384101901d088e375ed3dd21991782a3854087
rbedeoan-plenty <121931658+rbedeoan-plenty@users.noreply.github.com>
2025-01-30 11:13:44 +0200

    fix: accesibility erors

--- apps/web/components/ui/BlockActions/BlockActions.vue
+++ apps/web/components/ui/BlockActions/BlockActions.vue
@@ -3,7 +3,7 @@
     class="absolute z-[0] md:z-[1] lg:z-[50] right-0 top-0 flex items-center space-x-4 border border-[#538AEA] bg-white p-2 shadow-md"
     data-testid="edit-block-actions"
   >
-    <button class="text-black hover:bg-gray-100 p-1 rounded" data-testid="open-editor-button" @click="triggerEdit">
+    <button class="text-black hover:bg-gray-100 p-1 rounded" data-testid="open-editor-button" aria-label="editor button" @click="triggerEdit">
       <SfIconBase size="xs" viewBox="0 0 18 18" class="fill-primary-900 cursor-pointer">
         <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path :d="editPath" fill="black" />
@@ -16,6 +16,7 @@
     <button
       class="text-black hover:bg-gray-100 p-1 rounded"
       data-testid="move-up-button"
+      aria-label="move up button"
       :disabled="props.index === 0"
       :class="{ 'opacity-40 cursor-not-allowed': props.index === 0 }"
       @click="changePosition(-1)"
@@ -26,6 +27,7 @@
     <button
       class="text-black hover:bg-gray-100 p-1 rounded"
       data-testid="move-down-button"
+      aria-label="move down button"
       :disabled="isLastBlock"
       :class="{ 'opacity-40 cursor-not-allowed': isLastBlock }"
       @click="changePosition(1)"
@@ -35,7 +37,7 @@
 
     <div class="w-px h-4 bg-gray-300" />
 
-    <button class="text-black hover:bg-gray-100 p-1 rounded" data-testid="delete-block-button" @click="triggerDelete">
+    <button class="text-black hover:bg-gray-100 p-1 rounded" aria-label="aria label" data-testid="delete-block-button" @click="triggerDelete">
       <SfIconDelete />
     </button>
   </div>
