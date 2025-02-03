47855848ff24802fc5754ef463d2e1228a915b7e
Kevin Stederoth <43753494+ksted@users.noreply.github.com>
2025-02-03 16:57:03 +0100

    chore: use npm as package manager

--- apps/web/configuration/i18n.config.ts
+++ apps/web/configuration/i18n.config.ts
@@ -3,7 +3,7 @@ import type { NuxtI18nOptions } from '@nuxtjs/i18n';
 import { getLocaleObject } from './locale.config';
 import { readdirSync } from 'node:fs';
 
-const languages = readdirSync(path.resolve(__dirname, '../lang'))
+const languages = readdirSync(path.resolve(__dirname, '../i18n/lang'))
   .map((file: string) => file.replace('.json', ''))
   .join(',');
 
@@ -12,7 +12,7 @@ const localeObject = getLocaleObject(languages);
 export const nuxtI18nOptions: NuxtI18nOptions = {
   locales: localeObject,
   langDir: 'lang',
-  defaultLocale: process.env.DEFAULTLANGUAGE ?? 'en',
+  defaultLocale: (process.env.DEFAULTLANGUAGE === 'de' || process.env.DEFAULTLANGUAGE === 'en') ? process.env.DEFAULTLANGUAGE : 'en',
   strategy: 'prefix_and_default',
   vueI18n: '~/configuration/vueI18n.config.ts',
   detectBrowserLanguage: false,
