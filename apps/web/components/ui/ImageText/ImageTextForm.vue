671475b3d8f815a776f622db74bc71f056973d26
aoltean-plenty <alexandru.oltean@plentysystems.com>
2025-02-04 05:25:00 -0800

    use computed

--- apps/web/components/ui/ImageText/ImageTextForm.vue
+++ apps/web/components/ui/ImageText/ImageTextForm.vue
@@ -15,10 +15,10 @@
           <UiFormLabel>Desktop Image</UiFormLabel>
         </div>
         <label>
-          <SfInput v-model="imageDesktop" type="text">
+          <SfInput v-model="uiImageTextBlock.image.desktop" type="text">
             <template #suffix>
               <label for="image-tabletr" class="rounded-lg cursor-pointer">
-                <input id="image-tablet" v-model="imageDesktop" type="text" class="invisible w-8" />
+                <input id="image-tablet" v-model="uiImageTextBlock.image.desktop" type="text" class="invisible w-8" />
               </label>
             </template>
           </SfInput>
@@ -31,10 +31,10 @@
           <UiFormLabel>Tablet Image</UiFormLabel>
         </div>
         <label>
-          <SfInput v-model="imageTablet" type="text">
+          <SfInput v-model="uiImageTextBlock.image.tablet" type="text">
             <template #suffix>
               <label for="image-tablet" class="rounded-lg cursor-pointer">
-                <input id="image-tablet" v-model="imageTablet" type="text" class="invisible w-8" />
+                <input id="image-tablet" v-model="uiImageTextBlock.image.tablet" type="text" class="invisible w-8" />
               </label>
             </template>
           </SfInput>
@@ -47,10 +47,10 @@
           <UiFormLabel>Mobile Image</UiFormLabel>
         </div>
         <label>
-          <SfInput v-model="imageMobile" type="text">
+          <SfInput v-model="uiImageTextBlock.image.mobile" type="text">
             <template #suffix>
               <label for="image-mobile" class="rounded-lg cursor-pointer">
-                <input id="image-mobile" v-model="imageMobile" type="text" class="invisible w-8" />
+                <input id="image-mobile" v-model="uiImageTextBlock.image.mobile" type="text" class="invisible w-8" />
               </label>
             </template>
           </SfInput>
@@ -63,10 +63,10 @@
           <UiFormLabel>Alt</UiFormLabel>
         </div>
         <label>
-          <SfInput v-model="altText" type="text">
+          <SfInput v-model="uiImageTextBlock.image.alt" type="text">
             <template #suffix>
               <label for="alt" class="rounded-lg cursor-pointer">
-                <input id="alt" v-model="altText" type="text" class="invisible w-8" />
+                <input id="alt" v-model="uiImageTextBlock.image.alt" type="text" class="invisible w-8" />
               </label>
             </template>
           </SfInput>
@@ -88,7 +88,7 @@
           </div>
 
           <div class="flex items-center">
-            <SfInput  type="radio" value="right">
+            <SfInput type="radio" value="right">
               <template #suffix>
                 <label for="align-right" class="rounded-lg cursor-pointer">
                   <input id="align-right" type="radio" value="right" class="invisible w-8" />
@@ -102,226 +102,220 @@
     </UiAccordionItem>
     <!-- End Image Group -->
 
-    <!-- Text Group -->
-    <div>
-      <label for="pretitle" class="block text-sm font-medium text-gray-700">Pre title</label>
-      <input
-        type="text"
-        id="pretitle"
-        name="pretitle"
-        class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-      />
-    </div>
-
-    <div>
-      <label for="title" class="block text-sm font-medium text-gray-700">Title</label>
-      <input
-        type="text"
-        id="title"
-        v-model="title"
-        name="title"
-        class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-      />
-    </div>
-    <div>
-      <label for="subtitle" class="block text-sm font-medium text-gray-700">Subtitle</label>
-      <input
-        type="text"
-        id="subtitle"
-        v-model="subtitle"
-        name="subtitle"
-        class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-      />
-    </div>
-
-    <div>
-      <label for="html-description" class="block text-sm font-medium text-gray-700">HTML Description</label>
-      <textarea
-        id="html-description"
-        v-model="htmlDescription"
-        name="html-description"
-        rows="3"
-        class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-      />
-    </div>
-
-    <div class="py-2">
-      <div class="flex justify-between mb-2">
-        <UiFormLabel>Primary color</UiFormLabel>
-        <SfTooltip
-          label="The shop uses a primary and secondary color palette. Each palette consists of ten shades. The colors configured here serve as the base value for the respective palette. All other shades are automatically generated during the build process."
-          :placement="'top'"
-          :show-arrow="true"
-          class="ml-2 z-10"
-        >
-          <SfIconInfo :size="'sm'" />
-        </SfTooltip>
+    <UiAccordionItem
+      v-model="textGroup"
+      summary-active-class="bg-neutral-100 border-t-0"
+      summary-class="w-full hover:bg-neutral-100 px-4 py-5 flex justify-between items-center select-none border-b"
+    >
+      <template #summary>
+        <h2>Text</h2>
+      </template>
+      <!-- Text Group -->
+      <div class="py-2">
+        <div class="flex justify-between mb-2">
+          <UiFormLabel>Pre title</UiFormLabel>
+        </div>
+        <label>
+          <SfInput v-model="uiImageTextBlock.text.pretitle" type="text">
+            <template #suffix>
+              <label for="pretitle" class="rounded-lg cursor-pointer">
+                <input id="pretitle" v-model="uiImageTextBlock.text.pretitle" type="text" class="invisible w-8" />
+              </label>
+            </template>
+          </SfInput>
+        </label>
       </div>
-      <label>
-        <SfInput type="text">
-          <template #suffix>
-            <label for="primary-color" class="rounded-lg cursor-pointer">
-              <input id="primary-color" type="color" class="invisible w-8" />
-            </label>
-          </template>
-        </SfInput>
-        <span class="typography-text-xs text-neutral-700">Choose primary color</span>
-      </label>
-    </div>
 
-    <fieldset>
-      <legend class="text-sm font-medium text-gray-700">Text Align</legend>
-      <div class="mt-2 space-y-2">
-        <div class="flex items-center">
-          <input
-            id="align-left"
-            name="text-align"
-            type="radio"
-            value="left"
-            v-model="textAlign"
-            class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300"
-          />
-          <label for="align-left" class="ml-3 block text-sm font-medium text-gray-700">Left</label>
+      <div class="py-2">
+        <div class="flex justify-between mb-2">
+          <UiFormLabel>Title</UiFormLabel>
         </div>
-        <div class="flex items-center">
-          <input
-            id="align-center"
-            name="text-align"
-            type="radio"
-            value="center"
-            v-model="textAlign"
-            class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300"
-          />
-          <label for="align-center" class="ml-3 block text-sm font-medium text-gray-700">Center</label>
-        </div>
-        <div class="flex items-center">
-          <input
-            id="align-right"
-            name="text-align"
-            type="radio"
-            value="right"
-            v-model="textAlign"
-            class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300"
-          />
-          <label for="align-right" class="ml-3 block text-sm font-medium text-gray-700">Right</label>
-        </div>
+        <label>
+          <SfInput v-model="uiImageTextBlock.text.title" type="text">
+            <template #suffix>
+              <label for="title" class="rounded-lg cursor-pointer">
+                <input id="title" v-model="uiImageTextBlock.text.title" type="text" class="invisible w-8" />
+              </label>
+            </template>
+          </SfInput>
+        </label>
       </div>
-    </fieldset>
 
+      <div class="py-2">
+        <div class="flex justify-between mb-2">
+          <UiFormLabel>Subtitle</UiFormLabel>
+        </div>
+        <label>
+          <SfInput v-model="uiImageTextBlock.text.subtitle" type="text">
+            <template #suffix>
+              <label for="subtitle" class="rounded-lg cursor-pointer">
+                <input id="subtitle" v-model="uiImageTextBlock.text.subtitle" type="text" class="invisible w-8" />
+              </label>
+            </template>
+          </SfInput>
+        </label>
+      </div>
+
+      <div class="py-2">
+        <UiFormLabel>HTML Description</UiFormLabel>
+        <SfTextarea
+          id="html-description"
+          v-model="uiImageTextBlock.text.htmlDescription"
+          name="html-description"
+          rows="3"
+          class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+        />
+      </div>
+
+      <div class="py-2">
+        <div class="flex justify-between mb-2">
+          <UiFormLabel>Primary color</UiFormLabel>
+          <SfTooltip
+            label="The shop uses a primary and secondary color palette. Each palette consists of ten shades. The colors configured here serve as the base value for the respective palette. All other shades are automatically generated during the build process."
+            :placement="'top'"
+            :show-arrow="true"
+            class="ml-2 z-10"
+          >
+            <SfIconInfo :size="'sm'" />
+          </SfTooltip>
+        </div>
+        <label>
+          <SfInput type="text">
+            <template #suffix>
+              <label for="primary-color" class="rounded-lg cursor-pointer">
+                <input id="primary-color" type="color" class="invisible w-8" />
+              </label>
+            </template>
+          </SfInput>
+          <span class="typography-text-xs text-neutral-700">Choose primary color</span>
+        </label>
+      </div>
+
+      <fieldset>
+        <legend class="text-sm font-medium text-gray-700">Text Align</legend>
+        <div class="mt-2 space-y-2">
+          <div class="flex items-center">
+            <SfInput
+              id="align-left"
+              name="text-align"
+              type="radio"
+              value="left"
+              v-model="uiImageTextBlock.text.textAlignment"
+              class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300"
+            />
+            <label for="align-left" class="ml-3 block text-sm font-medium text-gray-700">Left</label>
+          </div>
+          <div class="flex items-center">
+            <SfInput
+              id="align-center"
+              name="text-align"
+              type="radio"
+              value="center"
+              v-model="uiImageTextBlock.text.textAlignment"
+              class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300"
+            />
+            <label for="align-center" class="ml-3 block text-sm font-medium text-gray-700">Center</label>
+          </div>
+          <div class="flex items-center">
+            <SfInput
+              id="align-right"
+              name="text-align"
+              type="radio"
+              value="right"
+              v-model="uiImageTextBlock.text.textAlignment"
+              class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300"
+            />
+            <label for="align-right" class="ml-3 block text-sm font-medium text-gray-700">Right</label>
+          </div>
+        </div>
+      </fieldset>
+    </UiAccordionItem>
     <!-- End Text Group -->
 
     <!-- Button Group -->
-    <div>
-      <label for="button-label" class="block text-sm font-medium text-gray-700">Button Label</label>
-      <input
-        type="text"
-        id="button-label"
-        v-model="buttonLabel"
-        name="button-label"
-        class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-      />
-    </div>
-    <div>
-      <label for="button-link" class="block text-sm font-medium text-gray-700">Button Link</label>
-      <input
-        type="text"
-        id="button-link"
-        v-model="buttonLink"
-        name="button-link"
-        class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-      />
-    </div>
 
-    <fieldset>
-      <legend class="text-sm font-medium text-gray-700">Outline</legend>
-      <div class="mt-2 space-y-2">
-        <div class="flex items-center">
-          <input
-            id="align-left"
-            name="text-align"
-            type="radio"
-            value="on"
-            class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300"
-          />
-          <label for="align-left" class="ml-3 block text-sm font-medium text-gray-700">On</label>
-        </div>
+    <UiAccordionItem
+      v-model="buttonGroup"
+      summary-active-class="bg-neutral-100 border-t-0"
+      summary-class="w-full hover:bg-neutral-100 px-4 py-5 flex justify-between items-center select-none border-b"
+    >
+      <template #summary>
+        <h2>Button</h2>
+      </template>
 
-        <div class="flex items-center">
-          <input
-            id="align-right"
-            name="text-align"
-            type="radio"
-            value="off"
-            class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300"
-          />
-          <label for="align-right" class="ml-3 block text-sm font-medium text-gray-700">Off</label>
+      <div class="py-2">
+        <div class="flex justify-between mb-2">
+          <UiFormLabel>Button Label</UiFormLabel>
         </div>
+        <label>
+          <SfInput v-model="uiImageTextBlock.button.label" type="text">
+            <template #suffix>
+              <label for="button-label" class="rounded-lg cursor-pointer">
+                <input id="button-label" v-model="uiImageTextBlock.button.label" type="text" class="invisible w-8" />
+              </label>
+            </template>
+          </SfInput>
+        </label>
       </div>
-    </fieldset>
 
+      <div class="py-2">
+        <div class="flex justify-between mb-2">
+          <UiFormLabel>Button Link</UiFormLabel>
+        </div>
+        <label>
+          <SfInput v-model="uiImageTextBlock.button.link" type="text">
+            <template #suffix>
+              <label for="button-link" class="rounded-lg cursor-pointer">
+                <input id="button-link" v-model="uiImageTextBlock.button.link" type="text" class="invisible w-8" />
+              </label>
+            </template>
+          </SfInput>
+        </label>
+      </div>
+
+      <fieldset>
+        <legend class="text-sm font-medium text-gray-700">Outline</legend>
+        <div class="mt-2 space-y-2">
+          <div class="flex items-center">
+            <SfInput
+              id="align-left"
+              name="text-align"
+              type="radio"
+              value="on"
+              class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300"
+            />
+            <label for="align-left" class="ml-3 block text-sm font-medium text-gray-700">On</label>
+          </div>
+
+          <div class="flex items-center">
+            <SfInput
+              id="align-right"
+              name="text-align"
+              type="radio"
+              value="off"
+              class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300"
+            />
+            <label for="align-right" class="ml-3 block text-sm font-medium text-gray-700">Off</label>
+          </div>
+        </div>
+      </fieldset>
+    </UiAccordionItem>
     <!-- EndButton Group -->
   </form>
 </template>
 
 <script setup lang="ts">
-import type { ImageTextProps } from './types';
-import { SfTooltip, SfInput } from '@storefront-ui/vue';
+import type { ImageTextProps2 } from './types';
+import { SfTooltip, SfInput, SfTextarea } from '@storefront-ui/vue';
 const { data } = useHomepage();
-const uiImageTextBlock = ref(data.value.blocks.find((block) => block.name === 'UiImageText'));
-
-const settings = uiImageTextBlock.value?.options as ImageTextProps;
-
-const title = ref(settings.text?.title);
-const subtitle = ref(settings.text?.subtitle);
-const buttonLabel = ref(settings.button?.label);
-const buttonLink = ref(settings.button?.link);
-const imageDesktop = ref(settings.image?.desktop);
-const imageTablet = ref(settings.image?.tablet);
-const imageMobile = ref(settings.image?.mobile);
-const altText = ref(settings.image?.alt);
-const htmlDescription = ref(settings.text?.htmlDescription);
-const textAlign = ref(settings.text?.textAlignment);
 
 const imageGroupOpen = ref(false);
+const buttonGroup = ref(false);
+const textGroup = ref(false);
 
-watch(
-  [
-    title,
-    subtitle,
-    buttonLabel,
-    buttonLink,
-    imageDesktop,
-    imageTablet,
-    imageMobile,
-    altText,
-    htmlDescription,
-    textAlign,
-  ],
-  ([
-    newTitle,
-    newSubtitle,
-    newButtonLabel,
-    newButtonLink,
-    newImageDesktop,
-    newImageTablet,
-    newImageMobile,
-    newAltText,
-    newHtmlDescription,
-    newTextAlign,
-  ]) => {
-    if (settings && settings.text && settings.button && settings.image) {
-      settings.text.title = newTitle;
-      settings.text.subtitle = newSubtitle;
-      settings.button.label = newButtonLabel;
-      settings.button.link = newButtonLink;
-      settings.image.desktop = newImageDesktop;
-      settings.image.tablet = newImageTablet;
-      settings.image.mobile = newImageMobile;
-      settings.image.alt = newAltText ?? '';
-      settings.text.htmlDescription = newHtmlDescription;
-      settings.text.textAlignment = newTextAlign;
-    }
-  },
-  { deep: true },
+const uiImageTextBlock = computed(
+  () => data.value.blocks.find((block) => block.name === 'UiImageText')?.options as ImageTextProps2,
 );
+
+
 </script>
--- apps/web/components/ui/ImageText/types.ts
+++ apps/web/components/ui/ImageText/types.ts
@@ -25,3 +25,32 @@ export interface ImageDimensions {
   width: number;
   height: number;
 }
+
+
+export type ImageTextProps2= {
+  image: {
+    desktop?: string;
+    tablet?: string;
+    mobile?: string;
+    alt: string;
+    imageAlignment: 'left' | 'right';
+  };
+
+  text: {
+    pretitle?: string;
+    title?: string;
+    subtitle?: string;
+    htmlDescription?: string;
+    textAlignment?: 'left' | 'center' | 'right';
+  };
+  button: {
+    label?: string;
+    link?: string;
+    variant?: 'primary' | 'secondary';
+  };
+};
+
+export interface ImageDimensions {
+  width: number;
+  height: number;
+}
