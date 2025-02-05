d917c7a6d3da7c52e4a11eb8f2e9d710b136cecf
oivan-plenty <oana.ivan@plentysystems.com>
2025-02-05 10:49:20 +0200

    added condition for placeholder

--- apps/web/components/ui/TextCard/types.ts
+++ apps/web/components/ui/TextCard/types.ts
@@ -5,8 +5,25 @@ export type TextCardProps = {
     subtitle?: string;
     htmlDescription?: string;
     textAlignment?: 'left' | 'center' | 'right';
+    color?: string;
   };
   button?: {
+    label?: string;
+    link?: string;
+    variant?: 'primary' | 'secondary';
+  };
+};
+
+export type TextCardProps2 = {
+  text: {
+    pretitle?: string;
+    title?: string;
+    subtitle?: string;
+    htmlDescription?: string;
+    textAlignment?: 'left' | 'center' | 'right';
+    color?: string;
+  };
+  button: {
     label?: string;
     link?: string;
     variant?: 'primary' | 'secondary';
