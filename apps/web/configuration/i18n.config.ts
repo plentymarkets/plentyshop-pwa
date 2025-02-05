21f5a1aa75cae210288c583e77a892891085cf42
Kevin Stederoth <43753494+ksted@users.noreply.github.com>
2025-02-05 07:55:56 +0100

    fix: read language files

--- apps/web/configuration/i18n.config.ts
+++ apps/web/configuration/i18n.config.ts
@@ -2,21 +2,36 @@ import path from 'node:path';
 import type { LocaleObject, NuxtI18nOptions } from '@nuxtjs/i18n';
 import { readdirSync } from 'node:fs';
 
-const languages = readdirSync(path.resolve(__dirname, '../i18n/lang'))
-  .map((file: string) => file.replace('.json', ''))
-  .join(',');
 
-const locales = languages.split(',');
+const getLocales = (): LocaleObject[] => {
+  const locales: LocaleObject[] = [];
+  const languages = readdirSync(path.resolve(__dirname, '../i18n/lang'))
+    .map((file: string) => file.replace('.json', ''));
+
+  languages.forEach((language) => {
+    locales.push({
+      code: language as LocaleObject['*'],
+      file: `${language}.json`,
+    });
+  });
+
+  return locales;
+};
 
 const getDefaultLocale = () => {
-  const defaultLanguage = process.env.DEFAULTLANGUAGE as LocaleObject['*'];
-  const fallbackLanguage = locales[0] as LocaleObject['*'];
-  return locales.includes(defaultLanguage) ? defaultLanguage : fallbackLanguage;
+  const locales = getLocales();
+  const localeKeys = locales.map((locale) => locale.code);
+  
+  const defaultLocale = process.env.DEFAULTLANGUAGE as LocaleObject['*'];
+  const fallbackLocale = locales[0] as LocaleObject['*'];
+
+  return localeKeys.includes(defaultLocale) ? defaultLocale : fallbackLocale;
 };
 
 export const nuxtI18nOptions: NuxtI18nOptions = {
-  locales: locales,
+  locales: getLocales(),
   defaultLocale: getDefaultLocale(),
+  langDir: 'lang',
   strategy: 'prefix_and_default',
   vueI18n: '~/configuration/vueI18n.config.ts',
   detectBrowserLanguage: false,
