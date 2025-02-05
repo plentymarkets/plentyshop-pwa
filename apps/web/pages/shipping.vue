503f0526c60aeb4f1960c41cdee29c205a88ea53
plentydev <iulian.vladu@plentymarkets.com>
2025-02-05 11:33:48 +0200

    feat: Shipping page no configuration

--- apps/web/pages/shipping.vue
+++ apps/web/pages/shipping.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="w-full p-5 overflow-x-auto no-preflight" v-html="text" />
+  <h5 v-if="!text && noShippingMessage" class="text-center mb-5">{{ noShippingMessage }}</h5>
 </template>
 
 <script setup lang="ts">
@@ -9,6 +10,9 @@ definePageMeta({
 const { data, fetchCategoryTemplate } = useCategoryTemplate();
 const runtimeConfig = useRuntimeConfig();
 await fetchCategoryTemplate(Number(runtimeConfig.public.shippingTextCategoryId));
+const { t } = useI18n();
+
+const noShippingMessage = t('shipping.noShippingMessage');
 
 const text = computed(() => data?.value?.data);
 </script>
