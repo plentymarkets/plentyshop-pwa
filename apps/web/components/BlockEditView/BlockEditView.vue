080b6e89e1cd7e9aa1b6efcac7458861da10cfa3
oivan-plenty <oana.ivan@plentysystems.com>
2025-02-05 17:37:16 +0200

    new text card form

--- apps/web/components/BlockEditView/BlockEditView.vue
+++ apps/web/components/BlockEditView/BlockEditView.vue
@@ -18,6 +18,7 @@ const { drawerOpen, blockType } = useSiteConfiguration();
 
 const getBlock = (view: string) => {
   if (view === 'UiCarousel') return resolveComponent('BlockSlider');
+  if (view === 'UiTextCard') return resolveComponent('UiTextCardForm');
   // if (view === 'recommendedProducts') return resolveComponent('BlockRecommendedProducts');
 };
 </script>
