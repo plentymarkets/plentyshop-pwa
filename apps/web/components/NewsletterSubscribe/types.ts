ba3df9ce39b6dc3523ca32ed34f0bae5df30acdb
clincu-plenty <cristian.lincu@plentysystems.com>
2025-02-06 16:24:39 +0200

    delete unused type

--- apps/web/components/NewsletterSubscribe/types.ts
+++ apps/web/components/NewsletterSubscribe/types.ts
@@ -12,18 +12,3 @@ export type NewsletterSubscribeProps = {
     label?: string;
   };
 };
-
-export type NewsletterSubscribeProps2 = {
-  text: {
-    bgColor: string;
-    title: string;
-    htmlDescription: string;
-  };
-  input: {
-    displayNameInput: boolean;
-    nameIsRequired: boolean;
-  };
-  button: {
-    label: string;
-  };
-};
