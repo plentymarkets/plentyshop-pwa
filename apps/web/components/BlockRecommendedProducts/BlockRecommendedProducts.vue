dd573b68c54cffe1d2985f6b9b0ff307afafca66
rbedeoan-plenty <121931658+rbedeoan-plenty@users.noreply.github.com>
2025-02-07 13:51:10 +0200

    fix: refactor

--- apps/web/components/BlockRecommendedProducts/BlockRecommendedProducts.vue
+++ apps/web/components/BlockRecommendedProducts/BlockRecommendedProducts.vue
@@ -2,12 +2,12 @@
   <div class="p-2 sticky top-[52px] h-[calc(100vh-150px)] overflow-y-auto">
     <label>
       <UiFormLabel class="mb-1">Headline</UiFormLabel>
-      <SfInput v-model="recommendedBlockValue.headline" name="Headline" type="text" placeholder="Enter Headline" />
+      <SfInput v-model="recommendedProductsOptions.headline" name="Headline" type="text" placeholder="Enter Headline" />
     </label>
     <label>
       <UiFormLabel class="mb-1">Category Id</UiFormLabel>
       <SfInput
-        v-model="recommendedBlockValue.categoryId"
+        v-model="recommendedProductsOptions.categoryId"
         name="category Id"
         type="text"
         placeholder="Enter Category Id"
@@ -16,20 +16,30 @@
     <div>
       <div class="mb-6">
         <UiFormLabel class="mb-1">Pre Title</UiFormLabel>
-        <SfInput v-model="recommendedBlockValue.text.pretitle" name="preTitle" type="text" placeholder="PreTitle" />
+        <SfInput
+          v-model="recommendedProductsOptions.text.pretitle"
+          name="preTitle"
+          type="text"
+          placeholder="PreTitle"
+        />
       </div>
       <div class="mb-6">
         <UiFormLabel class="mb-1">Title</UiFormLabel>
-        <SfInput v-model="recommendedBlockValue.text.title" name="Title" type="text" placeholder="Title" />
+        <SfInput v-model="recommendedProductsOptions.text.title" name="Title" type="text" placeholder="Title" />
       </div>
       <div class="mb-6">
         <UiFormLabel class="mb-1">Subtitle</UiFormLabel>
-        <SfInput v-model="recommendedBlockValue.text.subtitle" name="Subtitle" type="text" placeholder="Subtitle" />
+        <SfInput
+          v-model="recommendedProductsOptions.text.subtitle"
+          name="Subtitle"
+          type="text"
+          placeholder="Subtitle"
+        />
       </div>
       <div class="mb-6">
         <UiFormLabel class="mb-1">Description</UiFormLabel>
         <SfTextarea
-          v-model="recommendedBlockValue.text.htmlDescription"
+          v-model="recommendedProductsOptions.text.htmlDescription"
           name="description"
           type="text"
           class="w-full min-h-[232px]"
@@ -39,14 +49,19 @@
       <div class="mb-6">
         <UiFormLabel class="mb-1">Text Color</UiFormLabel>
 
-        <SfInput v-model="recommendedBlockValue.text.color" type="text">
+        <SfInput v-model="recommendedProductsOptions.text.color" type="text">
           <template #suffix>
             <label
               for="text-color"
-              :style="{ backgroundColor: recommendedBlockValue.text.color }"
+              :style="{ backgroundColor: recommendedProductsOptions.text.color }"
               class="rounded-lg cursor-pointer"
             >
-              <input id="text-color" v-model="recommendedBlockValue.text.color" type="color" class="invisible w-8" />
+              <input
+                id="text-color"
+                v-model="recommendedProductsOptions.text.color"
+                type="color"
+                class="invisible w-8"
+              />
             </label>
           </template>
         </SfInput>
@@ -58,13 +73,13 @@
             for="text-align-left"
             class="flex items-center justify-center w-1/2 px-4 py-2 cursor-pointer text-sm"
             :class="{
-              'bg-gray-100 text-gray-900 font-semibold': recommendedBlockValue.text.textAlignment === 'left',
+              'bg-gray-100 text-gray-900 font-semibold': recommendedProductsOptions.text.textAlignment === 'left',
             }"
-            @click="recommendedBlockValue.text.textAlignment = 'left'"
+            @click="recommendedProductsOptions.text.textAlignment = 'left'"
           >
             <SfIconCheck
               class="mr-1 w-[1.1rem]"
-              :class="{ invisible: recommendedBlockValue.text.textAlignment !== 'left' }"
+              :class="{ invisible: recommendedProductsOptions.text.textAlignment !== 'left' }"
             />
             Left
           </div>
@@ -73,13 +88,13 @@
             for="text-align-center"
             class="flex items-center justify-center w-1/2 px-4 py-2 cursor-pointer text-sm"
             :class="{
-              'bg-gray-100 text-gray-900 font-semibold': recommendedBlockValue.text.textAlignment === 'center',
+              'bg-gray-100 text-gray-900 font-semibold': recommendedProductsOptions.text.textAlignment === 'center',
             }"
-            @click="recommendedBlockValue.text.textAlignment = 'center'"
+            @click="recommendedProductsOptions.text.textAlignment = 'center'"
           >
             <SfIconCheck
               class="mr-1 w-[1.1rem]"
-              :class="{ invisible: recommendedBlockValue.text.textAlignment !== 'center' }"
+              :class="{ invisible: recommendedProductsOptions.text.textAlignment !== 'center' }"
             />
             Center
           </div>
@@ -88,13 +103,13 @@
             for="text-align-right"
             class="flex items-center justify-center w-1/2 px-4 py-2 cursor-pointer text-sm"
             :class="{
-              'bg-gray-100 text-gray-900 font-semibold': recommendedBlockValue.text.textAlignment === 'right',
+              'bg-gray-100 text-gray-900 font-semibold': recommendedProductsOptions.text.textAlignment === 'right',
             }"
-            @click="recommendedBlockValue.text.textAlignment = 'right'"
+            @click="recommendedProductsOptions.text.textAlignment = 'right'"
           >
             <SfIconCheck
               class="mr-1 w-[1.1rem]"
-              :class="{ invisible: recommendedBlockValue.text.textAlignment !== 'right' }"
+              :class="{ invisible: recommendedProductsOptions.text.textAlignment !== 'right' }"
             />
             Right
           </div>
@@ -112,25 +127,24 @@ const { blockIndex } = useSiteConfiguration();
 const recommendedBlock = computed(
   () => (data.value.blocks[blockIndex.value].options || {}) as ProductRecommendedProductsOptions,
 );
-
-const recommendedBlockValue = reactive({
-  ...recommendedBlock.value,
-  text: recommendedBlock.value.text || {
-    pretitle: '',
-    title: '',
-    subtitle: '',
-    htmlDescription: '',
-    color: '#000',
-    textAlignment: 'left',
+const recommendedProductsOptions = computed({
+  get: () => {
+    return {
+      ...recommendedBlock.value,
+      text: recommendedBlock.value.text || {
+        pretitle: '',
+        title: '',
+        subtitle: '',
+        htmlDescription: '',
+        color: '#000',
+        textAlignment: 'left',
+      },
+    };
+  },
+  set: (value) => {
+    updateProductRecommendedProductsOptions(value);
   },
 });
-recommendedBlockValue.text.color = recommendedBlockValue.text.color || '#000';
-
-watch(
-  () => recommendedBlockValue,
-  (newVal) => {
-    updateProductRecommendedProductsOptions(newVal);
-  },
-  { deep: true },
-);
+recommendedProductsOptions.value.text.color = recommendedProductsOptions.value.text.color ?? '#000';
+recommendedProductsOptions.value.text.textAlignment = recommendedProductsOptions.value.text.textAlignment ?? 'left';
 </script>
