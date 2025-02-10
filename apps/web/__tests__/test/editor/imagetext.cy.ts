aa309cbcabc72a9ff93d37f98b880f3fd5f40082
aoltean-plenty <alexandru.oltean@plentysystems.com>
2025-02-10 05:44:41 -0800

    jesus

--- apps/web/__tests__/test/editor/imagetext.cy.ts
+++ apps/web/__tests__/test/editor/imagetext.cy.ts
@@ -8,25 +8,30 @@ describe('EditMode', () => {
   });
 
   it('should test image text', () => {
-    cy.get('[data-testid="open-editor-button"]').eq(3).click().should('exist');
+    cy.get('[data-testid="open-editor-button"]').should('have.length.at.least', 4); 
+
+    cy.get('[data-testid="open-editor-button"]')
+      .eq(3)
+      .should('exist')
+      .click({ force: true });
     cy.wait(1000);
-    cy.get('[data-testid="image-text-form"]', { timeout: 100000 }).should('exist');
-    cy.get('[data-testid="text-group"]', { timeout: 100000 }).should('exist').click();
+    cy.get('[data-testid="image-text-form"]', { timeout: 30000 }).should('exist');
+    cy.get('[data-testid="text-group"]', { timeout: 30000 }).should('exist').click();
 
     
 
-    cy.get('[data-testid="input-pre-title"]', { timeout: 100000 }).clear({ force: true }).type('New Pre title', { delay: 0, force: true });
-    cy.get('[data-testid="input-title"]', { timeout: 100000 }).clear({ force: true }).type('New title', { delay: 0, force: true });
-    cy.get('[data-testid="input-sub-title"]', { timeout: 100000 }).clear({ force: true }).type('New sub title', { delay: 0, force: true });
-    cy.get('[data-testid="textarea-html"]', { timeout: 100000 }).clear({ force: true }).type('New Text Area Content', { delay: 0, force: true });
+    cy.get('[data-testid="input-pre-title"]', { timeout: 30000 }).clear({ force: true }).type('New Pre title', { delay: 0, force: true });
+    cy.get('[data-testid="input-title"]', { timeout: 30000 }).clear({ force: true }).type('New title', { delay: 0, force: true });
+    cy.get('[data-testid="input-sub-title"]', { timeout: 30000 }).clear({ force: true }).type('New sub title', { delay: 0, force: true });
+    cy.get('[data-testid="textarea-html"]', { timeout: 30000 }).clear({ force: true }).type('New Text Area Content', { delay: 0, force: true });
 
-    cy.get('[data-testid="imagetext"]', { timeout: 100000 }).scrollIntoView().should('exist');
+    cy.get('[data-testid="imagetext"]', { timeout: 30000 }).scrollIntoView().should('exist');
 
-    cy.get('[data-testid="imagetext"]', { timeout: 100000 }).within(() => {
-    cy.get('[data-testid="text-pretitle"]', { timeout: 100000 }).should('have.text', 'New Pre title');
-    cy.get('[data-testid="text-title"]', { timeout: 100000 }).should('have.text', 'New title');
-    cy.get('[data-testid="text-subtitle"]', { timeout: 100000 }).should('have.text', 'New sub title');
-    cy.get('[data-testid="text-html"]', { timeout: 100000 }).should('have.text', 'New Text Area Content');
+    cy.get('[data-testid="imagetext"]', { timeout: 30000 }).within(() => {
+    cy.get('[data-testid="text-pretitle"]', { timeout: 30000 }).should('have.text', 'New Pre title');
+    cy.get('[data-testid="text-title"]', { timeout: 30000 }).should('have.text', 'New title');
+    cy.get('[data-testid="text-subtitle"]', { timeout: 30000 }).should('have.text', 'New sub title');
+    cy.get('[data-testid="text-html"]', { timeout: 30000 }).should('have.text', 'New Text Area Content');
 
       });
 
