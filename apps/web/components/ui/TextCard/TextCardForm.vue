4e3306b8c3c022610d247473e10c92e8cdfff216
oivan-plenty <114394643+oivan-plenty@users.noreply.github.com>
2025-02-10 11:17:41 +0200

    fix: fixed color in placeholder (#1032)
    Co-authored-by: oivan-plenty <oana.ivan@plentysystems.com>

--- apps/web/components/ui/TextCard/TextCardForm.vue
+++ apps/web/components/ui/TextCard/TextCardForm.vue
@@ -67,7 +67,7 @@
         </div>
         <div class="py-2">
           <div class="flex justify-between mb-2">
-            <UiFormLabel>Text color</UiFormLabel>
+            <UiFormLabel>Text Color</UiFormLabel>
           </div>
           <label>
             <SfInput v-model="textCardBlock.text.color" type="text">
@@ -81,14 +81,13 @@
                 </label>
               </template>
             </SfInput>
-            <span class="typography-text-xs text-neutral-700">Choose primary color</span>
           </label>
         </div>
 
         <fieldset class="py-2">
-          <legend class="text-sm font-medium text-gray-700">Text alignment</legend>
+          <legend class="text-sm font-medium text-black">Text alignment</legend>
 
-          <div class="mt-2 w-full inline-flex rounded-lg border border-gray-300 bg-white text-gray-700 overflow-hidden">
+          <div class="w-full inline-flex rounded-lg border border-gray-300 bg-white text-gray-700 overflow-hidden">
             <div
               for="text-align-left"
               class="flex items-center justify-center w-1/3 px-4 py-2 cursor-pointer text-sm border-r"
