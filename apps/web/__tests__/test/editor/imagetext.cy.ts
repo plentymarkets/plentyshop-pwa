cd07f5d3408a4982366618911e20401271d52b6b
aoltean-plenty <alexandru.oltean@plentysystems.com>
2025-02-10 05:54:16 -0800

    Tests

--- apps/web/__tests__/test/editor/imagetext.cy.ts
+++ apps/web/__tests__/test/editor/imagetext.cy.ts
@@ -14,24 +14,32 @@ describe('EditMode', () => {
       .eq(3)
       .should('exist')
       .click({ force: true });
-    cy.wait(1000);
-    cy.get('[data-testid="image-text-form"]', { timeout: 30000 }).should('exist');
-    cy.get('[data-testid="text-group"]', { timeout: 30000 }).should('exist').click();
+    cy.wait(5000);
+    
+    cy.get('[data-testid="image-text-form"]', { timeout: 3000 }).should('exist');
+    cy.get('body').then(($body) => {
+        if ($body.find('[data-testid="image-text-form"]').length > 0) {
+          cy.log('✅ Form is present');
+        } else {
+          cy.log('❌ Form is missing');
+        }
+      });
+    cy.get('[data-testid="text-group"]', { timeout: 3000 }).should('exist').click();
 
     
 
-    cy.get('[data-testid="input-pre-title"]', { timeout: 30000 }).clear({ force: true }).type('New Pre title', { delay: 0, force: true });
-    cy.get('[data-testid="input-title"]', { timeout: 30000 }).clear({ force: true }).type('New title', { delay: 0, force: true });
-    cy.get('[data-testid="input-sub-title"]', { timeout: 30000 }).clear({ force: true }).type('New sub title', { delay: 0, force: true });
-    cy.get('[data-testid="textarea-html"]', { timeout: 30000 }).clear({ force: true }).type('New Text Area Content', { delay: 0, force: true });
+    cy.get('[data-testid="input-pre-title"]', { timeout: 3000 }).clear({ force: true }).type('New Pre title', { delay: 0, force: true });
+    cy.get('[data-testid="input-title"]', { timeout: 3000 }).clear({ force: true }).type('New title', { delay: 0, force: true });
+    cy.get('[data-testid="input-sub-title"]', { timeout: 3000 }).clear({ force: true }).type('New sub title', { delay: 0, force: true });
+    cy.get('[data-testid="textarea-html"]', { timeout: 3000 }).clear({ force: true }).type('New Text Area Content', { delay: 0, force: true });
 
-    cy.get('[data-testid="imagetext"]', { timeout: 30000 }).scrollIntoView().should('exist');
+    cy.get('[data-testid="imagetext"]', { timeout: 3000 }).scrollIntoView().should('exist');
 
-    cy.get('[data-testid="imagetext"]', { timeout: 30000 }).within(() => {
-    cy.get('[data-testid="text-pretitle"]', { timeout: 30000 }).should('have.text', 'New Pre title');
-    cy.get('[data-testid="text-title"]', { timeout: 30000 }).should('have.text', 'New title');
-    cy.get('[data-testid="text-subtitle"]', { timeout: 30000 }).should('have.text', 'New sub title');
-    cy.get('[data-testid="text-html"]', { timeout: 30000 }).should('have.text', 'New Text Area Content');
+    cy.get('[data-testid="imagetext"]', { timeout: 3000 }).within(() => {
+    cy.get('[data-testid="text-pretitle"]', { timeout: 3000 }).should('have.text', 'New Pre title');
+    cy.get('[data-testid="text-title"]', { timeout: 3000 }).should('have.text', 'New title');
+    cy.get('[data-testid="text-subtitle"]', { timeout: 3000 }).should('have.text', 'New sub title');
+    cy.get('[data-testid="text-html"]', { timeout: 3000 }).should('have.text', 'New Text Area Content');
 
       });
 
