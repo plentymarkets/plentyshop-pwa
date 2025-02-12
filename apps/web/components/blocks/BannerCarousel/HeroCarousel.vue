da6de9201a32375ea7ca1019199045229644d2e7
abocsan-plenty <129151096+abocsan-plenty@users.noreply.github.com>
2025-02-12 11:08:28 +0200

    chore: test lhci

--- apps/web/components/blocks/BannerCarousel/HeroCarousel.vue
+++ apps/web/components/blocks/BannerCarousel/HeroCarousel.vue
@@ -1,36 +1,41 @@
 <template>
-  <Swiper
-    :key="index"
-    :modules="enableModules ? [Pagination, Navigation] : []"
-    :slides-per-view="1"
-    :loop="true"
-    :pagination="paginationConfig"
-    :navigation="navigationConfig"
-    class="!z-0 !w-full !max-h-[85vh]"
-    @swiper="onSwiperInit"
-    @slide-change="onSlideChange"
-  >
-    <SwiperSlide v-for="(bannerItem, slideIndex) in bannerItems" :key="slideIndex">
-      <BlocksBannerCarouselBanner :banner-props="bannerItem" :index="slideIndex" />
-    </SwiperSlide>
-    <div
-      v-if="enableModules"
-      :class="`swiper-pagination swiper-pagination-${index} swiper-pagination-bullets swiper-pagination-horizontal`"
-    />
-  </Swiper>
+  <ClientOnly>
+    <Swiper
+      :key="index"
+      :modules="enableModules ? [Pagination, Navigation] : []"
+      :slides-per-view="1"
+      :loop="true"
+      :pagination="paginationConfig"
+      :navigation="navigationConfig"
+      class="!z-0 !w-full !max-h-[85vh]"
+      @swiper="onSwiperInit"
+      @slide-change="onSlideChange"
+    >
+      <SwiperSlide v-for="(bannerItem, slideIndex) in bannerItems" :key="slideIndex">
+        <BlocksBannerCarouselBanner :banner-props="bannerItem" :index="slideIndex" />
+      </SwiperSlide>
+      <div
+        v-if="enableModules"
+        :class="`swiper-pagination swiper-pagination-${index} swiper-pagination-bullets swiper-pagination-horizontal`"
+      />
+    </Swiper>
 
-  <div
-    v-if="enableModules && handleArrows()"
-    :key="`prev-${index}`"
-    :class="`swiper-button-prev swiper-button-prev-${index}`"
-    :style="{ color: controls.color + ' !important' }"
-  />
-  <div
-    v-if="enableModules && handleArrows()"
-    :key="`next-${index}`"
-    :class="`swiper-button-next swiper-button-next-${index}`"
-    :style="{ color: controls.color + ' !important' }"
-  />
+    <div
+      v-if="enableModules && handleArrows()"
+      :key="`prev-${index}`"
+      :class="`swiper-button-prev swiper-button-prev-${index}`"
+      :style="{ color: controls.color + ' !important' }"
+    />
+    <div
+      v-if="enableModules && handleArrows()"
+      :key="`next-${index}`"
+      :class="`swiper-button-next swiper-button-next-${index}`"
+      :style="{ color: controls.color + ' !important' }"
+    />
+    <template #fallback>
+      <BlocksBannerCarouselBannerSkeleton />
+    </template>
+  </ClientOnly>
 </template>
 
 <script setup lang="ts">
