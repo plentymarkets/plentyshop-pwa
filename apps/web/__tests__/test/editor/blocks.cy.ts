e34ca4899ee5b90139d5a00fbc1703653bf55506
aoltean <78078265+aoltean-plenty@users.noreply.github.com>
2025-02-18 04:01:46 -0800

    fix: remove only (#1063)
    Co-authored-by: aoltean-plenty <alexandru.oltean@plentysystems.com>

--- apps/web/__tests__/test/editor/blocks.cy.ts
+++ apps/web/__tests__/test/editor/blocks.cy.ts
@@ -33,8 +33,7 @@ describe('Blocks', () => {
       editor.recommendedProductsExist();
     });
 
-    it.only('shpuld ensure correct spacings', () => {
+    it('should ensure correct spacings', () => {
       editor.checkWrapperSpacings();
     });
-    
 });
