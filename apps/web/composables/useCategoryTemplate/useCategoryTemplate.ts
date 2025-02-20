79797d7bb8aa8a565434d2da1f2e6c5da1ee3aaa
doproiescu-plenty <126688988+doproiescu-plenty@users.noreply.github.com>
2025-02-20 10:14:12 +0200

    chore: revert lazy async

--- apps/web/composables/useCategoryTemplate/useCategoryTemplate.ts
+++ apps/web/composables/useCategoryTemplate/useCategoryTemplate.ts
@@ -24,9 +24,7 @@ export const useCategoryTemplate: UseCategoryTemplateReturn = () => {
    */
   const fetchCategoryTemplate: FetchCategoryTemplate = async (categoryId) => {
     state.value.loading = true;
-    const { data } = await useAsyncData(() => useSdk().plentysystems.getCategoryTemplate({ id: categoryId }), {
-      lazy: true,
-    });
+    const { data } = await useAsyncData(() => useSdk().plentysystems.getCategoryTemplate({ id: categoryId }));
 
     state.value.loading = false;
     state.value.data = data?.value?.data ?? state.value.data;
