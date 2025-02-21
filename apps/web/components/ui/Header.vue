fed5fd77d6f4ecddc11a120b58f48d3edbd1a509
oivan-plenty <oana.ivan@plentysystems.com>
2025-02-21 14:01:58 +0200

    exit preview mode

--- apps/web/components/ui/Header.vue
+++ apps/web/components/ui/Header.vue
@@ -11,7 +11,7 @@
             variant="tertiary"
             square
             data-testid="open-languageselect-button"
-            :disabled="(showConfigurationDrawer && isEditing) || (showConfigurationDrawer && disableActions)"
+            :disabled="isPreview || (showConfigurationDrawer && isEditing) || (showConfigurationDrawer && disableActions)"
             @click="toggleLanguageSelect()"
           >
             <template #prefix>
@@ -220,6 +220,7 @@ const viewport = useViewport();
 const runtimeConfig = useRuntimeConfig();
 const showConfigurationDrawer = runtimeConfig.public.showConfigurationDrawer;
 const { isEditing, disableActions } = useEditor();
+const { isPreview } = useBlockManager();
 
 onNuxtReady(() => {
   cartItemsCount.value = cart.value?.items?.reduce((price, { quantity }) => price + quantity, 0) ?? 0;
