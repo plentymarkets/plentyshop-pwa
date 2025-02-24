cc02a5c12cc54501b8fa877408b46b01013495c6
cristian sandru <cristian.sandru@plentysystems.com>
2025-02-24 13:07:01 +0200

    feat: guest checkout login option

--- apps/web/components/ContactInformation/ContactInformation.vue
+++ apps/web/components/ContactInformation/ContactInformation.vue
@@ -2,7 +2,7 @@
   <div data-testid="contact-information" class="md:px-4 py-6">
     <div class="flex justify-between items-center">
       <h2 class="text-neutral-900 text-lg font-bold">{{ $t('contactInfo.heading') }}</h2>
-      <UiButton v-if="!disabled && cart.customerEmail && !isAuthorized" variant="secondary" @click="open">
+      <UiButton v-if="!disabled && cart.customerEmail && !isAuthorized" variant="secondary" @click="openGuest">
         <SfIconBase v-if="isMobile" viewBox="0 0 34 40" class="w-6 h-6 pt-[3px]">
           <path :d="penPath" />
         </SfIconBase>
@@ -13,21 +13,21 @@
     <p v-if="cart.customerEmail" class="mt-4 md:w-[520px]">{{ cart.customerEmail }}</p>
     <div v-else class="w-full md:max-w-[520px]">
       <p>{{ $t('contactInfo.description') }}</p>
-      <UiButton v-if="!disabled" class="mt-4 w-full md:w-auto" variant="secondary" @click="open">
+      <UiButton v-if="!disabled" class="mt-4 w-full md:w-auto" variant="secondary" @click="openGuest">
         {{ $t('contactInfo.add') }}
       </UiButton>
     </div>
 
-    <div class="w-full mt-2 flex flex-col sm:flex-row">
+    <div v-if="isGuest" class="w-full mt-2 flex flex-col sm:flex-row">
       <div>{{ $t('auth.signup.alreadyHaveAccount') }}</div>
-      <SfLink class="select-none hover:cursor-pointer sm:ml-2">
-        {{ $t('auth.signup.signInLinkLabel') }}
+      <SfLink class="select-none hover:cursor-pointer sm:ml-2" @click="openLoginModal">
+        {{ $t('auth.signup.logInLinkLabel') }}
       </SfLink>
     </div>
 
     <UiModal
-      v-if="isOpen"
-      v-model="isOpen"
+      v-if="isGuestOpen"
+      v-model="isGuestOpen"
       :disable-click-away="!cart.customerEmail"
       :disable-esc="!cart.customerEmail"
       tag="section"
@@ -36,14 +36,46 @@
       aria-labelledby="contact-modal-title"
     >
       <header>
-        <UiButton v-if="cart.customerEmail" square variant="tertiary" class="absolute right-2 top-2" @click="close">
+        <UiButton
+          v-if="cart.customerEmail"
+          square
+          variant="tertiary"
+          class="absolute right-2 top-2"
+          @click="closeGuest"
+        >
           <SfIconClose />
         </UiButton>
-        <h3 id="contact-modal-title" class="text-neutral-900 text-lg md:text-2xl font-bold mb-4">
+        <h3 id="contact-modal-title" class="text-neutral-900 text-lg md:text-2xl font-bold">
           {{ $t('contactInfo.heading') }}
         </h3>
+        <div class="w-full flex flex-col sm:flex-row mb-4">
+          <div>{{ $t('auth.signup.alreadyHaveAccount') }}</div>
+          <SfLink class="select-none hover:cursor-pointer sm:ml-2" @click="openLoginModal">
+            {{ $t('auth.signup.logInLinkLabel') }}
+          </SfLink>
+        </div>
       </header>
-      <ContactInformationForm @on-save="saveContactInformation" @on-cancel="close" />
+      <ContactInformationForm @on-save="saveContactInformation" @on-cancel="closeGuest" />
+    </UiModal>
+
+    <UiModal
+      v-if="isAuthenticationOpen"
+      v-model="isAuthenticationOpen"
+      tag="section"
+      class="h-full md:w-[500px] md:h-fit m-0 p-0 overflow-y-auto"
+    >
+      <header>
+        <UiButton
+          :aria-label="$t('closeDialog')"
+          square
+          variant="tertiary"
+          class="absolute right-2 top-2"
+          @click="openGuestModal"
+        >
+          <SfIconClose />
+        </UiButton>
+      </header>
+      <LoginComponent :is-soft-login="true" :is-modal="true" @logged-in="closeAuthentication" />
     </UiModal>
   </div>
 </template>
@@ -56,8 +88,9 @@ import { penPath } from '~/assets/icons/paths/pen';
 const { disabled = false } = defineProps<ContactInformationProps>();
 
 const viewport = useViewport();
-const { data: sessionData, loginAsGuest, getSession, isAuthorized } = useCustomer();
-const { isOpen, open, close } = useDisclosure();
+const { data: sessionData, loginAsGuest, getSession, isAuthorized, isGuest } = useCustomer();
+const { isOpen: isGuestOpen, open: openGuest, close: closeGuest } = useDisclosure();
+const { isOpen: isAuthenticationOpen, open: openAuthentication, close: closeAuthentication } = useDisclosure();
 const cart = ref({ customerEmail: sessionData.value?.user?.email ?? sessionData.value?.user?.guestMail ?? '' });
 const isMobile = computed(() => viewport.isLessThan('md'));
 
@@ -68,11 +101,25 @@ const saveContactInformation = async (email: string) => {
   close();
 };
 
+const openGuestModal = () => {
+  if (!isGuestOpen.value) {
+    if (isAuthenticationOpen.value) closeAuthentication();
+    if (!isAuthorized.value && !isGuest.value && !cart.value.customerEmail) openGuest();
+  }
+};
+
+const openLoginModal = () => {
+  if (!isAuthenticationOpen.value) {
+    if (isGuestOpen.value) closeGuest();
+    openAuthentication();
+  }
+};
+
 watch(
   () => sessionData.value?.user,
   (userData) => {
     cart.value.customerEmail = userData?.email ?? userData?.guestMail ?? '';
-    cart.value.customerEmail ? close() : open();
+    cart.value.customerEmail ? closeGuest() : openGuest();
   },
   { immediate: true },
 );
