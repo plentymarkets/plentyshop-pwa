003892faa251a15b07616e3f647685d26fb6bfa6
cristian sandru <cristian.sandru@plentysystems.com>
2025-02-26 09:06:25 +0200

    feat: guest email auto save

--- apps/web/components/ContactInformation/ContactInformation.vue
+++ apps/web/components/ContactInformation/ContactInformation.vue
@@ -1,62 +1,35 @@
 <template>
   <div data-testid="contact-information" class="md:px-4 py-6">
-    <div class="flex justify-between items-center">
-      <h2 class="text-neutral-900 text-lg font-bold">{{ $t('contactInfo.heading') }}</h2>
-      <UiButton v-if="!disabled && cart.customerEmail && !isAuthorized" variant="secondary" @click="openGuest">
-        <SfIconBase v-if="isMobile" viewBox="0 0 34 40" class="w-6 h-6 pt-[3px]">
-          <path :d="penPath" />
-        </SfIconBase>
-        <template v-else>{{ $t('contactInfo.edit') }}</template>
-      </UiButton>
-    </div>
+    <h2 class="w-full text-neutral-900 text-lg font-bold">
+      {{ t('contactInfo.heading') }}
+    </h2>
 
-    <p v-if="cart.customerEmail" class="mt-4 md:w-[520px]">{{ cart.customerEmail }}</p>
-    <div v-else class="w-full md:max-w-[520px]">
-      <p>{{ $t('contactInfo.description') }}</p>
-      <UiButton v-if="!disabled" class="mt-4 w-full md:w-auto" variant="secondary" @click="openGuest">
-        {{ $t('contactInfo.add') }}
-      </UiButton>
-    </div>
-
-    <div v-if="isGuest" class="w-full mt-2 flex flex-col sm:flex-row">
-      <div>{{ $t('auth.signup.alreadyHaveAccount') }}</div>
-      <SfLink class="select-none hover:cursor-pointer sm:ml-2" @click="openLoginModal">
-        {{ $t('auth.signup.logInLinkLabel') }}
+    <div v-if="!disabled && (isGuest || (!isAuthorized && !isGuest))" class="w-full flex flex-col sm:flex-row mb-4">
+      <div>{{ t('auth.signup.alreadyHaveAccount') }}</div>
+      <SfLink class="select-none hover:cursor-pointer sm:ml-2" @click="openAuthentication">
+        {{ t('auth.signup.logInLinkLabel') }}
       </SfLink>
     </div>
 
-    <UiModal
-      v-if="isGuestOpen"
-      v-model="isGuestOpen"
-      :disable-click-away="!cart.customerEmail"
-      :disable-esc="!cart.customerEmail"
-      tag="section"
-      role="dialog"
-      class="h-full w-full overflow-auto md:w-[500px] md:h-fit"
-      aria-labelledby="contact-modal-title"
-    >
-      <header>
-        <UiButton
-          v-if="cart.customerEmail"
-          square
-          variant="tertiary"
-          class="absolute right-2 top-2"
-          @click="closeGuest"
-        >
-          <SfIconClose />
-        </UiButton>
-        <h3 id="contact-modal-title" class="text-neutral-900 text-lg md:text-2xl font-bold">
-          {{ $t('contactInfo.heading') }}
-        </h3>
-        <div class="w-full flex flex-col sm:flex-row mb-4">
-          <div>{{ $t('auth.signup.alreadyHaveAccount') }}</div>
-          <SfLink class="select-none hover:cursor-pointer sm:ml-2" @click="openLoginModal">
-            {{ $t('auth.signup.logInLinkLabel') }}
-          </SfLink>
-        </div>
-      </header>
-      <ContactInformationForm @on-save="saveContactInformation" @on-cancel="closeGuest" />
-    </UiModal>
+    <div v-if="customerEmail && isAuthorized" class="w-full">{{ t('contactInfo.email') }}: {{ customerEmail }}</div>
+
+    <form v-if="(!isAuthorized && !isGuest) || isGuest" data-testid="contact-information-form" novalidate>
+      <label>
+        <UiFormLabel>{{ t('contactInfo.email') }} {{ t('form.required') }}</UiFormLabel>
+        <SfInput
+          v-model="customerEmail"
+          wrapper-class="focus:outline focus:outline-offset-1 focus:outline-1 focus-within:outline focus-within:outline-offset-1 focus-within:outline-1"
+          :autofocus="!customerEmail"
+          v-bind="customerEmailAttributes"
+          :invalid="Boolean(errors['customerEmail'])"
+          name="customerEmail"
+          type="email"
+          autocomplete="email"
+          @blur="validateAndSubmitEmail"
+        />
+        <ErrorMessage as="span" name="customerEmail" class="flex text-negative-700 text-sm mt-2" />
+      </label>
+    </form>
 
     <UiModal
       v-if="isAuthenticationOpen"
@@ -66,11 +39,11 @@
     >
       <header>
         <UiButton
-          :aria-label="$t('closeDialog')"
+          :aria-label="t('closeDialog')"
           square
           variant="tertiary"
           class="absolute right-2 top-2"
-          @click="openGuestModal"
+          @click="closeAuthentication"
         >
           <SfIconClose />
         </UiButton>
@@ -81,40 +54,47 @@
 </template>
 
 <script lang="ts" setup>
-import { SfIconBase, SfIconClose, SfLink, useDisclosure } from '@storefront-ui/vue';
-import type { ContactInformationProps } from './types';
-import { penPath } from '~/assets/icons/paths/pen';
 import { AddressType } from '@plentymarkets/shop-api';
+import { SfIconClose, SfInput, SfLink, useDisclosure } from '@storefront-ui/vue';
+import { toTypedSchema } from '@vee-validate/yup';
+import { ErrorMessage, useForm } from 'vee-validate';
+import { object, string } from 'yup';
+import type { ContactInformationProps } from './types';
 
 const { disabled = false } = defineProps<ContactInformationProps>();
 
-const viewport = useViewport();
+const { t } = useI18n();
 const { data: sessionData, loginAsGuest, getSession, isAuthorized, isGuest } = useCustomer();
-const { isOpen: isGuestOpen, open: openGuest, close: closeGuest } = useDisclosure();
 const { isOpen: isAuthenticationOpen, open: openAuthentication, close: closeAuthentication } = useDisclosure();
 const { persistShippingAddress, persistBillingAddress } = useCheckout();
-const cart = ref({ customerEmail: sessionData.value?.user?.email ?? sessionData.value?.user?.guestMail ?? '' });
-const isMobile = computed(() => viewport.isLessThan('md'));
+const guestEmail = ref(sessionData.value?.user?.guestMail ?? '');
+
+const emailSchema = toTypedSchema(
+  object({
+    customerEmail: string()
+      .email(t('errorMessages.email.valid'))
+      .required(t('errorMessages.email.required'))
+      .default(sessionData.value?.user?.email ?? guestEmail.value),
+  }),
+);
+
+const { errors, defineField, validate } = useForm({ validationSchema: emailSchema });
+const [customerEmail, customerEmailAttributes] = defineField('customerEmail');
+
+const validateAndSubmitEmail = async () => {
+  const formData = await validate();
+  const updatedEmail = customerEmail.value as string;
+  const shouldSaveEmail = updatedEmail.trim().toLowerCase() !== guestEmail.value.trim().toLowerCase();
+
+  if (formData.valid && shouldSaveEmail) {
+    await saveContactInformation(updatedEmail);
+    guestEmail.value = updatedEmail;
+  }
+};
 
 const saveContactInformation = async (email: string) => {
-  cart.value.customerEmail = email;
   await loginAsGuest(email);
   await getSession();
-  closeGuest();
-};
-
-const openGuestModal = () => {
-  if (!isGuestOpen.value) {
-    if (isAuthenticationOpen.value) closeAuthentication();
-    if (!isAuthorized.value && !isGuest.value && !cart.value.customerEmail) openGuest();
-  }
-};
-
-const openLoginModal = () => {
-  if (!isAuthenticationOpen.value) {
-    if (isGuestOpen.value) closeGuest();
-    openAuthentication();
-  }
 };
 
 const checkPayPalPaymentsEligible = async () => {
@@ -141,13 +121,4 @@ const handleSuccessfulLogin = async () => {
   await checkPayPalPaymentsEligible();
   closeAuthentication();
 };
-
-watch(
-  () => sessionData.value?.user,
-  (userData) => {
-    cart.value.customerEmail = userData?.email ?? userData?.guestMail ?? '';
-    cart.value.customerEmail ? closeGuest() : openGuest();
-  },
-  { immediate: true },
-);
 </script>
