cfa2738be236dcfee0eb6c84fbec08c12f19a013
aoltean-plenty <alexandru.oltean@plentysystems.com>
2025-03-03 05:58:55 -0800

    Only one call

--- apps/web/composables/usePages/usePages.ts
+++ apps/web/composables/usePages/usePages.ts
@@ -1,32 +1,49 @@
 import type { Category } from './types';
+import { watch } from 'vue';
+
 export const usePages = async () => {
-  const { t } = useI18n();
+  const { t, locale } = useI18n();
   const { getCategoryTree } = useCategoryTree();
 
-  const data = await getCategoryTree();
+  const pages = useState<{ name: string; path: string; children?: { name: string; path: string }[] | undefined }[]>(
+    'pages',
+    () => [],
+  );
 
-  const transformData = (
-    data: Category[],
-    isRoot = true,
-  ): { name: string; path: string; children?: { name: string; path: string }[] | undefined }[] => {
-    const transformedData = data.map((item: Category) => ({
-      name: item.details[0].name,
-      path: `/${item.details[0].nameUrl}`,
-      children: item.children ? transformData(item.children, false) : undefined,
-    }));
+  const fetchPages = async () => {
+    const data = await getCategoryTree();
 
-    if (isRoot && !transformedData.some((page) => page.name === 'Homepage')) {
-      transformedData.unshift({
-        name: t('homepage.homepagetitle'),
-        path: '/',
-        children: undefined,
-      });
-    }
+    const transformData = (
+      data: Category[],
+      isRoot = true,
+    ): { name: string; path: string; children?: { name: string; path: string }[] | undefined }[] => {
+      const transformedData = data.map((item: Category) => ({
+        name: item.details[0].name,
+        path: `/${item.details[0].nameUrl}`,
+        children: item.children ? transformData(item.children, false) : undefined,
+      }));
 
-    return transformedData;
+      if (isRoot && !transformedData.some((page) => page.name === 'Homepage')) {
+        transformedData.unshift({
+          name: t('homepage.homepagetitle'),
+          path: '/',
+          children: undefined,
+        });
+      }
+
+      return transformedData;
+    };
+
+    pages.value = transformData(data);
   };
 
-  const pages = ref(transformData(data));
+  if (pages.value.length === 0) {
+    await fetchPages();
+  }
+
+  watch(locale, async () => {
+    await fetchPages();
+  });
 
   return {
     pages,
