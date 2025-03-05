e5d1adf3da2b2ec20752a4d0167bec304b8785dd
aoltean-plenty <alexandru.oltean@plentysystems.com>
2025-03-05 01:26:56 -0800

    Fix z-index tablet

--- apps/web/components/MegaMenu/MegaMenu.vue
+++ apps/web/components/MegaMenu/MegaMenu.vue
@@ -1,5 +1,5 @@
 <template>
-  <header ref="referenceRef" :class="headerClass" class="relative w-full md:sticky md:shadow-md z-10">
+  <header ref="referenceRef" :class="headerClass" class="relative w-full md:sticky md:shadow-md">
     <div
       class="flex justify-between items-center flex-wrap md:flex-nowrap px-4 md:px-10 py-2 md:py-5 w-full border-0 bg-primary-500 border-neutral-200"
       data-testid="navbar-top"
