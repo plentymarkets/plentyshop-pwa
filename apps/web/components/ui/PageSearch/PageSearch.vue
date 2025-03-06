22c090230beca5bfa1bfe62b7ebcdf0ea73b3b68
oivan-plenty <oana.ivan@plentysystems.com>
2025-03-06 12:45:08 +0200

    filtering

--- apps/web/components/ui/PageSearch/PageSearch.vue
+++ apps/web/components/ui/PageSearch/PageSearch.vue
@@ -1,56 +1,71 @@
 <template>
 <div
-  class="p-2 absolute left-0 top-full mt-2 bg-white shadow-lg rounded-lg border border-gray-200 overflow-hidden w-auto min-w-max"
+  class="p-2 absolute left-0 top-full mt-2 bg-white shadow-lg rounded-lg border border-gray-200 overflow-hidden w-auto min-w-max h-[300px]"
 >
-  <form ref="referenceRef" role="search" @submit.prevent="submit">
-    <SfInput
-      ref="inputRef"
-      v-model="inputModel"
-      aria-label="Search"
-      placeholder="Search"
-      @focus="open"
-      @keydown="handleInputKeyDown"
-    >
-      <template #prefix><SfIconSearch /></template>
-      <template #suffix>
-        <button
-          v-if="inputModel"
-          type="button"
-          aria-label="Reset search"
-          class="flex rounded-md focus-visible:outline focus-visible:outline-offset"
-          @click="reset"
-        >
-          <SfIconCancel /></button
-      ></template>
-    </SfInput>
-  </form>
+<!--  <form ref="referenceRef" role="search" @submit.prevent="submit">-->
+<!--    <SfInput-->
+<!--      ref="inputRef"-->
+<!--      v-model="inputModel"-->
+<!--      aria-label="Search"-->
+<!--      placeholder="Search"-->
+<!--      @focus="open"-->
+<!--      @keydown="handleInputKeyDown"-->
+<!--    >-->
+<!--      <template #prefix><SfIconSearch /></template>-->
+<!--      <template #suffix>-->
+<!--        <button-->
+<!--          v-if="inputModel"-->
+<!--          type="button"-->
+<!--          aria-label="Reset search"-->
+<!--          class="flex rounded-md focus-visible:outline focus-visible:outline-offset"-->
+<!--          @click="reset"-->
+<!--        >-->
+<!--          <SfIconCancel /></button-->
+<!--      ></template>-->
+<!--    </SfInput>-->
+<!--  </form>-->
 
-  <div>
-    <ul v-if="flattenedPages.length > 0" class="p-2 transition-all duration-300 max-h-[300px] w-auto min-w-max overflow-y-auto">
-      <li v-for="page in flattenedPages" :key="page.name">
-        <SfListItem
-          tag="button"
-          type="button"
-          class="p-2 text-gray-900 hover:bg-gray-100 rounded-md cursor-pointer flex justify-between text-left"
-          @click="() => selectValue(page)"
-          @keydown.enter.space.prevent="selectValue(page)"
-        >
-          <div class="flex items-center">
-            <span class="flex items-center">
-              <SfIconHome v-if="page.name === t('homepage.homepagetitle')" class="w-4 h-4 mr-2" />
-              {{ page.name }}
-            </span>
-            <span v-if="page.path.split('/').length > 2">
-              {{ page.path }}
-            </span>
-          </div>
-        </SfListItem>
-      </li>
-    </ul>
-    <div v-else class="p-2 text-gray-500">No results found.</div>
-  </div>
+<!--  <div>-->
+<!--    <ul v-if="filteredPages.length > 0" class="p-2 transition-all duration-300 max-h-[300px] w-auto min-w-max overflow-y-auto">-->
+<!--      <li v-for="page in filteredPages" :key="page.name">-->
+<!--        <SfListItem-->
+<!--          tag="button"-->
+<!--          type="button"-->
+<!--          class="p-2 text-gray-900 hover:bg-gray-100 rounded-md cursor-pointer flex justify-between text-left"-->
+<!--          @click="() => selectValue(page)"-->
+<!--          @keydown.enter.space.prevent="selectValue(page)"-->
+<!--        >-->
+<!--          <div class="flex items-center">-->
+<!--            <span class="flex items-center">-->
+<!--              <SfIconHome v-if="page.name === t('homepage.homepagetitle')" class="w-4 h-4 mr-2" />-->
+<!--              {{ page.name }}-->
+<!--            </span>-->
+<!--            <span v-if="page.path.split('/').length > 2">-->
+<!--              {{ page.path }}-->
+<!--            </span>-->
+<!--          </div>-->
+<!--        </SfListItem>-->
+<!--      </li>-->
+<!--    </ul>-->
+<!--    <div v-else class="p-2 text-gray-500">No results found.</div>-->
+<!--  </div>-->
+  <Multiselect
+    v-model="inputModel"
+    data-testid="font-select"
+    :options="options"
+    label="name"
+    track-by="name"
+    :allow-empty="false"
+    class="cursor-pointer"
+    select-label=""
+    deselect-label="Selected"
+    @select="submit"
+>
+    <template #singleLabel>
+test
+    </template>
+  </Multiselect>
 </div>
-
 </template>
 
 <script lang="ts" setup>
@@ -66,6 +81,7 @@ import {
   useTrapFocus,
   SfIconHome,
 } from '@storefront-ui/vue';
+import Multiselect from 'vue-multiselect';
 const emit = defineEmits(['pageSelected']);
 
 const inputModel = ref('');
@@ -75,21 +91,22 @@ const dropdownListRef = ref();
 const { pages } = await usePages();
 
 const { isOpen, close, open } = useDisclosure();
-const { referenceRef } = useDropdown({
-  isOpen,
-  onClose: close,
-  placement: 'bottom-start',
-  middleware: [offset(4)],
-});
-const { focusables: focusableElements } = useTrapFocus(dropdownListRef as Ref<HTMLElement>, {
-  trapTabs: false,
-  arrowKeysUpDown: true,
-  activeState: isOpen,
-  initialFocus: false,
-});
+// const { referenceRef } = useDropdown({
+//   isOpen,
+//   onClose: close,
+//   placement: 'bottom-start',
+//   middleware: [offset(4)],
+// });
+// const { focusables: focusableElements } = useTrapFocus(dropdownListRef as Ref<HTMLElement>, {
+//   trapTabs: false,
+//   arrowKeysUpDown: true,
+//   activeState: isOpen,
+//   initialFocus: false,
+// });
 
 const { locale } = useI18n();
 const { t } = useI18n();
+// const filteredPages = ref<{ name: string; path: string }[]>([]);
 
 const flattenPages = (
   pages: { name: string; path: string; children?: unknown[] }[],
@@ -103,9 +120,10 @@ const flattenPages = (
       );
     }
   });
-  console.log('flatPages', flatPages);
   return flatPages;
 };
+const options = ref(flattenPages(pages.value))
+console.log('options', options.value);
 
 // const filteredPages = computed(() => {
 //   const searchTerm = inputModel.value.toLowerCase();
@@ -114,15 +132,14 @@ const flattenPages = (
 //   );
 // });
 
-const filteredPages = computed(() => {
-  const searchTerm = inputModel.value.toLowerCase();
-  const allPages = flattenPages(pages.value).filter((page) =>
-    page.name.toLowerCase().includes(searchTerm) || page.path.toLowerCase().includes(searchTerm),);
-  console.log("allPages", allPages);
-  return allPages;
-});
+// const filteredPages = computed(() => {
+//   const searchTerm = inputModel.value.toLowerCase();
+//   const allPages = flattenPages(pages.value).filter((page) =>
+//     page.name.toLowerCase().includes(searchTerm) || page.path.toLowerCase().includes(searchTerm),);
+//   console.log("allPages", allPages);
+//   return allPages;
+// });
 
-const flattenedPages = computed(() => filteredPages.value);
 
 const submit = () => {
   close();
@@ -145,25 +162,25 @@ const selectValue = (page: { name: string; path: string }) => {
   navigateTo(`/${locale.value}${page.path}`);
 };
 
-const handleInputKeyDown = (event: KeyboardEvent) => {
-  if (event.key === 'Escape') reset();
-  if (event.key === 'ArrowUp') {
-    open();
-    if (isOpen && focusableElements.value.length > 0) {
-      focusableElements.value[focusableElements.value.length - 1].focus();
-    }
-  }
-  if (event.key === 'ArrowDown') {
-    open();
-    if (isOpen && focusableElements.value.length > 0) {
-      focusableElements.value[0].focus();
-    }
-  }
-};
+// const handleInputKeyDown = (event: KeyboardEvent) => {
+//   if (event.key === 'Escape') reset();
+//   if (event.key === 'ArrowUp') {
+//     open();
+//     if (isOpen && focusableElements.value.length > 0) {
+//       focusableElements.value[focusableElements.value.length - 1].focus();
+//     }
+//   }
+//   if (event.key === 'ArrowDown') {
+//     open();
+//     if (isOpen && focusableElements.value.length > 0) {
+//       focusableElements.value[0].focus();
+//     }
+//   }
+// };
 
-watch(inputModel, () => {
-  if (inputModel.value === '') {
-    reset();
-  }
-});
+// watch(inputModel, () => {
+//   if (inputModel.value === '') {
+//     reset();
+//   }
+// });
 </script>
