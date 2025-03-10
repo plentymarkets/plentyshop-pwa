2a11ff70ce80250b63a6642f6516429c618fead3
abocsan-plenty <129151096+abocsan-plenty@users.noreply.github.com>
2025-03-10 15:35:37 +0200

    chore: unit tests fix

--- apps/web/components/blocks/BannerCarousel/__tests__/Banner.button.spec.ts
+++ apps/web/components/blocks/BannerCarousel/__tests__/Banner.button.spec.ts
@@ -1,115 +1,115 @@
-// import { describe, it, expect } from 'vitest';
-// import { mount } from '@vue/test-utils'
-// import { BlocksBannerCarouselBanner } from '#components';
-//
-// describe('Banner button', () => {
-//   it('should not render a button if no button is provided', () => {
-//     const wrapper = mount(BlocksBannerCarouselBanner, {
-//       props: {
-//         bannerProps: {
-//           name: 'Banner',
-//           type: 'content',
-//           content: {
-//           text: {
-//             pretitle: 'Test pretitle',
-//             title: 'Test title',
-//             subtitle: 'Test subtitle',
-//             htmlDescription: '<p>Test description</p>',
-//           },
-//           image: {},
-//           button: {},
-//       }
-//         },
-//
-//         index: 0,
-//       },
-//     });
-//
-//     expect(wrapper.find('[data-testid="banner-button-0"]').exists()).toBe(false);
-//   });
-//
-//   it('should not render a button if no label is provided', () => {
-//     const wrapper = mount(BlocksBannerCarouselBanner, {
-//       props: {
-//         bannerProps: {
-//           name: 'Banner',
-//           type: 'content',
-//           content: {
-//             text: {
-//               pretitle: 'Test pretitle',
-//               title: 'Test title',
-//               subtitle: 'Test subtitle',
-//               htmlDescription: '<p>Test description</p>',
-//             },
-//             image: {},
-//             button: {
-//               link: '/test-link',
-//             },
-//           }
-//         },
-//
-//         index: 0,
-//       },
-//     });
-//
-//     expect(wrapper.find('[data-testid="banner-button-0"]').exists()).toBe(false);
-//   });
-//
-//   it('should not render a button if no link is provided', () => {
-//     const wrapper = mount(BlocksBannerCarouselBanner, {
-//       props: {
-//         bannerProps: {
-//           name: 'Banner',
-//           type: 'content',
-//           content: {
-//             text: {
-//               pretitle: 'Test pretitle',
-//               title: 'Test title',
-//               subtitle: 'Test subtitle',
-//               htmlDescription: '<p>Test description</p>',
-//             },
-//             image: {},
-//             button: {
-//               label: 'Test button',
-//             },
-//           }
-//         },
-//
-//         index: 0,
-//       },
-//     });
-//
-//     expect(wrapper.find('[data-testid="banner-button-0"]').exists()).toBe(false);
-//   });
-//
-//   it('should set the given button variant', () => {
-//     const wrapper = mount(BlocksBannerCarouselBanner, {
-//       props: {
-//         bannerProps: {
-//           name: 'Banner',
-//           type: 'content',
-//           content: {
-//             text: {
-//               pretitle: 'Test pretitle',
-//               title: 'Test title',
-//               subtitle: 'Test subtitle',
-//               htmlDescription: '<p>Test description</p>',
-//             },
-//             image: {},
-//             button: {
-//               label: 'Test button',
-//               link: '/test-link',
-//               variant: 'secondary',
-//             },
-//           }
-//         },
-//
-//         index: 0,
-//       },
-//     });
-//
-//     const button = wrapper.find('[data-testid="banner-button-0"]');
-//
-//     expect(button.attributes('class')).toContain('text-primary-500');
-//   });
-// });
+import { describe, it, expect } from 'vitest';
+import { mount } from '@vue/test-utils'
+import { BlocksBannerCarouselBanner } from '#components';
+
+describe('Banner button', () => {
+  it('should not render a button if no button is provided', () => {
+    const wrapper = mount(BlocksBannerCarouselBanner, {
+     props: {
+       name: 'Banner',
+       type: 'content',
+       content: {
+         text: {
+           pretitle: 'Test pretitle',
+           title: 'Test title',
+           subtitle: 'Test subtitle',
+           htmlDescription: '<p>Test description</p>',
+         },
+         image: {},
+         button: {},
+       },
+       index: 0,
+       meta: {
+         uuid: '11111111-1111-4111-8111-111111111111'
+       }
+     }
+    });
+
+    expect(wrapper.find('[data-testid="banner-button-0"]').exists()).toBe(false);
+  });
+
+  it('should not render a button if no label is provided', () => {
+    const wrapper = mount(BlocksBannerCarouselBanner, {
+     props: {
+       name: 'Banner',
+       type: 'content',
+       content: {
+         text: {
+           pretitle: 'Test pretitle',
+           title: 'Test title',
+           subtitle: 'Test subtitle',
+           htmlDescription: '<p>Test description</p>',
+         },
+         image: {},
+         button: {
+           link: '/test-link',
+         },
+       },
+       index: 0,
+       meta: {
+         uuid: '11111111-1111-4111-8111-111111111111'
+       }
+     }
+    });
+
+    expect(wrapper.find('[data-testid="banner-button-0"]').exists()).toBe(false);
+  });
+
+  it('should not render a button if no link is provided', () => {
+    const wrapper = mount(BlocksBannerCarouselBanner, {
+     props: {
+       name: 'Banner',
+       type: 'content',
+       content: {
+         text: {
+           pretitle: 'Test pretitle',
+           title: 'Test title',
+           subtitle: 'Test subtitle',
+           htmlDescription: '<p>Test description</p>',
+         },
+         image: {},
+         button: {
+           label: 'Test button',
+         },
+       },
+       index: 0,
+       meta: {
+         uuid: '11111111-1111-4111-8111-111111111111'
+       }
+     }
+    });
+
+    expect(wrapper.find('[data-testid="banner-button-0"]').exists()).toBe(false);
+  });
+
+  it('should set the given button variant', () => {
+    const wrapper = mount(BlocksBannerCarouselBanner, {
+    props: {
+      name: 'Banner',
+      type: 'content',
+      content: {
+        text: {
+          pretitle: 'Test pretitle',
+          title: 'Test title',
+          subtitle: 'Test subtitle',
+          htmlDescription: '<p>Test description</p>',
+        },
+        image: {},
+        button: {
+          label: 'Test button',
+          link: '/test-link',
+          variant: 'secondary',
+        },
+      },
+      index: 0,
+      meta: {
+        uuid: '11111111-1111-4111-8111-111111111111'
+      }
+    }
+    });
+
+    const button = wrapper.find('[data-testid="banner-button-0"]');
+
+    expect(button.attributes('class')).toContain('text-primary-500');
+  });
+});
