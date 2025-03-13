9e5d61b88bd3744f609a11fcd78f6d82951a2112
doproiescu-plenty <126688988+doproiescu-plenty@users.noreply.github.com>
2025-03-13 14:22:47 +0200

    Update apps/web/components/PagesView/PagesView.vue
    Co-authored-by: Kevin Stederoth <43753494+ksted@users.noreply.github.com>

--- apps/web/components/PagesView/PagesView.vue
+++ apps/web/components/PagesView/PagesView.vue
@@ -2,7 +2,7 @@
   <div class="pages-view sticky top-[52px]" data-testid="site-settings-drawer">
     <header class="flex items-center justify-between px-4 py-5 border-b mb-4">
       <div class="flex items-center text-xl font-bold">Pages</div>
-      <button data-testid="design-view-close" class="!p-0" @click="closeDrawer">
+      <button data-testid="pages-view-close" class="!p-0" @click="closeDrawer">
         <SfIconClose />
       </button>
     </header>
