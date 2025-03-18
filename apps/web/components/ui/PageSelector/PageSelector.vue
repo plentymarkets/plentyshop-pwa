52d267412c03cd4f4408bee7e89b84074ef8c828
aoltean-plenty <alexandru.oltean@plentysystems.com>
2025-03-18 01:52:40 -0700

    fix: page search closes when clicking on button

--- apps/web/components/ui/PageSelector/PageSelector.vue
+++ apps/web/components/ui/PageSelector/PageSelector.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="relative flex items-center space-x-1 md:space-x-2">
     <SfIconHome v-if="pageMeta.icon === 'home'" class="w-4 h-4 md:w-6 md:h-6" />
+
     <div class="relative flex items-center">
       <button @click="toggleDropdown">
         {{ pageMeta.name }}
       </button>
-
       <div class="flex items-center">
         <SfIconExpandMore class="w-4 h-4 md:w-6 md:h-6" />
       </div>
     </div>
   </div>
+
   <UiPageSearch v-if="isOpen" @close="closeDropdown" @page-selected="handlePageSelected" />
 </template>
 
@@ -18,18 +19,20 @@
 import { SfIconHome, SfIconExpandMore } from '@storefront-ui/vue';
 
 const { pageMeta } = usePageMeta();
-const currentPageName = ref('Homepage');
-
-const currentPageIcon = ref('home');
 const isOpen = ref(false);
 
-const toggleDropdown = () => (isOpen.value = !isOpen.value);
-const handlePageSelected = ({ name, icon }: { name: string; icon: string }) => {
-  isOpen.value = false;
-  currentPageName.value = name;
-  currentPageIcon.value = icon;
+const toggleDropdown = (event: Event) => {
+  event.stopPropagation();
+  isOpen.value = !isOpen.value;
 };
+
 const closeDropdown = () => {
-  isOpen.value = false;
+  setTimeout(() => {
+    isOpen.value = false;
+  }, 100);
+};
+const handlePageSelected = ({ name }: { name: string }) => {
+  closeDropdown();
+  pageMeta.value.name = name;
 };
 </script>
