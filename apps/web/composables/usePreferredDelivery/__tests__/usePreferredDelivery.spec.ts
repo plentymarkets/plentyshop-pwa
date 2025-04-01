06581beafa288893e0c9756b7fb88cb1bc6181de
cristian sandru <cristian.sandru@plentysystems.com>
2025-04-01 15:22:50 +0300

    wip

--- apps/web/composables/usePreferredDelivery/__tests__/usePreferredDelivery.spec.ts
+++ apps/web/composables/usePreferredDelivery/__tests__/usePreferredDelivery.spec.ts
@@ -1,3 +1,17 @@
+import { PreferredProfilesMock } from '../../../__tests__/__mocks__/preferred-profiles.mock';
+import { PreferredDeliveryServicesMock } from '../../../__tests__/__mocks__/preferred-delivery-services.mock';
+import { mockNuxtImport } from '@nuxt/test-utils/runtime';
+
+const { useSdk } = vi.hoisted(() => {
+  return {
+    useSdk: vi.fn().mockReturnValue({
+      plentysystems: {},
+    }),
+  };
+});
+
+mockNuxtImport('useSdk', () => useSdk);
+
 describe('usePreferredDelivery', () => {
   afterEach(() => {
     clearNuxtState();
@@ -27,88 +41,51 @@ describe('usePreferredDelivery', () => {
     expect(data.value.neighbour.enabled).toBe(false);
   });
 
-  // it('should call getPreferredProfiles and update preferredProfiles state', async () => {
-  //   const { getPreferredProfiles, data } = usePreferredDelivery();
-  //   useSdk().plentysystems.getPreferredDeliveryShippingProfiles = vi.fn().mockResolvedValue({
-  //     data: {
-  //       6: ['PreferredNeighbour', 'PreferredLocation', 'PreferredDay'],
-  //     },
-  //   });
-
-  //   await getPreferredProfiles();
-
-  //   expect(data.value.preferredProfiles).toEqual({
-  //     6: ['PreferredNeighbour', 'PreferredLocation', 'PreferredDay'],
-  //   });
-  // });
-
-  it('should call getPreferredDeliveryServices and update the state correctly', async () => {
-    const { getPreferredDeliveryServices, data } = usePreferredDelivery();
-
-    useSdk().plentysystems.getPreferredDeliveryServices = vi.fn().mockResolvedValue({
-      data: {
-        preferredLocation: true,
-        preferredNeighbour: true,
-        additionalCharge: 1.99,
-        preferredDay: {
-          '02-04-2025': { date: '2025-04-02', dayNumber: '02', dayName: 'Wednesday' },
-          '03-04-2025': { date: '2025-04-03', dayNumber: '03', dayName: 'Thursday' },
-          '04-04-2025': { date: '2025-04-04', dayNumber: '04', dayName: 'Friday' },
-          '05-04-2025': { date: '2025-04-05', dayNumber: '05', dayName: 'Saturday' },
-          '07-04-2025': { date: '2025-04-07', dayNumber: '07', dayName: 'Monday' },
-          '08-04-2025': { date: '2025-04-08', dayNumber: '08', dayName: 'Tuesday' },
+  it('should call getPreferredProfiles and update preferredProfiles state', async () => {
+    useSdk.mockImplementation(() => {
+      return {
+        plentysystems: {
+          getPreferredDeliveryShippingProfiles: vi.fn().mockResolvedValue({ data: PreferredProfilesMock }),
         },
-      },
+      };
     });
 
-    //   await getPreferredDeliveryServices();
+    const { getPreferredProfiles, data } = usePreferredDelivery();
+    await getPreferredProfiles();
 
-    //   expect(data.value.additionalCharge).toBe(1.99);
-    //   expect(data.value.preferredDays).toEqual([
-    //     { date: '', dayNumber: '', dayName: 'None' },
-    //     { date: '2025-04-02', dayNumber: '02', dayName: 'Wednesday' },
-    //     { date: '2025-04-03', dayNumber: '03', dayName: 'Thursday' },
-    //     { date: '2025-04-04', dayNumber: '04', dayName: 'Friday' },
-    //     { date: '2025-04-05', dayNumber: '05', dayName: 'Saturday' },
-    //     { date: '2025-04-07', dayNumber: '07', dayName: 'Monday' },
-    //     { date: '2025-04-08', dayNumber: '08', dayName: 'Tuesday' },
-    //   ]);
-    //   expect(data.value.day.enabled).toBe(true);
-    //   expect(data.value.location.enabled).toBe(true);
-    //   expect(data.value.neighbour.enabled).toBe(true);
-    // });
+    expect(data.value.preferredProfiles).toEqual(PreferredProfilesMock);
+  });
 
-    // it('should handle day checkbox change correctly', () => {
-    //   const { dayCheckboxChange, data } = usePreferredDelivery();
-    //   data.value.day.checked = true;
-    //   data.value.preferredDays = [{ date: '2025-04-02', dayNumber: '02', dayName: 'Wednesday' }];
+  it('should call getPreferredDeliveryServices and update the state correctly', async () => {
+    useSdk.mockImplementation(() => {
+      return {
+        plentysystems: {
+          getPreferredDeliveryServices: vi.fn().mockResolvedValue({ data: PreferredDeliveryServicesMock }),
+        },
+      };
+    });
 
-    //   dayCheckboxChange();
+    const { getPreferredDeliveryServices, data } = usePreferredDelivery();
 
-    //   expect(data.value.day.value).toBe('2025-04-02');
-    // });
+    await getPreferredDeliveryServices();
 
-    // it('should submit form with correct parameters', async () => {
-    //   const { submitForm, data } = usePreferredDelivery();
+    expect(data.value.additionalCharge).toEqual(expect.any(Number));
+    expect(data.value.preferredDays).toEqual([
+      { date: '', dayNumber: '', dayName: 'None' },
+      ...Object.values(PreferredDeliveryServicesMock.preferredDay || {}),
+    ]);
+    expect(typeof data.value.day.enabled).toBe('boolean');
+    expect(typeof data.value.location.enabled).toBe('boolean');
+    expect(typeof data.value.neighbour.enabled).toBe('boolean');
+  });
 
-    //   data.value.day.checked = true;
-    //   data.value.day.value = '2025-04-02';
-    //   data.value.location.checked = true;
-    //   data.value.location.value = 'Location';
-    //   data.value.neighbour.checked = true;
-    //   data.value.neighbour.name = 'John Doe';
-    //   data.value.neighbour.address = '123 Street';
+  it('should handle day checkbox change correctly', () => {
+    const { dayCheckboxChange, data } = usePreferredDelivery();
+    data.value.day.checked = true;
+    data.value.preferredDays = [{ date: '2025-04-02', dayNumber: '02', dayName: 'Wednesday' }];
 
-    //   useSdk().plentysystems.doSavePreferredDeliveryServices = vi.fn().mockResolvedValue({});
+    dayCheckboxChange();
 
-    //   await submitForm();
-
-    //   expect(useSdk().plentysystems.doSavePreferredDeliveryServices).toHaveBeenCalledWith({
-    //     preferredDay: '2025-04-02',
-    //     surcharge: '1.99',
-    //     preferredLocation: 'Location',
-    //     preferredNeighbourName: 'John Doe',
-    //     preferredNeighbourAddress: '123 Street',
-    //   });
+    expect(data.value.day.value).toBe('2025-04-02');
   });
 });
