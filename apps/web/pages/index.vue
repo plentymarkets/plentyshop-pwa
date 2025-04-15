14cf7fd0c19adb3707f87f3c06443dfcc27852fe
Kevin Stederoth <43753494+ksted@users.noreply.github.com>
2025-04-15 09:10:18 +0200

    refactor: remove duplicate check on index

--- apps/web/pages/index.vue
+++ apps/web/pages/index.vue
@@ -1,13 +1,10 @@
 <template>
   <div>
-    <EmptyBlock v-if="dataIsEmpty" />
     <EditablePage :identifier="'index'" :type="'immutable'" />
   </div>
 </template>
-<script lang="ts" setup>
-const { data } = useCategoryTemplate();
-const dataIsEmpty = computed(() => data.value.length === 0);
 
+<script lang="ts" setup>
 const { t } = useI18n();
 const { setPageMeta } = usePageMeta();
 const icon = 'home';
