159ca87d5c50f3af88efa779533f65c2c1131ce7
rbedeoan-plenty <121931658+rbedeoan-plenty@users.noreply.github.com>
2025-04-15 12:34:47 +0300

    fix: refactor

--- apps/web/pages/declaration-of-accessibility.vue
+++ apps/web/pages/declaration-of-accessibility.vue
@@ -4,7 +4,6 @@
 
 <script setup lang="ts">
 const { data, getLegalTexts } = useLegalInformation();
-const { getRobots, setRobotForStaticPage } = useRobots();
 const { t } = useI18n();
 const { setPageMeta } = usePageMeta();
 
