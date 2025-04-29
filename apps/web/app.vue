540af0da1c46e778bbd4d9dfe6344fc40168fd90
Maximilian Röll <30629157+maxiroellplenty@users.noreply.github.com>
2025-04-29 11:40:23 +0200

    test: reduce app.vue

--- apps/web/app.vue
+++ apps/web/app.vue
@@ -1,14 +1,7 @@
 <template>
-  <UiToolbar v-if="isPreview" :style="`font-family: ${config.font}`" />
-  <div
-    class="w-100 relative md:flex"
-    :class="{
-      'lg:flex-row-reverse': placement !== 'left',
-      'md:max-lg:w-[calc(100%-54px)]': disableActions && drawerOpen,
-      'md:max-lg:w-[calc(100%-66px)]': disableActions && !drawerOpen,
-    }"
-  >
-    <SettingsToolbar
+  <!-- <UiToolbar v-if="isPreview" :style="`font-family: ${config.font}`" /> -->
+  <div>
+    <!-- <SettingsToolbar
       v-if="isPreview && disableActions"
       :class="{
         'order-first': placement === 'left',
@@ -22,14 +15,13 @@
       class="absolute lg:relative bg-white"
       :class="{ 'mr-3': placement === 'left', 'ml-3': placement === 'right' }"
       :style="`font-family: ${config.font}`"
-    />
+    /> -->
 
     <div
       class="bg-white w-full relative"
-      :class="{ 'lg:w-3/4': drawerOpen, 'lg:w-[calc(100%-66px)]': isPreview && !drawerOpen && disableActions }"
     >
-      <Body class="font-body bg-editor-body-bg" :class="bodyClass" :style="currentFont" />
-      <UiNotifications />
+      <Body class="font-body bg-editor-body-bg" />
+      <!-- <UiNotifications /> -->
       <VitePwaManifest v-if="$pwa?.isPWAInstalled" />
       <NuxtLoadingIndicator color="repeating-linear-gradient(to right, #008ebd 0%,#80dfff 50%,#e0f7ff 100%)" />
       <NuxtLayout>
@@ -37,49 +29,42 @@
       </NuxtLayout>
     </div>
   </div>
-  <UiPageModal />
-  <UiUnlinkCategoryModal />
+ <!--  <UiPageModal />
+  <UiUnlinkCategoryModal /> -->
 </template>
 
 <script setup lang="ts">
-import type { Locale } from '#i18n';
 
 const { $pwa } = useNuxtApp();
-const bodyClass = ref('');
-const { getCategoryTree } = useCategoryTree();
 const { setInitialDataSSR } = useInitialSetup();
 const { setVsfLocale } = useLocalization();
 const route = useRoute();
 const { locale } = useI18n();
 const { setStaticPageMeta } = useCanonical();
 
-const { drawerOpen, currentFont, placement } = useSiteConfiguration();
-const { disableActions } = useEditor();
 
-const isPreview = ref(false);
-const config = useRuntimeConfig().public;
-const showConfigurationDrawer = config.showConfigurationDrawer;
 
-onMounted(() => {
+/* onMounted(() => {
   const pwaCookie = useCookie('pwa');
   isPreview.value = !!pwaCookie.value || (showConfigurationDrawer as boolean);
 });
-
+ */
 await setInitialDataSSR();
 setVsfLocale(locale.value);
 
 if (route?.meta.pageType === 'static') setStaticPageMeta();
 usePageTitle();
 
-onNuxtReady(async () => {
+/* onNuxtReady(async () => {
   bodyClass.value = 'hydrated'; // Need this class for cypress testing
-});
 
-watch(
-  () => locale.value,
-  async (locale: Locale) => {
-    setVsfLocale(locale);
-    await getCategoryTree();
-  },
-);
+  watch(
+    () => locale.value,
+    async (locale: Locale) => {
+      setVsfLocale(locale);
+      await getCategoryTree();
+    },
+  );
+}); */
+
 </script>
