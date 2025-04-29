83bf1870e21a9990733d26394ef1d45a1920906b
Noah Feist <noah.feist@plentysystems.com>
2025-04-29 09:38:47 +0200

    feat: hide customer wish behind env

--- apps/web/components/CustomWish/CustomWish.vue
+++ apps/web/components/CustomWish/CustomWish.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="md:px-4 py-6">
+  <div v-if="showCustomerWishComponent" class="md:px-4 py-6">
     <h2 class="w-full text-neutral-900 text-lg font-bold mb-4">
       {{ t('orderContactWish') }}
     </h2>
@@ -18,4 +18,8 @@ import { SfTextarea } from '@storefront-ui/vue';
 const { t } = useI18n();
 const { setCustomerWish } = useAdditionalInformation();
 const customerWish = ref('');
+const config = useRuntimeConfig().public;
+const showCustomerWishComponent = config.showCustomerWishComponent;
+
+console.log(showCustomerWishComponent);
 </script>
