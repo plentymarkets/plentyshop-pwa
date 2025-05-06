f58e880b920eb84b8be3fd6f4ff69994bf6b8c6e
oivan-plenty <114394643+oivan-plenty@users.noreply.github.com>
2025-05-06 15:55:08 +0300

    feat: changed design for settings drawer (#1246)
    Co-authored-by: oivan-plenty <oana.ivan@plentysystems.com>

--- apps/web/composables/useCategoryIdHelper/useCategoryIdHelper.ts
+++ apps/web/composables/useCategoryIdHelper/useCategoryIdHelper.ts
@@ -1,9 +1,9 @@
 const currentCategoryId = ref<number>();
 const currentParentCategoryId = ref<number | null>(null);
 const currentCategoryName = ref<string | null>(null);
-
+const currentCategoryPath = ref<string | null>(null);
 export const useCategoryIdHelper = () => {
-  const setCategoryId = (id: number, parentId?: number, name?: string) => {
+  const setCategoryId = (id: number, parentId?: number, name?: string, path?: string) => {
     currentCategoryId.value = id;
     if (parentId !== undefined) {
       currentParentCategoryId.value = parentId;
@@ -11,16 +11,21 @@ export const useCategoryIdHelper = () => {
     if (name !== undefined) {
       currentCategoryName.value = name;
     }
+    if (path !== undefined) {
+      currentCategoryPath.value = path;
+    }
   };
 
   const getCategoryId = computed(() => currentCategoryId.value);
   const getParentCategoryId = computed(() => currentParentCategoryId.value);
   const getCategoryName = computed(() => currentCategoryName.value);
+  const getCategoryPath = computed(() => currentCategoryPath.value);
 
   return {
     setCategoryId,
     getCategoryId,
     getParentCategoryId,
     getCategoryName,
+    getCategoryPath,
   };
 };
