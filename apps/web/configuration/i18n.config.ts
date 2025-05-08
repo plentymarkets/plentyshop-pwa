f9c05ae794fe707c2ed7e5ac0ec7686dbfd681af
Maximilian Röll <30629157+maxiroellplenty@users.noreply.github.com>
2025-05-08 12:23:10 +0200

    test: remove i18n lazy

--- apps/web/configuration/i18n.config.ts
+++ apps/web/configuration/i18n.config.ts
@@ -33,5 +33,4 @@ export const nuxtI18nOptions: NuxtI18nOptions = {
   strategy: 'prefix_and_default',
   vueI18n: '~/configuration/vueI18n.config.ts',
   detectBrowserLanguage: false,
-  lazy: true,
 };
