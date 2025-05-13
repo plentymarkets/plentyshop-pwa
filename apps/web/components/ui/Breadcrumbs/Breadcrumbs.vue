4d60308f7fe4f03042a8020a0b4493f4154a09f6
rbedeoan-plenty <121931658+rbedeoan-plenty@users.noreply.github.com>
2025-05-13 12:58:40 +0300

    fix: cookiebar line break and category breadcrumb on mobile (#1277)

--- apps/web/components/ui/Breadcrumbs/Breadcrumbs.vue
+++ apps/web/components/ui/Breadcrumbs/Breadcrumbs.vue
@@ -1,7 +1,7 @@
 <template>
   <nav data-testid="breadcrumbs" class="inline-flex items-center text-sm font-normal">
     <ol class="flex w-auto leading-none group md:flex-wrap">
-      <li class="flex items-center sm:hidden text-neutral-500 z-10">
+      <li class="flex items-center sm:hidden text-neutral-500 z-9">
         <NuxtLazyHydrate :on-interaction="['click', 'touchstart']">
           <SfDropdown v-model="dropdownOpened" strategy="absolute" placement="bottom-start" @update:model-value="close">
             <template #trigger>
