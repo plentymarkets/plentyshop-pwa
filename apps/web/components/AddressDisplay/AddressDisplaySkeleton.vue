d9246f0e1b49c14b201c5b4c9816a1ad627047aa
N-Feist <144685636+N-Feist@users.noreply.github.com>
2025-05-16 09:30:14 +0200

    Update apps/web/components/AddressDisplay/AddressDisplaySkeleton.vue
    Co-authored-by: Maximilian Röll <30629157+maxiroellplenty@users.noreply.github.com>

--- apps/web/components/AddressDisplay/AddressDisplaySkeleton.vue
+++ apps/web/components/AddressDisplay/AddressDisplaySkeleton.vue
@@ -1,7 +1,15 @@
 <template>
-  <p class="h-4 w-36 bg-gray-300 rounded animate-pulse" />
-  <p class="mt-2 h-4 w-36 bg-gray-300 rounded animate-pulse" />
-  <p class="mt-2 h-4 w-36 bg-gray-300 rounded animate-pulse" />
-  <p class="mt-2 h-4 w-36 bg-gray-300 rounded animate-pulse" />
-  <p class="mt-2 h-4 w-36 bg-gray-300 rounded animate-pulse" />
+  <div class="flex gap-2">
+    <span class="h-3 w-20 bg-gray-300 rounded animate-pulse" />
+    <span class="h-3 w-20 bg-gray-300 rounded animate-pulse" />
+  </div>
+  <div class="flex gap-2 mt-2">
+    <span class="h-3 w-24 bg-gray-300 rounded animate-pulse" />
+    <span class="h-3 w-5 bg-gray-300 rounded animate-pulse" />
+  </div>
+  <div class="flex gap-2 mt-2">
+    <span class="h-3 w-12 bg-gray-300 rounded animate-pulse" />
+    <span class="h-3 w-12 bg-gray-300 rounded animate-pulse" />
+  </div>
+  <p class="mt-2 h-3 w-20 bg-gray-300 rounded animate-pulse" />
 </template>
