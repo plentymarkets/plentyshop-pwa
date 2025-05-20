64c83e42f3f75872e2c3e3a910ccab08b3c2b6f8
Noah Feist <noah.feist@plentysystems.com>
2025-05-20 15:39:53 +0200

    Revert "chore: testing a change in github actions"
    This reverts commit c298d4c3c653a77ddd252592d9ebde0052e73bce.

--- apps/web/__tests__/test/checkout/checkout-addresses.cy.ts
+++ apps/web/__tests__/test/checkout/checkout-addresses.cy.ts
@@ -12,13 +12,13 @@ const productListPage = new ProductListPageObject();
 beforeEach(() => {
   cy.clearCookies();
   cy.visitAndHydrate(paths.home);
-
-  homePage.goToCategory();
-  productListPage.addToCart();
 });
 
 describe('Checkout Addresses', () => {
   it('should display same as shipping text if a guest creates his shipping address with billing same as shipping checked', () => {
+    homePage.goToCategory();
+    productListPage.addToCart();
+
     cart.openCart();
     checkout
       .goToCheckout()
@@ -29,6 +29,8 @@ describe('Checkout Addresses', () => {
   });
 
   it('should not display shipping and billing address selection if a guest user creates his address', () => {
+    homePage.goToCategory();
+    productListPage.addToCart();
     cart.openCart();
 
     checkout
@@ -41,8 +43,10 @@ describe('Checkout Addresses', () => {
   });
 
   it('should be able to edit the billing address as a guest user if selected same as shipping', () => {
-    cart.openCart();
+    homePage.goToCategory();
+    productListPage.addToCart();
 
+    cart.openCart();
     checkout
       .goToCheckout()
       .goToGuestCheckout()
