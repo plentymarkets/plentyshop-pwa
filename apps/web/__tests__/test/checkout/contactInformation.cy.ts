80dc0e0b49d806ff92a672d372f72de384d7dfd4
Maximilian Röll <30629157+maxiroellplenty@users.noreply.github.com>
2025-05-22 16:13:08 +0200

    chore: refactor contactInformation test

--- apps/web/__tests__/test/checkout/contactInformation.cy.ts
+++ apps/web/__tests__/test/checkout/contactInformation.cy.ts
@@ -1,12 +1,6 @@
-import { CartPageObject } from '../../support/pageObjects/CartPageObject';
 import { CheckoutPageObject } from '../../support/pageObjects/CheckoutPageObject';
-import { HomePageObject } from '../../support/pageObjects/HomePageObject';
-import { ProductListPageObject } from '../../support/pageObjects/ProductListPageObject';
 
 const checkout = new CheckoutPageObject();
-const cart = new CartPageObject();
-const homePage = new HomePageObject();
-const productListPage = new ProductListPageObject();
 
 beforeEach(() => {
   cy.clearCookies().visitSmoke();
@@ -15,9 +9,7 @@ beforeEach(() => {
 describe('Contact Information', () => {
   it('should scroll to contact information if email is invalid and buy button is clicked', () => {
     cy.addToCart();
-    checkout.goToCheckoutPath();
-
-    checkout.contactInformationForm.type('invalid-email');
+    checkout.goToCheckoutPath().contactInformationForm.type('invalid-email');
 
     checkout.acceptTerms();
     checkout.placeOrderButtons.click();
@@ -26,55 +18,39 @@ describe('Contact Information', () => {
 
   it('should scroll to contact information if email is invalid and shipping address save is clicked', () => {
     cy.addToCart();
-    checkout.goToCheckoutPath();
-
-    checkout.contactInformationForm.type('invalid-email');
+    checkout.goToCheckoutPath().contactInformationForm.type('invalid-email');
     checkout.fillAddressForm('shipping');
     checkout.contactInformationForm.isScrolledTo();
   });
 
   it('should scroll to contact information if email is invalid and billing address save is clicked', () => {
     cy.addToCart();
-    checkout.goToCheckoutPath();
-
-    checkout.contactInformationForm.type('invalid-email');
+    checkout.goToCheckoutPath().contactInformationForm.type('invalid-email');
     checkout.fillAddressForm('billing');
     checkout.contactInformationForm.isScrolledTo();
   });
 
   it('should validate email input', () => {
     cy.addToCart();
-    checkout.goToCheckoutPath();
-    checkout.contactInformationForm.type('invalid-email').blur();
+    checkout.goToCheckoutPath().contactInformationForm.type('invalid-email').blur();
     cy.get('#customerEmailError').should('exist');
   });
 
   it('should validate email input with minus "-"', () => {
     cy.addToCart();
-    checkout.goToCheckoutPath();
-
-    checkout.contactInformationForm.type('valid-email@plentyone.com').blur();
+    checkout.goToCheckoutPath().contactInformationForm.type('valid-email@plentyone.com').blur();
     cy.get('#customerEmailError').should('not.exist');
   });
 
   it('should validate email input with dot "."', () => {
     cy.addToCart();
-    checkout.goToCheckoutPath();
-
-    checkout.contactInformationForm.type('valid.email@plentyone.com').blur();
+    checkout.goToCheckoutPath().contactInformationForm.type('valid.email@plentyone.com').blur();
     cy.get('#customerEmailError').should('not.exist');
   });
 
   it('should be able to change the email after address is changed', () => {
-    homePage.goToCategory();
-    productListPage.addToCart();
-    cart.openCart();
+    cy.addToCart();
 
-    checkout
-      .goToCheckout()
-      .goToGuestCheckout()
-      .fillContactInformationForm()
-      .fillShippingAddressForm()
-      .fillContactInformationForm();
+    checkout.goToCheckoutPath().fillContactInformationForm().fillShippingAddressForm().fillContactInformationForm();
   });
 });
