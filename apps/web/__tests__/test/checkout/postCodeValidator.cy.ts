18a06bb79298c5808eddf4b5e6bc3754299df9ec
cristian sandru <cristian.sandru@plentysystems.com>
2025-05-26 17:22:54 +0300

    feat: test zip codes validation

--- apps/web/__tests__/test/checkout/postCodeValidator.cy.ts
+++ apps/web/__tests__/test/checkout/postCodeValidator.cy.ts
@@ -1,31 +1,19 @@
 import postCodeMapper from '../../fixtures/postCodeMapper.json';
-import { CartPageObject } from '../../support/pageObjects/CartPageObject';
 import { CheckoutPageObject } from '../../support/pageObjects/CheckoutPageObject';
-import { HomePageObject } from '../../support/pageObjects/HomePageObject';
-import { ProductListPageObject } from '../../support/pageObjects/ProductListPageObject';
-import { paths } from '../../../utils/paths';
 
 const checkout = new CheckoutPageObject();
-const cart = new CartPageObject();
-const homePage = new HomePageObject();
-const productListPage = new ProductListPageObject();
-
 const selectedCountry = Cypress.env('POST_CODE_VALIDATION_COUNTRY');
 const countryList = selectedCountry ? postCodeMapper.filter((c) => c.name === selectedCountry) : postCodeMapper;
 
 if (selectedCountry && countryList.length === 0) throw new Error(`Country "${selectedCountry}" not found.`);
 
 beforeEach(() => {
-  cy.clearCookies();
-  cy.visitAndHydrate(paths.home);
-  homePage.goToCategory();
-  productListPage.addToCart();
-  cart.openCart();
-  checkout.goToCheckout().goToGuestCheckout().fillContactInformationForm().useShippingAsBilling.uncheck();
+  cy.clearCookies().visitSmoke().addToCart();
+  checkout.goToCheckoutPath().fillContactInformationForm().useShippingAsBilling.uncheck();
 });
 
 countryList.forEach(({ countryId, name, valid, invalid }) => {
-  describe(`${name} Postcode Validator`, () => {
+  describe(`${name} Post Code Validator`, () => {
     valid.forEach((zipCode) => {
       it(`accepts valid postcode: ${zipCode}`, () => {
         checkout.fillPostCodeBillingForm({ country: countryId, zipCode });
