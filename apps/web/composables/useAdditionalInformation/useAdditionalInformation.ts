15452df05030896885ed4218cdd2158250ee2e2c
Fabian Gerke <124085586+FabianGerke@users.noreply.github.com>
2025-06-02 16:02:10 +0200

    feat: useAdditionalInformation

--- apps/web/composables/useAdditionalInformation/useAdditionalInformation.ts
+++ apps/web/composables/useAdditionalInformation/useAdditionalInformation.ts
@@ -1,4 +1,4 @@
-import type { AdditionalInformationParams } from '@plentymarkets/shop-api';
+import type { AdditionalInformationParams, ApiError } from '@plentymarkets/shop-api';
 import type {
   UseAdditionalInformationState,
   DoAdditionalInformation,
@@ -40,13 +40,13 @@ export const useAdditionalInformation: DoAdditionalInformationReturn = () => {
   const doAdditionalInformation: DoAdditionalInformation = async (params: AdditionalInformationParams) => {
     state.value.loading = true;
     try {
-      const { error } = await useAsyncData(() => useSdk().plentysystems.doAdditionalInformation(params));
-      useHandleError(error.value);
+      await useSdk().plentysystems.doAdditionalInformation(params);
       state.value.data = null;
 
       return state.value.data;
     } catch (error) {
-      throw new Error(error as string);
+      useHandleError(error as ApiError);
+      return state.value.data;
     } finally {
       state.value.loading = false;
     }
