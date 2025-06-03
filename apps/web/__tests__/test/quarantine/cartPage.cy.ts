52f9691d0616c6cf0fbd29d2d29c89abb53edc54
Maximilian Röll <30629157+maxiroellplenty@users.noreply.github.com>
2025-06-03 11:33:44 +0200

    test: quantity de/increase on cart items

--- apps/web/__tests__/test/quarantine/cartPage.cy.ts
+++ apps/web/__tests__/test/quarantine/cartPage.cy.ts
@@ -1,16 +1,16 @@
 import { CartPageObject } from '../../support/pageObjects/CartPageObject';
-import { paths } from '../../../utils/paths';
 
 const cart = new CartPageObject();
 
-before(() => {
-  cy.intercept('/plentysystems/doAddCartItem').as('doAddCartItem');
-  cy.clearCookies();
-
-  cy.visitAndHydrate(paths.home);
-});
-
 describe('Smoke: Cart Page', () => {
+
+  beforeEach(() => {
+    cy.intercept('/plentysystems/doAddCartItem').as('doAddCartItem');
+    cy.clearCookies();
+  
+    cy.visitSmoke();
+  });
+
   it('[smoke] Add coupon to cart, check the totals, then remove coupon and check totals again', () => {
     cy.visitAndHydrate('/study-room-office/office-chair/design-chair-brookhaven-leather-black_105_1003');
 
@@ -31,5 +31,36 @@ describe('Smoke: Cart Page', () => {
       .orderSummaryAfterCouponApplied()
       .removeCoupon()
       .orderSummaryAfterCouponRemoved();
+  });
+
+  it('should add a product to the cart and then increase the quantity', () => {
+    cy.addToCart();
+    cart
+      .openCart()
+      .increaseCartItemQuantity()
+      .summaryItems()
+  });
+
+  it('should add a product to the cart and then decrease the quantity', () => {
+    cy.addToCart();
+    cart
+      .openCart()
+      .increaseCartItemQuantity()
+      .decreaseCartItemQuantity()
+      .summaryItems('Items: 1');
+  });
+
+  it('should add a product to the cart and increase the quantity 3 times', () => {
+    cy.addToCart();
+    cart
+      .openCart()
+      .increaseCartItemQuantity()
+
+    cy.wait(1000)
+
+    cart
+      .increaseCartItemQuantity()
+      .increaseCartItemQuantity()
+      .summaryItems('Items: 4');
   });
 });
