2d2312d193ba907ac141a09128120c1009c1544b
Maximilian Röll <30629157+maxiroellplenty@users.noreply.github.com>
2025-06-11 15:32:38 +0200

    fix: add validation to product url parameters

--- apps/web/__tests__/test/smoke/productDetailPage.cy.ts
+++ apps/web/__tests__/test/smoke/productDetailPage.cy.ts
@@ -8,12 +8,71 @@ const homePage = new HomePageObject();
 const productListPage = new ProductListPageObject();
 
 describe('Smoke: Product Detail Page', () => {
-  it('[smoke] Open product page and check displayed data', () => {
+  beforeEach(() => {
     cy.clearCookies();
+  });
+
+  it('[smoke] Open product page and check displayed data', () => {
     cy.visitAndHydrate(paths.home);
     homePage.goToCategory();
     productListPage.goToProduct();
 
     productDetailPage.displayCheck();
+  });
+
+  describe('should validate product url params', () => {
+    it('should succeed to validate product url params with one valid params', () => {
+      cy.request({
+        url: '/backpack-gaia_164',
+        failOnStatusCode: false,
+      }).then((response) => {
+        expect(response.status).to.eq(200);
+      });
+    });
+
+    it('should succeed to validate product url params with 2 valid params', () => {
+      cy.request({
+        url: '/backpack-gaia_164_1107',
+        failOnStatusCode: false,
+      }).then((response) => {
+        expect(response.status).to.eq(200);
+      });
+    });
+
+    it('should fail to validate product url params with string', () => {
+      cy.request({
+        url: '/backpack-gaia_164_notallowdstring',
+        failOnStatusCode: false,
+      }).then((response) => {
+        expect(response.status).to.eq(404);
+      });
+    });
+
+    it('should fail to validate product url params with string', () => {
+      cy.request({
+        url: '/backpack-gaia_notallowdstring_notallowdstring',
+        failOnStatusCode: false,
+      }).then((response) => {
+        expect(response.status).to.eq(404);
+      });
+    });
+
+    it('should fail to validate product url with 3 params', () => {
+      cy.request({
+        url: '/backpack-gaia_1234_1234_1234',
+        failOnStatusCode: false,
+      }).then((response) => {
+        expect(response.status).to.eq(404);
+      });
+    });
+
+    it('should fail to validate product url with 1 string param', () => {
+      cy.request({
+        url: '/my-category/testurl_param',
+        failOnStatusCode: false,
+      }).then((response) => {
+        expect(response.status).to.eq(404);
+      });
+    });
   });
 });
