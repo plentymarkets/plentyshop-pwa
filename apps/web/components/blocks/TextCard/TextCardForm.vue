4efe4e2818d56eae77cb1639ea5c9a377dc14b0c
aoltean-plenty <alexandru.oltean@plentysystems.com>
2025-06-16 23:54:45 -0700

    move test id

--- apps/web/components/blocks/TextCard/TextCardForm.vue
+++ apps/web/components/blocks/TextCard/TextCardForm.vue
@@ -9,125 +9,130 @@
       <h2>Text</h2>
     </template>
 
-    <div class="py-2">
-      <div class="flex justify-between mb-2">
-        <UiFormLabel>Pre-title</UiFormLabel>
-      </div>
-      <label>
-        <SfInput v-model="textCardBlock.text.pretitle" type="text" data-testid="input-pretitle">
-          <template #suffix>
-            <label for="text-pretitle" class="rounded-lg cursor-pointer">
-              <input id="text-pretitle" v-model="textCardBlock.text.pretitle" type="text" class="invisible w-8" />
-            </label>
-          </template>
-        </SfInput>
-      </label>
-    </div>
-
-    <div class="py-2">
-      <div class="flex justify-between mb-2">
-        <UiFormLabel>Main title</UiFormLabel>
-      </div>
-      <label>
-        <SfInput v-model="textCardBlock.text.title" type="text" data-testid="input-main-title">
-          <template #suffix>
-            <label for="text-title" class="rounded-lg cursor-pointer">
-              <input id="text-title" v-model="textCardBlock.text.title" type="text" class="invisible w-8" />
-            </label>
-          </template>
-        </SfInput>
-      </label>
-    </div>
-
-    <div class="py-2">
-      <div class="flex justify-between mb-2">
-        <UiFormLabel>Subtitle</UiFormLabel>
-      </div>
-      <label>
-        <SfInput v-model="textCardBlock.text.subtitle" type="text" data-testid="input-subtitle">
-          <template #suffix>
-            <label for="text-subtitle" class="rounded-lg cursor-pointer">
-              <input id="text-subtitle" v-model="textCardBlock.text.subtitle" type="text" class="invisible w-8" />
-            </label>
-          </template>
-        </SfInput>
-      </label>
-    </div>
-
-    <div class="py-2">
-      <UiFormLabel>HTML Description</UiFormLabel>
-      <SfTextarea
-        id="text-html-description"
-        v-model="textCardBlock.text.htmlDescription"
-        data-testid="textarea-description"
-        name="text-html-description"
-        rows="3"
-        class="min-h-[232px] mt-1 block w-full border border-gray-300 rounded-md shadow-sm sm:text-sm"
-      />
-    </div>
-    <div class="py-2">
-      <div class="flex justify-between mb-2">
-        <UiFormLabel>Text Color</UiFormLabel>
-      </div>
-      <label>
-        <SfInput v-model="textCardBlock.text.color" type="text" data-testid="input-text-color">
-          <template #suffix>
-            <label
-              for="primary-color"
-              :style="{ backgroundColor: textCardBlock.text.color }"
-              class="border border-[#a0a0a0] rounded-lg cursor-pointer"
-            >
-              <input
-                id="primary-color"
-                v-model="textCardBlock.text.color"
-                data-testid="color-input"
-                type="color"
-                class="invisible w-8"
-              />
-            </label>
-          </template>
-        </SfInput>
-      </label>
-    </div>
-
-    <fieldset class="py-2">
-      <legend class="text-sm font-medium text-black">Text alignment</legend>
-
-      <div class="w-full inline-flex rounded-lg border border-gray-300 bg-white text-gray-700 overflow-hidden">
-        <div
-          for="text-align-left"
-          data-testid="text-align-left"
-          class="flex items-center justify-center w-1/3 px-4 py-2 cursor-pointer text-sm border-r"
-          :class="{ 'bg-gray-100 text-gray-900 font-semibold': textCardBlock.text.textAlignment === 'left' }"
-          @click="textCardBlock.text.textAlignment = 'left'"
-        >
-          <SfIconCheck :class="{ invisible: textCardBlock.text.textAlignment !== 'left' }" class="mr-1 w-[1.1rem]" />
-          Left
+    <div data-testid="text-card-form">
+      <div class="py-2">
+        <div class="flex justify-between mb-2">
+          <UiFormLabel>Pre-title</UiFormLabel>
         </div>
-
-        <div
-          for="text-align-center"
-          data-testid="text-align-center"
-          class="flex items-center justify-center w-1/3 px-4 py-2 cursor-pointer text-sm border-r"
-          :class="{ 'bg-gray-100 text-gray-900 font-semibold': textCardBlock.text.textAlignment === 'center' }"
-          @click="textCardBlock.text.textAlignment = 'center'"
-        >
-          <SfIconCheck :class="{ invisible: textCardBlock.text.textAlignment !== 'center' }" class="mr-1 w-[1.1rem]" />
-          Center
-        </div>
-
-        <div
-          for="text-align-right"
-          data-testid="text-align-right"
-          class="flex items-center justify-center w-1/3 px-4 py-2 cursor-pointer text-sm"
-          :class="{ 'bg-gray-100 text-gray-900 font-semibold': textCardBlock.text.textAlignment === 'right' }"
-          @click="textCardBlock.text.textAlignment = 'right'"
-        >
-          <SfIconCheck :class="{ invisible: textCardBlock.text.textAlignment !== 'right' }" class="mr-1 w-[1.1rem]" />
-          Right
-        </div>
+        <label>
+          <SfInput v-model="textCardBlock.text.pretitle" type="text" data-testid="input-pretitle">
+            <template #suffix>
+              <label for="text-pretitle" class="rounded-lg cursor-pointer">
+                <input id="text-pretitle" v-model="textCardBlock.text.pretitle" type="text" class="invisible w-8" />
+              </label>
+            </template>
+          </SfInput>
+        </label>
       </div>
-    </fieldset>
+
+      <div class="py-2">
+        <div class="flex justify-between mb-2">
+          <UiFormLabel>Main title</UiFormLabel>
+        </div>
+        <label>
+          <SfInput v-model="textCardBlock.text.title" type="text" data-testid="input-main-title">
+            <template #suffix>
+              <label for="text-title" class="rounded-lg cursor-pointer">
+                <input id="text-title" v-model="textCardBlock.text.title" type="text" class="invisible w-8" />
+              </label>
+            </template>
+          </SfInput>
+        </label>
+      </div>
+
+      <div class="py-2">
+        <div class="flex justify-between mb-2">
+          <UiFormLabel>Subtitle</UiFormLabel>
+        </div>
+        <label>
+          <SfInput v-model="textCardBlock.text.subtitle" type="text" data-testid="input-subtitle">
+            <template #suffix>
+              <label for="text-subtitle" class="rounded-lg cursor-pointer">
+                <input id="text-subtitle" v-model="textCardBlock.text.subtitle" type="text" class="invisible w-8" />
+              </label>
+            </template>
+          </SfInput>
+        </label>
+      </div>
+
+      <div class="py-2">
+        <UiFormLabel>HTML Description</UiFormLabel>
+        <SfTextarea
+          id="text-html-description"
+          v-model="textCardBlock.text.htmlDescription"
+          data-testid="textarea-description"
+          name="text-html-description"
+          rows="3"
+          class="min-h-[232px] mt-1 block w-full border border-gray-300 rounded-md shadow-sm sm:text-sm"
+        />
+      </div>
+      <div class="py-2">
+        <div class="flex justify-between mb-2">
+          <UiFormLabel>Text Color</UiFormLabel>
+        </div>
+        <label>
+          <SfInput v-model="textCardBlock.text.color" type="text" data-testid="input-text-color">
+            <template #suffix>
+              <label
+                for="primary-color"
+                :style="{ backgroundColor: textCardBlock.text.color }"
+                class="border border-[#a0a0a0] rounded-lg cursor-pointer"
+              >
+                <input
+                  id="primary-color"
+                  v-model="textCardBlock.text.color"
+                  data-testid="color-input"
+                  type="color"
+                  class="invisible w-8"
+                />
+              </label>
+            </template>
+          </SfInput>
+        </label>
+      </div>
+
+      <fieldset class="py-2">
+        <legend class="text-sm font-medium text-black">Text alignment</legend>
+
+        <div class="w-full inline-flex rounded-lg border border-gray-300 bg-white text-gray-700 overflow-hidden">
+          <div
+            for="text-align-left"
+            data-testid="text-align-left"
+            class="flex items-center justify-center w-1/3 px-4 py-2 cursor-pointer text-sm border-r"
+            :class="{ 'bg-gray-100 text-gray-900 font-semibold': textCardBlock.text.textAlignment === 'left' }"
+            @click="textCardBlock.text.textAlignment = 'left'"
+          >
+            <SfIconCheck :class="{ invisible: textCardBlock.text.textAlignment !== 'left' }" class="mr-1 w-[1.1rem]" />
+            Left
+          </div>
+
+          <div
+            for="text-align-center"
+            data-testid="text-align-center"
+            class="flex items-center justify-center w-1/3 px-4 py-2 cursor-pointer text-sm border-r"
+            :class="{ 'bg-gray-100 text-gray-900 font-semibold': textCardBlock.text.textAlignment === 'center' }"
+            @click="textCardBlock.text.textAlignment = 'center'"
+          >
+            <SfIconCheck
+              :class="{ invisible: textCardBlock.text.textAlignment !== 'center' }"
+              class="mr-1 w-[1.1rem]"
+            />
+            Center
+          </div>
+
+          <div
+            for="text-align-right"
+            data-testid="text-align-right"
+            class="flex items-center justify-center w-1/3 px-4 py-2 cursor-pointer text-sm"
+            :class="{ 'bg-gray-100 text-gray-900 font-semibold': textCardBlock.text.textAlignment === 'right' }"
+            @click="textCardBlock.text.textAlignment = 'right'"
+          >
+            <SfIconCheck :class="{ invisible: textCardBlock.text.textAlignment !== 'right' }" class="mr-1 w-[1.1rem]" />
+            Right
+          </div>
+        </div>
+      </fieldset>
+    </div>
   </UiAccordionItem>
 
   <UiAccordionItem
