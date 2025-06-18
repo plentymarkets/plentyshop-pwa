f76d39fa5940efa4861a18123270527e1fba06d1
rbedeoan-plenty <121931658+rbedeoan-plenty@users.noreply.github.com>
2025-06-18 13:49:25 +0300

    fix: hotfix

--- apps/web/pages/shipping.vue
+++ apps/web/pages/shipping.vue
@@ -1,6 +1,8 @@
 <template>
-  <div v-if="text" class="w-full p-5 overflow-x-auto no-preflight" v-html="text" />
-  <h5 v-else class="text-center m-5 p-5">{{ t('shipping.noShippingMessage') }}</h5>
+  <client-only>
+    <div v-if="text" class="w-full p-5 overflow-x-auto no-preflight" v-html="text" />
+    <h5 v-else class="text-center m-5 p-5">{{ t('shipping.noShippingMessage') }}</h5>
+  </client-only>
 </template>
 
 <script setup lang="ts">
@@ -11,7 +13,9 @@ const { setPageMeta } = usePageMeta();
 
 const { categoryTemplateData, fetchCategoryTemplate } = useCategoryTemplate();
 const runtimeConfig = useRuntimeConfig();
-await fetchCategoryTemplate(Number(runtimeConfig.public.shippingTextCategoryId));
+onMounted(async () => {
+  await fetchCategoryTemplate(Number(runtimeConfig.public.shippingTextCategoryId));
+});
 const { t } = useI18n();
 
 const icon = 'page';
