d057367b74779ad1d695596808da205357cbb52a
abocsan-plenty <129151096+abocsan-plenty@users.noreply.github.com>
2025-06-19 12:10:48 +0300

    fix: shipping 500 error

--- apps/web/composables/useCategoryTemplate/useCategoryTemplate.ts
+++ apps/web/composables/useCategoryTemplate/useCategoryTemplate.ts
@@ -60,17 +60,9 @@ export const useCategoryTemplate: UseCategoryTemplateReturn = () => {
    * ```
    */
   const fetchCategoryTemplate: FetchCategoryTemplate = async (categoryId) => {
-    try {
-      state.value.loading = true;
+    const { data } = await useAsyncData(() => useSdk().plentysystems.getCategoryTemplate({ id: categoryId }));
 
-      const data = await useSdk().plentysystems.getCategoryTemplate({ id: categoryId });
-
-      state.value.categoryTemplateData = data.data ?? state.value.categoryTemplateData;
-    } catch (error) {
-      console.error('Error fetching category template:', error);
-    } finally {
-      state.value.loading = false;
-    }
+    state.value.categoryTemplateData = data?.value?.data ?? state.value.categoryTemplateData;
   };
 
   const saveBlocks: SaveBlocks = async (identifier: string | number, type: string, content: string) => {
