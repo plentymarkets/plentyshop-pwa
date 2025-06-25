d1a60851ef80120b68d68005db4dcdc382a3d549
oivan-plenty <114394643+oivan-plenty@users.noreply.github.com>
2025-06-25 10:30:26 +0300

    fix: fix pages pagination (#1407)
    Co-authored-by: oivan-plenty <oana.ivan@plentysystems.com>
    Co-authored-by: clincu-plenty <cristian.lincu@plentysystems.com>

--- apps/web/components/PagesView/PagesView.vue
+++ apps/web/components/PagesView/PagesView.vue
@@ -70,8 +70,11 @@
           <h2>Content Pages</h2>
         </template>
 
-        <div :class="['mb-6 mt-4 overflow-auto', limitAccordionHeight ? 'max-h-[400px]' : 'max-h-[500px]']">
-          <ul class="rounded-lg" @scroll="(e) => handleScroll(e, 'content')">
+        <div
+          :class="['mb-6 mt-4 overflow-auto', limitAccordionHeight ? 'max-h-[400px]' : 'max-h-[500px]']"
+          @scroll="(e) => handleScroll(e, 'content')"
+        >
+          <ul class="rounded-lg">
             <PagesItem
               :key="locale"
               :item="homepageItem"
@@ -97,8 +100,11 @@
           <h2>Product Categories</h2>
         </template>
 
-        <div :class="['mb-6 mt-4 overflow-auto', limitAccordionHeight ? 'max-h-[400px]' : 'max-h-[500px]']">
-          <ul class="rounded-lg" @scroll="(e) => handleScroll(e, 'item')">
+        <div
+          :class="['mb-6 mt-4 overflow-auto', limitAccordionHeight ? 'max-h-[400px]' : 'max-h-[500px]']"
+          @scroll="(e) => handleScroll(e, 'item')"
+        >
+          <ul class="rounded-lg">
             <PagesItem v-for="item in itemItems" :key="item.id" :item="item" :parent-id="item.id" />
             <li v-if="loadingItem" class="flex justify-center items-center py-4">
               <SfLoaderCircular size="sm" />
@@ -156,8 +162,14 @@ watch(productPagesOpen, (opened) => {
 });
 
 watch(locale, () => {
-  fetchCategories('content');
-  fetchCategories('item');
+  if (contentPagesOpen.value) {
+    resetCategories();
+    fetchCategories('content');
+  }
+  if (productPagesOpen.value) {
+    resetCategories();
+    fetchCategories('item');
+  }
 });
 
 const openHelpPage = () => {
