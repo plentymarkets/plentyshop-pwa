be81112b86dc4bf1bad4091bb16ecfb42b855420
Noah Feist <noah.feist@plentysystems.com>
2025-07-03 09:35:00 +0200

    feat: added the ordergetters and removed unused code

--- apps/web/components/OrderTrackingButton/OrderTrackingButton.vue
+++ apps/web/components/OrderTrackingButton/OrderTrackingButton.vue
@@ -1,29 +1,21 @@
 <template>
   <UiButton class="mt-4 w-full cursor-pointer" variant="secondary" @click="handleTrackingUrl()">
-    {{ t('returns.returnItems') }}
+    {{ t('orderConfirmation.trackingOrder', {
+      trackingNumber: order.order.tracking?.trackingNumbers[0]
+    }) }}
   </UiButton>
-
-  <UiModal v-model="openModal">
-    Placeholder
-  </UiModal>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
 import type { OrderTrackingButtonProps } from './types';
-import { useI18n } from 'vue-i18n';
+import { orderGetters } from '@plentymarkets/shop-api';
 
 const { t } = useI18n();
 const { order } = defineProps<OrderTrackingButtonProps>();
-const openModal = ref(false);
 
 const handleTrackingUrl = () => {
-  if (order.order.tracking?.trackingURLs.length && (order.order.tracking.trackingURLs.length > 1)) {
-    const url = order.order.tracking.trackingURLs[0];
-    window.open(url, '_blank');
-    // navigator.clipboard.writeText(url); // if i have to copy to clipboard
-  } else {
-    openModal.value = true;
+  if (order.order.tracking?.trackingURLs.length && !(order.order.tracking.trackingURLs.length > 1)) {
+    window.open(orderGetters.getOrderTrackingUrl(order));
   }
 };
 </script>
