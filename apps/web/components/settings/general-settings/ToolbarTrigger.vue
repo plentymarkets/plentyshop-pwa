6b19bd641adae9ef5d67cce4f7663b2546571000
rbedeoan-plenty <121931658+rbedeoan-plenty@users.noreply.github.com>
2025-07-17 08:02:45 +0300

    fix: refactor

--- apps/web/components/settings/general-settings/ToolbarTrigger.vue
+++ apps/web/components/settings/general-settings/ToolbarTrigger.vue
@@ -3,8 +3,8 @@
     type="button"
     class="editor-button relative py-2 flex justify-center"
     :class="{ 'bg-editor-button text-white rounded-md': active }"
-    aria-label="Open design drawer"
-    data-testid="open-design-drawer"
+    aria-label="Open general settings drawer"
+    data-testid="open-general-settings-drawer"
   >
     <NuxtImg v-if="active" width="24" height="24px" :src="gearWhite" />
     <NuxtImg v-else width="24" height="24px" :src="gearBlack" />
