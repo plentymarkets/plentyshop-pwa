c7dd446b3ae8381b803217e5c1a8f52dbb29eb1e
Maximilian Röll <30629157+maxiroellplenty@users.noreply.github.com>
2025-07-17 15:55:17 +0200

    fix: wishlistpagecontent unittest

--- apps/web/components/WishlistPageContent/__tests__/WishlistPageContent.spec.ts
+++ apps/web/components/WishlistPageContent/__tests__/WishlistPageContent.spec.ts
@@ -1,7 +1,15 @@
 import { mount } from '@vue/test-utils';
 import { WishlistPageContent } from '#components';
+import { mockNuxtImport } from '@nuxt/test-utils/runtime';
 
 describe('<CategoryPageContent />', () => {
+
+  mockNuxtImport('useWishlist', () => {
+    return () => {
+      return { data: computed(() => []), fetchWishlist: () => {} };
+    };
+  });
+
   it('should render component', () => {
     const { getByTestId } = mount(WishlistPageContent, {
       props: {
