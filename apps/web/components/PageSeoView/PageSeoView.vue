e990d28821f99a1da95d91391069b0099928407a
oivan-plenty <oana.ivan@plentysystems.com>
2025-07-31 10:13:22 +0300

    changed defaults for seo

--- apps/web/components/PageSeoView/PageSeoView.vue
+++ apps/web/components/PageSeoView/PageSeoView.vue
@@ -165,10 +165,10 @@ const { getCategoryId } = useCategoryIdHelper();
 const { data, loading, fetchCategorySettings } = useCategorySettings();
 const isInSitemap = computed({
   get() {
-    return data.value.sitemap === 'Y';
+    return data.value.sitemap === 'N';
   },
   set(value: boolean) {
-    data.value.sitemap = value ? 'Y' : 'N';
+    data.value.sitemap = value ? 'N' : 'Y';
   },
 });
 
@@ -211,12 +211,14 @@ const robotNames = [
 
 const pageRobots = computed({
   get() {
-    return robotNames.find((option) => option.value === data.value.details[0]?.metaRobots) || null;
+    const current = data.value.details[0]?.metaRobots;
+    return robotNames.find((option) => option.value === current) || robotNames[0];
   },
   set(selectedOption) {
     data.value.details[0].metaRobots = selectedOption ? selectedOption.value : '';
   },
 });
+
 
 const titleTooltip =
   'Title displayed in search results of search engines. Enter text here to add a prefix before the shop name in the page title (shown as “your text / shop name”).';
