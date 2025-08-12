e3c0af0ba4b2760bcfbbdf928a828a03979d4eb8
aoltean-plenty <alexandru.oltean@plentysystems.com>
2025-08-12 03:56:05 -0700

    address pr comments

--- apps/web/composables/useImageAdd/useImageAdd.ts
+++ apps/web/composables/useImageAdd/useImageAdd.ts
@@ -1,21 +1,14 @@
 export const useImageAdd = <T extends Record<string, unknown> | Ref<unknown>>(target: T, key?: string) => {
-  const isRefTarget = !!(target as Ref<unknown>).value;
-
   const handleImageAdd = (payload: { image: string; type?: string }) => {
-    switch (true) {
-      case isRefTarget:
-        (target as Ref<unknown>).value = payload.image;
-        break;
-      case !!key:
-        (target as Record<string, unknown>)[key as string] = payload.image;
-        break;
-      case !!payload.type:
-        (target as Record<string, unknown>)[payload.type as string] = payload.image;
-        break;
-      default:
-        break;
+    if (isRef(target)) {
+      target.value = payload.image;
+      return;
+    }
+    const targetKey = key || payload.type;
+
+    if (targetKey) {
+      target[targetKey] = payload.image;
     }
   };
-
   return { handleImageAdd };
 };
