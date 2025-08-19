ff7b81a57e084ff233fac2a5059ec1b83dca9303
aoltean-plenty <alexandru.oltean@plentysystems.com>
2025-08-19 05:22:18 -0700

    fix: blocks list visible while scrolling

--- apps/web/components/BlocksNavigation/BlocksNavigation.vue
+++ apps/web/components/BlocksNavigation/BlocksNavigation.vue
@@ -1,6 +1,8 @@
 <template>
-  <BlocksNavigationHeader />
-  <BlocksNavigationList />
+  <div class="site-settings-view sticky top-[52px] relative overflow-hidden">
+    <BlocksNavigationHeader />
+    <BlocksNavigationList />
+  </div>
 </template>
 
 <script setup lang="ts"></script>
