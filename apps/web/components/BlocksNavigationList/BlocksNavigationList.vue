1b04c5e16570d3bf7da36a1ed527113ed1347ca7
clincu-plenty <cristian.lincu@plentysystems.com>
2025-08-22 12:47:09 +0300

    fix: multigrid can be placed only on level 0 (not inside another multigrid)

--- apps/web/components/BlocksNavigationList/BlocksNavigationList.vue
+++ apps/web/components/BlocksNavigationList/BlocksNavigationList.vue
@@ -14,14 +14,16 @@
         <div class="relative w-fit mx-auto">
           <NuxtImg :src="variation.image" class="block" :alt="variation.title" width="253" height="120" />
           <button
-            class="absolute right-[-5px] top-[100%] -translate-y-1/2 bg-[#72CBEE] text-[#062633] w-10 h-10 rounded-full shadow-md cursor-pointer"
+            class="absolute right-[-5px] top-[100%] -translate-y-1/2 bg-[#72CBEE] text-[#062633] w-10 h-10 rounded-full shadow-md"
             :data-testid="'block-add-' + categoryIndex + '-' + variationIndex"
+            :disabled="isNestedMultigrid(category, targetUuid)"
+            :class="{'cursor-not-allowed opacity-50': isNestedMultigrid(category, targetUuid)}"
             @click="
               drawerOpen = false;
               addNewBlock(category.category, variationIndex, targetUuid, visiblePlaceholder.position);
             "
           >
-            <SfIconAdd class="cursor-pointer" />
+            <SfIconAdd />
           </button>
         </div>
       </div>
@@ -31,12 +33,16 @@
 
 <script setup lang="ts">
 import { SfIconAdd } from '@storefront-ui/vue';
+import type { Category } from '~/components/BlocksNavigationList/types';
 const { blocksLists, getBlocksLists } = useBlockManager();
 getBlocksLists();
 
-const { addNewBlock, visiblePlaceholder } = useBlockManager();
+const { addNewBlock, visiblePlaceholder, getBlockDepth } = useBlockManager();
 const { drawerOpen } = useSiteConfiguration();
 const { multigridColumnUuid } = useBlockManager();
 
 const targetUuid = computed(() => multigridColumnUuid.value || visiblePlaceholder.value.uuid);
+const isNestedMultigrid = (category: Category, uuid: string) => {
+  return category.blockName === 'MultiGrid' && getBlockDepth(uuid) > 0;
+}
 </script>
