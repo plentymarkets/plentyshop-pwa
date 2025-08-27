abe311e6cad0b8c98cbfa9ba495aba48923416eb
clincu-plenty <cristian.lincu@plentysystems.com>
2025-08-27 11:58:56 +0300

    Revert "adding translations"
    This reverts commit 2de0b1f284ca520e6d308167b66b891060b24154.

--- apps/web/components/blocks/structure/MultiGrid/EmptyGridBlock.vue
+++ apps/web/components/blocks/structure/MultiGrid/EmptyGridBlock.vue
@@ -5,8 +5,8 @@
       data-testid="active-empty-multicolumn"
       class="h-[196px] flex-1 border-2 border-dashed border-sky-400 bg-sky-50 p-6 flex flex-col items-center justify-center text-center"
     >
-      <p class="font-semibold text-gray-800">{{ t('errorMessages.editor.add.placeholder') }}</p>
-      <p class="text-sm text-gray-500">{{ t('errorMessages.editor.add.choose') }}</p>
+      <p class="font-semibold text-gray-800">Block will be placed here</p>
+      <p class="text-sm text-gray-500">Choose one from templates</p>
     </div>
     <div
       v-else
@@ -25,7 +25,6 @@ import { SfIconAdd } from '@storefront-ui/vue';
 import type { EmptyGridBlockProps } from '~/components/blocks/structure/MultiGrid/types';
 
 const { $isPreview } = useNuxtApp();
-const { t } = useI18n();
 const props = defineProps<EmptyGridBlockProps>();
 const { multigridColumnUuid, updateMultigridColumnUuid, visiblePlaceholder } = useBlockManager();
 const { openDrawerWithView, drawerOpen } = useSiteConfiguration();
