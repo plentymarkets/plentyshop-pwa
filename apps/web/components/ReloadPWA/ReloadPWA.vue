4c4dd3942e31106265077b191dbf0b5ac61c118b
Maximilian Röll <30629157+maxiroellplenty@users.noreply.github.com>
2025-09-04 13:22:16 +0200

    refactor: styles

--- apps/web/components/ReloadPWA/ReloadPWA.vue
+++ apps/web/components/ReloadPWA/ReloadPWA.vue
@@ -1,10 +1,10 @@
 <template>
   <div
     v-if="needRefresh"
-    class="fixed right-4 bottom-4 p-4 border border-gray-400 rounded z-[1000] bg-white shadow-lg flex items-center gap-4"
+    class="fixed right-4 bottom-4 p-4 border border-primary-500 rounded z-[1000] bg-white shadow-lg flex items-center gap-4"
     role="alert"
   >
-    <div class="mb-2">
+    <div>
       <span>{{ t('reloadPWA.newVersionAvailable') }}</span>
     </div>
     <UiButton @click="updatePWA">
