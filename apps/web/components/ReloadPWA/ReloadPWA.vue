37b073065b16350c46f731b494043fefdee291d7
Maximilian Röll <30629157+maxiroellplenty@users.noreply.github.com>
2025-09-04 13:08:33 +0200

    chore: format

--- apps/web/components/ReloadPWA/ReloadPWA.vue
+++ apps/web/components/ReloadPWA/ReloadPWA.vue
@@ -1,25 +1,25 @@
 <template>
-    <div
-        v-if="needRefresh"
-        class="fixed right-4 bottom-4 p-4 border border-gray-400 rounded z-[1000] bg-white shadow-lg flex items-center gap-4"
-        role="alert"
-    >
-        <div class="mb-2">
-            <span>{{ t('reloadPWA.newVersionAvailable') }}</span>
-        </div>
-        <UiButton @click="updatePWA">
-            {{ t('reloadPWA.refresh') }}
-        </UiButton>
+  <div
+    v-if="needRefresh"
+    class="fixed right-4 bottom-4 p-4 border border-gray-400 rounded z-[1000] bg-white shadow-lg flex items-center gap-4"
+    role="alert"
+  >
+    <div class="mb-2">
+      <span>{{ t('reloadPWA.newVersionAvailable') }}</span>
     </div>
+    <UiButton @click="updatePWA">
+      {{ t('reloadPWA.refresh') }}
+    </UiButton>
+  </div>
 </template>
 
 <script setup lang="ts">
-const { $pwa } = useNuxtApp()
+const { $pwa } = useNuxtApp();
 const { t } = useI18n();
 
-const needRefresh = computed(() => $pwa?.needRefresh)
+const needRefresh = computed(() => $pwa?.needRefresh);
 
 const updatePWA = async () => {
-    await $pwa?.updateServiceWorker()
-}
+  await $pwa?.updateServiceWorker();
+};
 </script>
