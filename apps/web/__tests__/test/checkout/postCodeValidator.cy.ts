2bcb49c61c8294c7c7bdbf500819cc57966eefba
Maximilian Röll <30629157+maxiroellplenty@users.noreply.github.com>
2025-09-04 15:11:51 +0200

    ci: refactor postCodeValidator cypress e2e test

--- apps/web/__tests__/test/checkout/postCodeValidator.cy.ts
+++ apps/web/__tests__/test/checkout/postCodeValidator.cy.ts
@@ -7,27 +7,36 @@ const countryList = selectedCountry ? postCodeMapper.filter((c) => c.name === se
 
 if (selectedCountry && countryList.length === 0) throw new Error(`Country "${selectedCountry}" not found.`);
 
-beforeEach(() => {
-  cy.clearCookies().visitSmoke().addToCart();
-  checkout.goToCheckoutPath().fillContactInformationForm().useShippingAsBilling.uncheck();
-});
 
-// only test one case (UK, set in env) to safe test execution time
-// other zipcodes are tested in a unit test
-countryList.forEach(({ countryId, name, valid, invalid }) => {
-  describe(`${name} Post Code Validator`, () => {
-    valid.forEach((zipCode) => {
-      it(`accepts valid postcode: ${zipCode}`, () => {
+describe('Post Code Validator', () => {
+  beforeEach(() => {
+    cy.clearCookies().visitSmoke().addToCart();
+    checkout.goToCheckoutPath().fillContactInformationForm().useShippingAsBilling.uncheck();
+  });
+
+  // only test one case (UK, set in env) to safe test execution time
+  // other zipcodes are tested in a unit test
+
+  it('should accept valid postcodes', () => {
+    countryList.forEach(({ countryId, name, valid }) => {
+      valid.forEach((zipCode) => {
+        cy.log('Testing valid postcode', name, zipCode);
         checkout.fillPostCodeBillingForm({ country: countryId, zipCode });
         cy.get('#billingZipCodeError').should('not.exist');
+        checkout.clearPostCodeBillingForm();
       });
     });
+  });
 
-    invalid.forEach((zipCode) => {
-      it(`rejects invalid postcode: ${zipCode}`, () => {
+  it('should reject invalid postcodes', () => {
+    countryList.forEach(({ countryId, name, invalid }) => {
+      invalid.forEach((zipCode) => {
+        cy.log('Testing invalid postcode', name, zipCode);
         checkout.fillPostCodeBillingForm({ country: countryId, zipCode });
         cy.get('#billingZipCodeError').should('exist');
+        checkout.clearPostCodeBillingForm();
       });
     });
   });
 });
+
