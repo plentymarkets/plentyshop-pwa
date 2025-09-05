e0369310e12655ea27557c739828b404397dc575
cristian sandru <cristian.sandru@plentysystems.com>
2025-09-05 16:28:34 +0300

    fix hydration errors

--- apps/web/components/ProductSlider/ProductSlider.vue
+++ apps/web/components/ProductSlider/ProductSlider.vue
@@ -1,33 +1,31 @@
 <template>
-  <ClientOnly>
-    <SfScrollable
-      buttons-placement="floating"
-      class="pb-4 scrollbar-hidden"
-      :wrapper-class="wrapperClass"
-      data-testid="product-slider"
-    >
-      <UiProductCard
-        v-for="product in items"
-        :key="productGetters.getId(product)"
-        :product="product"
-        :name="productGetters.getName(product)"
-        :slug="productGetters.getSlug(product) + `-${productGetters.getId(product)}`"
-        :image-url="addModernImageExtension(productGetters.getSecondPreviewImage(product))"
-        :image-alt="
-          productImageGetters.getImageAlternate(productImageGetters.getFirstImage(product)) ||
-          productGetters.getName(product) ||
-          ''
-        "
-        :image-title="productImageGetters.getImageName(productImageGetters.getFirstImage(product)) || ''"
-        :image-height="productGetters.getImageHeight(product) || 600"
-        :image-width="productGetters.getImageWidth(product) || 600"
-        :rating-count="productGetters.getTotalReviews(product)"
-        :rating="productGetters.getAverageRating(product, 'half')"
-        is-from-slider
-        class="max-w-48"
-      />
-    </SfScrollable>
-  </ClientOnly>
+  <SfScrollable
+    buttons-placement="floating"
+    class="pb-4 scrollbar-hidden"
+    :wrapper-class="wrapperClass"
+    data-testid="product-slider"
+  >
+    <UiProductCard
+      v-for="product in items"
+      :key="productGetters.getId(product)"
+      :product="product"
+      :name="productGetters.getName(product)"
+      :slug="productGetters.getSlug(product) + `-${productGetters.getId(product)}`"
+      :image-url="addModernImageExtension(productGetters.getSecondPreviewImage(product))"
+      :image-alt="
+        productImageGetters.getImageAlternate(productImageGetters.getFirstImage(product)) ||
+        productGetters.getName(product) ||
+        ''
+      "
+      :image-title="productImageGetters.getImageName(productImageGetters.getFirstImage(product)) || ''"
+      :image-height="productGetters.getImageHeight(product) || 600"
+      :image-width="productGetters.getImageWidth(product) || 600"
+      :rating-count="productGetters.getTotalReviews(product)"
+      :rating="productGetters.getAverageRating(product, 'half')"
+      is-from-slider
+      class="max-w-48"
+    />
+  </SfScrollable>
 
   <div class="mt-4 typography-text-xs flex gap-1">
     <span>{{ t('asterisk') }}</span>
