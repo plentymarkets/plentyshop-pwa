3983863cb3f9a7169ef2a008d7375148b880c040
plentydev <iulian.vladu@plentymarkets.com>
2025-09-17 13:33:34 +0300

    fix fitness quality

--- apps/web/components/blocks/CategoryData/CategoryData.vue
+++ apps/web/components/blocks/CategoryData/CategoryData.vue
@@ -1,8 +1,5 @@
 <template>
-  <h1
-    class="my-10 font-bold typography-headline-3 md:typography-headline-2"
-    data-testid="category-name"
-  >
+  <h1 class="my-10 font-bold typography-headline-3 md:typography-headline-2" data-testid="category-name">
     {{ title }}
   </h1>
 </template>
