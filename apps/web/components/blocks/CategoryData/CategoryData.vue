f4070766fa72f41dc96b60688e3a1ffa5dea5ef3
plentydev <iulian.vladu@plentymarkets.com>
2025-09-17 13:15:54 +0300

    code review changes

--- apps/web/components/blocks/CategoryData/CategoryData.vue
+++ apps/web/components/blocks/CategoryData/CategoryData.vue
@@ -2,7 +2,6 @@
   <h1
     class="my-10 font-bold typography-headline-3 md:typography-headline-2"
     data-testid="category-name"
-    :index="props.index"
   >
     {{ title }}
   </h1>
@@ -10,19 +9,6 @@
 
 <script setup lang="ts">
 import { categoryGetters } from '@plentymarkets/shop-api';
-import type { CategoryDataProps } from './types';
-
-const props = withDefaults(defineProps<CategoryDataProps>(), {
-  name: undefined,
-  type: undefined,
-  content: undefined,
-  configuration: undefined,
-  index: 0,
-  meta: () => ({
-    uuid: '',
-  }),
-  shouldLoad: undefined,
-});
 
 const { data: productsCatalog } = useProducts();
 
--- apps/web/components/blocks/CategoryData/types.ts
+++ apps/web/components/blocks/CategoryData/types.ts
@@ -1,10 +1,10 @@
 export type CategoryDataProps = {
-  name?: string;
-  type?: string;
-  content?: CategoryDataContent;
+  name: string;
+  type: string;
+  content: CategoryDataContent;
   configuration?: object;
   index?: number;
-  meta?: {
+  meta: {
     uuid: string;
   };
   shouldLoad?: boolean;
