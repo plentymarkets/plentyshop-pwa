16b3931a26bafce24ded9767c045048484eacd02
abocsan-plenty <129151096+abocsan-plenty@users.noreply.github.com>
2025-09-19 10:21:30 +0300

    chore: remove console log

--- apps/web/plugins/00.init-initial-data.ts
+++ apps/web/plugins/00.init-initial-data.ts
@@ -1,7 +1,6 @@
 export default defineNuxtPlugin(async () => {
   const { setInitialDataSSR } = useInitialSetup();
   await callOnce(async () => {
-    console.log('Setting initial data for SSR...');
     await setInitialDataSSR();
   });
 });
