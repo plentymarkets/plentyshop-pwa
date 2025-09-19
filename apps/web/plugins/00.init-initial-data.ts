968a181028a93d6ef5fce78a17272aa8f2290ea8
abocsan-plenty <129151096+abocsan-plenty@users.noreply.github.com>
2025-09-19 09:58:35 +0300

    chore: test initial data in plugin

--- apps/web/plugins/00.init-initial-data.ts
+++ apps/web/plugins/00.init-initial-data.ts
@@ -1,6 +1,7 @@
 export default defineNuxtPlugin(async () => {
   const { setInitialDataSSR } = useInitialSetup();
   await callOnce(async () => {
+    console.log('Setting initial data for SSR...');
     await setInitialDataSSR();
   });
 });
