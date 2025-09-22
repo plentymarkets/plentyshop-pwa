71508ae36fa5adbb9e134680326c5a45630b47a6
abocsan-plenty <129151096+abocsan-plenty@users.noreply.github.com>
2025-09-22 12:09:13 +0300

    feat: add item grid block + form (#1689)
    Co-authored-by: aoltean <78078265+aoltean-plenty@users.noreply.github.com>

--- apps/web/components/ui/ProductCard/types.ts
+++ apps/web/components/ui/ProductCard/types.ts
@@ -1,23 +1,11 @@
 import type { Product } from '@plentymarkets/shop-api';
+import type { ItemGridContent } from '~/components/blocks/ItemGrid/types';
 
 export type ProductCardProps = {
   product: Product;
-  name: string;
-  imageUrl: string;
-  imageAlt?: string;
-  imageTitle?: string;
-  imageWidth: number;
-  imageHeight: number;
-  description?: string;
-  rating?: number;
-  ratingCount?: number;
-  slug?: string;
-  priority?: boolean;
-  lazy?: boolean;
-  unitContent?: number;
-  unitName?: string;
-  basePrice?: string;
-  showBasePrice?: boolean;
+  configuration?: ItemGridContent;
+  index?: number;
   isFromWishlist?: boolean;
   isFromSlider?: boolean;
+  lazy?: boolean;
 };
