b18c79970328810dd2ebc05e2b4aa376fdd7fc2b
plentydev <iulian.vladu@plentymarkets.com>
2025-09-22 15:11:21 +0300

    add types

---
+++ apps/web/components/blocks/SortFilter/SortFilterForm.vue
@@ -0,0 +1,236 @@
+<template>
+  <UiAccordionItem
+      data-testid="open-sorting-and-filters-settings"
+      summary-active-class="bg-neutral-100 border-t-0"
+      summary-class="w-full hover:bg-neutral-100 px-4 py-5 flex justify-between items-center select-none border-b"
+  >
+    <template #summary>
+      <h2>{{ getEditorTranslation('display-and-order-label') }}</h2>
+    </template>
+
+    <div data-testid="text-card-form">
+
+      <!-- Enable filters toggle -->
+      <div class="py-2">
+        <div class="flex items-center justify-between gap-3">
+          <UiFormLabel for="keep-transparent" class="m-0">
+            {{ getEditorTranslation('enable-filters-label') }}
+          </UiFormLabel>
+
+          <SfSwitch
+              id="keep-transparent" v-model="sortFilterBlock.enableFilters"
+              data-testid="switch-keep-transparent"
+              class="checked:bg-editor-button checked:before:hover:bg-editor-button checked:border-gray-500 checked:hover:border:bg-gray-700 hover:border-gray-700 hover:before:bg-gray-700 checked:hover:bg-gray-300 checked:hover:border-gray-400"
+          />
+        </div>
+      </div>
+
+      <!-- Filters list -->
+      <div class="py-4">
+        <draggable
+            v-if="sortFilterBlock.filtersOrder.length"
+            v-model="sortFilterBlock.filtersOrder"
+            item-key="meta.uuid"
+            handle=".drag-slides-handle"
+            class="rounded"
+            :filter="'.no-drag'"
+        >
+          <template #item="{ element: elem, index }: { element: SortFilterFieldKey; index: number }">
+            <div :key="elem" class="flex items-center justify-between drag-slides-handle cursor-move">
+              <div class="flex items-center gap-3">
+                <button
+                    class="drag-slides-handle top-2 left-2 z-50 cursor-grab p-2 hover:bg-gray-100 rounded-full"
+                    :aria-label="getEditorTranslation('drag-reorder-aria')"
+                    :data-testid="`actions-drag-slide-handle-${index}`"
+                >
+                  <NuxtImg width="18" height="18" :src="dragIcon" />
+                </button>
+
+                <span>{{ fieldLabels[elem] }}</span>
+              </div>
+              <SfSwitch
+                  v-model="sortFilterBlock.fields[elem]"
+                  :disabled="sortFilterBlock.filtersDisabled?.includes(elem)"
+                  :data-testid="`sort-filters-field-${elem}`"
+              />
+            </div>
+          </template>
+        </draggable>
+      </div>
+
+      <!-- Show all filters immediately toggle -->
+      <div class="py-2">
+        <div class="flex items-center justify-between gap-3">
+          <UiFormLabel for="keep-transparent" class="m-0">
+            {{ getEditorTranslation('show-filters-immediately-label') }}
+          </UiFormLabel>
+
+          <SfSwitch
+              id="keep-transparent"
+              v-model="sortFilterBlock.showAllFiltersImmediately"
+              data-testid="switch-keep-transparent"
+              class="checked:bg-editor-button checked:before:hover:bg-editor-button checked:border-gray-500 checked:hover:border:bg-gray-700 hover:border-gray-700 hover:before:bg-gray-700 checked:hover:bg-gray-300 checked:hover:border-gray-400"
+          />
+        </div>
+      </div>
+
+      <!-- Number of filters to show initially -->
+      <div v-if="!sortFilterBlock.showAllFiltersImmediately" class="py-2">
+        <div class="flex justify-between mb-2">
+          <UiFormLabel>{{ getEditorTranslation('number-of-filters-label') }}</UiFormLabel>
+        </div>
+        <label>
+          <SfInput v-model="sortFilterBlock.numberOfFiltersToShowInitially" type="text" data-testid="input-number-of-filters">
+            <template #suffix>
+              <label for="sorting-and-filters-number-of-filters" class="rounded-lg cursor-pointer">
+                <input id="sorting-and-filters-number-of-filters" v-model="sortFilterBlock.numberOfFiltersToShowInitially" type="text" class="invisible w-8" />
+              </label>
+            </template>
+          </SfInput>
+        </label>
+      </div>
+
+      <!-- Items per page -->
+      <div class="py-2">
+        <UiFormLabel>{{ getEditorTranslation('items-per-page-label') }}</UiFormLabel>
+        <div class="w-full inline-flex rounded-lg border border-gray-300 bg-white text-gray-700 overflow-hidden">
+          <div
+              for="items-per-page-10"
+              class="flex items-center justify-center w-1/4 px-4 py-2 cursor-pointer text-sm border-r"
+              :class="{ 'bg-gray-100 text-gray-900 font-semibold': itemsPerPageSelected('10') }"
+              data-testid="sort-filter-form-items-per-page-10"
+              @click="sortFilterBlock.itemsPerPage = '10'"
+          >
+            <SfIconCheck :class="{ invisible: !itemsPerPageSelected('10') }" class="mr-1 w-[1.1rem]" />
+            10
+          </div>
+          <div
+              for="items-per-page-20"
+              class="flex items-center justify-center w-1/4 px-4 py-2 cursor-pointer text-sm border-r"
+              :class="{ 'bg-gray-100 text-gray-900 font-semibold': itemsPerPageSelected('20') }"
+              data-testid="sort-filter-form-items-per-page-20"
+              @click="sortFilterBlock.itemsPerPage = '20'"
+          >
+            <SfIconCheck :class="{ invisible: !itemsPerPageSelected('20') }" class="mr-1 w-[1.1rem]" />
+            20
+          </div>
+          <div
+              for="items-per-page-50"
+              class="flex items-center justify-center w-1/4 px-4 py-2 cursor-pointer text-sm border-r"
+              :class="{ 'bg-gray-100 text-gray-900 font-semibold': itemsPerPageSelected('50') }"
+              data-testid="sort-filter-form-items-per-page-50"
+              @click="sortFilterBlock.itemsPerPage = '50'"
+          >
+            <SfIconCheck :class="{ invisible: !itemsPerPageSelected('50') }" class="mr-1 w-[1.1rem]" />
+            50
+          </div>
+
+          <div
+              for="items-per-page-100"
+              class="flex items-center justify-center w-1/4 px-4 py-2 cursor-pointer text-sm"
+              :class="{ 'bg-gray-100 text-gray-900 font-semibold': itemsPerPageSelected('100') }"
+              data-testid="sort-filter-form-items-per-page-100"
+              @click="sortFilterBlock.itemsPerPage = '100'"
+          >
+            <SfIconCheck :class="{ invisible: !itemsPerPageSelected('100') }" class="mr-1 w-[1.1rem]" />
+            100
+          </div>
+        </div>
+      </div>
+    </div>
+  </UiAccordionItem>
+
+</template>
+
+<script setup lang="ts">
+import {
+  SfInput,
+  SfSwitch,
+  SfIconCheck,
+} from '@storefront-ui/vue';
+import type { SortFilterFormProps, SortFilterContent, SortFilterFieldKey, SortFilterFieldsVisibility } from './types';
+import dragIcon from 'assets/icons/paths/drag.svg';
+import draggable from 'vuedraggable/src/vuedraggable';
+
+const { data } = useCategoryTemplate();
+const { blockUuid } = useSiteConfiguration();
+const { findOrDeleteBlockByUuid } = useBlockManager();
+
+
+const props = defineProps<SortFilterFormProps>();
+
+const sortFilterBlock = computed<SortFilterContent>(() => {
+  const rawContent = findOrDeleteBlockByUuid(data.value, props.uuid || blockUuid.value)?.content ?? {};
+
+  const content = rawContent as Partial<SortFilterContent>;
+  console.log(content);
+  if (!content.enableFilters) content.enableFilters = false;
+  if (!content.showAllFiltersImmediately) content.showAllFiltersImmediately = false;
+  if (!content.numberOfFiltersToShowInitially) content.numberOfFiltersToShowInitially = '0';
+  if (!content.itemsPerPage) content.itemsPerPage = '10';
+
+  return content as SortFilterContent;
+});
+
+const fieldLabels: Record<string, string> = {
+  category: getEditorTranslation('category'),
+  sortBy: getEditorTranslation('sortBy'),
+  perPage: getEditorTranslation('perPage'),
+  itemRating: getEditorTranslation('itemRating'),
+  manufacturer: getEditorTranslation('manufacturer'),
+  price: getEditorTranslation('price'),
+  availability: getEditorTranslation('availability'),
+  customizedFilters: getEditorTranslation('customizedFilters'),
+};
+
+
+const enableFilters = ref(false);
+
+const itemsPerPageSelected = (value: '10' | '20' | '50' | '100') => {
+  return (sortFilterBlock.value.itemsPerPage || '10') === value;
+};
+
+
+watch([enableFilters], () => {
+
+});
+</script>
+
+<i18n lang="json">
+{
+  "en": {
+    "display-and-order-label": "Display and order",
+    "enable-filters-label": "Enable filters",
+
+    "category": "Category",
+    "sortBy": "Sort by",
+    "perPage": "Per page",
+    "itemRating": "Item rating",
+    "manufacturer": "Manufacturer",
+    "price": "Price",
+    "availability": "Availability",
+    "customizedFilters": "Customized filters",
+
+    "show-filters-immediately-label": "Show all filters immediately",
+    "number-of-filters-label": "Number of filters to show initially",
+    "items-per-page-label": "Items per page"
+  },
+  "de": {
+    "display-and-order-label": "Display and order",
+
+    "category": "Category",
+    "sortBy": "Sort by",
+    "perPage": "Per page",
+    "itemRating": "Item rating",
+    "manufacturer": "Manufacturer",
+    "price": "Price",
+    "availability": "Availability",
+    "customizedFilters": "Customized filters",
+
+    "enable-filters-label": "Enable filters",
+    "show-filters-immediately-label": "Show all filters immediately",
+    "number-of-filters-label": "Number of filters to show initially",
+    "items-per-page-label": "Items per page"
+  }
+}
+</i18n>
--- apps/web/components/blocks/SortFilter/types.ts
+++ apps/web/components/blocks/SortFilter/types.ts
@@ -1,10 +1,37 @@
+export type SortFilterFieldKey =
+    'category' |
+    'sortBy' |
+    'perPage' |
+    'itemRating' |
+    'manufacturer' |
+    'price' |
+    'availability' |
+    'customizedFilters';
+
+export type SortFilterFieldsVisibility = Record<SortFilterFieldKey, boolean>;
+
 export type SortFilterProps = {
   name: string;
   type: string;
   configuration?: object;
+  content: SortFilterContent,
   index?: number;
   meta: {
     uuid: string;
   };
   shouldLoad?: boolean;
+};
+
+export type SortFilterContent = {
+  enableFilters: boolean,
+  fields: SortFilterFieldsVisibility;
+  filtersOrder: SortFilterFieldKey[];
+  filtersDisabled: SortFilterFieldKey[];
+  showAllFiltersImmediately: boolean,
+  numberOfFiltersToShowInitially?: string,
+  itemsPerPage: '10' | '20' | '50' | '100';
+};
+
+export type SortFilterFormProps = {
+  uuid?: string;
 };
