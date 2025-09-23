14e1166b2f2fc22e7319a5ef3ac161512e7e20be
cristian sandru <cristian.sandru@plentysystems.com>
2025-09-23 09:45:31 +0300

    feat: register with address

--- apps/web/middleware/guest-guard.ts
+++ apps/web/middleware/guest-guard.ts
@@ -1,9 +1,9 @@
-export default defineNuxtRouteMiddleware((to) => {
+export default defineNuxtRouteMiddleware(() => {
   const { isGuest, isAuthorized } = useCustomer();
   const localePath = useLocalePath();
-  const publicPaths = [localePath(paths.authLogin)];
-  if (publicPaths.includes(to.path)) return;
 
+  if (isAuthorized.value) return navigateTo(localePath(paths.home));
   if (isGuest.value || (!isGuest.value && !isAuthorized.value)) return;
-  return navigateTo(to);
+
+  return;
 });
