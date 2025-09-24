4bf8b6f8816d5c502d3be169080b073c37ff2e12
plentydev <iulian.vladu@plentymarkets.com>
2025-09-24 11:00:00 +0300

    run format fix

--- apps/web/components/CategoryFilters/CategoryFilters.vue
+++ apps/web/components/CategoryFilters/CategoryFilters.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="flex flex-col gap-2 px-4">
     <h6
-        class="py-2 px-4 mt-6 mb-4 bg-neutral-100 typography-headline-6 font-bold text-neutral-900 uppercase tracking-widest rounded-none select-none"
-        data-testid="category-filters"
+      class="py-2 px-4 mt-6 mb-4 bg-neutral-100 typography-headline-6 font-bold text-neutral-900 uppercase tracking-widest rounded-none select-none"
+      data-testid="category-filters"
     >
       {{ t('filters') }}
     </h6>
