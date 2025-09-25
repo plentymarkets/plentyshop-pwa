589bd606bc8e54753ad943a1450feb3ee1638c3d
ivladu-plenty <112869931+ivladu-plenty@users.noreply.github.com>
2025-09-25 17:13:55 +0300

    feat: changes to sort filter (#1734)

--- apps/web/components/CategoryItemsPerPage/CategoryItemsPerPage.vue
+++ apps/web/components/CategoryItemsPerPage/CategoryItemsPerPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full md:max-w-[376px]" data-testid="category-items-per-page">
+  <div class="w-full" data-testid="category-items-per-page">
     <h6
       class="bg-neutral-100 mb-4 px-4 py-2 rounded-none uppercase typography-headline-6 font-bold tracking-widest select-none"
     >
