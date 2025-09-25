9f168dabf297cd23beca2f7fc8f12f3559d7d692
aoltean <78078265+aoltean-plenty@users.noreply.github.com>
2025-09-25 05:40:52 -0700

    fix: ensure MultiGrid margins are set for existing blocks before form is opened (#1733)
    Co-authored-by: aoltean-plenty <alexandru.oltean@plentysystems.com>
    Co-authored-by: clincu-plenty <117285582+clincu-plenty@users.noreply.github.com>

--- apps/web/components/blocks/structure/MultiGrid/MultiGridForm.vue
+++ apps/web/components/blocks/structure/MultiGrid/MultiGridForm.vue
@@ -177,6 +177,7 @@ const multiGridStructure = computed(() => {
   }
   return block;
 });
+
 const gapOptions = ['None', 'S', 'M', 'L', 'XL'];
 const gapBtnClasses =
   'py-2 leading-6 px-4 gap-2 !hover:bg-gray-100 inline-flex items-center justify-center font-medium text-base focus-visible:outline focus-visible:outline-offset rounded-md disabled:text-disabled-500 disabled:bg-disabled-300 disabled:shadow-none disabled:ring-0 disabled:cursor-not-allowed';
