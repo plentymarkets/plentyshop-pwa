deffbec4ec8b7ae254260f05823256bf1399cbc5
rbedeoan-plenty <121931658+rbedeoan-plenty@users.noreply.github.com>
2025-09-30 12:48:38 +0300

    fix: email

---
+++ apps/web/components/settings/general/contact-form/View.vue
@@ -0,0 +1,28 @@
+<template>
+  <SiteConfigurationView>
+    <template #setting-title>{{ getEditorTranslation('groups-title') }}</template>
+
+    <template #setting-description>
+      <div class="flex flex-col px-4 text-sm">
+        <p class="pb-2">
+          <span class="align-middle font-bold">{{ getEditorTranslation('description') }}</span>
+        </p>
+      </div>
+    </template>
+  </SiteConfigurationView>
+</template>
+
+<script setup lang="ts"></script>
+
+<i18n lang="json">
+{
+  "en": {
+    "groups-title": "Contact Form",
+    "description": ""
+  },
+  "de": {
+    "groups-title": "Contact Form",
+    "description": ""
+  }
+}
+</i18n>
