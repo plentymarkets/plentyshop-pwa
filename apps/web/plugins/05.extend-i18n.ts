efa040171ec5d37a4ca74d7ab2d25e27ae955f3b
abocsan-plenty <129151096+abocsan-plenty@users.noreply.github.com>
2025-09-30 13:07:56 +0300

    refactor: extract i18n declaration

--- apps/web/plugins/05.extend-i18n.ts
+++ apps/web/plugins/05.extend-i18n.ts
@@ -1,17 +1,15 @@
 export default defineNuxtPlugin(() => {
+  const { $i18n } = useNuxtApp();
+
   return {
     provide: {
       registerMessages: (msgs: Record<string, Record<string, string>>) => {
-        const { $i18n } = useNuxtApp();
-
         Object.entries(msgs).forEach(([locale, dict]) => {
           $i18n.mergeLocaleMessage(locale, dict);
         });
       },
       dynamicEditorTranslation: (key: string) => {
         const locale = 'en';
-
-        const { $i18n } = useNuxtApp();
 
         return $i18n.t(key, {}, { locale }) as string;
       },
