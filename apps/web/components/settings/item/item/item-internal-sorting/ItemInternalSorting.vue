048ba6e98db5db20d5df2c7e10899c6709fa9359
rbedeoan-plenty <121931658+rbedeoan-plenty@users.noreply.github.com>
2025-10-01 11:52:38 +0300

    fix: refactor

--- apps/web/components/settings/item/item/item-internal-sorting/ItemInternalSorting.vue
+++ apps/web/components/settings/item/item/item-internal-sorting/ItemInternalSorting.vue
@@ -5,10 +5,10 @@
       <UiFormLabel>{{ getEditorTranslation('label') }}</UiFormLabel>
     </div>
     <label>
-      <SfSelect v-model="paginationSortingDynamicInherit" data-testid="editor-bundleSettings-select" class="w-full">
-        <option v-for="bundleOption in fromItemSortingOptions" :key="bundleOption.key" :value="bundleOption.key"
+      <SfSelect v-model="paginationSortingDynamicInherit" data-testid="editor-internal-item-sorting" class="w-full">
+        <option v-for="sortingOption in fromItemSortingOptions" :key="sortingOption" :value="sortingOption"
           class="font-medium text-sm md:text-base">
-          {{ getEditorTranslation('option-' + bundleOption.key + '-label') }}
+          {{ getEditorTranslation(sortingOption) }}
         </option>
       </SfSelect>
     </label>
@@ -17,23 +17,17 @@
 
 <script setup lang="ts">
 import { SfSelect } from '@storefront-ui/vue';
-const { updateSetting, getSetting } = useSiteSettings('dontSplitItemBundle');
+const { updateSetting, getSetting } = useSiteSettings('paginationSortingDynamicInherit');
 
 const fromItemSortingOptions = ref([
-  {
-    key: 'filter.prices.price_asc',
-    text: 'Price ⬆',
-  },
-  {
-    key: 'filter.prices.price_desc',
-    text: 'Price ⬇',
-  },
-  { key: 'filter.position_asc', text: 'Availability ⬆' },
-  { key: 'filter.position_desc', text: 'Availability ⬇' },
-  { key: 'filter.availabilityAverageDays_asc', text: 'Variation number ⬆' },
-  { key: 'filter.availabilityAverageDays_desc', text: 'Variation number ⬇' },
-  { key: 'analyzed.number.sorting_asc', text: 'Variation ID ⬆' },
-  { key: 'analyzed.number.sorting_desc', text: 'Variation ID ⬆' },
+  'filter.prices.price_asc',
+  'filter.prices.price_desc',
+  'filter.position_asc',
+  'filter.position_desc',
+  'filter.availabilityAverageDays_asc',
+  'filter.availabilityAverageDays_desc',
+  'analyzed.number.sorting_asc',
+  'analyzed.number.sorting_desc',
 ]);
 
 const paginationSortingDynamicInherit = computed({
@@ -46,15 +40,25 @@ const paginationSortingDynamicInherit = computed({
   "en": {
     "label": "Internal Item Sorting",
     "description": "Use the following sorting options to determine which variation is shown on an item tile, for instance in the category page. Use the first selection to determine whether the item sorting option that the customer selects in the shop should also apply to the sorting of variations on the item tile. In this case, the selected sorting value serves as the first sorting value for the item tile. The first and second sorting options below only become effective if no clear order can be derived from the adopted item sorting, or if the adopted item sorting is not used. Sorting options that contradict each other (e.g. Manufacturer A-Z and Manufacturer Z-A) are neglected in the sorting.",
-    "option-0-label": "Only list the components of the item bundle and replace the item bundle with the basic items in the order process",
-    "option-1-label": "Only show item bundle without individual components and do not split the item bundle in the order process",
-    "option-2-label": "List both the item bundle and its individual components"
+    "filter.prices.price_asc": "Price ⬆",
+    "filter.prices.price_desc": "Price ⬇",
+    "filter.position_asc": "Availability ⬆",
+    "filter.position_desc": "Availability ⬇",
+    "filter.availabilityAverageDays_asc": "Variation number ⬆",
+    "filter.availabilityAverageDays_desc": "Variation number ⬇",
+    "analyzed.number.sorting_asc": "Variation ID ⬆",
+    "analyzed.number.sorting_desc": "Variation ID ⬇"
   },
   "de": {
     "label": "Internal Item Sorting",
     "description": "Use the following sorting options to determine which variation is shown on an item tile, for instance in the category page. Use the first selection to determine whether the item sorting option that the customer selects in the shop should also apply to the sorting of variations on the item tile. In this case, the selected sorting value serves as the first sorting value for the item tile. The first and second sorting options below only become effective if no clear order can be derived from the adopted item sorting, or if the adopted item sorting is not used. Sorting options that contradict each other (e.g. Manufacturer A-Z and Manufacturer Z-A) are neglected in the sorting.",
-    "option-0-label": "Only list the components of the item bundle and replace the item bundle with the basic items in the order process",
-    "option-1-label": "Only show item bundle without individual components and do not split the item bundle in the order process",
-    "option-2-label": "List both the item bundle and its individual components"
-  }
+    "filter.prices.price_asc": "Preis ⬆",
+    "filter.prices.price_desc": "Price ⬇",
+    "filter.position_asc": "Availability ⬆",
+    "filter.position_desc": "Availability ⬇",
+    "filter.availabilityAverageDays_asc": "Variation number ⬆",
+    "filter.availabilityAverageDays_desc": "Variation number ⬇",
+    "analyzed.number.sorting_asc": "Variation ID ⬆",
+    "analyzed.number.sorting_desc": "Variation ID ⬇"
+  },
 }</i18n>
