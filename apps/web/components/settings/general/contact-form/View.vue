3043d6228541ce4d49d76d3baba8d40c3607452c
rbedeoan-plenty <121931658+rbedeoan-plenty@users.noreply.github.com>
2025-10-03 16:09:14 +0300

    fix: remove description

--- apps/web/components/settings/general/contact-form/View.vue
+++ apps/web/components/settings/general/contact-form/View.vue
@@ -1,14 +1,6 @@
 <template>
   <SiteConfigurationView>
     <template #setting-title>{{ getEditorTranslation('groups-title') }}</template>
-
-    <template #setting-description>
-      <div class="flex flex-col px-4 text-sm">
-        <p class="pb-2">
-          <span class="align-middle font-bold">{{ getEditorTranslation('description') }}</span>
-        </p>
-      </div>
-    </template>
   </SiteConfigurationView>
 </template>
 
@@ -18,11 +10,9 @@
 {
   "en": {
     "groups-title": "Contact Form",
-    "description": ""
   },
   "de": {
     "groups-title": "Contact Form",
-    "description": ""
   }
 }
 </i18n>
