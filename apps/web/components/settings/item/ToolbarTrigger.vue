bde388c5bf2fb3b51f77f5b7b9ec5f8a9fa4bf95
abocsan-plenty <129151096+abocsan-plenty@users.noreply.github.com>
2025-10-08 17:44:42 +0300

    chore: add flag to enable all site settings

--- apps/web/components/settings/item/ToolbarTrigger.vue
+++ apps/web/components/settings/item/ToolbarTrigger.vue
@@ -1,7 +1,7 @@
 <template>
   <SfTooltip :label="getEditorTranslation('tooltip')" placement="right" class="inline-grid" :show-arrow="true">
     <button
-      v-if="runtimeConfig.public.isDev"
+      v-if="runtimeConfig.public.isDev || runtimeConfig.public.enableAllEditorSettings"
       type="button"
       class="editor-button relative py-2 flex justify-center"
       :class="{ 'bg-editor-button text-white rounded-md': active }"
