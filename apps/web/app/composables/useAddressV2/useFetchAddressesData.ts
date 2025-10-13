d2336471e4ee239ad99dfdf550571eebcde42f0a
Noah Feist <144685636+N-Feist@users.noreply.github.com>
2025-10-13 13:16:54 +0200

    feat: working useFetchAddressesData
    probably needs to have an updated use of useAggregatedCountries as well

--- apps/web/app/composables/useAddressV2/useFetchAddressesData.ts
+++ apps/web/app/composables/useAddressV2/useFetchAddressesData.ts
@@ -10,19 +10,30 @@ export const useFetchAddressesData = () => {
   const { set: setShippingAddressStore } = useAddressStore(AddressType.Shipping);
   const { set: setBillingAddressStore } = useAddressStore(AddressType.Billing);
 
+  const setAddresses = (billingAddresses: Address[], shippingAddresses: Address[]) => {
+    setBillingAddressStore(billingAddresses);
+    setShippingAddressStore(shippingAddresses);
+
+    if (billingAddresses.length) {
+      usePrimaryAddress(AddressType.Billing).primaryAddressId.value =
+        billingAddresses.find((item) => item.primary)?.id || -1;
+    }
+    if (shippingAddresses.length) {
+      usePrimaryAddress(AddressType.Shipping).primaryAddressId.value =
+        shippingAddresses.find((item) => item.primary)?.id || -1;
+    }
+  };
+
   const fetch = async () => {
     try {
       state.value.loading = true;
 
       const data = await useSdk().plentysystems.getAddressesData({ types: [] });
 
-      // TODO: need to have a typeID on the addresses so i can split them up properly
-      const uniqueAddresses = data.data.addresses.filter(
-        (addr, index, self) => index === self.findIndex((a) => a.id === addr.id),
-      );
+      const billingAddresses = data.data.addresses.filter((addr) => addr.typeId === AddressType.Billing);
+      const shippingAddresses = data.data.addresses.filter((addr) => addr.typeId === AddressType.Shipping);
 
-      setAddresses(uniqueAddresses, uniqueAddresses);
-
+      setAddresses(billingAddresses, shippingAddresses);
       state.value.loading = false;
     } catch (error: unknown) {
       useHandleError(error as ApiError);
@@ -40,29 +51,13 @@ export const useFetchAddressesData = () => {
     useHandleError(error.value ?? null);
 
     if (data.value?.data.addresses) {
-      // TODO: need to have a typeID on the addresses so i can split them up properly
-      const uniqueAddresses = data.value.data.addresses.filter(
-        (addr, index, self) => index === self.findIndex((a) => a.id === addr.id),
-      );
+      const billingAddresses = data.value.data.addresses.filter((addr) => addr.typeId === AddressType.Billing);
+      const shippingAddresses = data.value.data.addresses.filter((addr) => addr.typeId === AddressType.Shipping);
 
-      setAddresses(uniqueAddresses, uniqueAddresses);
+      setAddresses(billingAddresses, shippingAddresses);
     }
 
     state.value.loading = false;
-  };
-
-  const setAddresses = (billingAddresses: Address[], shippingAddresses: Address[]) => {
-    setBillingAddressStore(billingAddresses);
-    setShippingAddressStore(shippingAddresses);
-
-    if (billingAddresses.length) {
-      usePrimaryAddress(AddressType.Billing).primaryAddressId.value =
-        billingAddresses.find((item) => item.primary)?.id || -1;
-    }
-    if (shippingAddresses.length) {
-      usePrimaryAddress(AddressType.Shipping).primaryAddressId.value =
-        shippingAddresses.find((item) => item.primary)?.id || -1;
-    }
   };
 
   return {
