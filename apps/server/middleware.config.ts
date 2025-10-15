d1eba71c3e7ed63f7fc8bab0875403c3a5c4763b
Dominik Meyer <dominik.meyer@plentysystems.com>
2025-10-15 16:36:58 +0200

    add tls agent

--- apps/server/middleware.config.ts
+++ apps/server/middleware.config.ts
@@ -1,7 +1,18 @@
 import dotenv from 'dotenv';
 import * as path from 'path';
+import * as https from 'https';
+import consola from "consola";
 dotenv.config({
   path: path.resolve(__dirname, '../web/.env'),
+});
+
+const tlsAgent = new https.Agent({
+  // Use a protocol known to be widely supported by older enterprise systems
+  minVersion: 'TLSv1.2',
+
+  // Set SECLEVEL=0 to allow all ciphers, just in case the prior env var failed due to syntax
+  // and we need to be absolutely permissive here.
+  ciphers: 'DEFAULT@SECLEVEL=0',
 });
 
 const config = {
@@ -16,11 +27,14 @@ const config = {
           url: process.env.API_ENDPOINT,
           securityToken: process.env.API_SECURITY_TOKEN ?? '',
           debug: true,
+          httpsAgent: tlsAgent
         },
       },
       errorHandler: (error: any, req: any, res: any) => {
         // override the default error handler to preserve the original error response
         // https://docs.alokai.com/middleware/guides/custom-error-handler#customize-the-error-handler
+        consola.log('error calling API');
+        consola.log(error);
         if (error?.response?.status) {
           res.status(error.response.status).send(error.response?.data);
         } else {
