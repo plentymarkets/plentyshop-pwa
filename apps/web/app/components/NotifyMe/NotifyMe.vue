93fbdedd364e61552fd6bd540d863c804eda8386
Noah Feist <144685636+N-Feist@users.noreply.github.com>
2025-10-15 09:33:02 +0200

    fix: unused styling

--- apps/web/app/components/NotifyMe/NotifyMe.vue
+++ apps/web/app/components/NotifyMe/NotifyMe.vue
@@ -26,7 +26,6 @@
       tag="section"
       role="dialog"
       class="h-full w-full overflow-auto md:w-[600px] md:h-fit"
-      style="z-index: 9999;"
       aria-labelledby="notify-modal-title"
       aria-describedby="notify-modal-description"
     >
