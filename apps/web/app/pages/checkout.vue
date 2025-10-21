e15e5b2c78784ec93b333ebacd8b04540645a148
Fabian Gerke <124085586+FabianGerke@users.noreply.github.com>
2025-10-21 12:24:48 +0200

    feat: checkout ux - billing address section (#1828)

--- apps/web/app/pages/checkout.vue
+++ apps/web/app/pages/checkout.vue
@@ -7,8 +7,10 @@
         <UiDivider id="top-shipping-divider" class="w-screen md:w-auto -mx-4 md:mx-0" />
         <AddressContainer id="shipping-address" :key="0" :type="AddressType.Shipping" />
         <UiDivider id="top-billing-divider" class="w-screen md:w-auto -mx-4 md:mx-0" />
-        <AddressContainer id="billing-address" :key="1" :type="AddressType.Billing" />
-        <UiDivider id="bottom-billing-divider" class-name="w-screen md:w-auto -mx-4 md:mx-0" />
+        <div v-if="showBillingAddressSection">
+          <AddressContainer id="billing-address" :key="1" :type="AddressType.Billing" />
+          <UiDivider id="bottom-billing-divider" class-name="w-screen md:w-auto -mx-4 md:mx-0" />
+        </div>
         <div class="relative" :class="{ 'pointer-events-none opacity-50': disableShippingPayment }">
           <ShippingMethod
             :disabled="disableShippingPayment"
@@ -74,6 +76,7 @@ const {
   persistBillingAddress,
   setBillingSkeleton,
   setShippingSkeleton,
+  showBillingAddressSection,
 } = useCheckout();
 const { preferredDeliveryAvailable } = usePreferredDelivery();
 const { fetchPaymentMethods } = usePaymentMethods();
