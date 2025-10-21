6a76c6dbdddf9a9849e95039916087ae9de79ead
Fabian Gerke <124085586+FabianGerke@users.noreply.github.com>
2025-10-21 11:34:50 +0200

    feat: checkout ux shipping method display

--- apps/web/app/components/ShippingMethod/ShippingMethod.vue
+++ apps/web/app/components/ShippingMethod/ShippingMethod.vue
@@ -1,38 +1,39 @@
 <template>
   <div data-testid="shipping-method" class="md:px-4 my-6">
     <h3 class="text-neutral-900 text-lg font-bold">{{ t('shippingMethod.heading') }}</h3>
-    <div v-if="!loading" class="mt-4">
-      <ul
-        v-if="shippingMethods && shippingMethods.length > 0"
-        class="grid gap-y-4 md:grid-cols-2 md:gap-x-4"
-        role="radiogroup"
-        data-testid="shipping-method-list"
-      >
-        <SfListItem
-          v-for="(method, index) in shippingMethods"
-          :key="`shipping-method-${index}`"
-          :disabled="disabled"
-          tag="label"
-          children-tag="div"
-          class="border rounded-md items-start select-none"
-          @click.prevent="updateShippingMethod(shippingProviderGetters.getParcelServicePresetId(method))"
+    <div v-if="hasCheckoutAddress">
+      <div v-if="!loading" class="mt-4">
+        <ul
+            v-if="shippingMethods && shippingMethods.length > 0"
+            class="grid gap-y-4 md:grid-cols-2 md:gap-x-4"
+            role="radiogroup"
+            data-testid="shipping-method-list"
         >
-          <template #prefix>
-            <SfRadio
-              v-model="radioModel"
-              :checked="
+          <SfListItem
+              v-for="(method, index) in shippingMethods"
+              :key="`shipping-method-${index}`"
+              :disabled="disabled"
+              tag="label"
+              children-tag="div"
+              class="border rounded-md items-start select-none"
+              @click.prevent="updateShippingMethod(shippingProviderGetters.getParcelServicePresetId(method))"
+          >
+            <template #prefix>
+              <SfRadio
+                  v-model="radioModel"
+                  :checked="
                 shippingProviderGetters.getShippingProfileId(cart).toString() ===
                 shippingProviderGetters.getParcelServicePresetId(method)
               "
-              :value="shippingProviderGetters.getParcelServicePresetId(method)"
-              class="flex items-center"
-            />
-          </template>
-          <div class="flex items-center flex-row gap-2">
-            <span>{{ shippingProviderGetters.getShippingMethodName(method) }}</span>
-            <span class="ml-auto">{{ getShippingAmount(shippingProviderGetters.getShippingAmount(method)) }}</span>
-          </div>
-          <div v-if="getDeliveryDays(shippingProviderGetters.getParcelServicePresetId(method))">
+                  :value="shippingProviderGetters.getParcelServicePresetId(method)"
+                  class="flex items-center"
+              />
+            </template>
+            <div class="flex items-center flex-row gap-2">
+              <span>{{ shippingProviderGetters.getShippingMethodName(method) }}</span>
+              <span class="ml-auto">{{ getShippingAmount(shippingProviderGetters.getShippingAmount(method)) }}</span>
+            </div>
+            <div v-if="getDeliveryDays(shippingProviderGetters.getParcelServicePresetId(method))">
             <span class="text-sm">
               {{
                 t('shippingMethod.maxDeliveryDays', {
@@ -40,25 +41,28 @@
                 })
               }}</span
             >
-          </div>
-        </SfListItem>
-      </ul>
+            </div>
+          </SfListItem>
+        </ul>
 
-      <div
-        v-else
-        class="flex items-start bg-warning-100 shadow-md pr-2 pl-4 ring-1 ring-warning-200 typography-text-sm md:typography-text-base py-1 rounded-md"
-        data-testid="no-shipping-method-available"
-      >
-        <SfIconWarning class="mt-2 mr-2 text-warning-700 shrink-0" />
-        <div class="py-2 mr-2">
-          <p v-if="hasCheckoutAddress">{{ t('shippingMethod.noMethodsAvailable') }}</p>
-          <p v-else>{{ t('shippingMethod.description') }}</p>
+        <div
+            v-else
+            class="flex items-start bg-warning-100 shadow-md pr-2 pl-4 ring-1 ring-warning-200 typography-text-sm md:typography-text-base py-1 rounded-md"
+            data-testid="no-shipping-method-available"
+        >
+          <SfIconWarning class="mt-2 mr-2 text-warning-700 shrink-0" />
+          <div class="py-2 mr-2">
+            {{ t('shippingMethod.noMethodsAvailable') }}
+          </div>
         </div>
       </div>
-    </div>
-    <ShippingMethodSkeleton v-else />
+      <ShippingMethodSkeleton v-else />
 
-    <ShippingPrivacy v-if="showShippingPrivacy" />
+      <ShippingPrivacy v-if="showShippingPrivacy" />
+    </div>
+    <div v-else class="mt-4 bg-gray-200 shadow-md p-4 ring-1 ring-gray-300 text-gray-700 typography-text-sm md:typography-text-base rounded-md">
+      {{ t('shipping.noShippingMethodsNoAddress') }}
+    </div>
   </div>
 </template>
 
@@ -79,10 +83,10 @@ const { shippingMethods } = useCheckoutPagePaymentAndShipping();
 const radioModel = ref(shippingProviderGetters.getShippingProfileId(cart.value));
 
 const showShippingPrivacy = computed(
-  () =>
-    shippingMethods.value.length > 0 &&
-    selectedMethod.value &&
-    shippingProviderGetters.getDataPrivacyAgreementHint(selectedMethod.value),
+    () =>
+        shippingMethods.value.length > 0 &&
+        selectedMethod.value &&
+        shippingProviderGetters.getDataPrivacyAgreementHint(selectedMethod.value),
 );
 
 const getDeliveryDays = (method: string) => {
