1335f0efc41e3c27404a8a0d378ca4362db1b2bc
abocsan-plenty <129151096+abocsan-plenty@users.noreply.github.com>
2025-10-23 16:49:12 +0300

    chore: revert flat helper

--- apps/web/app/utils/pages.ts
+++ apps/web/app/utils/pages.ts
@@ -1,28 +1,10 @@
 export const flattenPages = (
-  pages: { name: string; path: string; id: number; children?: unknown[]; right?: string; type?: string }[],
-  skipsFilters = true,
-): { name: string; path: string; id: string; right?: string; type?: string }[] => {
-  return pages.reduce<{ name: string; path: string; id: string; right?: string; type?: string }[]>((acc, page) => {
-    const passesFilters = skipsFilters || (page.right !== 'customer' && page.type === 'item');
-
-    if (passesFilters) {
-      acc.push({ name: page.name, path: page.path, id: page.id.toString(), right: page.right, type: page.type });
-    }
-
+  pages: { name: string; path: string; id: number; children?: unknown[] }[],
+): { name: string; path: string; id: string }[] => {
+  return pages.reduce<{ name: string; path: string; id: string }[]>((acc, page) => {
+    acc.push({ name: page.name, path: page.path, id: page.id.toString() });
     if (page.children) {
-      acc.push(
-        ...flattenPages(
-          page.children as {
-            name: string;
-            path: string;
-            id: number;
-            children?: unknown[];
-            right?: string;
-            type?: string;
-          }[],
-          skipsFilters,
-        ),
-      );
+      acc.push(...flattenPages(page.children as { name: string; path: string; id: number; children?: unknown[] }[]));
     }
     return acc;
   }, []);
