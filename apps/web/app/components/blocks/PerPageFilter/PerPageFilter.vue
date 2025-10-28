88dfd86847271109a02baf654576a781df6938b0
oivan-plenty <oana.ivan@plentysystems.com>
2025-10-28 11:15:00 +0200

    individual perPage block

---
+++ apps/web/app/components/blocks/PerPageFilter/PerPageFilter.vue
@@ -0,0 +1,7 @@
+<template v-if="key === 'perPage' && props.content?.fields.perPage">
+  <CategoryItemsPerPage class="mb-6" :total-products="productsCatalog.pagination?.totals ?? 0" />
+</template>
+<script setup lang="ts">
+const { data: productsCatalog } = useProducts();
+
+</script>
