9b570ae1ac1d8fa7b1ba490b64803c0d88899993
Fabian Gerke <124085586+FabianGerke@users.noreply.github.com>
2025-10-28 10:18:22 +0100

    remove non-sense todo

--- apps/web/app/composables/useAddress/useAddress.ts
+++ apps/web/app/composables/useAddress/useAddress.ts
@@ -135,7 +135,6 @@ export const useAddress: UseAddressReturn = (type: AddressType, cacheKey = '') =
     state.value.loading = true;
 
     try {
-      //TODO: check for type and return value after sdk release
       const { data } = await useSdk().plentysystems.getAddresses({
         typeId: type,
       });
