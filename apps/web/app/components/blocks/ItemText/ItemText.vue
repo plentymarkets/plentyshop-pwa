bccd0eaa36fd4bb483bc7c627d1386e3e5770350
rbedeoan-plenty <121931658+rbedeoan-plenty@users.noreply.github.com>
2025-10-29 14:49:33 +0200

    fix: add dummy data

--- apps/web/app/components/blocks/ItemText/ItemText.vue
+++ apps/web/app/components/blocks/ItemText/ItemText.vue
@@ -31,5 +31,14 @@ const initiallyCollapsed = props.content?.initiallyCollapsed
 const displayAsCollapsable = props.content?.displayAsCollapsable
 const { productId } = createProductParams(route.params);
 const { data: product } = useProduct(productId);
-const text = computed(() => { return productGetters.getDescription(product.value) ?? 'see tab "item text"' });
+const text = computed(() => { return productGetters.getDescription(product.value) ?? getEditorTranslation('dummyData') });
 </script>
+
+<i18n lang="json">{
+  "en": {
+    "dummyData": "On this page, you can define how your item detail pages should be structured. You decide which content is visible, in what order the elements appear, and how much information you want to provide.  The field you are currently looking at is the item text. This is a free text field that can be as long as you like. Use this space to give your customers detailed information about the item, such as special features or possible uses.  The text you see here is, of course, just placeholder text. It is meant to show you where the item description will be positioned on the item detail page and how it will be displayed.  The actual item texts are managed in the backend, within the item management section. There, you have a variety of formatting options to help you present your content in an attractive way, such as:  - Adding links  - Inserting small images or icons  - Creating clear and structured tables  - Using paragraphs and lists for better readability  - And much more.  Make use of these features to customize your item detail pages and create a convincing and enjoyable shopping experience for your customers.",
+  },
+  "de": {
+    "dummyData": "Auf dieser Seite können Sie festlegen, wie Ihre Produktdetailseiten gestaltet werden sollen. Sie bestimmen selbst, welche Inhalte sichtbar sind, in welcher Reihenfolge diese erscheinen und wie umfangreich die Informationen sein sollen.",
+  }
+}</i18n>
