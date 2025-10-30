3ccaa1ad601e0c4ec982cf087554f88b76299ee1
oivan-plenty <oana.ivan@plentysystems.com>
2025-10-30 07:59:15 +0200

    updated sort block

---
+++ apps/web/app/components/blocks/Sort/types.ts
@@ -0,0 +1,16 @@
+export interface SortSettings {
+  selectionModeAlways: boolean;
+}
+export type SortContent = {
+  layout: {
+    paddingTop: number;
+    paddingBottom: number;
+    paddingLeft: number;
+    paddingRight: number;
+  }
+  settings: SortSettings;
+
+};
+export interface SortFormProps {
+  uuid?: string;
+}
