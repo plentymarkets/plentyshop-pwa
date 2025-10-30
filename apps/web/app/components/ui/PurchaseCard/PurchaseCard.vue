8418dacdbcfee599612291262b4d82fb066f9e7a
abocsan-plenty <129151096+abocsan-plenty@users.noreply.github.com>
2025-10-30 14:04:41 +0200

    chore: default configuration

--- apps/web/app/components/ui/PurchaseCard/PurchaseCard.vue
+++ apps/web/app/components/ui/PurchaseCard/PurchaseCard.vue
@@ -218,7 +218,7 @@ const { product, configuration } = withDefaults(defineProps<PurchaseCardProps>()
       previewText: true,
       attributes: true,
       itemBundle: false,
-      graduatedPrices: false,
+      graduatedPrices: true,
       addToWishlist: true,
       quantityAndAddToCart: true,
       itemText: false,
