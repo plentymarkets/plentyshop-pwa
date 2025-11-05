734d791ac29f44f1545f0d44e679908951042c54
abocsan-plenty <129151096+abocsan-plenty@users.noreply.github.com>
2025-11-05 15:19:06 +0200

    fix: fix default data content by properly instantiation use category template

--- apps/web/app/components/ui/Toolbar/Toolbar.vue
+++ apps/web/app/components/ui/Toolbar/Toolbar.vue
@@ -38,7 +38,6 @@
           placement="bottom"
           :show-arrow="true"
         >
-          {{ settingsIsDirty }} / {{ isEditingEnabled }}
           <button
             class="self-start bg-[#062633] text-white px-2 py-1 rounded-md font-inter font-medium text-sm leading-5 flex items-center md:px-4 md:py-2 md:text-base md:leading-6"
             :class="{ 'opacity-40 cursor-not-allowed': !isTouched || settingsLoading }"
