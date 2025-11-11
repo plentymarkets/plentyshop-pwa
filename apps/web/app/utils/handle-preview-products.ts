aa38ccc1c4dd6fe226aa271299b2e3bddfb5f82f
rbedeoan-plenty <121931658+rbedeoan-plenty@users.noreply.github.com>
2025-11-11 14:06:08 +0200

    fix: fake data for categories should be just in english for now

--- apps/web/app/utils/handle-preview-products.ts
+++ apps/web/app/utils/handle-preview-products.ts
@@ -1,5 +1,5 @@
 import { facetMock } from './facets/fakeFacetCall';
-import { fakeProductDE } from './facets/fakeProductDE';
+import { fakeProductEN } from './facets/fakeProductEN';
 
 import type { UseProductsState } from '~/composables/useProducts/types';
 
@@ -9,9 +9,9 @@ export const handlePreviewProducts = (state: Ref<UseProductsState>) => {
   if (state.value.data.category.type === 'item' && $isPreview && state.value.data.products.length === 0) {
     state.value.data = facetMock.data;
     state.value.data.products = Array.from({ length: 8 }, (_, ind) => ({
-      ...fakeProductDE,
+      ...fakeProductEN,
       texts: {
-        ...(fakeProductDE.texts ?? {}),
+        ...(fakeProductEN.texts ?? {}),
         name1: 'Example Product ' + (ind + 1),
       },
     }));
