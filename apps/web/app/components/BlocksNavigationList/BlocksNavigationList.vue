dbc5042da8027a7411aa075c7ea4d15a7e4da293
aoltean-plenty <alexandru.oltean@plentysystems.com>
2025-11-11 05:38:33 -0800

    safe side

--- apps/web/app/components/BlocksNavigationList/BlocksNavigationList.vue
+++ apps/web/app/components/BlocksNavigationList/BlocksNavigationList.vue
@@ -3,55 +3,57 @@
     <UiAccordionItem
       v-if="pageHasAccessToCategory(category)"
       summary-active-class="bg-neutral-100 border-t-0"
-      summary-class="w-full hover:bg-neutral-100 px-4 py-5 flex justify-between items-center select-none border-b"
+      summary-class="w-full hover:bg-neutral-100 px-4 py-3 flex justify-between items-center select-none border-b"
       :data-testid="'block-category-' + categoryIndex"
     >
       <template #summary>
         <h2>{{ category.title }}</h2>
       </template>
-      <div class="px-4 py-4 mb-4">
-        <div v-for="(variation, variationIndex) in category.variations" :key="variationIndex" class="mb-10">
-          <div class="relative w-fit mx-auto">
-            <NuxtImg :src="variation.image" class="block" :alt="variation.title" width="253" height="120" />
-            <button
-              class="absolute right-[-5px] top-[100%] -translate-y-1/2 bg-[#72CBEE] text-[#062633] w-10 h-10 rounded-full shadow-md"
-              :data-testid="'block-add-' + categoryIndex + '-' + variationIndex"
-              :disabled="isAddDisabled(category, variation, targetUuid)"
-              :class="{
-                'cursor-not-allowed opacity-50': isAddDisabled(category, variation, targetUuid),
-              }"
-              @click="
-                drawerOpen = false;
-                addNewBlock(category.category, variationIndex, targetUuid, blockPosition);
-              "
-            >
-              <SfTooltip
-                v-if="isSingleInstanceOnPage(variation.template.en.name)"
-                :label="getEditorTranslation('errorMessages.editor.add.disabledOneInstancePerPage')"
-                placement="top"
-                :show-arrow="true"
+      <div class="py-4 mb-4">
+        <div class="overflow-y-auto max-h-[400px]">
+          <div v-for="(variation, variationIndex) in category.variations" :key="variationIndex" class="mb-10">
+            <div class="relative w-fit mx-auto">
+              <NuxtImg :src="variation.image" class="block" :alt="variation.title" width="253" height="120" />
+              <button
+                class="absolute right-[-5px] top-[100%] -translate-y-1/2 bg-[#72CBEE] text-[#062633] w-10 h-10 rounded-full shadow-md"
+                :data-testid="'block-add-' + categoryIndex + '-' + variationIndex"
+                :disabled="isAddDisabled(category, variation, targetUuid)"
+                :class="{
+                  'cursor-not-allowed opacity-50': isAddDisabled(category, variation, targetUuid),
+                }"
+                @click="
+                  drawerOpen = false;
+                  addNewBlock(category.category, variationIndex, targetUuid, blockPosition);
+                "
               >
-                <SfIconAdd />
-              </SfTooltip>
-              <SfIconAdd v-else />
-            </button>
+                <SfTooltip
+                  v-if="isSingleInstanceOnPage(variation.template.en.name)"
+                  :label="getEditorTranslation('errorMessages.editor.add.disabledOneInstancePerPage')"
+                  placement="top"
+                  :show-arrow="true"
+                >
+                  <SfIconAdd />
+                </SfTooltip>
+                <SfIconAdd v-else />
+              </button>
+            </div>
           </div>
-        </div>
-        <div
-          v-if="isNestedMultigrid(category, targetUuid)"
-          class="mx-4 mt-4 mb-4 flex items-start gap-2 text-sm text-neutral-600"
-        >
-          <SfIconWarning class="mt-0.5 shrink-0 text-yellow-500" />
-          <span class="italic">{{ getEditorTranslation('errorMessages.editor.add.disabledTooDeeplyNested') }}</span>
-        </div>
-        <div
-          v-if="isForbiddenBlock(category, targetUuid)"
-          class="mx-4 mt-4 mb-4 flex items-start gap-2 text-sm text-neutral-600"
-        >
-          <SfIconWarning class="mt-0.5 shrink-0 text-yellow-500" />
-          <span class="italic">{{
-            getEditorTranslation('errorMessages.editor.add.disabledNotCompatibleWithLayouts')
-          }}</span>
+          <div
+            v-if="isNestedMultigrid(category, targetUuid)"
+            class="mx-4 mt-4 mb-4 flex items-start gap-2 text-sm text-neutral-600"
+          >
+            <SfIconWarning class="mt-0.5 shrink-0 text-yellow-500" />
+            <span class="italic">{{ getEditorTranslation('errorMessages.editor.add.disabledTooDeeplyNested') }}</span>
+          </div>
+          <div
+            v-if="isForbiddenBlock(category, targetUuid)"
+            class="mx-4 mt-4 mb-4 flex items-start gap-2 text-sm text-neutral-600"
+          >
+            <SfIconWarning class="mt-0.5 shrink-0 text-yellow-500" />
+            <span class="italic">{{
+              getEditorTranslation('errorMessages.editor.add.disabledNotCompatibleWithLayouts')
+            }}</span>
+          </div>
         </div>
       </div>
     </UiAccordionItem>
