460ac9a3e2c7f06ad9df6237b1ce232e85fdeaa6
Cristi Sandru <149154151+csandru-plenty@users.noreply.github.com>
2025-11-12 16:41:09 +0200

    feat: editor settings entry (#1946)

---
+++ apps/web/app/components/settings/localization/ToolbarTrigger.vue
@@ -0,0 +1,37 @@
+<template>
+  <SfTooltip
+    :label="getEditorTranslation('tooltip')"
+    placement="right"
+    class="inline-grid font-editor"
+    :show-arrow="true"
+  >
+    <button
+      type="button"
+      class="editor-button relative py-2 flex justify-center"
+      :class="{ 'bg-editor-button text-white rounded-md': active }"
+      aria-label="Open localization settings drawer"
+      data-testid="open-localization-settings-drawer"
+    >
+      <SfIconLanguage width="24" height="24px" />
+    </button>
+  </SfTooltip>
+</template>
+
+<script setup lang="ts">
+import { SfIconLanguage, SfTooltip } from '@storefront-ui/vue';
+
+defineProps({
+  active: Boolean,
+});
+</script>
+
+<i18n lang="json">
+{
+  "en": {
+    "tooltip": "Localization: Manage translations for static texts inside the shop."
+  },
+  "de": {
+    "tooltip": "Localization: Manage translations for static texts inside the shop."
+  }
+}
+</i18n>
---
+++ apps/web/app/components/settings/localization/edit-translations/View.vue
@@ -0,0 +1,53 @@
+<template>
+  <SiteConfigurationView>
+    <template #setting-title>{{ getEditorTranslation('label') }}</template>
+    <template #setting-description>
+      <div class="flex flex-col px-4 text-sm">
+        <p class="pb-2">
+          <span class="align-middle font-bold">{{ getEditorTranslation('description') }}</span>
+        </p>
+
+        <UiButton variant="secondary" class="mt-2 mb-5" :disabled="loading" @click="fetchAllTranslations">
+          <SfLoaderCircular v-if="loading" class="flex justify-center items-center" size="base" />
+          <span v-else class="flex items-center">
+            {{ getEditorTranslation('label') }}
+            <SfIconBase size="xs" viewBox="0 0 18 18" class="fill-primary-900 cursor-pointer ml-2">
+              <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path :d="editPath" fill="black" />
+              </svg>
+            </SfIconBase>
+          </span>
+        </UiButton>
+      </div>
+    </template>
+  </SiteConfigurationView>
+</template>
+
+<script setup lang="ts">
+import { SfIconBase, SfLoaderCircular } from '@storefront-ui/vue';
+import { editPath } from '~/assets/icons/paths/edit';
+
+const { loadKeys, keys, loading } = useEditorLocalizationKeys();
+
+const fetchAllTranslations = async () => {
+  try {
+    await loadKeys();
+    console.log(keys.value);
+  } catch (error) {
+    console.error(error);
+  }
+};
+</script>
+
+<i18n lang="json">
+{
+  "en": {
+    "label": "Edit Translations",
+    "description": "Translate static texts of your shop."
+  },
+  "de": {
+    "label": "Edit Translations",
+    "description": "Translate static texts of your shop."
+  }
+}
+</i18n>
