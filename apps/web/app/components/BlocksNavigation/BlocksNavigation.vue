0c6c1bbfb5704d135b07fd0fdf9e1229d7bbb582
aoltean-plenty <alexandru.oltean@plentysystems.com>
2025-11-12 01:06:14 -0800

    prettier

--- apps/web/app/components/BlocksNavigation/BlocksNavigation.vue
+++ apps/web/app/components/BlocksNavigation/BlocksNavigation.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="site-settings-view sticky top-[52px] relative overflow-y-auto h-[calc(100vh-150px)]"  >
+  <div class="site-settings-view sticky top-[52px] relative overflow-y-auto h-[calc(100vh-150px)]">
     <BlocksNavigationHeader />
     <BlocksNavigationList />
   </div>
