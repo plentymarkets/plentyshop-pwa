6dfbe973573aa22838bdd14fdf4bb0c3e79771df
cristian sandru <cristian.sandru@plentysystems.com>
2025-11-12 16:32:14 +0200

    Revert "feat: editor settings entry"
    This reverts commit bff79af4803f96555f7315fe267e9b265af971a8.

--- apps/web/app/components/settings/localization/View.vue
+++ apps/web/app/components/settings/localization/View.vue
@@ -1,6 +1,13 @@
 <template>
   <SiteConfigurationView>
     <template #setting-title>{{ getEditorTranslation('label') }}</template>
+    <template #setting-description>
+      <div class="flex flex-col px-4 text-sm">
+        <p class="pb-2">
+          <span class="align-middle font-bold">{{ getEditorTranslation('description') }}</span>
+        </p>
+      </div>
+    </template>
   </SiteConfigurationView>
 </template>
 
@@ -9,10 +16,12 @@
 <i18n lang="json">
 {
   "en": {
-    "label": "Localization"
+    "label": "Localization",
+    "description": "Manage translations for static texts inside the shop."
   },
   "de": {
-    "label": "Localization"
+    "label": "Localization",
+    "description": "Manage translations for static texts inside the shop."
   }
 }
 </i18n>
--- apps/web/app/components/settings/localization/edit-translations/View.vue
+++ apps/web/app/components/settings/localization/edit-translations/View.vue
@@ -1,20 +1,46 @@
 <template>
   <SiteConfigurationView>
     <template #setting-title>{{ getEditorTranslation('label') }}</template>
+    <template #setting-description>
+      <div class="flex flex-col px-4 text-sm">
+        <p class="pb-2">
+          <span class="align-middle font-bold">{{ getEditorTranslation('description') }}</span>
+        </p>
+
+        <UiButton variant="secondary" class="mt-2 mb-5" @click="fetchAllTranslations">
+          {{ getEditorTranslation('label') }}
+          <SfIconBase size="xs" viewBox="0 0 18 18" class="fill-primary-900 cursor-pointer mr-2">
+            <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path :d="editPath" fill="black" />
+            </svg>
+          </SfIconBase>
+        </UiButton>
+      </div>
+    </template>
   </SiteConfigurationView>
 </template>
 
 <script setup lang="ts">
-//
+import { SfIconBase } from '@storefront-ui/vue';
+import { editPath } from '~/assets/icons/paths/edit';
+
+const { loadKeys, keys } = useEditorLocalizationKeys();
+
+const fetchAllTranslations = async () => {
+  await loadKeys();
+  console.log(keys.value);
+};
 </script>
 
 <i18n lang="json">
 {
   "en": {
-    "label": "Translations"
+    "label": "Edit Translations",
+    "description": "Translate static texts of your shop."
   },
   "de": {
-    "label": "Translations"
+    "label": "Edit Translations",
+    "description": "Translate static texts of your shop."
   }
 }
 </i18n>
