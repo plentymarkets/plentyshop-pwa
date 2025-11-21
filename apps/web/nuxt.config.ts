8b615ad1391d73808ef3ef3e0c7bf5a83bfd3bd9
Kevin Stederoth <43753494+ksted@users.noreply.github.com>
2025-11-21 14:38:33 +0100

    perf: enable css code splitting

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -64,10 +64,11 @@ export default defineNuxtConfig({
       ],
     },
     build: {
+      cssCodeSplit: true,
       rollupOptions: {
         output: {
           manualChunks: {
-            vuetify: ['vuetify'],
+            vuetify: ['vuetify', '@mdi/js'],
           },
         },
       },
@@ -268,6 +269,7 @@ export default defineNuxtConfig({
   fonts: {
     defaults: {
       weights: [300, 400, 500, 700],
+      preload: true,
     },
     assets: {
       prefix: '/_nuxt-plenty/fonts/',
