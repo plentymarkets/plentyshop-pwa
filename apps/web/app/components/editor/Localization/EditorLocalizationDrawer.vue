385b867ebcd46104277d0e30ccbb0eadb0123ed9
Noah Feist <144685636+N-Feist@users.noreply.github.com>
2025-12-02 15:26:18 +0100

    fix: possible smoothscroll for all browsers

--- apps/web/app/components/editor/Localization/EditorLocalizationDrawer.vue
+++ apps/web/app/components/editor/Localization/EditorLocalizationDrawer.vue
@@ -154,7 +154,7 @@ const leftVirtualizerOptions = computed(() => ({
   count: displayedKeys.value.length,
   getScrollElement: () => leftScrollerRef.value,
   estimateSize: () => 48,
-  overscan: 20,
+  overscan: 30,
   getItemKey: (index: number) => displayedKeys.value[index]?.key ?? index,
 }));
 
@@ -162,7 +162,7 @@ const rightVirtualizerOptions = computed(() => ({
   count: displayedKeys.value.length,
   getScrollElement: () => rightScrollerRef.value,
   estimateSize: () => 48,
-  overscan: 20,
+  overscan: 30,
   getItemKey: (index: number) => displayedKeys.value[index]?.key ?? index,
 }));
 
@@ -183,25 +183,38 @@ const rightVirtualItems = computed(() => {
   }));
 });
 
-let scrollPending = false;
-let headerScrollPending = false;
+const isFirefox = typeof navigator !== 'undefined' && /firefox/i.test(navigator.userAgent);
+
+let isScrolling = false;
+let rafId: number | null = null;
+let timeoutId: ReturnType<typeof setTimeout> | null = null;
+
+const scheduleSync = (callback: () => void) => {
+  if (isFirefox) {
+    if (timeoutId) clearTimeout(timeoutId);
+    timeoutId = setTimeout(callback, 10);
+  } else {
+    if (rafId) cancelAnimationFrame(rafId);
+    rafId = requestAnimationFrame(callback);
+  }
+};
 
 const syncScrollLeft = () => {
-  if (!scrollPending && leftScrollerRef.value && rightScrollerRef.value) {
-    scrollPending = true;
-    requestAnimationFrame(() => {
+  if (!isScrolling && leftScrollerRef.value && rightScrollerRef.value) {
+    scheduleSync(() => {
+      isScrolling = true;
       if (leftScrollerRef.value && rightScrollerRef.value) {
         rightScrollerRef.value.scrollTop = leftScrollerRef.value.scrollTop;
       }
-      scrollPending = false;
+      isScrolling = false;
     });
   }
 };
 
 const syncScrollRight = () => {
-  if (!scrollPending && leftScrollerRef.value && rightScrollerRef.value) {
-    scrollPending = true;
-    requestAnimationFrame(() => {
+  if (!isScrolling && leftScrollerRef.value && rightScrollerRef.value) {
+    scheduleSync(() => {
+      isScrolling = true;
       if (leftScrollerRef.value && rightScrollerRef.value) {
         leftScrollerRef.value.scrollTop = rightScrollerRef.value.scrollTop;
 
@@ -209,19 +222,17 @@ const syncScrollRight = () => {
           headerScroll.value.scrollLeft = rightScrollerRef.value.scrollLeft;
         }
       }
-      scrollPending = false;
+      isScrolling = false;
     });
   }
 };
 
 const headerScrollHandler = () => {
-  if (!headerScrollPending && headerScroll.value && rightScrollerRef.value) {
-    headerScrollPending = true;
-    requestAnimationFrame(() => {
+  if (headerScroll.value && rightScrollerRef.value && !isScrolling) {
+    scheduleSync(() => {
       if (headerScroll.value && rightScrollerRef.value) {
         rightScrollerRef.value.scrollLeft = headerScroll.value.scrollLeft;
       }
-      headerScrollPending = false;
     });
   }
 };
@@ -232,6 +243,8 @@ onMounted(() => {
 
 onBeforeUnmount(() => {
   headerScroll.value?.removeEventListener('scroll', headerScrollHandler);
+  if (timeoutId) clearTimeout(timeoutId);
+  if (rafId) cancelAnimationFrame(rafId);
 });
 </script>
 
