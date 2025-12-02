44f03579dcb74172d887bed519e8871a1efb0346
aoltean-plenty <alexandru.oltean@plentysystems.com>
2025-12-01 23:26:08 -0800

    remove comment

--- apps/web/app/components/blocks/structure/MultiGrid/MultiGridForm.vue
+++ apps/web/app/components/blocks/structure/MultiGrid/MultiGridForm.vue
@@ -212,24 +212,6 @@ const multiGridStructure = computed(() => {
 
 const { isFullWidth } = useFullWidthToggleForConfig(computed(() => multiGridStructure.value.configuration));
 
-// const isFullWidth = computed({
-//   get: () => multiGridStructure.value.configuration.layout?.fullWidth ?? false,
-//   set: (value: boolean) => {
-//     if (!multiGridStructure.value.configuration.layout) {
-//       multiGridStructure.value.configuration.layout = {
-//         marginTop: 0,
-//         marginBottom: defaultMarginBottom.value,
-//         marginLeft: 40,
-//         marginRight: 40,
-//         backgroundColor: '#ffffff',
-//         gap: 'M',
-//         fullWidth: false,
-//       };
-//     }
-//     multiGridStructure.value.configuration.layout.fullWidth = value;
-//   },
-// });
-
 const gapOptions = ['None', 'S', 'M', 'L', 'XL'];
 const gapBtnClasses =
   'py-2 leading-6 px-4 gap-2 !hover:bg-gray-100 inline-flex items-center justify-center font-medium text-base focus-visible:outline focus-visible:outline-offset rounded-md disabled:text-disabled-500 disabled:bg-disabled-300 disabled:shadow-none disabled:ring-0 disabled:cursor-not-allowed';
