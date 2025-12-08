6f5e11401b04f0ad5e908036980754a483d879a4
Maximilian Röll <30629157+maxiroellplenty@users.noreply.github.com>
2025-12-08 10:36:12 +0100

    refactor: use ProductSlider component again

--- apps/web/modules/last-seen/runtime/components/LastSeen/LastSeen.vue
+++ apps/web/modules/last-seen/runtime/components/LastSeen/LastSeen.vue
@@ -1,35 +1,16 @@
 <template>
-    <div v-if="products.length > 0" class="w-full mt-10">
-        <h3 class="font-bold mb-3">Last seen products</h3>
-        <SfScrollable buttons-placement="floating" class="pb-4 scrollbar-hidden"
-            data-testid="product-slider">
-            <UiProductCard v-for="product in products" :key="productGetters.getId(product)" :product="product"
-                is-from-slider class="max-w-48" />
-        </SfScrollable>
-        <div class="mt-4 typography-text-xs flex gap-1">
-            <span>{{ t('asterisk') }}</span>
-            <span v-if="showNetPrices">{{ t('itemExclVAT') }}</span>
-            <span v-else>{{ t('itemInclVAT') }}</span>
-            <i18n-t keypath="excludedShipping" scope="global">
-                <template #shipping>
-                    <SfLink :href="localePath(paths.shipping)" target="_blank"
-                        class="focus:outline focus:outline-offset-2 focus:outline-2 outline-secondary-600 rounded">
-                        {{ t('delivery') }}
-                    </SfLink>
-                </template>
-            </i18n-t>
-        </div>
-
-        <UiButton v-if="totalPages > 1" @click="nextPage()">load more</UiButton>
-    </div>
-    <div v-else-if="loading">
-        <p>Loading...</p>
+    <div class="w-full mt-10">
+        <template v-if="products.length > 0" class="w-full mt-10">
+            <h3 class="font-bold mb-3">Last seen products</h3>
+            <ProductSlider :items="products"></ProductSlider>
+            <UiButton v-if="totalPages > 1" @click="nextPage()">load more</UiButton>
+        </template>
+        <template v-else-if="loading">
+            <p>Loading...</p>
+        </template>
     </div>
 </template>
 <script setup lang="ts">
-import { productGetters } from '@plentymarkets/shop-api';
-import { SfScrollable, SfLink } from '@storefront-ui/vue';
-import { paths } from '~/utils/paths';
 import { useLastSeen } from '../../composables/useLastSeen';
 const { data, fetchLastSeenProducts, nextPage, loading, itemsNotFetched, totalPages, page } = useLastSeen();
 
