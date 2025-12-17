920ec9ddd2622de6b8c8e8b8fb3acdc8ad25c9e1
plentydev <iulian.vladu@plentymarkets.com>
2025-12-17 15:13:41 +0200

    add false

--- apps/web/app/utils/assets.ts
+++ apps/web/app/utils/assets.ts
@@ -7,5 +7,5 @@ export const isCssUrl = (url: string | undefined | null): boolean => {
 export const isJsUrl = (url: string | undefined | null): boolean => {
   if (!url) return false;
   const clean = url.split('?')[0]?.split('#')[0]?.toLowerCase();
-  return clean?.endsWith('.js') || clean?.endsWith('.mjs');
+  return clean?.endsWith('.js') || clean?.endsWith('.mjs') || false;
 };
