37400d5bd871845609339aa03abe0e1f14fd9142
aoltean <78078265+aoltean-plenty@users.noreply.github.com>
2025-12-30 04:07:25 -0800

    fix: mega menu broken scroll when having a high number of menu entries and line break categories with long names (#2100)
    Co-authored-by: aoltean-plenty <alexandru.oltean@plentysystems.com>

--- apps/web/app/components/MegaMenu/MegaMenu.vue
+++ apps/web/app/components/MegaMenu/MegaMenu.vue
@@ -74,7 +74,7 @@
             :key="activeMenu.id"
             ref="megaMenuReference"
             :style="style"
-            class="hidden md:grid gap-x-6 grid-cols-4 bg-white shadow-lg p-6 pt-5 left-0 right-0 outline-none z-40"
+            class="hidden md:grid gap-x-6 grid-cols-4 bg-white shadow-lg p-6 pt-5 left-0 right-0 outline-none z-40 max-h-[calc(100vh-300px)] overflow-y-auto"
             tabindex="0"
             @mouseleave="onMouseLeave"
             @keydown.esc="focusTrigger(index)"
@@ -97,7 +97,7 @@
                   :tag="NuxtLink"
                   size="sm"
                   :href="localePath(generateCategoryLink(node))"
-                  class="typography-text-base font-medium text-neutral-900 whitespace-nowrap px-4 py-1.5 border-b border-b-neutral-200 border-b-solid hover:bg-secondary-100 rounded"
+                  class="typography-text-base font-medium text-neutral-900 px-4 py-1.5 border-b border-b-neutral-200 border-b-solid hover:bg-secondary-100 rounded whitespace-normal break-words"
                 >
                   {{ categoryTreeGetters.getName(node) }}
                 </SfListItem>
