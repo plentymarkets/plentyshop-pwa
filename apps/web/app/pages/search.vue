11e5f6ab1551d1b8fb65228c855d627a9d4bacd7
Fabian Gerke <124085586+FabianGerke@users.noreply.github.com>
2026-01-12 10:27:41 +0100

    fix: translations keys on the search are missing/wrong (#2131)
    Co-authored-by: Cristi Sandru <149154151+csandru-plenty@users.noreply.github.com>

--- apps/web/app/pages/search.vue
+++ apps/web/app/pages/search.vue
@@ -4,7 +4,7 @@
       <SfLoaderCircular v-if="loading" class="fixed top-[50%] right-0 left-0 m-auto z-[99999]" size="2xl" />
       <CategoryPageContent
         v-if="productsCatalog"
-        :title="t('resultsFor', { phrase: route.query.term })"
+        :title="t('search.searchResults', { phrase: route.query.term })"
         :total-products="productsCatalog.pagination.totals"
         :products="productsCatalog.products"
         :items-per-page="Number(productsPerPage)"
