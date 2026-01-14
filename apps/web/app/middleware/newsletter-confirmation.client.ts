8a309b230c8ecf567d25cb5f18ea97b44c3007b4
cristian sandru <cristian.sandru@plentysystems.com>
2026-01-14 17:45:34 +0200

    perf: ttfb improvements

--- apps/web/app/middleware/newsletter-confirmation.client.ts
+++ apps/web/app/middleware/newsletter-confirmation.client.ts
@@ -1,5 +1,5 @@
 export default defineNuxtRouteMiddleware(async ({ query }) => {
-  if (query.ActionCall === 'WebActionConfirmNewsletter' && import.meta.client) {
+  if (query.ActionCall === 'WebActionConfirmNewsletter') {
     const { confirmEmail } = useNewsletterConfirmation();
     const { send } = useNotification();
 
---
+++ apps/web/app/middleware/notifyme-interactions.client.ts
@@ -0,0 +1,41 @@
+export default defineNuxtRouteMiddleware(async ({ query }) => {
+  const { send } = useNotification();
+
+  if (query.ActionCall === 'WebActionConfirmNotifyMe' && query.token) {
+    const { confirmNotifyMe } = useNotifyMe();
+
+    const data = await confirmNotifyMe({ token: query.token.toString() });
+
+    if (data) {
+      send({
+        message: t('notifyMe.notifyMeOptInMessage'),
+        type: 'positive',
+        persist: true,
+      });
+    } else {
+      send({
+        message: t('notifyMe.notifyMeOptInMessageError'),
+        type: 'negative',
+      });
+    }
+  }
+
+  if (query.ActionCall === 'WebActionUnsubscribeNotifyMe' && query.token) {
+    const { unsubscribeNotifyMe } = useNotifyMe();
+
+    const data = await unsubscribeNotifyMe({ token: query.token.toString() });
+
+    if (data) {
+      send({
+        message: t('notifyMe.notifyMeUnsubscribeMessage'),
+        type: 'positive',
+        persist: true,
+      });
+    } else {
+      send({
+        message: t('notifyMe.notifyMeUnsubscribeMessageError'),
+        type: 'negative',
+      });
+    }
+  }
+});
