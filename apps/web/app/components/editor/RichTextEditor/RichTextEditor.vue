69d4c6d77d29432e70f568cd74eb6c5114dddfd1
Noah Feist <144685636+N-Feist@users.noreply.github.com>
2026-02-02 15:29:47 +0100

    fix: format

--- apps/web/app/components/editor/RichTextEditor/RichTextEditor.vue
+++ apps/web/app/components/editor/RichTextEditor/RichTextEditor.vue
@@ -16,7 +16,9 @@
 
     <EditorRichTextEditorMenuButton :active="isActive('bold')" @click="cmd('toggleBold')">
       <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor">
-        <path d="M272-200v-560h221q65 0 120 40t55 111q0 51-23 78.5T602-491q25 11 55.5 41t30.5 90q0 89-65 124.5T501-200H272Zm121-112h104q48 0 58.5-24.5T566-372q0-11-10.5-35.5T494-432H393v120Zm0-228h93q33 0 48-17t15-38q0-24-17-39t-44-15h-95v109Z" />
+        <path
+          d="M272-200v-560h221q65 0 120 40t55 111q0 51-23 78.5T602-491q25 11 55.5 41t30.5 90q0 89-65 124.5T501-200H272Zm121-112h104q48 0 58.5-24.5T566-372q0-11-10.5-35.5T494-432H393v120Zm0-228h93q33 0 48-17t15-38q0-24-17-39t-44-15h-95v109Z"
+        />
       </svg>
     </EditorRichTextEditorMenuButton>
 
@@ -193,7 +195,9 @@
 
     <EditorRichTextEditorMenuButton :disabled="!canUndo" @click="undo">
       <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor">
-        <path d="M280-200v-80h284q63 0 109.5-40T720-420q0-60-46.5-100T564-560H312l104 104-56 56-200-200 200-200 56 56-104 104h252q97 0 166.5 63T800-420q0 94-69.5 157T564-200H280Z" />
+        <path
+          d="M280-200v-80h284q63 0 109.5-40T720-420q0-60-46.5-100T564-560H312l104 104-56 56-200-200 200-200 56 56-104 104h252q97 0 166.5 63T800-420q0 94-69.5 157T564-200H280Z"
+        />
       </svg>
     </EditorRichTextEditorMenuButton>
 
