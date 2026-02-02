4df79344f09eb941316ec8b772c6cd5c5a8dad05
Noah Feist <144685636+N-Feist@users.noreply.github.com>
2026-02-02 12:04:21 +0100

    feat: use icons for the rte

--- apps/web/app/components/editor/RichTextEditor/RichTextEditor.vue
+++ apps/web/app/components/editor/RichTextEditor/RichTextEditor.vue
@@ -32,7 +32,9 @@
       @mousedown.prevent
       @click="cmd('toggleBold')"
     >
-      <b>B</b>
+      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#062633">
+        <path d="M272-200v-560h221q65 0 120 40t55 111q0 51-23 78.5T602-491q25 11 55.5 41t30.5 90q0 89-65 124.5T501-200H272Zm121-112h104q48 0 58.5-24.5T566-372q0-11-10.5-35.5T494-432H393v120Zm0-228h93q33 0 48-17t15-38q0-24-17-39t-44-15h-95v109Z"/>
+      </svg>
     </button>
 
     <button
@@ -42,7 +44,9 @@
       @mousedown.prevent
       @click="cmd('toggleItalic')"
     >
-      <i>I</i>
+      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#062633">
+        <path d="M200-200v-100h160l120-360H320v-100h400v100H580L460-300h140v100H200Z"/>
+      </svg>
     </button>
 
     <button
@@ -52,7 +56,9 @@
       @mousedown.prevent
       @click="cmd('toggleUnderline')"
     >
-      <span class="underline">U</span>
+      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#062633">
+        <path d="M200-120v-80h560v80H200Zm280-160q-101 0-157-63t-56-167v-330h103v336q0 56 28 91t82 35q54 0 82-35t28-91v-336h103v330q0 104-56 167t-157 63Z"/>
+      </svg>
     </button>
 
     <button
@@ -62,7 +68,9 @@
       @mousedown.prevent
       @click="toggleLink"
     >
-      🔗
+      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#062633">
+        <path d="M318-120q-82 0-140-58t-58-140q0-40 15-76t43-64l134-133 56 56-134 134q-17 17-25.5 38.5T200-318q0 49 34.5 83.5T318-200q23 0 45-8.5t39-25.5l133-134 57 57-134 133q-28 28-64 43t-76 15Zm79-220-57-57 223-223 57 57-223 223Zm251-28-56-57 134-133q17-17 25-38t8-44q0-50-34-85t-84-35q-23 0-44.5 8.5T558-726L425-592l-57-56 134-134q28-28 64-43t76-15q82 0 139.5 58T839-641q0 39-14.5 75T782-502L648-368Z"/>
+      </svg>
     </button>
 
     <button
@@ -74,7 +82,11 @@
       @mousedown.prevent
       @click="expandedLocal = !expandedLocal"
     >
-      <span class="inline-block transition-transform duration-150" :class="{ 'rotate-180': expandedLocal }">▾</span>
+      <span class="inline-block transition-transform duration-150" :class="{ 'rotate-180': expandedLocal }">
+        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#062633">
+          <path d="M480-360 280-560h400L480-360Z"/>
+        </svg>
+      </span>
     </button>
   </div>
 
@@ -90,7 +102,10 @@
       @mousedown.prevent
       @click="cmd('toggleBlockquote')"
     >
-      ❝
+      <svg width="24px" height="24px" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M18.8533 9.11599C11.3227 13.9523 7.13913 19.5812 6.30256 26.0029C5.00021 36 13.9404 40.8933 18.4703 36.4967C23.0002 32.1002 20.2848 26.5196 17.0047 24.9942C13.7246 23.4687 11.7187 24 12.0686 21.9616C12.4185 19.9231 17.0851 14.2713 21.1849 11.6392C21.4569 11.4079 21.5604 10.9591 21.2985 10.6187C21.1262 10.3947 20.7883 9.95557 20.2848 9.30114C19.8445 8.72888 19.4227 8.75029 18.8533 9.11599Z" fill="#000000"/>
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M38.6789 9.11599C31.1484 13.9523 26.9648 19.5812 26.1282 26.0029C24.8259 36 33.7661 40.8933 38.296 36.4967C42.8259 32.1002 40.1105 26.5196 36.8304 24.9942C33.5503 23.4687 31.5443 24 31.8943 21.9616C32.2442 19.9231 36.9108 14.2713 41.0106 11.6392C41.2826 11.4079 41.3861 10.9591 41.1241 10.6187C40.9519 10.3947 40.614 9.95557 40.1105 9.30114C39.6702 8.72888 39.2484 8.75029 38.6789 9.11599Z" fill="#000000"/>
+      </svg>
     </button>
 
     <button
@@ -100,7 +115,9 @@
       @mousedown.prevent
       @click="cmd('toggleStrike')"
     >
-      S
+      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#062633">
+        <path d="M486-160q-76 0-135-45t-85-123l88-38q14 48 48.5 79t85.5 31q42 0 76-20t34-64q0-18-7-33t-19-27h112q5 14 7.5 28.5T694-340q0 86-61.5 133T486-160ZM80-480v-80h800v80H80Zm402-326q66 0 115.5 32.5T674-674l-88 39q-9-29-33.5-52T484-710q-41 0-68 18.5T386-640h-96q2-69 54.5-117.5T482-806Z"/>
+      </svg>
     </button>
 
     <label class="inline-flex items-center" @mousedown.stop @click.stop>
@@ -123,7 +140,9 @@
       @mousedown.prevent
       @click="setAlign('left')"
     >
-      ⬅
+      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#062633">
+        <path d="M120-120v-80h720v80H120Zm0-160v-80h480v80H120Zm0-160v-80h720v80H120Zm0-160v-80h480v80H120Zm0-160v-80h720v80H120Z"/>
+      </svg>
     </button>
     <button
       type="button"
@@ -132,7 +151,9 @@
       @mousedown.prevent
       @click="setAlign('center')"
     >
-      ⬌
+      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#062633">
+        <path d="M120-120v-80h720v80H120Zm160-160v-80h400v80H280ZM120-440v-80h720v80H120Zm160-160v-80h400v80H280ZM120-760v-80h720v80H120Z"/>
+      </svg>
     </button>
     <button
       type="button"
@@ -141,7 +162,9 @@
       @mousedown.prevent
       @click="setAlign('right')"
     >
-      ➡
+      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#062633">
+        <path d="M120-760v-80h720v80H120Zm240 160v-80h480v80H360ZM120-440v-80h720v80H120Zm240 160v-80h480v80H360ZM120-120v-80h720v80H120Z"/>
+      </svg>
     </button>
     <button
       type="button"
@@ -150,7 +173,9 @@
       @mousedown.prevent
       @click="setAlign('justify')"
     >
-      ☰
+      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#062633">
+        <path d="M120-120v-80h720v80H120Zm0-160v-80h720v80H120Zm0-160v-80h720v80H120Zm0-160v-80h720v80H120Zm0-160v-80h720v80H120Z"/>
+      </svg>
     </button>
 
     <span class="w-px h-5 bg-gray-200 mx-0.5" />
@@ -162,7 +187,9 @@
       @mousedown.prevent
       @click="cmd('toggleBulletList')"
     >
-      ••
+      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#062633">
+        <path d="M360-200v-80h480v80H360Zm0-240v-80h480v80H360Zm0-240v-80h480v80H360ZM200-160q-33 0-56.5-23.5T120-240q0-33 23.5-56.5T200-320q33 0 56.5 23.5T280-240q0 33-23.5 56.5T200-160Zm0-240q-33 0-56.5-23.5T120-480q0-33 23.5-56.5T200-560q33 0 56.5 23.5T280-480q0 33-23.5 56.5T200-400Zm0-240q-33 0-56.5-23.5T120-720q0-33 23.5-56.5T200-800q33 0 56.5 23.5T280-720q0 33-23.5 56.5T200-640Z"/>
+      </svg>
     </button>
     <button
       type="button"
@@ -171,7 +198,9 @@
       @mousedown.prevent
       @click="cmd('toggleOrderedList')"
     >
-      1.
+      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#062633">
+        <path d="M120-80v-60h100v-30h-60v-60h60v-30H120v-60h120q17 0 28.5 11.5T280-280v40q0 17-11.5 28.5T240-200q17 0 28.5 11.5T280-160v40q0 17-11.5 28.5T240-80H120Zm0-280v-110q0-17 11.5-28.5T160-510h60v-30H120v-60h120q17 0 28.5 11.5T280-560v70q0 17-11.5 28.5T240-450h-60v30h100v60H120Zm60-280v-180h-60v-60h120v240h-60Zm180 440v-80h480v80H360Zm0-240v-80h480v80H360Zm0-240v-80h480v80H360Z"/>
+      </svg>
     </button>
     <button
       type="button"
@@ -179,7 +208,9 @@
       @mousedown.prevent
       @click="cmd('setHorizontalRule')"
     >
-      ─
+      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#062633">
+        <path d="M160-440v-80h640v80H160Z"/>
+      </svg>
     </button>
 
     <span class="w-px h-5 bg-gray-200 mx-0.5" />
@@ -191,7 +222,9 @@
       @mousedown.prevent
       @click="undo"
     >
-      ↶
+      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#062633">
+        <path d="M280-200v-80h284q63 0 109.5-40T720-420q0-60-46.5-100T564-560H312l104 104-56 56-200-200 200-200 56 56-104 104h252q97 0 166.5 63T800-420q0 94-69.5 157T564-200H280Z"/>
+      </svg>
     </button>
     <button
       type="button"
@@ -200,7 +233,9 @@
       @mousedown.prevent
       @click="redo"
     >
-      ↷
+      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#062633">
+        <path d="M396-200q-97 0-166.5-63T160-420q0-94 69.5-157T396-640h252L544-744l56-56 200 200-200 200-56-56 104-104H396q-63 0-109.5 40T240-420q0 60 46.5 100T396-280h284v80H396Z"/>
+      </svg>
     </button>
 
     <button
@@ -209,7 +244,9 @@
       @mousedown.prevent
       @click="clearFormatting"
     >
-      ⌫
+      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#062633">
+        <path d="m456-320 104-104 104 104 56-56-104-104 104-104-56-56-104 104-104-104-56 56 104 104-104 104 56 56Zm-96 160q-19 0-36-8.5T296-192L80-480l216-288q11-15 28-23.5t36-8.5h440q33 0 56.5 23.5T880-720v480q0 33-23.5 56.5T800-160H360ZM180-480l180 240h440v-480H360L180-480Zm400 0Z"/>
+      </svg>
     </button>
   </div>
 
