471317b5f089b37520361a05cee04221dbdf45db
Maximilian Röll <30629157+maxiroellplenty@users.noreply.github.com>
2026-02-02 13:41:54 +0100

    chore: set back to 600kb

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -41,7 +41,7 @@ export default defineNuxtConfig({
         name: 'fail-on-large-chunks',
         generateBundle(_, bundle) {
           if (!process.env.FAIL_BUILD_ON_LARGE_CHUNKS) return;
-          const LIMIT = 500 * 1024; // 600 KB
+          const LIMIT = 600 * 1024; // 600 KB
           for (const [fileName, chunk] of Object.entries(bundle)) {
             if (fileName === 'server.mjs') continue; // skip server bundle
             if (chunk.type === 'chunk' && chunk.code.length > LIMIT) {
