9111c5275f4a6fc3665428111f8252b50cfe97d3
Noah Feist <144685636+N-Feist@users.noreply.github.com>
2026-02-02 14:54:13 +0100

    feat: style active buttons

--- apps/web/app/components/editor/RichTextEditor/RichTextEditor.vue
+++ apps/web/app/components/editor/RichTextEditor/RichTextEditor.vue
@@ -16,12 +16,14 @@
 
     <button
       type="button"
-      class="w-8 h-8 rounded border border-transparent bg-transparent inline-flex items-center justify-center text-sm cursor-pointer hover:bg-gray-100"
-      :class="{ 'bg-gray-200 border-gray-300': isActive('bold') }"
+      class="w-8 h-8 rounded inline-flex items-center justify-center text-sm cursor-pointer"
+      :class="isActive('bold')
+        ? 'bg-slate-800 border border-gray-300 text-white hover:bg-slate-700'
+        : 'bg-transparent border border-transparent text-gray-800 hover:bg-gray-100'"
       @mousedown.prevent
       @click="cmd('toggleBold')"
     >
-      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#062633">
+      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor">
         <path
           d="M272-200v-560h221q65 0 120 40t55 111q0 51-23 78.5T602-491q25 11 55.5 41t30.5 90q0 89-65 124.5T501-200H272Zm121-112h104q48 0 58.5-24.5T566-372q0-11-10.5-35.5T494-432H393v120Zm0-228h93q33 0 48-17t15-38q0-24-17-39t-44-15h-95v109Z"
         />
@@ -30,24 +32,28 @@
 
     <button
       type="button"
-      class="w-8 h-8 rounded border border-transparent bg-transparent inline-flex items-center justify-center text-sm cursor-pointer hover:bg-gray-100"
-      :class="{ 'bg-gray-200 border-gray-300': isActive('italic') }"
+      class="w-8 h-8 rounded inline-flex items-center justify-center text-sm cursor-pointer"
+      :class="isActive('italic')
+        ? 'bg-slate-800 border border-gray-300 text-white hover:bg-slate-700'
+        : 'bg-transparent border border-transparent text-gray-800 hover:bg-gray-100'"
       @mousedown.prevent
       @click="cmd('toggleItalic')"
     >
-      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#062633">
+      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor">
         <path d="M200-200v-100h160l120-360H320v-100h400v100H580L460-300h140v100H200Z" />
       </svg>
     </button>
 
     <button
       type="button"
-      class="w-8 h-8 rounded border border-transparent bg-transparent inline-flex items-center justify-center text-sm cursor-pointer hover:bg-gray-100"
-      :class="{ 'bg-gray-200 border-gray-300': isActive('underline') }"
+      class="w-8 h-8 rounded inline-flex items-center justify-center text-sm cursor-pointer"
+      :class="isActive('underline')
+        ? 'bg-slate-800 border border-gray-300 text-white hover:bg-slate-700'
+        : 'bg-transparent border border-transparent text-gray-800 hover:bg-gray-100'"
       @mousedown.prevent
       @click="cmd('toggleUnderline')"
     >
-      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#062633">
+      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor">
         <path
           d="M200-120v-80h560v80H200Zm280-160q-101 0-157-63t-56-167v-330h103v336q0 56 28 91t82 35q54 0 82-35t28-91v-336h103v330q0 104-56 167t-157 63Z"
         />
@@ -56,12 +62,14 @@
 
     <button
       type="button"
-      class="w-8 h-8 rounded border border-transparent bg-transparent inline-flex items-center justify-center text-sm cursor-pointer hover:bg-gray-100"
-      :class="{ 'bg-gray-200 border-gray-300': isActive('link') }"
+      class="w-8 h-8 rounded inline-flex items-center justify-center text-sm cursor-pointer"
+      :class="isActive('link')
+        ? 'bg-slate-800 border border-gray-300 text-white hover:bg-slate-700'
+        : 'bg-transparent border border-transparent text-gray-800 hover:bg-gray-100'"
       @mousedown.prevent
       @click="toggleLink"
     >
-      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#062633">
+      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor">
         <path
           d="M318-120q-82 0-140-58t-58-140q0-40 15-76t43-64l134-133 56 56-134 134q-17 17-25.5 38.5T200-318q0 49 34.5 83.5T318-200q23 0 45-8.5t39-25.5l133-134 57 57-134 133q-28 28-64 43t-76 15Zm79-220-57-57 223-223 57 57-223 223Zm251-28-56-57 134-133q17-17 25-38t8-44q0-50-34-85t-84-35q-23 0-44.5 8.5T558-726L425-592l-57-56 134-134q28-28 64-43t76-15q82 0 139.5 58T839-641q0 39-14.5 75T782-502L648-368Z"
         />
@@ -113,35 +121,37 @@
   >
     <button
       type="button"
-      class="w-8 h-8 rounded border border-transparent bg-transparent inline-flex items-center justify-center text-sm cursor-pointer hover:bg-gray-100"
-      :class="{ 'bg-gray-200 border-gray-300': isActive('blockquote') }"
+      class="w-8 h-8 rounded inline-flex items-center justify-center text-sm cursor-pointer"
+      :class="isActive('blockquote')
+        ? 'bg-slate-800 border border-gray-300 text-white hover:bg-slate-700'
+        : 'bg-transparent border border-transparent text-gray-800 hover:bg-gray-100'"
       @mousedown.prevent
       @click="cmd('toggleBlockquote')"
     >
-      <svg width="24px" height="24px" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <svg width="24px" height="24px" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg" fill="currentColor">
         <path
           fill-rule="evenodd"
           clip-rule="evenodd"
           d="M18.8533 9.11599C11.3227 13.9523 7.13913 19.5812 6.30256 26.0029C5.00021 36 13.9404 40.8933 18.4703 36.4967C23.0002 32.1002 20.2848 26.5196 17.0047 24.9942C13.7246 23.4687 11.7187 24 12.0686 21.9616C12.4185 19.9231 17.0851 14.2713 21.1849 11.6392C21.4569 11.4079 21.5604 10.9591 21.2985 10.6187C21.1262 10.3947 20.7883 9.95557 20.2848 9.30114C19.8445 8.72888 19.4227 8.75029 18.8533 9.11599Z"
-          fill="#000000"
         />
         <path
           fill-rule="evenodd"
           clip-rule="evenodd"
           d="M38.6789 9.11599C31.1484 13.9523 26.9648 19.5812 26.1282 26.0029C24.8259 36 33.7661 40.8933 38.296 36.4967C42.8259 32.1002 40.1105 26.5196 36.8304 24.9942C33.5503 23.4687 31.5443 24 31.8943 21.9616C32.2442 19.9231 36.9108 14.2713 41.0106 11.6392C41.2826 11.4079 41.3861 10.9591 41.1241 10.6187C40.9519 10.3947 40.614 9.95557 40.1105 9.30114C39.6702 8.72888 39.2484 8.75029 38.6789 9.11599Z"
-          fill="#000000"
         />
       </svg>
     </button>
 
     <button
       type="button"
-      class="w-8 h-8 rounded border border-transparent bg-transparent inline-flex items-center justify-center text-sm cursor-pointer hover:bg-gray-100"
-      :class="{ 'bg-gray-200 border-gray-300': isActive('strike') }"
+      class="w-8 h-8 rounded inline-flex items-center justify-center text-sm cursor-pointer"
+      :class="isActive('strike')
+        ? 'bg-slate-800 border border-gray-300 text-white hover:bg-slate-700'
+        : 'bg-transparent border border-transparent text-gray-800 hover:bg-gray-100'"
       @mousedown.prevent
       @click="cmd('toggleStrike')"
     >
-      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#062633">
+      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor">
         <path
           d="M486-160q-76 0-135-45t-85-123l88-38q14 48 48.5 79t85.5 31q42 0 76-20t34-64q0-18-7-33t-19-27h112q5 14 7.5 28.5T694-340q0 86-61.5 133T486-160ZM80-480v-80h800v80H80Zm402-326q66 0 115.5 32.5T674-674l-88 39q-9-29-33.5-52T484-710q-41 0-68 18.5T386-640h-96q2-69 54.5-117.5T482-806Z"
         />
@@ -175,12 +185,14 @@
 
     <button
       type="button"
-      class="w-8 h-8 rounded border border-transparent bg-transparent inline-flex items-center justify-center text-sm cursor-pointer hover:bg-gray-100"
-      :class="{ 'bg-gray-200 border-gray-300': isActiveAlign('left') }"
+      class="w-8 h-8 rounded inline-flex items-center justify-center text-sm cursor-pointer"
+      :class="isActiveAlign('left')
+        ? 'bg-slate-800 border border-gray-300 text-white hover:bg-slate-700'
+        : 'bg-transparent border border-transparent text-gray-800 hover:bg-gray-100'"
       @mousedown.prevent
       @click="setAlign('left')"
     >
-      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#062633">
+      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor">
         <path
           d="M120-120v-80h720v80H120Zm0-160v-80h480v80H120Zm0-160v-80h720v80H120Zm0-160v-80h480v80H120Zm0-160v-80h720v80H120Z"
         />
@@ -188,12 +200,14 @@
     </button>
     <button
       type="button"
-      class="w-8 h-8 rounded border border-transparent bg-transparent inline-flex items-center justify-center text-sm cursor-pointer hover:bg-gray-100"
-      :class="{ 'bg-gray-200 border-gray-300': isActiveAlign('center') }"
+      class="w-8 h-8 rounded inline-flex items-center justify-center text-sm cursor-pointer"
+      :class="isActiveAlign('center')
+        ? 'bg-slate-800 border border-gray-300 text-white hover:bg-slate-700'
+        : 'bg-transparent border border-transparent text-gray-800 hover:bg-gray-100'"
       @mousedown.prevent
       @click="setAlign('center')"
     >
-      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#062633">
+      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor">
         <path
           d="M120-120v-80h720v80H120Zm160-160v-80h400v80H280ZM120-440v-80h720v80H120Zm160-160v-80h400v80H280ZM120-760v-80h720v80H120Z"
         />
@@ -201,12 +215,14 @@
     </button>
     <button
       type="button"
-      class="w-8 h-8 rounded border border-transparent bg-transparent inline-flex items-center justify-center text-sm cursor-pointer hover:bg-gray-100"
-      :class="{ 'bg-gray-200 border-gray-300': isActiveAlign('right') }"
+      class="w-8 h-8 rounded inline-flex items-center justify-center text-sm cursor-pointer"
+      :class="isActiveAlign('right')
+        ? 'bg-slate-800 border border-gray-300 text-white hover:bg-slate-700'
+        : 'bg-transparent border border-transparent text-gray-800 hover:bg-gray-100'"
       @mousedown.prevent
       @click="setAlign('right')"
     >
-      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#062633">
+      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor">
         <path
           d="M120-760v-80h720v80H120Zm240 160v-80h480v80H360ZM120-440v-80h720v80H120Zm240 160v-80h480v80H360ZM120-120v-80h720v80H120Z"
         />
@@ -214,12 +230,14 @@
     </button>
     <button
       type="button"
-      class="w-8 h-8 rounded border border-transparent bg-transparent inline-flex items-center justify-center text-sm cursor-pointer hover:bg-gray-100"
-      :class="{ 'bg-gray-200 border-gray-300': isActiveAlign('justify') }"
+      class="w-8 h-8 rounded inline-flex items-center justify-center text-sm cursor-pointer"
+      :class="isActiveAlign('justify')
+        ? 'bg-slate-800 border border-gray-300 text-white hover:bg-slate-700'
+        : 'bg-transparent border border-transparent text-gray-800 hover:bg-gray-100'"
       @mousedown.prevent
       @click="setAlign('justify')"
     >
-      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#062633">
+      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor">
         <path
           d="M120-120v-80h720v80H120Zm0-160v-80h720v80H120Zm0-160v-80h720v80H120Zm0-160v-80h720v80H120Zm0-160v-80h720v80H120Z"
         />
@@ -230,12 +248,14 @@
 
     <button
       type="button"
-      class="w-8 h-8 rounded border border-transparent bg-transparent inline-flex items-center justify-center text-sm cursor-pointer hover:bg-gray-100"
-      :class="{ 'bg-gray-200 border-gray-300': isActive('bulletList') }"
+      class="w-8 h-8 rounded inline-flex items-center justify-center text-sm cursor-pointer"
+      :class="isActive('bulletList')
+        ? 'bg-slate-800 border border-gray-300 text-white hover:bg-slate-700'
+        : 'bg-transparent border border-transparent text-gray-800 hover:bg-gray-100'"
       @mousedown.prevent
       @click="cmd('toggleBulletList')"
     >
-      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#062633">
+      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor">
         <path
           d="M360-200v-80h480v80H360Zm0-240v-80h480v80H360Zm0-240v-80h480v80H360ZM200-160q-33 0-56.5-23.5T120-240q0-33 23.5-56.5T200-320q33 0 56.5 23.5T280-240q0 33-23.5 56.5T200-160Zm0-240q-33 0-56.5-23.5T120-480q0-33 23.5-56.5T200-560q33 0 56.5 23.5T280-480q0 33-23.5 56.5T200-400Zm0-240q-33 0-56.5-23.5T120-720q0-33 23.5-56.5T200-800q33 0 56.5 23.5T280-720q0 33-23.5 56.5T200-640Z"
         />
@@ -243,12 +263,14 @@
     </button>
     <button
       type="button"
-      class="w-8 h-8 rounded border border-transparent bg-transparent inline-flex items-center justify-center text-sm cursor-pointer hover:bg-gray-100"
-      :class="{ 'bg-gray-200 border-gray-300': isActive('orderedList') }"
+      class="w-8 h-8 rounded inline-flex items-center justify-center text-sm cursor-pointer"
+      :class="isActive('orderedList')
+        ? 'bg-slate-800 border border-gray-300 text-white hover:bg-slate-700'
+        : 'bg-transparent border border-transparent text-gray-800 hover:bg-gray-100'"
       @mousedown.prevent
       @click="cmd('toggleOrderedList')"
     >
-      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#062633">
+      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor">
         <path
           d="M120-80v-60h100v-30h-60v-60h60v-30H120v-60h120q17 0 28.5 11.5T280-280v40q0 17-11.5 28.5T240-200q17 0 28.5 11.5T280-160v40q0 17-11.5 28.5T240-80H120Zm0-280v-110q0-17 11.5-28.5T160-510h60v-30H120v-60h120q17 0 28.5 11.5T280-560v70q0 17-11.5 28.5T240-450h-60v30h100v60H120Zm60-280v-180h-60v-60h120v240h-60Zm180 440v-80h480v80H360Zm0-240v-80h480v80H360Zm0-240v-80h480v80H360Z"
         />
