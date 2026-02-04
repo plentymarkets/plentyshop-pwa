1fa6154a3d57a5fc932ae22bd1e23d3cd1af845b
Noah Feist <144685636+N-Feist@users.noreply.github.com>
2026-02-04 10:44:21 +0100

    chore: move svgs into seperate file

--- apps/web/app/components/editor/RichTextEditor/RichTextEditorMenuButton.vue
+++ apps/web/app/components/editor/RichTextEditor/RichTextEditorMenuButton.vue
@@ -9,17 +9,53 @@
     "
     @mousedown.prevent
   >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      height="24px"
+      width="24px"
+      :viewBox="getViewBox()"
+      fill="currentColor"
+    >
+      <template v-if="getPaths()">
+        <path v-if="typeof getPaths() === 'string'" :d="getPaths() as string" />
+        <path v-for="(path, index) in getPaths()" v-else :key="index" :d="path" />
+      </template>
+    </svg>
     <slot />
   </button>
 </template>
 
 <script setup lang="ts">
-withDefaults(
+import { icons } from './icons';
+
+const props = withDefaults(
   defineProps<{
     active?: boolean;
+    iconName?: string;
   }>(),
   {
     active: false,
+    iconName: '',
   },
 );
+
+const getViewBox = () => {
+  const icon = icons[props.iconName];
+  if (!icon) return '0 -960 960 960';
+
+  if (typeof icon === 'object' && 'viewBox' in icon) {
+    return icon.viewBox;
+  }
+  return '0 -960 960 960';
+};
+
+const getPaths = () => {
+  const icon = icons[props.iconName];
+  if (!icon) return null;
+
+  if (typeof icon === 'string' || Array.isArray(icon)) {
+    return icon;
+  }
+  return icon.paths;
+};
 </script>
