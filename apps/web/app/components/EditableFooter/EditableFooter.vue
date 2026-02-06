e23ffc0e622c7f4bbaf82fb336f1b1121793f767
cristian sandru <cristian.sandru@plentysystems.com>
2026-02-06 16:08:50 +0200

    refactor: be driven prototype

--- apps/web/app/components/EditableFooter/EditableFooter.vue
+++ apps/web/app/components/EditableFooter/EditableFooter.vue
@@ -1,24 +1,37 @@
 <template>
   <div data-testid="editable-footer">
-    <PageBlock
-      v-for="(block, index) in props.footer"
-      :key="block.meta?.uuid"
-      :block="block"
-      :index="index"
-      :root="true"
-      :enable-actions="enabledActions"
-      :is-clicked="isClicked"
-      :clicked-block-index="clickedBlockIndex"
-      :is-tablet="isTablet"
-      :change-block-position="changeBlockPosition"
-      class="group"
-      @click="tabletEdit(index)"
-    />
+    <draggable
+      v-if="props.footer && props.footer.length > 0"
+      v-model="footerBlocks"
+      item-key="meta.uuid"
+      handle=".drag-handle"
+      class="content"
+      :filter="'.no-drag'"
+      :prevent-on-filter="false"
+      @start="handleDragStart"
+      @end="handleDragEnd"
+    >
+      <template #item="{ element: block, index }">
+        <PageBlock
+          :block="block"
+          :index="index"
+          :root="true"
+          :enable-actions="enabledActions"
+          :is-clicked="isClicked"
+          :clicked-block-index="clickedBlockIndex"
+          :is-tablet="isTablet"
+          :change-block-position="changeBlockPosition"
+          class="group"
+          @click="tabletEdit(index)"
+        />
+      </template>
+    </draggable>
   </div>
 </template>
 
 <script setup lang="ts">
 import type { Block } from '@plentymarkets/shop-api';
+import draggable from 'vuedraggable/src/vuedraggable';
 
 interface EditableFooterProps {
   footer?: Block[];
@@ -28,9 +41,19 @@ const props = withDefaults(defineProps<EditableFooterProps>(), {
   footer: () => [],
 });
 
-const { shouldShowEditorUI } = useEditorState();
+const footerBlocks = computed({
+  get: () => props.footer ?? [],
+  set: () => {},
+});
+
+const { isInEditor, shouldShowEditorUI } = useEditorState();
 const { drawerOpen: localizationDrawerOpen } = useEditorLocalizationKeys();
-const { isClicked, clickedBlockIndex, isTablet, tabletEdit, changeBlockPosition } = useBlockManager();
+const { isClicked, clickedBlockIndex, isTablet, tabletEdit, changeBlockPosition, handleDragStart, handleDragEnd } =
+  useBlockManager();
 
 const enabledActions = computed(() => shouldShowEditorUI.value && !localizationDrawerOpen.value);
+
+onMounted(async () => {
+  if (isInEditor.value) await import('../EditableMain/draggable.css');
+});
 </script>
--- apps/web/app/components/EditableHeader/EditableHeader.vue
+++ apps/web/app/components/EditableHeader/EditableHeader.vue
@@ -1,24 +1,37 @@
 <template>
   <div data-testid="editable-header">
-    <PageBlock
-      v-for="(block, index) in props.header"
-      :key="block.meta?.uuid"
-      :block="block"
-      :index="index"
-      :root="true"
-      :enable-actions="enabledActions"
-      :is-clicked="isClicked"
-      :clicked-block-index="clickedBlockIndex"
-      :is-tablet="isTablet"
-      :change-block-position="changeBlockPosition"
-      class="group"
-      @click="tabletEdit(index)"
-    />
+    <draggable
+      v-if="props.header && props.header.length > 0"
+      v-model="headerBlocks"
+      item-key="meta.uuid"
+      handle=".drag-handle"
+      class="content"
+      :filter="'.no-drag'"
+      :prevent-on-filter="false"
+      @start="handleDragStart"
+      @end="handleDragEnd"
+    >
+      <template #item="{ element: block, index }">
+        <PageBlock
+          :block="block"
+          :index="index"
+          :root="true"
+          :enable-actions="enabledActions"
+          :is-clicked="isClicked"
+          :clicked-block-index="clickedBlockIndex"
+          :is-tablet="isTablet"
+          :change-block-position="changeBlockPosition"
+          class="group"
+          @click="tabletEdit(index)"
+        />
+      </template>
+    </draggable>
   </div>
 </template>
 
 <script setup lang="ts">
 import type { Block } from '@plentymarkets/shop-api';
+import draggable from 'vuedraggable/src/vuedraggable';
 
 interface EditableHeaderProps {
   header?: Block[];
@@ -28,9 +41,19 @@ const props = withDefaults(defineProps<EditableHeaderProps>(), {
   header: () => [],
 });
 
-const { shouldShowEditorUI } = useEditorState();
+const headerBlocks = computed({
+  get: () => props.header ?? [],
+  set: () => {},
+});
+
+const { isInEditor, shouldShowEditorUI } = useEditorState();
 const { drawerOpen: localizationDrawerOpen } = useEditorLocalizationKeys();
-const { isClicked, clickedBlockIndex, isTablet, tabletEdit, changeBlockPosition } = useBlockManager();
+const { isClicked, clickedBlockIndex, isTablet, tabletEdit, changeBlockPosition, handleDragStart, handleDragEnd } =
+  useBlockManager();
 
 const enabledActions = computed(() => shouldShowEditorUI.value && !localizationDrawerOpen.value);
+
+onMounted(async () => {
+  if (isInEditor.value) await import('../EditableMain/draggable.css');
+});
 </script>
