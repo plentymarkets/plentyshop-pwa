fab37bd8624b5d9f6fb7ec7a43e52436c4dbb86c
Noah Feist <144685636+N-Feist@users.noreply.github.com>
2026-02-16 11:17:50 +0100

    feat: add announcementbar block

---
+++ apps/web/app/components/blocks/AnnouncementBar/AnnouncementBar.vue
@@ -0,0 +1,3 @@
+<template>
+  Placeholder for AnnouncementBar
+</template>
---
+++ apps/web/app/components/blocks/AnnouncementBar/AnnouncementBarForm.vue
@@ -0,0 +1,14 @@
+<template>
+  Placeholder for AnnouncementBarForm
+</template>
+
+<i18n lang="json">
+{
+  "en": {
+    "text-group-label": "Text"
+  },
+  "de": {
+    "text-group-label": "Text"
+  }
+}
+</i18n>
