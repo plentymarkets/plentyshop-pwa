bf8c4a3818e391350a7be80d8050b43cff48c8b8
Fabian Gerke <124085586+FabianGerke@users.noreply.github.com>
2026-02-18 09:23:00 +0100

    feat: add callisto feature flag

--- apps/web/app/configuration/feature-flags.config.ts
+++ apps/web/app/configuration/feature-flags.config.ts
@@ -1,4 +1,5 @@
 export default {
   /** @description Enables TipTap rich text editor in block forms */
   enableRichTextEditorV2: process.env?.ENABLE_RICH_TEXT_EDITOR_V2 === '1',
+  enableCallistoUrlScheme: process.env?.ENABLE_CALLISTO_URL_SCHEME === '1',
 };
