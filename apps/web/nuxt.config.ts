2e312d251ccf4e91544366a8ed5b2d11c90dc52d
Noah Feist <144685636+N-Feist@users.noreply.github.com>
2026-02-19 16:34:44 +0100

    fix: forgot to move related vue into module

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -4,7 +4,6 @@ import { nuxtI18nOptions } from './app/configuration/i18n.config';
 import { appConfiguration } from './app/configuration/app.config';
 import cookieConfig from './app/configuration/cookie.config';
 import { paths } from './app/utils/paths';
-import { resolve } from 'pathe';
 import settingsConfig from './app/configuration/settings.config';
 import featureFlagsConfig from './app/configuration/feature-flags.config';
 import { FailOnLargeChunksPlugin } from './app/configuration/vite.config';
