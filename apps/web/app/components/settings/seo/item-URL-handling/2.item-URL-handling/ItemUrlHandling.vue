21c36ec7972f957522823419fc23c0ad0b2e7c2b
plentydev <iulian.vladu@plentymarkets.com>
2026-02-19 10:20:42 +0200

    add description

--- apps/web/app/components/settings/seo/item-URL-handling/2.item-URL-handling/ItemUrlHandling.vue
+++ apps/web/app/components/settings/seo/item-URL-handling/2.item-URL-handling/ItemUrlHandling.vue
@@ -25,6 +25,58 @@
         {{ getEditorTranslation('button-legacy-label') }}
       </div>
     </div>
+
+    <div class="mt-5">
+      <span class="font-mono text-sm bg-gray-100 font-weight-bold px-1 py-1 rounded-md">
+        {{
+          urlScheme ? getEditorTranslation('description-legacy-code') : getEditorTranslation('description-modern-code')
+        }}
+      </span>
+      <p class="my-3 font-semibold">
+        {{
+          urlScheme
+            ? getEditorTranslation('description-legacy-body-one')
+            : getEditorTranslation('description-modern-body-one')
+        }}
+      </p>
+      <ul class="list-inside list-disc">
+        <li>
+          {{
+            urlScheme
+              ? getEditorTranslation('description-legacy-body-list-line-one')
+              : getEditorTranslation('description-modern-body-list-line-one')
+          }}
+        </li>
+        <li>
+          {{
+            urlScheme
+              ? getEditorTranslation('description-legacy-body-list-line-two')
+              : getEditorTranslation('description-modern-body-list-line-two')
+          }}
+        </li>
+        <li>
+          {{
+            urlScheme
+              ? getEditorTranslation('description-legacy-body-list-line-three')
+              : getEditorTranslation('description-modern-body-list-line-three')
+          }}
+        </li>
+        <li>
+          {{
+            urlScheme
+              ? getEditorTranslation('description-legacy-body-list-line-four')
+              : getEditorTranslation('description-modern-body-list-line-four')
+          }}
+        </li>
+      </ul>
+      <p class="my-3">
+        {{
+          urlScheme
+            ? getEditorTranslation('description-legacy-body-footer')
+            : getEditorTranslation('description-modern-body-footer')
+        }}
+      </p>
+    </div>
   </div>
 </template>
 
@@ -47,12 +99,44 @@ const urlScheme = computed({
   "en": {
     "label": "Item URL Scheme",
     "button-modern-label": "Modern",
-    "button-legacy-label": "Legacy"
+    "button-legacy-label": "Legacy",
+
+    "description-modern-code": "Example: /item-name_itemID_itemVariantID",
+    "description-modern-body-one": "Use when variants should be individually discoverable.",
+    "description-modern-body-list-line-one": "Each variant gets its own URL",
+    "description-modern-body-list-line-two": "Enables direct linking to specific variants",
+    "description-modern-body-list-line-three": "Supports variant-level SEO potential",
+    "description-modern-body-list-line-four": "Useful for campaigns or ads targeting a specific option",
+    "description-modern-body-footer": "This is ideal when variants differ meaningfully (e.g. material, features, item texts, images) or when you want users and search engines to land directly on a preselected variant.",
+
+    "description-legacy-code": "Example: /item-name/a-variantID",
+    "description-legacy-body-one": "Use when variants are mostly cosmetic and SEO simplicity matters.",
+    "description-legacy-body-list-line-one": "One shared URL for all variants",
+    "description-legacy-body-list-line-two": "Prevents duplicate or near-identical pages",
+    "description-legacy-body-list-line-three": "Consolidates SEO signals to a single product page",
+    "description-legacy-body-list-line-four": "Simplifies analytics and maintenance",
+    "description-legacy-body-footer": "Best suited when variants don’t have unique content or search demand and you prefer stronger authority on one page over variant-level linking."
   },
   "de": {
     "label": "Item URL Scheme",
     "button-modern-label": "Modern",
-    "button-legacy-label": "Legacy"
+    "button-legacy-label": "Legacy",
+
+    "description-modern-code": "Example: /item-name_itemID_itemVariantID",
+    "description-modern-body-one": "Use when variants should be individually discoverable.",
+    "description-modern-body-list-line-one": "Each variant gets its own URL",
+    "description-modern-body-list-line-two": "Enables direct linking to specific variants",
+    "description-modern-body-list-line-three": "Supports variant-level SEO potential",
+    "description-modern-body-list-line-four": "Useful for campaigns or ads targeting a specific option",
+    "description-modern-body-footer": "This is ideal when variants differ meaningfully (e.g. material, features, item texts, images) or when you want users and search engines to land directly on a preselected variant.",
+
+    "description-legacy-code": "Example: /item-name/a-variantID",
+    "description-legacy-body-one": "Use when variants are mostly cosmetic and SEO simplicity matters.",
+    "description-legacy-body-list-line-one": "One shared URL for all variants",
+    "description-legacy-body-list-line-two": "Prevents duplicate or near-identical pages",
+    "description-legacy-body-list-line-three": "Consolidates SEO signals to a single product page",
+    "description-legacy-body-list-line-four": "Simplifies analytics and maintenance",
+    "description-legacy-body-footer": "Best suited when variants don’t have unique content or search demand and you prefer stronger authority on one page over variant-level linking."
   }
 }
 </i18n>
--- apps/web/app/components/settings/seo/item-URL-handling/View.vue
+++ apps/web/app/components/settings/seo/item-URL-handling/View.vue
@@ -1,14 +1,6 @@
 <template>
   <SiteConfigurationView>
     <template #setting-title>{{ getEditorTranslation('label') }}</template>
-
-    <template #setting-description>
-      <div class="flex flex-col px-4 text-sm">
-        <p class="pb-2">
-          <span class="align-middle font-bold">{{ getEditorTranslation('description') }}</span>
-        </p>
-      </div>
-    </template>
   </SiteConfigurationView>
 </template>
 
@@ -17,12 +9,10 @@
 <i18n lang="json">
 {
   "en": {
-    "label": "Item URL Handling",
-    "description": "Use this setting to determine a custom canonical URL for item pages."
+    "label": "Item URL Handling"
   },
   "de": {
-    "label": "Item URL Handling",
-    "description": "Use this setting to determine a custom canonical URL for item pages."
+    "label": "Item URL Handling"
   }
 }
 </i18n>
