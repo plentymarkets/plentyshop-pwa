c9f53a6fd44f803e0230ddbb6de071743ab370fb
Fabian Gerke <124085586+FabianGerke@users.noreply.github.com>
2026-02-24 10:36:25 +0100

    feat: init rest calls parallelisation

--- apps/web/app/plugins/00.init-initial-data.server.ts
+++ apps/web/app/plugins/00.init-initial-data.server.ts
@@ -2,8 +2,36 @@ export default defineNuxtPlugin({
   name: 'init-initial-data',
   async setup() {
     const { setInitialDataSSR } = useInitialSetup();
+    const { fetchFooterBlock } = useCategoryTemplate();
+    const route = useRoute();
+
     await callOnce(async () => {
-      await setInitialDataSSR();
+      const promises: Promise<unknown>[] = [
+        setInitialDataSSR(),
+      ];
+
+      const identifier = String(route.meta.identifier);
+      const type = String(route.meta.type);
+
+      if (
+        route.meta.isBlockified &&
+        type &&
+        identifier !== undefined &&
+        identifier !== '0'
+      ) {
+        const { $i18n } = useNuxtApp();
+        const { getBlocksServer } = useCategoryTemplate(
+          identifier,
+          type,
+          $i18n.locale.value,
+        );
+
+        promises.push(getBlocksServer(identifier, type));
+      } else {
+        promises.push(fetchFooterBlock());
+      }
+
+      await Promise.all(promises);
     });
   },
 });
