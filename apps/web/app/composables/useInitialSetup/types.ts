80b7823b70cb4d02c101aa91e3d0684945117e67
Fabian Gerke <124085586+FabianGerke@users.noreply.github.com>
2026-03-03 09:59:21 +0100

    feat: fetch settings from microservice + remove settings from getInit (#2321)

--- apps/web/app/composables/useInitialSetup/types.ts
+++ apps/web/app/composables/useInitialSetup/types.ts
@@ -3,6 +3,7 @@ export type SetInitialData = () => Promise<boolean>;
 export interface UseInitialSetup {
   setInitialData: SetInitialData;
   setInitialDataSSR: SetInitialData;
+  fetchSettings: () => Promise<void>;
 }
 
 export type UseInitialSetupReturn = () => UseInitialSetup;
