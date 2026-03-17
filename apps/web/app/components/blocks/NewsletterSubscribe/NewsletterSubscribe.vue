4ea6ac37f32d5d3a0bdcf3caec331d98d133704a
N-Feist <noah.feist@plentyone.com>
2026-03-17 16:24:59 +0100

    fix: remove not needed text displays

--- apps/web/app/components/blocks/NewsletterSubscribe/NewsletterSubscribe.vue
+++ apps/web/app/components/blocks/NewsletterSubscribe/NewsletterSubscribe.vue
@@ -4,29 +4,7 @@
     :style="{ backgroundColor: props.content.text?.bgColor ?? '#f5f5f5' }"
     data-testid="newsletter-block"
   >
-    <TextContent
-      v-if="useRuntimeConfig().public.enableRichTextEditorV2"
-      :text="{ ...props.content.text, textAlignment: props.content.text?.textAlignment ?? 'center' }"
-    />
-    <div v-else>
-      <h1
-        v-if="props.index === 0"
-        class="typography-display-3 md:typography-display-2 lg:typography-display-1 font-bold my-2 lg:leading-[4rem]"
-        data-testid="newsletter-title"
-        v-html="props.content.text?.title ?? t('newsletter.heading')"
-      />
-      <h2
-        v-if="props.index !== 0"
-        class="typography-headline-4 sm:typography-headline-3 font-bold mb-2"
-        data-testid="newsletter-title"
-        v-html="props.content.text?.title ?? t('newsletter.heading')"
-      />
-      <p
-        class="typography-text-sm sm:typography-text-base my-2 mb-4"
-        data-testid="newsletter-description"
-        v-html="props.content.text?.htmlDescription ?? t('newsletter.info')"
-      />
-    </div>
+    <TextContent :text="{ ...props.content.text, textAlignment: props.content.text?.textAlignment ?? 'center' }" />
 
     <form class="mx-auto max-w-[550px] pt-2" novalidate @submit.prevent="onSubmit">
       <div
