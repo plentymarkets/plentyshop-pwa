aafcf7b9d47a074a0bc5c758908d652393f8ad10
Tim Müller-Späth <tim.mueller.spaeth@gmail.com>
2026-03-19 18:39:11 +0100

    fix: remove unnecessary css

--- apps/web/app/components/editor/Localization/EditorLocalizationDrawer.vue
+++ apps/web/app/components/editor/Localization/EditorLocalizationDrawer.vue
@@ -53,7 +53,6 @@
                 :key="lang"
                 class="w-64 px-4 py-3 mr-3 font-semibold flex-shrink-0 last:mr-0"
                 :class="{
-                  '!w-[calc(100%-12px)]': selectedLocales.length === 1,
                   '!w-[calc(50%-12px)]': selectedLocales.length === 2,
                 }"
               >
