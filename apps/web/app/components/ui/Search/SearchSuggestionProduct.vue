8e451867597ccb6e5a5820ea2d6e8082f75ff0f1
Fabian Gerke <124085586+FabianGerke@users.noreply.github.com>
2026-03-20 14:39:03 +0100

    Update SearchSuggestionProduct.vue

--- apps/web/app/components/ui/Search/SearchSuggestionProduct.vue
+++ apps/web/app/components/ui/Search/SearchSuggestionProduct.vue
@@ -10,11 +10,11 @@
           <div class="text-sm font-medium text-neutral-900 line-clamp-2">{{ item.label }}</div>
         </div>
       </div>
-      <div class="text-sm px-2 pb-2">
+      <div class="text-sm px-2 pb-2 flex flex-wrap gap-x-2 items-center">
         <span class="text-gray-900 font-semibold">{{ item.price.formatted }}</span>
         <span
           v-if="item.crossedPrice && item.crossedPrice !== item.price && item.crossedPrice.value !== 0"
-          class="text-neutral-500 ml-2 text-xs line-through"
+          class="text-neutral-500 text-xs line-through"
           >{{ item.crossedPrice.formatted }}</span
         >
       </div>
