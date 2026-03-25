86c33b04443d7dea4d4a9e3d0afb8374b2ded3cd
N-Feist <noah.feist@plentyone.com>
2026-03-25 12:28:15 +0100

    fix: exclude every header block from migration

--- apps/web/app/composables/useBlockTemplates/useBlockTemplates.ts
+++ apps/web/app/composables/useBlockTemplates/useBlockTemplates.ts
@@ -391,8 +391,10 @@ export const useBlockTemplates: UseBlockTemplatesReturn = (
     const config = useRuntimeConfig().public;
 
     const firstTextContentBlock = (() => {
+      let headerContainerBlock: Block = {} as Block;
       for (const block of blocks) {
-        if (isHeaderContainerBlock(block) || isHeaderBlock(block)) continue;
+        if (isHeaderContainerBlock(block)) headerContainerBlock = block;
+        if((Array.isArray(headerContainerBlock.content) && headerContainerBlock?.content.includes(block)) || isHeaderBlock(block) || isHeaderContainerBlock(block)) continue;
         if (blocksToMigrateTextContent.includes(block.name)) return block;
         if (Array.isArray(block.content)) {
           const firstChild = block.content.find((child) => blocksToMigrateTextContent.includes(child.name));
