af5f69e0bb4dbd495d21d3ea8268870943f335a8
cristian sandru <cristian.sandru@plentysystems.com>
2026-03-25 14:56:43 +0200

    fix: various issues

--- apps/web/app/components/ProductSlider/types.ts
+++ apps/web/app/components/ProductSlider/types.ts
@@ -1,8 +1,6 @@
-import type { SfScrollable } from '@storefront-ui/vue';
-import type { ComponentProps } from 'vue-component-type-helpers';
 import type { Product } from '@plentymarkets/shop-api';
 
 export type ProductSliderProps = {
   items?: Product[];
-  wrapperClass?: ComponentProps<typeof SfScrollable>['wrapperClass'];
+  wrapperClass?: string | Record<string, unknown> | unknown[];
 };
