96b1d6e4ba6c898e9cc6092da8f263883169ce3e
N-Feist <noah.feist@plentyone.com>
2026-04-13 14:22:03 +0200

    Revert "fix: no double entries for category pages in editor"
    This reverts commit 8f23ab35660c2abe6929b05a4e642fcb42c62223.

--- apps/web/app/composables/useCategoriesSearch/useCategoriesSearch.ts
+++ apps/web/app/composables/useCategoriesSearch/useCategoriesSearch.ts
@@ -141,8 +141,7 @@ export const useCategoriesSearch: UseCategoriesSearchMethodsReturn = () => {
       );
 
       const result: CategoryData = data.value?.data ?? createEmptyCategoryData();
-      const newEntries = filterTopLevelOnly(filterNewlyAddedPages(result.entries));
-      state.value[itemsKey].push(...newEntries);
+      state.value[itemsKey].push(...filterNewlyAddedPages(result.entries));
       state.value[hasMoreKey] = !result.isLastPage;
       state.value[pageKey] = currentPage + 1;
     } catch (error) {
@@ -176,10 +175,6 @@ export const useCategoriesSearch: UseCategoriesSearchMethodsReturn = () => {
 
   const filterNewlyAddedPages = (entries: CategoryEntry[]) => {
     return entries.filter((entry) => !state.value.newPages.includes(entry.id));
-  };
-
-  const filterTopLevelOnly = (entries: CategoryEntry[]) => {
-    return entries.filter((entry) => !entry.parentCategoryId);
   };
 
   const usePaginatedChildren = (category: CategoryEntry) => {
