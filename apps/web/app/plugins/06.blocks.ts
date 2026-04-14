c0b2621f0278fd4cb41b2255d3fbce051840ff94
abocsan-plenty <129151096+abocsan-plenty@users.noreply.github.com>
2026-04-14 17:17:34 +0300

    chore: add back the block fetch via plugin

--- apps/web/app/plugins/06.blocks.ts
+++ apps/web/app/plugins/06.blocks.ts
@@ -5,23 +5,23 @@ export default defineNuxtPlugin({
     const router = useRouter();
     const { $i18n } = useNuxtApp();
 
-    const fetchForRoute = async () => {
+    const fetchForRoute = async (locale: string) => {
       const { meta } = router.currentRoute.value;
       const hasBlockIdentifier = meta.isBlockified && meta.identifier !== undefined;
       const identifier = (hasBlockIdentifier ? meta.identifier : 'index') as string | number;
       const type = (hasBlockIdentifier && meta.type ? meta.type : 'immutable') as string;
 
-      const { fetchBlocks } = useBlocks();
+      const { fetchBlocks } = useBlocks(locale);
       await fetchBlocks(identifier, type);
     };
 
-    await fetchForRoute();
+    await fetchForRoute($i18n.locale.value);
 
     if (import.meta.client) {
       router.afterEach(async (to) => {
         const { $i18n } = useNuxtApp();
 
-        await fetchForRoute();
+        await fetchForRoute($i18n.locale.value);
       });
     }
   },
