6d5bab576e8c854c42706bcf19479548bb23d708
oivan-plenty <oana.ivan@plentysystems.com>
2026-04-16 08:57:40 +0300

    added tests

---
+++ apps/web/__tests__/test/editor/footer.cy.ts
@@ -0,0 +1,84 @@
+import { paths } from '../../../app/utils/paths';
+import { CookieBarObject } from '../../support/pageObjects/CookieBarObject';
+
+describe('Footer Block', () => {
+  const cookieBar = new CookieBarObject();
+  const isFlagOn = Cypress.env('ENABLE_CONTRACT_WITHDRAWAL_BUTTON') === '1';
+
+  const visitPage = () => {
+    cy.clearCookies();
+    cy.clearConfig();
+
+    cy.setConfig({
+      isPreview: false,
+    });
+
+    cy.visitAndHydrate(paths.home);
+    cookieBar.acceptAll();
+  };
+
+  const getFooter = () => cy.getByTestId('footer');
+
+  it('should render footer', () => {
+    visitPage();
+
+    getFooter().should('be.visible');
+  });
+
+  it('should render the correct cancellation action depending on feature flag', () => {
+    visitPage();
+
+    getFooter().within(() => {
+      if (isFlagOn) {
+        cy.getByTestId('footer-cancellation-button')
+          .should('exist')
+          .and('contain.text', 'Withdraw from contract here')
+          .and('have.attr', 'href')
+          .and('include', paths.cancellationForm);
+
+        cy.contains('Cancellation Form').should('not.exist');
+      } else {
+        cy.getByTestId('footer-cancellation-button').should('not.exist');
+        cy.get(`a[href*="${paths.cancellationForm}"]`).should('exist');
+      }
+    });
+  });
+
+  it('should render cancellation rights before the withdrawal button when feature flag is ON', function () {
+    if (!isFlagOn) {
+      cy.log('Skipping because ENABLE_CONTRACT_WITHDRAWAL_BUTTON is OFF');
+      return;
+    }
+
+    visitPage();
+
+    getFooter().then(($footer) => {
+      const text = $footer.text();
+
+      expect(text.indexOf('Cancellation Rights')).to.be.lessThan(
+        text.indexOf('Withdraw from contract here'),
+      );
+    });
+  });
+
+  it('should apply footer colors', () => {
+    visitPage();
+
+    getFooter()
+      .should('have.css', 'background-color', 'rgb(207, 228, 236)')
+      .and('have.css', 'color', 'rgb(28, 28, 28)');
+  });
+
+  it('should navigate to cancellation form when clicking the withdrawal button if feature flag is ON', function () {
+    if (!isFlagOn) {
+      cy.log('Skipping because ENABLE_CONTRACT_WITHDRAWAL_BUTTON is OFF');
+      return;
+    }
+
+    visitPage();
+
+    cy.getByTestId('footer-cancellation-button')
+      .should('have.attr', 'href')
+      .and('include', paths.cancellationForm);
+  });
+});
