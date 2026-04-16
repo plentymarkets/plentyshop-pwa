58f3d128f994b5a16a72f5ab561e0403429dc8d8
abocsan-plenty <129151096+abocsan-plenty@users.noreply.github.com>
2026-04-16 12:40:07 +0300

    fix: add category fetching in slug

--- apps/web/app/pages/[...slug].vue
+++ apps/web/app/pages/[...slug].vue
@@ -30,8 +30,8 @@ const route = useRoute();
 const router = useRouter();
 const { setCategoriesPageMeta } = useUrlPageMeta();
 const { setBlocksListContext } = useBlocksList();
-const { getFacetsFromURL } = useCategoryFilter();
-const { data: productsCatalog, loading } = useProducts();
+const { getFacetsFromURL, checkFiltersInURL } = useCategoryFilter();
+const { fetchProducts, data: productsCatalog, loading } = useProducts();
 const { data: categoryTree } = useCategoryTree();
 const { buildCategoryLanguagePath } = useLocalization();
 
@@ -64,6 +64,10 @@ const breadcrumbs = computed(() => {
 const canonicalDb = productsCatalog.value.category?.details?.[0]?.canonicalLink;
 
 const handleQueryUpdate = async () => {
+  if (!productsCatalog.value.category) {
+    await fetchProducts(getFacetsFromURL()).then(() => checkFiltersInURL());
+  }
+
   if (!productsCatalog.value.category) {
     throw createError({
       statusCode: 404,
