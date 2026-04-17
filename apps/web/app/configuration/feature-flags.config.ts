d7490e469af8009b21f4fc6563a79f056c3cc4e1
oivan-plenty <oana.ivan@plentysystems.com>
2026-04-17 13:11:40 +0300

    cancellation form email settings

--- apps/web/app/configuration/feature-flags.config.ts
+++ apps/web/app/configuration/feature-flags.config.ts
@@ -3,5 +3,4 @@ export default {
   enableContractWithdrawalButton: process.env?.ENABLE_CONTRACT_WITHDRAWAL_BUTTON === '1',
   /** @description Enables the new menu entry in settings for cancellation email  */
   enableCancellationForm: process.env?.ENABLE_CANCELLATION_FORM === '1',
-
 };
