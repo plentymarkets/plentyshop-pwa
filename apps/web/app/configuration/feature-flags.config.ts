0eef90fd5a47b3fbe1c765a697d2bba43e500b01
oivan-plenty <oana.ivan@plentysystems.com>
2026-04-17 12:56:44 +0300

    cancellation form email settings

--- apps/web/app/configuration/feature-flags.config.ts
+++ apps/web/app/configuration/feature-flags.config.ts
@@ -1,4 +1,6 @@
 export default {
   /** @description Enables the new withdrawal button from the footer */
   enableContractWithdrawalButton: process.env?.ENABLE_CONTRACT_WITHDRAWAL_BUTTON === '1',
+  enableCancellationForm: process.env?.ENABLE_CANCELLATION_FORM === '1',
+
 };
