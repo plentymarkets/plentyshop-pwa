f64b46974cc4d46d06f1d3a5da7e0247863c0e2a
cristian sandru <cristian.sandru@plentysystems.com>
2026-04-20 14:01:15 +0300

    chore: dev tooling improvements

--- apps/web/app/plugins/02.pwa-cookie.ts
+++ apps/web/app/plugins/02.pwa-cookie.ts
@@ -1,3 +1,8 @@
+/**
+ * This plugin reads the preview cookie and sets the isPreview property in the context.
+ * This property is used to determine if the app is in preview mode or not.
+ * The plugin replaces the `onMounted` function to determine the mode on SSR.
+ */
 export default defineNuxtPlugin({
   name: 'pwa-cookie',
   parallel: true,
