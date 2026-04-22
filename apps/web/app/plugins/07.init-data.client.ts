719eec1e63c921e7ef9b4d415ba9105523369438
Maximilian Röll <30629157+maxiroellplenty@users.noreply.github.com>
2026-04-22 14:55:51 +0200

    feat: make the init client non blocking

--- apps/web/app/plugins/07.init-data.client.ts
+++ apps/web/app/plugins/07.init-data.client.ts
@@ -1,10 +1,10 @@
 export default defineNuxtPlugin({
   name: 'session-and-category-tree',
   parallel: true,
-  async setup() {
+  setup() {
     const route = useRoute();
     if (route.meta.cacheControl) {
-       await useInitialSetup().fetchSessionAndCategoryTree(); 
+      useInitialSetup().fetchSessionAndCategoryTree(); 
     }
   },
 });
