a1ac95de9a99c0c02dce73d814c66c6e0e7c93a6
Maximilian Röll <30629157+maxiroellplenty@users.noreply.github.com>
2026-04-27 13:09:31 +0200

    perf: exclude blocks call from getFacet and getProduct calls

--- apps/server/middleware.config.ts
+++ apps/server/middleware.config.ts
@@ -15,6 +15,10 @@ const config = {
         api: {
           url: process.env.API_ENDPOINT,
           securityToken: process.env.API_SECURITY_TOKEN ?? '',
+          experimental: {
+            excludeBlocksFromFacets: true,
+            excludeBlocksFromProducts: true,
+          }
         },
       },
       errorHandler: (error: any, req: any, res: any) => {
