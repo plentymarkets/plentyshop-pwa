22740f114388f7482b4d0611bbe8f88e35a0ad3d
cristian sandru <cristian.sandru@plentysystems.com>
2026-05-07 10:10:34 +0300

    fix: editor scroll position

--- apps/web/app/app.vue
+++ apps/web/app/app.vue
@@ -14,8 +14,16 @@
       class="flex-shrink-0 bg-white font-editor border-r border-gray-300 overflow-visible"
     />
 
-    <div class="flex-1 w-full bg-white relative" :class="clientPreview ? 'overflow-auto' : 'overflow-visible'">
-      <Body class="font-body bg-editor-body-bg" :class="bodyClass" :style="currentFont" />
+    <div
+      ref="contentRef"
+      class="flex-1 w-full bg-white relative"
+      :class="clientPreview ? 'overflow-auto' : 'overflow-visible'"
+    >
+      <Body
+        class="font-body bg-editor-body-bg"
+        :class="[bodyClass, { 'overflow-hidden': clientPreview }]"
+        :style="currentFont"
+      />
       <UiNotifications />
       <VitePwaManifest />
       <NuxtLoadingIndicator color="repeating-linear-gradient(to right, #008ebd 0%,#80dfff 50%,#e0f7ff 100%)" />
@@ -50,6 +58,7 @@ const { setStaticPageMeta } = useUrlPageMeta();
 const { isInEditorClient } = useEditorState();
 
 const clientPreview = computed(() => isInEditorClient.value && viewport.isGreaterOrEquals('lg'));
+const contentRef = ref<HTMLElement | null>(null);
 
 const { getSetting: getFavicon } = useSiteSettings('favicon');
 const { getSetting: getOgTitle } = useSiteSettings('ogTitle');
@@ -102,18 +111,10 @@ const getCategoryOgTitle = () => {
   return getOgTitle() || getMetaTitle();
 };
 
-const getCategoryOgDescription = () => {
-  if (isCategoryPage.value) {
-    const categoryMetaDescription = categoryGetters.getMetaDescription(category.value);
-    if (categoryMetaDescription) return categoryMetaDescription;
-  }
-  return getMetaDescription();
-};
-
 const title = ref(getCategoryMetaTitle());
 const ogTitle = ref(getCategoryOgTitle());
 const ogImage = ref(getOgImage());
-const ogDescription = ref(getCategoryOgDescription());
+const ogDescription = ref(getCategoryMetaDescription());
 const description = ref(getCategoryMetaDescription());
 const keywords = ref(getCategoryMetaKeywords());
 const robots = ref(getRobots());
@@ -148,7 +149,7 @@ watchEffect(() => {
   title.value = getCategoryMetaTitle();
   ogTitle.value = getCategoryOgTitle();
   ogImage.value = getOgImage();
-  ogDescription.value = getCategoryOgDescription();
+  ogDescription.value = getCategoryMetaDescription();
   description.value = getCategoryMetaDescription();
   keywords.value = getCategoryMetaKeywords();
   robots.value = getRobots();
@@ -215,6 +216,13 @@ if (import.meta.client) {
       })),
     ],
   });
+
+  watch(
+    () => route.path,
+    () => {
+      if (clientPreview.value) contentRef.value?.scrollTo({ top: 0 });
+    },
+  );
 }
 
 if (route?.meta.pageType === 'static') setStaticPageMeta();
