7a9d219c5a698be5c91eff25730b38a01b733134
Maximilian Röll <30629157+maxiroellplenty@users.noreply.github.com>
2026-05-11 10:04:59 +0200

    chore: format

--- apps/web/app/components/LocalizationView/LocalizationView.vue
+++ apps/web/app/components/LocalizationView/LocalizationView.vue
@@ -8,11 +8,7 @@
         <SfTooltip :label="getEditorTranslation('tooltip')" placement="right" :show-arrow="true" class="flex">
           <SfIconHelp class="cursor-pointer" />
         </SfTooltip>
-        <button
-          data-testid="pages-view-close"
-          class="!p-0"
-          @click="handleClose"
-        >
+        <button data-testid="pages-view-close" class="!p-0" @click="handleClose">
           <SfIconClose />
         </button>
       </div>
