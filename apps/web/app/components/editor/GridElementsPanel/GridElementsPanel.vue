af54d8fecdd3a5ab279e793ddf9884136e8cd0d9
abocsan-plenty <129151096+abocsan-plenty@users.noreply.github.com>
2026-05-18 17:07:45 +0300

    chore: test fix

--- apps/web/app/components/editor/GridElementsPanel/GridElementsPanel.vue
+++ apps/web/app/components/editor/GridElementsPanel/GridElementsPanel.vue
@@ -141,6 +141,7 @@
                   <span class="text-xs text-editor-text-subtle">{{ getEditorTranslation('visibility') }}</span>
                   <SfSwitch
                     :model-value="isVisible(block)"
+                    :data-testid="`actions-toggle-visibility-item-${index}`"
                     class="checked:bg-editor-button checked:before:hover:bg-editor-button checked:border-gray-500 checked:hover:border:bg-gray-700 hover:border-gray-700 hover:before:bg-gray-700 checked:hover:bg-gray-300 checked:hover:border-gray-400"
                     @update:model-value="onToggleVisibility(block)"
                   />
