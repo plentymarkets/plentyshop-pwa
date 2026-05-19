6f4e2b97d1defe2dcc3bace4120f2f3d3d705476
abocsan-plenty <129151096+abocsan-plenty@users.noreply.github.com>
2026-05-19 14:23:35 +0300

    feat: remove footer block

--- apps/web/app/utils/blockTemplates/footer/factory.ts
+++ apps/web/app/utils/blockTemplates/footer/factory.ts
@@ -1,6 +1,9 @@
 import type { Block } from '@plentymarkets/shop-api';
-import type { FooterContainerBlock } from '~/components/blocks/structure/FooterContainer/types';
-import type { FooterColumn, FooterContent } from '~/components/blocks/Footer/types';
+import type {
+  FooterContainerBlock,
+  FooterColumn,
+  FooterContent,
+} from '~/components/blocks/structure/FooterContainer/types';
 import type { ButtonConfig } from './types';
 import { FOOTER_SWITCH_DEFINITIONS } from '~/components/blocks/structure/FooterContainer/constants';
 import { v4 as uuid } from 'uuid';
