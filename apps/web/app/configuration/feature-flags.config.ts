ea9aef3723749e5774ff49049510de445d98b9d3
abocsan-plenty <129151096+abocsan-plenty@users.noreply.github.com>
2026-05-20 10:16:04 +0300

    chore: reput feature flags

--- apps/web/app/configuration/feature-flags.config.ts
+++ apps/web/app/configuration/feature-flags.config.ts
@@ -1,8 +1,8 @@
 export default {
   /** @description Enables the new popover for adding blocks & presets  */
-  enableAddBlockPopover: true,
+  enableAddBlockPopover: process.env?.ENABLE_ADD_BLOCK_POPOVER === '1',
   /** @description Enables the new MultiGrid editor with visual column resizing, row management and free-space display. */
-  enableMultiGridEditor: true,
+  enableMultiGridEditor: process.env?.ENABLE_MULTI_GRID_EDITOR === '1',
   /** @description Enables quick-add pills in structure block forms (Header, Footer, MultiGrid) */
-  enableQuickAdd: true,
+  enableQuickAdd: process.env?.ENABLE_QUICK_ADD === '1',
 };
