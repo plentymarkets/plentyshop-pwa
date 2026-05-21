2f4316bdab8cf539afe87fb02ef4866fc85130ed
Fabian Gerke <124085586+FabianGerke@users.noreply.github.com>
2026-05-21 13:35:22 +0200

    feat: use checkout changed event (#2661)

--- apps/web/app/pages/checkout.vue
+++ apps/web/app/pages/checkout.vue
@@ -128,7 +128,6 @@ onNuxtReady(async () => {
 });
 
 const disableShippingPayment = computed(() => shippingLoading.value || paymentLoading.value);
-const itemSumNet = computed(() => cartGetters.getItemSumNet(cart.value));
 const { processingOrder } = useProcessingOrder();
 
 watch(cartIsEmpty, async () => {
@@ -136,9 +135,5 @@ watch(cartIsEmpty, async () => {
     send({ type: 'neutral', message: t('cart.emptyNotification') });
     await navigateTo(localePath(paths.cart));
   }
-});
-
-watch(itemSumNet, async () => {
-  await fetchPaymentMethods();
 });
 </script>
