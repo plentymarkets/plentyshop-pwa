2572d2b7379a7a9a91625bf1f67441d3af875889
NickDim <129836405+irritop@users.noreply.github.com>
2026-06-01 17:30:22 +0300

    Update block-type-names.ts

--- apps/web/modules/nk-blocks/runtime/utils/blocks/block-type-names.ts
+++ apps/web/modules/nk-blocks/runtime/utils/blocks/block-type-names.ts
@@ -1,6 +1,3 @@
 export const blockTypeNames: Record<string, string> = {
   Tabs: 'Tabs',
-  FooterCompanyData: 'Company Data',
-  FooterPaymentShippingIcons: 'Payment & Shipment Icons',
-  FooterFootnote: 'Footer Footnote',
 };
