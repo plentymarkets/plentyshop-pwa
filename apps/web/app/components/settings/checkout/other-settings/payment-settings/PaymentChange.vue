b8a0dc68615bd9696893b4395df9ed374a33f87d
N-Feist <noah.feist@plentyone.com>
2026-06-04 11:26:10 +0200

    fix: address copilot comment

--- apps/web/app/components/settings/checkout/other-settings/payment-settings/PaymentChange.vue
+++ apps/web/app/components/settings/checkout/other-settings/payment-settings/PaymentChange.vue
@@ -7,7 +7,7 @@
       <UiFormLabel class="mb-1">{{ getEditorTranslation('label') }}</UiFormLabel>
       <SfSwitch
         v-model="useOrderChangePaymentMethod"
-        class="checked:bg-editor-button checked:before:hover:bg-editor-button checked:border-gray-500 checked:hover:border:bg-gray-700 hover:border-gray-700 hover:before:bg-gray-700 checked:hover:bg-gray-300 checked:hover:border-gray-400"
+        class="checked:bg-editor-button checked:before:hover:bg-editor-button checked:border-gray-500 hover:border-gray-700 hover:before:bg-gray-700 checked:hover:bg-gray-300 checked:hover:border-gray-400"
       />
     </div>
   </div>
