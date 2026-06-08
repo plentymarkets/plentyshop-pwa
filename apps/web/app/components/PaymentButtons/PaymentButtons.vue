1507328664b9f9df12c26b6b9996983cf839a441
Fabian Gerke <124085586+FabianGerke@users.noreply.github.com>
2026-06-08 16:02:11 +0200

    remove debug text

--- apps/web/app/components/PaymentButtons/PaymentButtons.vue
+++ apps/web/app/components/PaymentButtons/PaymentButtons.vue
@@ -21,15 +21,6 @@
     </template>
     <template v-else>{{ t('common.actions.buy') }}</template>
   </UiButton>
-
-  <div>
-    {{ createOrderLoading }}<br>
-    {{ disableShippingPayment }}<br>
-    {{ cartLoading }}<br>
-    {{ additionalInformationLoading }}<br>
-    {{ navigationInProgress }}<br>
-    {{ processingOrder }}
-  </div>
 </template>
 
 <script setup lang="ts">
