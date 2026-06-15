a9f4e462573b82d010ca3f401969e6c8409611a4
Jens Volke <desire.add@gmail.com>
2026-06-15 10:41:21 +0200

    Update PurchaseCard.vue

--- apps/web/app/components/ui/PurchaseCard/PurchaseCard.vue
+++ apps/web/app/components/ui/PurchaseCard/PurchaseCard.vue
@@ -124,6 +124,7 @@
               <div class="mt-4">
                 <div class="flex flex-col @md:flex-row flex-wrap gap-4">
                   <UiQuantitySelector
+                    v-if="productGetters.isActiveVariationSalable(product)"
                     :min-value="productGetters.getMinimumOrderQuantity(product)"
                     :value="quantitySelectorValue"
                     class="min-w-[145px] flex-grow-0 flex-shrink-0 basis-0"
