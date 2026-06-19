d6823a4f878f7d1a608bb76b72dc4e71157033b8
N-Feist <noah.feist@plentyone.com>
2026-06-19 13:15:35 +0200

    chore: refactor to clean structure for defaults

--- apps/web/app/components/blocks/AnnouncementBar/defaults.ts
+++ apps/web/app/components/blocks/AnnouncementBar/defaults.ts
@@ -1,7 +1,6 @@
 import { v4 as uuid } from 'uuid';
 import type { AnnouncementBarProps } from './types';
 import type { Block } from '@plentymarkets/shop-api';
-import type { BlockListCategory } from '~/composables/useBlocksList/types';
 
 const ANNOUNCEMENT_BAR_IMAGE =
   'https://cdn02.plentymarkets.com/v5vzmmmcb10k/frontend/PWA/Blocks/announcement-bar.svg';
@@ -36,41 +35,30 @@ const createAnnouncementCarousel = (firstText: string, secondText: string): Bloc
   content: [createAnnouncementBar(firstText), createAnnouncementBar(secondText)],
 });
 
-export const BlocksList = (): Block[] => ([
-  createAnnouncementCarousel('This is an example announcement.', 'This is another example announcement.'),
-]);
-
-export const blocksListCategory = {
-  category: 'header',
-  accessControl: ['content'],
-  title: 'Header',
-  blockName: 'Header',
-  variations: [
-    {
-      title: 'Announcement Bar',
-      image: ANNOUNCEMENT_BAR_IMAGE,
-      template: {
-        en: createAnnouncementCarousel('This is an example announcement.', 'This is another example announcement.'),
-        de: createAnnouncementCarousel(
-          'Dies ist eine Beispielankundigung.',
-          'Dies ist eine weitere Beispielankundigung.',
-        ),
+export const blocksList = {
+  header: {
+    category: 'header',
+    accessControl: 'content',
+    title: 'Header',
+    blockName: 'Header',
+    variations: [
+      {
+        title: 'Announcement Bar',
+        image: ANNOUNCEMENT_BAR_IMAGE,
+        template: {
+          en: createAnnouncementCarousel('This is an example announcement.', 'This is another example announcement.'),
+          de: createAnnouncementCarousel(
+            'Dies ist eine Beispielankündigung.',
+            'Dies ist eine weitere Beispielankündigung.',
+          ),
+        },
       },
-    },
-  ],
-} satisfies BlockListCategory;
+    ],
+  },
+} satisfies BlocksList;
 
-export const createDefault = (): AnnouncementBarProps => ({
-  name: 'AnnouncementBar',
-  type: 'content',
-  meta: { uuid: uuid() },
-  configuration: {
-    visible: true,
-  },
-  content: {
-    backgroundColor: '#EDFF6C',
-    text: 'This is an example announcement.',
-  },
-});
+export const createDefault = (): AnnouncementBarProps => {
+  return createAnnouncementBar('This is an example announcement.');
+};
 
 export const labelPath = 'content.text';
