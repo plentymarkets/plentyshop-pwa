e4d68874bbf9cd480956b8e8a1ca9db89df729be
cristian sandru <cristian.sandru@plentysystems.com>
2026-06-25 12:01:31 +0300

    fix: incorrect robots on paginated pages

--- apps/web/app/composables/useUrlPageMeta/useUrlPageMeta.ts
+++ apps/web/app/composables/useUrlPageMeta/useUrlPageMeta.ts
@@ -187,7 +187,9 @@ export const useUrlPageMeta: UseUrlPageMetaReturn = () => {
     const route = useRoute();
     const { getSetting: getSeoCategoryRobotsNoIndex } = useSiteSettings('seoCategoryRobotsNoIndex');
     const currentPage = computed(() => Number(route.query.page as string) || 1);
-    const maxIndexedPage = computed(() => Number(getSeoCategoryRobotsNoIndex()) || 1);
+    const maxIndexedPage = computed(() =>
+      Number(getSeoCategoryRobotsNoIndex()) > 0 ? Number(getSeoCategoryRobotsNoIndex()) : 1,
+    );
 
     return computed((): string => {
       if (!productsCatalog.value?.category) {
