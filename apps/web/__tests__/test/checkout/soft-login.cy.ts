934134e5fe39b4e9a27cf39e23c85d80ad47cb24
Fabian Gerke <124085586+FabianGerke@users.noreply.github.com>
2026-07-28 11:12:25 +0200

    fix: e2e test

--- apps/web/__tests__/test/checkout/soft-login.cy.ts
+++ apps/web/__tests__/test/checkout/soft-login.cy.ts
@@ -2,16 +2,14 @@ import { CheckoutPageObject } from '../../support/pageObjects/CheckoutPageObject
 
 describe('Soft login e2e', () => {
   const confirmationUrl = '/en/confirmation/26387/75GRLU7BX';
-  const email = 'soft-login-e2e@plentyone.com';
-  const password = 'Pa$$w0rd!';
+  const postalCode = '24651';
 
   const checkoutSupport = new CheckoutPageObject();
 
   it('logs in via confirmation link and accesses customer orders', () => {
     cy.visitAndHydrate(confirmationUrl);
-    cy.get('input[type="email"], input[name="email"], #email').first().clear().type(email);
-    cy.get('input[type="password"], input[name="password"], #password').first().clear().type(password);
-    cy.getByTestId('login-submit').click();
+    cy.get('#input-soft-login').first().clear().type(postalCode);
+    cy.get('#submit-soft-login').click();
     checkoutSupport.displaySuccessPage();
   });
 });
