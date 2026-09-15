30fdead9a4e13bbb50e27c3a0d0202b853796b07
Kevin Stederoth <43753494+ksted@users.noreply.github.com>
2026-09-15 08:42:27 +0200

    fix: a11y in ItemDataTable using row headers

--- apps/web/app/components/blocks/ItemData/ItemDataTable.vue
+++ apps/web/app/components/blocks/ItemData/ItemDataTable.vue
@@ -2,9 +2,9 @@
   <table class="w-full text-left">
     <tbody>
       <tr v-for="row in rows" :key="row.key">
-        <td class="@lg:py-3 py-2 @lg:pr-4 pr-2 align-top text-sm">
+        <th scope="row" class="@lg:py-3 py-2 @lg:pr-4 pr-2 align-top text-sm font-normal text-left">
           {{ row.label }}
-        </td>
+        </th>
         <td class="@lg:py-3 py-2 align-top text-sm">
           {{ row.value }}
         </td>
