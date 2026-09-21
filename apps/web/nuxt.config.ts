58d13c412d4efcf33dbe1de97db05fcf02fe01dc
Maximilian Röll <30629157+maxiroellplenty@users.noreply.github.com>
2026-09-21 13:17:47 +0200

    chore: only enable via .env

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -188,7 +188,7 @@ export default defineNuxtConfig({
     configId: Number(process.env.CONFIG_ID) || 1,
     middlewareSSRUrl: 'http://localhost:8181',
     experimental: {
-      forwardUserAgent: true,
+      forwardUserAgent: process.env.MIGRATE_LTS_SESSION === 'true',
     },
   },
   shopModuleMollie: {
