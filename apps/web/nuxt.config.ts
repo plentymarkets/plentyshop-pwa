4ee8a98a935189525ece95c853d2ab568b2ab17a
Maximilian Röll <30629157+maxiroellplenty@users.noreply.github.com>
2026-09-22 11:19:12 +0200

    feat: use feature flag to control user agent

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -187,9 +187,6 @@ export default defineNuxtConfig({
     apiEndpoint: process.env.API_ENDPOINT,
     configId: Number(process.env.CONFIG_ID) || 1,
     middlewareSSRUrl: 'http://localhost:8181',
-    experimental: {
-      forwardUserAgent: process.env.MIGRATE_LTS_SESSION === 'true',
-    },
   },
   shopModuleMollie: {
     checkoutUrl: paths.checkout,
